0cebd19d76c20da43c3a49ce34e12b2fbb2f4345
Richard Olsson <r@richardolsson.se>
2020-12-18 17:47:05 +0100

    Clean up boilerplate and move pages to src directory

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() : JSX.Element {
+    return (
+        <>
+            <Head>
+                <title>Zetkin</title>
+            </Head>
+            <div>
+                <h1>This will become Zetkin</h1>
+            </div>
+        </>
+    );
+}
