49de08cd90604b30f43f2bd77315b11f674de019
kristofferlarberg <mail@kristofferlarberg.se>
2021-01-08 17:30:52 +0100

    refactor with react query

--- src/pages/o/[orgId]/events.tsx
+++ src/pages/o/[orgId]/events.tsx
@@ -2,60 +2,80 @@ import {
     GetServerSideProps,
     NextPageContext,
 } from 'next';
+import { QueryClient, useQuery } from 'react-query';
+import { dehydrate } from 'react-query/hydration';
+
+function getEvents(orgId) {
+    return async () => {
+        try {
+            const cRes = await fetch(`http://api.zetk.in/v1/orgs/${orgId}/campaigns`);
+            const cData = await cRes.json();
+
+            let allEventsData = [];
+
+            for (const obj of cData.data) {
+                const eventsRes = await fetch(`https://api.zetk.in/v1/orgs/${orgId}/campaigns/${obj.id}/actions`);
+                const campaignEvents = await eventsRes.json();
+                allEventsData = allEventsData.concat(campaignEvents.data);
+            }
+
+            return allEventsData;
+        }
+        catch (err) {
+            if (err.name != 'FetchError') {
+                throw err;
+            }
+            return null;
+        }
+    };
+}
+
+function getOrg(orgId) {
+    return async () => {
+        try {
+            const oRes = await fetch(`https://api.zetk.in/v1/orgs/${orgId}`);
+            const oData = await oRes.json();
+
+            return oData.data;
+        }
+        catch (err) {
+            if (err.name != 'FetchError') {
+                throw err;
+            }
+            return null;
+        }
+    };
+}
 
 export const getServerSideProps : GetServerSideProps = async (context : NextPageContext) => {
-    let props;
+    const queryClient = new QueryClient();
+    const { orgId } = context.params;
 
-    try {
-        const { orgId } = context.params;
+    await queryClient.prefetchQuery('events', getEvents(orgId));
+    await queryClient.prefetchQuery(['org', orgId], getOrg(orgId));
 
-        const cRes = await fetch(`http://api.zetk.in/v1/orgs/${orgId}/campaigns`);
-        const cData = await cRes.json();
-        const oRes = await fetch(`https://api.zetk.in/v1/orgs/${orgId}`);
-        const oData = await oRes.json();
-
-        let allEventsData = [];
-
-        for (const obj of cData.data) {
-            const eventsRes = await fetch(`https://api.zetk.in/v1/orgs/${orgId}/campaigns/${obj.id}/actions`);
-            const campaignEvents = await eventsRes.json();
-            allEventsData = allEventsData.concat(campaignEvents.data);
-        }
-
-        props = {
-            org: oData.data,
-            events: allEventsData,
-        };
-    }
-    catch (err) {
-        if (err.name != 'FetchError') {
-            throw err;
-        }
-    }
-
-    if (props) {
-        return { props };
-    }
-    else {
-        return {
-            notFound: true,
-        };
-    }
+    return {
+        props: {
+            dehydratedState: dehydrate(queryClient),
+            orgId
+        },
+    };
 };
 
 type OrgEventsPageProps = {
-    org: Record<string, unknown>,
-    events: array<Record<string, unknown>>,
+    orgId: string,
 }
 
 export default function OrgEventsPage(props : OrgEventsPageProps) : JSX.Element {
-    const { org, events } = props;
+    const { orgId } = props;
+    const eventsQuery = useQuery('events', getEvents(orgId));
+    const orgQuery = useQuery(['org', orgId], getOrg(orgId));
 
     return (
         <>
-            <h1>Events for { org.title }</h1>
+            <h1>Events for { orgQuery.data.title }</h1>
             <ul>
-                { events.map((e) => (
+                { eventsQuery.data.map((e) => (
                     <li key={ e.id }>{ e.title }</li>
                 )) }
             </ul>
