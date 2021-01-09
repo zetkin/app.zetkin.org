58a73d4ab0e655d6cc019aecf052b2b1ca227132
Richard Olsson <r@richardolsson.se>
2021-01-09 15:26:56 +0100

    Disable linting entirely in webpack

--- next.config.js
+++ next.config.js
@@ -1,15 +1,5 @@
 module.exports = {
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    if (dev) {
-      config.module.rules.push({
-        test: /\.(j|t)sx?$/,
-        exclude: /node_modules/,
-        loader: 'eslint-loader',
-        options: {
-          emitWarning: true,
-        }
-      });
-    };
     return config;
   },
 };
