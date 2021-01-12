d29d96ec8fc7ad129bec0a4cec3dcf884fda3c30
kristofferlarberg <mail@kristofferlarberg.se>
2021-01-12 10:54:06 +0100

    create api proxy

--- next.config.js
+++ next.config.js
@@ -2,4 +2,12 @@ module.exports = {
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     return config;
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:slug*',
+        destination: 'https://api.zetk.in/v1/:slug*',
+      },
+    ]
+  },
 };
