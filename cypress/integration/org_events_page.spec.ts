104230e4c4d5df5eb1cd01b771102e82e4736256
Richard Olsson <r@richardolsson.se>
2021-01-23 10:29:45 +0100

    Add simple integration test

---
+++ cypress/integration/org_events_page.spec.ts
@@ -0,0 +1,21 @@
+describe('/o/[orgId]/events', () => {
+    beforeEach(() => {
+        cy.intercept({
+            method: 'GET',
+            url: /\/api\/orgs\/1/,
+        }, {
+            data: {
+                id: 1,
+                title: 'Mocked org',
+            },
+        });
+    });
+
+    it('contains name of organization', () => {
+        cy.visit('/o/1');
+        cy.contains('Mocked org');
+    });
+});
+
+// Hack to flag for typescript as module
+export {};
