84b72707822fba1120d2e518759e4b384f7914ab
Richard Olsson <r@richardolsson.se>
2021-01-29 20:56:09 +0100

    Install and configure Adobe Spectrum UI component library with NEXT

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,8 @@ import DefaultLayout from '../layout/DefaultLayout';
 import { Hydrate } from 'react-query/hydration';
 import { NextPage } from 'next/types';
 import { ReactQueryDevtools } from 'react-query/devtools';
+import { SSRProvider } from '@react-aria/ssr';
+import { Provider, defaultTheme } from '@adobe/react-spectrum';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 const queryClient = new QueryClient();
@@ -20,12 +22,16 @@ function MyApp({ Component, pageProps } : AppProps) : JSX.Element {
     const getLayout = c.getLayout || (page => <DefaultLayout>{ page }</DefaultLayout>);
 
     return (
-        <QueryClientProvider client={ queryClient }>
-            <Hydrate state={ dehydratedState }>
-                { getLayout(<Component { ...restProps } />, restProps) }
-            </Hydrate>
-            <ReactQueryDevtools initialIsOpen={ false } />
-        </QueryClientProvider>
+        <SSRProvider>
+            <Provider theme={ defaultTheme }>
+                <QueryClientProvider client={ queryClient }>
+                    <Hydrate state={ dehydratedState }>
+                        { getLayout(<Component { ...restProps } />, restProps) }
+                    </Hydrate>
+                    <ReactQueryDevtools initialIsOpen={ false } />
+                </QueryClientProvider>
+            </Provider>
+        </SSRProvider>
     );
 }
 
