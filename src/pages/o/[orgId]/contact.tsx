b994b6b8b0d5ad110c33317da50992d208445526
kristofferlarberg <mail@kristofferlarberg.se>
2021-02-01 18:44:58 +0100

    add changes in package-lock.json, add contact.tsx

---
+++ src/pages/o/[orgId]/contact.tsx
@@ -0,0 +1,45 @@
+import { GetServerSideProps } from 'next';
+import { QueryClient } from 'react-query';
+import { dehydrate } from 'react-query/hydration';
+
+import OrgLayout from '../../../layout/OrgLayout';
+import getOrg from '../../../fetching/getOrg';
+
+export const getServerSideProps : GetServerSideProps = async (context) => {
+    const queryClient = new QueryClient();
+    const { orgId } = context.params;
+
+    await queryClient.prefetchQuery(['org', orgId], getOrg(orgId as string));
+
+    const orgState = queryClient.getQueryState(['org', orgId]);
+
+    if (orgState.status === 'success') {
+        return {
+            props: {
+                dehydratedState: dehydrate(queryClient),
+                orgId
+            },
+        };
+    }
+    else {
+        return {
+            notFound: true,
+        };
+    }
+};
+
+export default function OrgContactPage() : JSX.Element {
+    return (
+        <>
+            <h1>Test</h1>
+        </>
+    );
+}
+
+OrgContactPage.getLayout = function getLayout(page, props) {
+    return (
+        <OrgLayout orgId={ props.orgId }>
+            { page }
+        </OrgLayout>
+    );
+};
