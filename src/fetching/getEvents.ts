9ffb111e9c2b23e97c36dcae34612a39ee5e643f
kristofferlarberg <mail@kristofferlarberg.se>
2021-02-04 15:31:52 +0100

    Add interface for getEvents

--- src/fetching/getEvents.ts
+++ src/fetching/getEvents.ts
@@ -1,13 +1,15 @@
+interface ZetkinEvent {
+    activity: { title: string },
+    campaign: { title: string },
+    end_time: string,
+    id: number,
+    location: { title: string },
+    start_time: string,
+    title: string
+}
+
 export default function getEvents(orgId : string) {
-    return async () : Promise<{
-        activity: { title: string },
-        campaign: { title: string },
-        end_time: string,
-        id: number,
-        location: { title: string },
-        start_time: string,
-        title: string
-    }[]> => {
+    return async () : Promise<ZetkinEvent[]> => {
         const cRes = await fetch(`http://localhost:3000/api/orgs/${orgId}/campaigns`);
         const cData = await cRes.json();
 
