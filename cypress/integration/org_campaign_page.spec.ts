97c9f227ddebc6660599e7c80ebfcb7ea4822aed
kristofferlarberg <mail@kristofferlarberg.se>
2021-02-09 09:33:56 +0100

    Fix indentation, add ZetkinCampaign interface

--- cypress/integration/org_campaign_page.spec.ts
+++ cypress/integration/org_campaign_page.spec.ts
@@ -1,22 +1,23 @@
 describe('/o/[orgId]/campaigns/[campId]', () => {
 
-        it('contains campaign content', () => {
-            cy.fixture('dummyCampaign.json')
-            .then((dummyCampaign) => {
-                cy.intercept('GET', /\/api\/orgs\/1\/campaigns\/941$/, dummyCampaign);
-
-                cy.visit('/o/1/campaigns/941');
-                cy.contains(`${dummyCampaign.data.title}`);
-                cy.contains(`${dummyCampaign.data.info_text.substring(0, 50)}`);
-            })
-        });
-
-        it('contains campaign events', () => {
-            cy.intercept('GET', /\/api\/orgs\/1\/campaigns\/941\/actions$/, { fixture: 'dummyEvents' });
+    it('contains campaign content', () => {
+        cy.fixture('dummyCampaign.json')
+        .then((dummyCampaign) => {
+            cy.intercept('GET', /\/api\/orgs\/1\/campaigns\/941$/, dummyCampaign);
 
             cy.visit('/o/1/campaigns/941');
-            cy.get('[data-test="event"]').should('be.visible');
-        });
+            cy.contains(`${dummyCampaign.data.title}`);
+            cy.contains(`${dummyCampaign.data.info_text.substring(0, 50)}`);
+        })
+    });
+
+    it('contains campaign events', () => {
+        cy.intercept('GET', /\/api\/orgs\/1\/campaigns\/941\/actions$/, { fixture: 'dummyEvents' });
+
+        cy.visit('/o/1/campaigns/941');
+        cy.get('[data-test="event"]').should('be.visible');
+    });
 });
+
 // Hack to flag for typescript as module
 export {};
--- src/fetching/getCampaign.ts
+++ src/fetching/getCampaign.ts
@@ -1,5 +1,10 @@
+export interface ZetkinCampaign {
+    info_text: string,
+    title: string
+}
+
 export default function getCampaign(orgId : string, campId : string) {
-    return async () : Promise<{ title: string, info_text: string }> => {
+    return async () : Promise<ZetkinCampaign> => {
         const cIdRes = await fetch(`http://localhost:3000/api/orgs/${orgId}/campaigns/${campId}`);
         const cIdData = await cIdRes.json();
         return cIdData.data;
