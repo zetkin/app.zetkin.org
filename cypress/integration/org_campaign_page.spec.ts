7f9e90fd54bba6bcc327a7d02545fa772e4317d0
kristofferlarberg <mail@kristofferlarberg.se>
2021-02-09 09:44:32 +0100

    fix additional eslint errors

--- cypress/integration/org_campaign_page.spec.ts
+++ cypress/integration/org_campaign_page.spec.ts
@@ -2,13 +2,13 @@ describe('/o/[orgId]/campaigns/[campId]', () => {
 
     it('contains campaign content', () => {
         cy.fixture('dummyCampaign.json')
-        .then((dummyCampaign) => {
-            cy.intercept('GET', /\/api\/orgs\/1\/campaigns\/941$/, dummyCampaign);
+            .then((dummyCampaign) => {
+                cy.intercept('GET', /\/api\/orgs\/1\/campaigns\/941$/, dummyCampaign);
 
-            cy.visit('/o/1/campaigns/941');
-            cy.contains(`${dummyCampaign.data.title}`);
-            cy.contains(`${dummyCampaign.data.info_text.substring(0, 50)}`);
-        })
+                cy.visit('/o/1/campaigns/941');
+                cy.contains(`${dummyCampaign.data.title}`);
+                cy.contains(`${dummyCampaign.data.info_text.substring(0, 50)}`);
+            });
     });
 
     it('contains campaign events', () => {
