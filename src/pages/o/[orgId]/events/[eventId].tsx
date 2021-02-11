3fbeae883bc9d6742010f6a13c78095c88f98222
kristofferlarberg <mail@kristofferlarberg.se>
2021-02-11 15:27:55 +0100

    Add spectrum components to event page

--- src/pages/o/[orgId]/events/[eventId].tsx
+++ src/pages/o/[orgId]/events/[eventId].tsx
@@ -3,7 +3,7 @@ import NextLink from 'next/link';
 import { dehydrate } from 'react-query/hydration';
 import getEvent from '../../../../fetching/getEvent';
 import getOrg from '../../../../fetching/getOrg';
-import { Button, Link } from '@adobe/react-spectrum';
+import { Button, Flex, Heading, Link, Text } from '@adobe/react-spectrum';
 import { QueryClient, useQuery } from 'react-query';
 
 export const getServerSideProps : GetServerSideProps = async (context) => {
@@ -43,25 +43,65 @@ export default function OrgEventPage(props : OrgEventPageProps) : JSX.Element {
     const orgQuery = useQuery(['org', orgId], getOrg(orgId));
 
     return (
-        <>
-            <h1 data-test='event-title'>
+        <Flex direction='column'>
+            <Heading data-test='event-title' level={ 1 }>
                 { eventQuery.data.title ? eventQuery.data.title : eventQuery.data.activity.title }
-            </h1>
-            <Link>
-                <NextLink href={ `/o/${orgId}` }>
-                    <a>{ orgQuery.data.title }</a>
-                </NextLink>
-            </Link>
-            <Link>
-                <NextLink href={ `/o/${orgId}/campaigns/${eventQuery.data.campaign.id}` }>
-                    <a>{ eventQuery.data.campaign.title }</a>
-                </NextLink>
-            </Link>
-            <p data-test='start-time'>{ eventQuery.data.start_time }</p>
-            <p data-test='end-time'>{ eventQuery.data.end_time }</p>
-            <p data-test='info-text'>{ eventQuery.data.info_text }</p>
-            <p data-test='location'>{ eventQuery.data.location.title }</p>
-            <Button data-test='sign-up-button' variant="cta">Sign-up</Button>
-        </>
+            </Heading>
+            <Flex>
+                <Text marginEnd='size-50'>
+                    Organization:
+                </Text>
+                <Link>
+                    <NextLink href={ `/o/${orgId}` }>
+                        <a>{ orgQuery.data.title }</a>
+                    </NextLink>
+                </Link>
+            </Flex>
+            <Flex>
+                <Text marginEnd='size-50'>
+                    Campaign:
+                </Text>
+                <Link>
+                    <NextLink href={ `/o/${orgId}/campaigns/${eventQuery.data.campaign.id}` }>
+                        <a>{ eventQuery.data.campaign.title }</a>
+                    </NextLink>
+                </Link>
+            </Flex>
+            <Flex>
+                <Text marginEnd='size-50'>
+                    Start:
+                </Text>
+                <Text data-test='start-time'>
+                    { eventQuery.data.start_time }
+                </Text>
+            </Flex>
+            <Flex>
+                <Text marginEnd='size-50'>
+                    End:
+                </Text>
+                <Text data-test='end-time'>
+                    { eventQuery.data.end_time }
+                </Text>
+            </Flex>
+            <Flex>
+                <Text marginEnd='size-50'>
+                    Information:
+                </Text>
+                <Text data-test='info-text'>
+                    { eventQuery.data.info_text }
+                </Text>
+            </Flex>
+            <Flex>
+                <Text marginEnd='size-50'>
+                    Location:
+                </Text>
+                <Text data-test='location'>
+                    { eventQuery.data.location.title }
+                </Text>
+            </Flex>
+            <Button data-test='sign-up-button' variant='cta' marginY='size-200'>
+                Sign-up
+            </Button>
+        </Flex>
     );
 }
