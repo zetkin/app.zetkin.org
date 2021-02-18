5b2c5b615a5385bfa2d0fa08569c9db27231f198
Richard Olsson <r@richardolsson.se>
2021-02-18 18:51:15 +0100

    Use access token from session in API proxy

--- src/pages/api/[...path].ts
+++ src/pages/api/[...path].ts
@@ -1,6 +1,9 @@
+import Z from 'zetkin';
+import { applySession } from 'next-session';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-import Z from 'zetkin';
+import { AppSession } from '../../types';
+import stringToBool from '../../utils/stringToBool';
 
 const HTTP_VERBS_TO_ZETKIN_METHODS = {
     'DELETE': (resource) => resource.del(),
@@ -10,11 +13,28 @@ const HTTP_VERBS_TO_ZETKIN_METHODS = {
     'PUT': (resource, req) => resource.put(req.body),
 };
 
-export default async function handle(req : NextApiRequest, res : NextApiResponse) : Promise<void> {
-    const z = Z.construct();
+type NextApiRequestWithSession = NextApiRequest & {
+    session: AppSession;
+}
+
+export default async function handle(req : NextApiRequestWithSession, res : NextApiResponse) : Promise<void> {
+    const z = Z.construct({
+        clientId: process.env.ZETKIN_CLIENT_ID,
+        clientSecret: process.env.ZETKIN_CLIENT_SECRET,
+        ssl: stringToBool(process.env.ZETKIN_USE_TLS),
+        zetkinDomain: process.env.ZETKIN_API_HOST,
+    });
 
     const path = req.query.path as string[];
     const resource = z.resource(path.join('/'));
+
+    try {
+        await applySession(req, res);
+        z.setTokenData(req.session.tokenData);
+    }
+    catch (err) {
+        // No problem if the session could not be found
+    }
 
     try {
         const method = HTTP_VERBS_TO_ZETKIN_METHODS[req.method];
