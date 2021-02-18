b269f6dfac7c5f6950b0d958ec99c3253a4b6af9
Richard Olsson <r@richardolsson.se>
2021-02-18 17:51:27 +0100

    Add /login path to redirect to OAuth login page

---
+++ src/pages/login.tsx
@@ -0,0 +1,27 @@
+import { GetServerSideProps } from "next";
+import Z from 'zetkin';
+
+import stringToBool from "../utils/stringToBool";
+
+export const getServerSideProps : GetServerSideProps = async () => {
+    const z = Z.construct({
+        clientId: process.env.ZETKIN_CLIENT_ID,
+        clientSecret: process.env.ZETKIN_CLIENT_SECRET,
+        ssl: stringToBool(process.env.ZETKIN_USE_TLS),
+        zetkinDomain: process.env.ZETKIN_API_HOST,
+    });
+
+    const protocol = stringToBool(process.env.APP_USE_TLS)? 'https' : 'http';
+    const host = process.env.APP_HOST;
+
+    return {
+        redirect: {
+            destination: z.getLoginUrl(`${protocol}://${host}`),
+            permanent: true,
+        }
+    };
+};
+
+export default function NotUsed() : JSX.Element {
+    return null;
+}
