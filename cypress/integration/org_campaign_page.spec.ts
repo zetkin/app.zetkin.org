717c77a901f1b3c88805aa61888d311142206718
Richard Olsson <r@richardolsson.se>
2021-02-20 11:55:48 +0100

    Change integration tests to use data from api.dev.zetkin.org

--- cypress/integration/org_campaign_page.spec.ts
+++ cypress/integration/org_campaign_page.spec.ts
@@ -1,20 +1,12 @@
 describe('/o/[orgId]/campaigns/[campId]', () => {
 
     it('contains campaign content', () => {
-        cy.fixture('dummyCampaign.json')
-            .then((dummyCampaign) => {
-                cy.intercept('GET', /\/api\/orgs\/1\/campaigns\/941$/, dummyCampaign);
-
-                cy.visit('/o/1/campaigns/941');
-                cy.contains(`${dummyCampaign.data.title}`);
-                cy.contains(`${dummyCampaign.data.info_text.substring(0, 50)}`);
-            });
+        cy.visit('/o/1/campaigns/2');
+        cy.contains('Second campaign');
     });
 
     it('contains campaign events', () => {
-        cy.intercept('GET', /\/api\/orgs\/1\/campaigns\/941\/actions$/, { fixture: 'dummyEvents' });
-
-        cy.visit('/o/1/campaigns/941');
+        cy.visit('/o/1/campaigns/2');
         cy.get('[data-test="event"]').should('be.visible');
     });
 });
