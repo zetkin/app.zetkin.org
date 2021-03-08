8065089d6a16096b5d8d6f14ba411a11fe71249c
Richard Olsson <r@richardolsson.se>
2021-03-08 10:08:55 +0100

    Don't open dev tools by default in Cypress

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -31,15 +31,5 @@ module.exports = (on, config) => {
         }
     }));
 
-    on('before:browser:launch', (browser = {}, options) => {
-        if (browser.name === 'firefox') {
-            options.args.push('--devtools');
-        }
-        else if (browser.name === 'chrome') {
-            options.args.push('--auto-open-devtools-for-tabs');
-        }
-
-        return options;
-    });
     return config;
 }
