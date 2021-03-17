8bc7ca59fe335943510c40a6132ecaf7ee110ca4
kristofferlarberg <mail@kristofferlarberg.se>
2021-03-17 18:57:57 +0100

    Replace else if with return and use type assertion on data variables

--- src/pages/o/[orgId]/events/[eventId].tsx
+++ src/pages/o/[orgId]/events/[eventId].tsx
@@ -13,6 +13,7 @@ import getEvent from '../../../../fetching/getEvent';
 import getOrg from '../../../../fetching/getOrg';
 import { scaffold } from '../../../../utils/next';
 import { ZetkinEvent } from '../../../../interfaces/ZetkinEvent';
+import { ZetkinOrganization } from '../../../../interfaces/ZetkinOrganization';
 
 const scaffoldOptions = {
     localeScope: [
@@ -61,86 +62,84 @@ export default function OrgEventPage(props : OrgEventPageProps) : JSX.Element | 
     if (!eventQuery.data) {
         return null;
     }
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    else if (eventQuery.data! && orgQuery.data!) {
-        const event : ZetkinEvent = eventQuery.data;
 
-        return (
-            <Flex direction="column">
-                <Heading data-test="event-title" level={ 1 }>
-                    { event.title ? event.title : event.activity.title }
-                </Heading>
-                <Flex>
-                    <Text marginEnd="size-50">
-                        <Msg id="pages.orgEvent.details.org"/>:
-                    </Text>
-                    <Link>
-                        <NextLink href={ `/o/${orgId}` }>
-                            <a>{ orgQuery.data.title }</a>
-                        </NextLink>
-                    </Link>
-                </Flex>
-                <Flex>
-                    <Text marginEnd="size-50">
-                        <Msg id="pages.orgEvent.details.campaign"/>:
-                    </Text>
-                    <Link>
-                        <NextLink href={ `/o/${orgId}/campaigns/${event.campaign.id}` }>
-                            <a>{ event.campaign.title }</a>
-                        </NextLink>
-                    </Link>
-                </Flex>
-                <Flex>
-                    <Text marginEnd="size-50">
-                        <Msg id="pages.orgEvent.details.start"/>:
-                    </Text>
-                    <Text data-test="start-time">
-                        <FormattedDate
-                            day="2-digit"
-                            month="long"
-                            value={ Date.parse(event.start_time) }
-                        />
-                        , <FormattedTime
-                            value={ Date.parse(event.start_time) }
-                        />
-                    </Text>
-                </Flex>
-                <Flex>
-                    <Text marginEnd="size-50">
-                        <Msg id="pages.orgEvent.details.end"/>:
-                    </Text>
-                    <Text data-test="end-time">
-                        <FormattedDate
-                            day="2-digit"
-                            month="long"
-                            value={ Date.parse(event.end_time) }
-                        />
-                        , <FormattedTime
-                            value={ Date.parse(event.end_time) }
-                        />
-                    </Text>
-                </Flex>
-                <Flex>
-                    <Text marginEnd="size-50">
-                        <Msg id="pages.orgEvent.details.info"/>:
-                    </Text>
-                    <Text data-test="info-text">
-                        { event.info_text }
-                    </Text>
-                </Flex>
-                <Flex>
-                    <Text marginEnd="size-50">
-                        <Msg id="pages.orgEvent.details.location"/>:
-                    </Text>
-                    <Text data-test="location">
-                        { event.location.title }
-                    </Text>
-                </Flex>
-                <Button data-test="sign-up-button" marginY="size-200" variant="cta">
-                    <Msg id="pages.orgEvent.actions.signUp"/>
-                </Button>
+    const event = eventQuery.data as ZetkinEvent;
+    const org = orgQuery.data as ZetkinOrganization;
+
+    return (
+        <Flex direction="column">
+            <Heading data-test="event-title" level={ 1 }>
+                { event.title ? event.title : event.activity.title }
+            </Heading>
+            <Flex>
+                <Text marginEnd="size-50">
+                    <Msg id="pages.orgEvent.details.org"/>:
+                </Text>
+                <Link>
+                    <NextLink href={ `/o/${orgId}` }>
+                        <a>{ org.title }</a>
+                    </NextLink>
+                </Link>
             </Flex>
-        );
-    }
-    return null;
+            <Flex>
+                <Text marginEnd="size-50">
+                    <Msg id="pages.orgEvent.details.campaign"/>:
+                </Text>
+                <Link>
+                    <NextLink href={ `/o/${orgId}/campaigns/${event.campaign.id}` }>
+                        <a>{ event.campaign.title }</a>
+                    </NextLink>
+                </Link>
+            </Flex>
+            <Flex>
+                <Text marginEnd="size-50">
+                    <Msg id="pages.orgEvent.details.start"/>:
+                </Text>
+                <Text data-test="start-time">
+                    <FormattedDate
+                        day="2-digit"
+                        month="long"
+                        value={ Date.parse(event.start_time) }
+                    />
+                    , <FormattedTime
+                        value={ Date.parse(event.start_time) }
+                    />
+                </Text>
+            </Flex>
+            <Flex>
+                <Text marginEnd="size-50">
+                    <Msg id="pages.orgEvent.details.end"/>:
+                </Text>
+                <Text data-test="end-time">
+                    <FormattedDate
+                        day="2-digit"
+                        month="long"
+                        value={ Date.parse(event.end_time) }
+                    />
+                    , <FormattedTime
+                        value={ Date.parse(event.end_time) }
+                    />
+                </Text>
+            </Flex>
+            <Flex>
+                <Text marginEnd="size-50">
+                    <Msg id="pages.orgEvent.details.info"/>:
+                </Text>
+                <Text data-test="info-text">
+                    { event.info_text }
+                </Text>
+            </Flex>
+            <Flex>
+                <Text marginEnd="size-50">
+                    <Msg id="pages.orgEvent.details.location"/>:
+                </Text>
+                <Text data-test="location">
+                    { event.location.title }
+                </Text>
+            </Flex>
+            <Button data-test="sign-up-button" marginY="size-200" variant="cta">
+                <Msg id="pages.orgEvent.actions.signUp"/>
+            </Button>
+        </Flex>
+    );
 }
