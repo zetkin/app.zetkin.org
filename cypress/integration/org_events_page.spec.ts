45f317f353e9614ba49f4dd270dac70b7650fc5b
kristofferlarberg <mail@kristofferlarberg.se>
2021-03-18 16:45:34 +0100

    Add links from EventList to event

--- cypress/integration/org_events_page.spec.ts
+++ cypress/integration/org_events_page.spec.ts
@@ -5,10 +5,14 @@ describe('/o/[orgId]/events', () => {
         cy.contains('My Organization');
     });
 
-    xit('contains events', () => {
+    xit('contains events which are linked to event pages', () => {
         // TODO: Figure out why this fails on GitHub
         cy.visit('/o/1/events');
         cy.get('[data-test="event"]').should('be.visible');
+        cy.get('[data-test="more-info-button-22"]')
+            .should('be.visible')
+            .click();
+        cy.url().should('match', /\/o\/1\/events\/[0-9]+$/);
     });
 
     xit('contains a placeholder if there are no events', () => {
