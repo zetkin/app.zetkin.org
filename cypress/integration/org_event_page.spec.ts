897001642d6df4664b9d03027ba6108045de8f13
kristofferlarberg <mail@kristofferlarberg.se>
2021-04-19 16:19:56 +0200

    Adjust integration tests for sign-up button

--- cypress/integration/org_event_page.spec.ts
+++ cypress/integration/org_event_page.spec.ts
@@ -1,4 +1,12 @@
 describe('/o/[orgId]/events/[eventId]', () => {
+    beforeEach(() => {
+        cy.request('delete', 'http://localhost:8001/_mocks');
+    });
+
+    after(() => {
+        cy.request('delete', 'http://localhost:8001/_mocks');
+    });
+
     it('contains non-interactive event content', () => {
         cy.visit('/o/1/events/25');
         cy.get('[data-test="event-title"]').should('be.visible');
@@ -20,18 +28,52 @@ describe('/o/[orgId]/events/[eventId]', () => {
         cy.url().should('match', /\/o\/1\/campaigns\/2$/);
     });
 
-    it.only('contains a conditional sign-up button', () => {
+    it('shows a sign-up button if user is not signed up to the event', () => {
+        cy.request('put', 'http://localhost:8001/v1/users/me/action_responses/_mocks/get', {
+            response: {
+                data: {
+                    data: [],
+                },
+            },
+        });
+
         cy.visit('/login');
         cy.get('input[aria-label="E-mail address"]').type('testadmin@example.com');
         cy.get('input[aria-label="Password"]').type('password');
         cy.get('input[aria-label="Log in"]')
             .click();
+
         cy.visit('/o/1/events/25');
         cy.waitUntilReactRendered();
-        cy.findByText('Sign-up')
-            .click();
-        cy.waitUntilReactRendered();
-        cy.findByText('Undo sign-up');
+        cy.findByText('Sign-up').click();
+        //TODO: Verify that API request is done corrently.
+    });
+
+    it('shows an undo sign-up button if user is signed up to the event', () => {
+        cy.fixture('dummyEventResponses').then(json => {
+            cy.request('put', 'http://localhost:8001/v1/users/me/action_responses/_mocks/get', {
+                response: {
+                    data: json,
+                },
+            });
+
+            cy.request('put', 'http://localhost:8001/v1/orgs/1/actions/25/responses/2/_mocks/delete', {
+                response: {
+                    status: 204,
+                },
+            });
+
+            cy.visit('/login');
+            cy.get('input[aria-label="E-mail address"]').type('testadmin@example.com');
+            cy.get('input[aria-label="Password"]').type('password');
+            cy.get('input[aria-label="Log in"]')
+                .click();
+
+            cy.visit('/o/1/events/25');
+            cy.waitUntilReactRendered();
+            cy.findByText('Undo sign-up').click();
+            //TODO: Verify that API request is done corrently.
+        });
     });
 });
 
