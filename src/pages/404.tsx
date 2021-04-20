45c4e0155fe80f2629e8fd258a71b7a711bc300e
nehahirve <neha.hirve@hyperisland.se>
2021-04-20 13:02:41 +0200

    Change AdobeLink to Link. Hard code localeScope array inside getStaticProps.

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,18 +2,14 @@ import { getMessages } from '../utils/locale';
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { FormattedMessage as Msg } from 'react-intl';
-import { Link as AdobeLink, Content, Flex, Heading } from '@adobe/react-spectrum';
-
-const options = {
-    localeScope: [
-        'pages.404',
-        'misc.publicHeader',
-    ],
-};
+import { Content, Flex, Heading, Link } from '@adobe/react-spectrum';
 
 export const getStaticProps: GetStaticProps = async () => {
     const lang = 'en';
-    const messages = await getMessages(lang, options?.localeScope ?? []);
+    const messages = await getMessages(lang, [
+        'pages.404',
+        'misc.publicHeader',
+    ]);
     return {
         props: { lang, messages },
     };
@@ -34,11 +30,11 @@ export default function Custom404() : JSX.Element {
                     <Heading level={ 1 }>
                         <Msg id="pages.404.pageNotFound"/>
                     </Heading>
-                    <AdobeLink>
+                    <Link>
                         <a href="/">
                             <Msg id="pages.404.backToHomePage"/>
                         </a>
-                    </AdobeLink>
+                    </Link>
                 </Flex>
             </Content>
         </>
