22a5dd5c019b15260d5487e631cf0afd74fe22c5
ziggi <ziggi.willpower@gmail.com>
2021-04-20 15:25:30 +0200

    Add logout page that resets session id and redirects to login page

---
+++ src/pages/logout.tsx
@@ -0,0 +1,27 @@
+import { GetServerSideProps } from 'next';
+
+import { AppSession } from '../types';
+import { scaffold } from '../utils/next';
+
+export const getServerSideProps: GetServerSideProps = scaffold(async (context) => {
+    try {
+        await context.z.resource('session').del();
+    }
+    catch (error) {
+        //if user cannot log out, they are prob already logged out
+    }
+
+    const reqWithSession = context.req as { session?: AppSession };
+    reqWithSession.session?.destroy();
+
+    return {
+        redirect: {
+            destination: '/',
+            permanent: false,
+        },
+    };
+});
+
+export default function NotUsed() : null {
+    return null;
+}
