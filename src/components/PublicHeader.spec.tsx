5ddf7150a48cf6de77905e78ec188c3e0afff6cd
ziggi <ziggi.willpower@gmail.com>
2021-04-21 11:56:35 +0200

    Update component test of PublicHeader to use mountWithProviders

--- src/components/PublicHeader.spec.tsx
+++ src/components/PublicHeader.spec.tsx
@@ -1,5 +1,3 @@
-import { mount } from '@cypress/react';
-
 import { mountWithProviders } from '../utils/testing';
 import PublicHeader from './PublicHeader';
 import { ZetkinOrganization } from '../interfaces/ZetkinOrganization';
@@ -19,29 +17,29 @@ describe('PublicHeader', () => {
     });
 
     it('contains no login button when logged in', () => {
-        mount(<PublicHeader user={ dummyUser }/>);
+        mountWithProviders(<PublicHeader user={ dummyUser }/>);
         cy.contains('misc.publicHeader.login').should('not.exist');
     });
 
     it('contains user name if logged in', () => {
-        mount(<PublicHeader user={ dummyUser }/>);
+        mountWithProviders(<PublicHeader user={ dummyUser }/>);
         cy.contains(`${dummyUser.first_name} ${dummyUser.last_name}`);
     });
 
     it('contains user avatar if logged in', () => {
-        mount(<PublicHeader user={ dummyUser }/>);
+        mountWithProviders(<PublicHeader user={ dummyUser }/>);
         cy.get('[data-test="user-avatar"]').should('be.visible');
     });
 
     it('contains a zetkin logo when component has no org prop', () => {
-        mount(<PublicHeader user={ dummyUser }/>);
+        mountWithProviders(<PublicHeader user={ dummyUser }/>);
         cy.get('[data-test="zetkin-logotype"]').should('be.visible');
     });
 
     it('contains org avatar instead of zetkin logo when component has an org prop', () => {
         cy.fixture('dummyOrg.json')
             .then((data : ZetkinOrganization) => {
-                mount(<PublicHeader org={ data } user={ dummyUser }/>);
+                mountWithProviders(<PublicHeader org={ data } user={ dummyUser }/>);
 
                 cy.get('[data-test="zetkin-logotype"]').should('not.exist');
                 cy.get('[data-test="org-avatar"]').should('be.visible');
