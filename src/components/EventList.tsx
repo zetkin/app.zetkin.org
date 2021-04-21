5ed9572090e619cce61ca1f398aa0b4acc0b21b3
ziggi <ziggi.willpower@gmail.com>
2021-04-21 16:13:10 +0200

    Create EventListItem as component in EventList

--- src/components/EventList.tsx
+++ src/components/EventList.tsx
@@ -22,6 +22,73 @@ interface EventListProps {
     onEventResponse: (eventId: number, orgId: number, response: boolean) => void;
 }
 
+interface EventListItemProps {
+    event: ZetkinEvent;
+    org: ZetkinOrganization;
+    eventResponses: ZetkinEventResponse[] | undefined;
+    onEventResponse: (eventId: number, orgId: number, response: boolean) => void;
+}
+
+const EventListItem = ( { event, eventResponses, onEventResponse, org } : EventListItemProps): JSX.Element => {
+
+    const response = eventResponses?.find(response => response.action_id === event.id);
+
+    return (
+        <Flex key={ event.id } data-test="event" direction="column" margin="size-200">
+            <View data-test="event-title">
+                { event.title ? event.title : event.activity.title }
+            </View>
+            <View data-test="org-title">{ org.title }</View>
+            <View data-test="campaign-title">{ event.campaign.title }</View>
+            <View data-test="start-time">
+                <FormattedDate
+                    day="2-digit"
+                    month="long"
+                    value={ Date.parse(event.start_time) }
+                />
+                , <FormattedTime
+                    value={ Date.parse(event.start_time) }
+                />
+            </View>
+            <View data-test="end-time">
+                <FormattedDate
+                    day="2-digit"
+                    month="long"
+                    value={ Date.parse(event.end_time) }
+                />
+                , <FormattedTime
+                    value={ Date.parse(event.end_time) }
+                />
+            </View>
+            <View data-test="location-title">{ event.location.title }</View>
+            { response ? (
+                <Button
+                    data-test="event-response-button"
+                    marginTop="size-50"
+                    onPress={ () => onEventResponse(event.id, org.id, true) }
+                    variant="cta">
+                    <Msg id="misc.eventList.undoSignup"/>
+                </Button>
+            ) : (
+                <Button
+                    data-test="event-response-button"
+                    marginTop="size-50"
+                    onPress={ () => onEventResponse(event.id, org.id, false) }
+                    variant="cta">
+                    <Msg id="misc.eventList.signup"/>
+                </Button>
+            ) }
+            <NextLink href={ `/o/${org.id}/events/${event.id}` }>
+                <a>
+                    <Button marginTop="size-50" variant="cta">
+                        <Msg id="misc.eventList.moreInfo"/>
+                    </Button>
+                </a>
+            </NextLink>
+        </Flex>
+    );
+}
+
 const EventList = ({ eventResponses, events, onEventResponse, org } : EventListProps) : JSX.Element => {
 
     if (!events || events.length === 0) {
@@ -35,63 +102,13 @@ const EventList = ({ eventResponses, events, onEventResponse, org } : EventListP
     return (
         <>
             <Flex data-test="event-list" direction="row" gap="100" wrap>
-                { events?.map((e) => {
-                    const response = eventResponses?.find(response => response.action_id === e.id);
-                    return (
-                        <Flex key={ e.id } data-test="event" direction="column" margin="size-200">
-                            <View data-test="event-title">
-                                { e.title ? e.title : e.activity.title }
-                            </View>
-                            <View data-test="org-title">{ org.title }</View>
-                            <View data-test="campaign-title">{ e.campaign.title }</View>
-                            <View data-test="start-time">
-                                <FormattedDate
-                                    day="2-digit"
-                                    month="long"
-                                    value={ Date.parse(e.start_time) }
-                                />
-                                , <FormattedTime
-                                    value={ Date.parse(e.start_time) }
-                                />
-                            </View>
-                            <View data-test="end-time">
-                                <FormattedDate
-                                    day="2-digit"
-                                    month="long"
-                                    value={ Date.parse(e.end_time) }
-                                />
-                                , <FormattedTime
-                                    value={ Date.parse(e.end_time) }
-                                />
-                            </View>
-                            <View data-test="location-title">{ e.location.title }</View>
-                            { response ? (
-                                <Button
-                                    data-test="event-response-button"
-                                    marginTop="size-50"
-                                    onPress={ () => onEventResponse(e.id, org.id, true) }
-                                    variant="cta">
-                                    <Msg id="misc.eventList.undoSignup"/>
-                                </Button>
-                            ) : (
-                                <Button
-                                    data-test="event-response-button"
-                                    marginTop="size-50"
-                                    onPress={ () => onEventResponse(e.id, org.id, false) }
-                                    variant="cta">
-                                    <Msg id="misc.eventList.signup"/>
-                                </Button>
-                            ) }
-                            <NextLink href={ `/o/${org.id}/events/${e.id}` }>
-                                <a>
-                                    <Button marginTop="size-50" variant="cta">
-                                        <Msg id="misc.eventList.moreInfo"/>
-                                    </Button>
-                                </a>
-                            </NextLink>
-                        </Flex>
-                    );
-                }) }
+                {events?.map((e) => {
+                    return <EventListItem
+                        event={e}
+                        eventResponses={eventResponses}
+                        onEventResponse={onEventResponse}
+                        org={org} />
+                })}
             </Flex>
         </>
     );
