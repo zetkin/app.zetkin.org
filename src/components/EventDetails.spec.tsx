b7843140f3b2d1d5efe42c40b2fda810098aec57
nehahirve <neha.hirve@hyperisland.se>
2021-04-22 12:01:55 +0200

    extract EventDetails to component. Write unit tests.

---
+++ src/components/EventDetails.spec.tsx
@@ -0,0 +1,137 @@
+import EventDetails from './EventDetails';
+import { mountWithProviders } from '../utils/testing';
+import { ZetkinEvent } from '../interfaces/ZetkinEvent';
+import { ZetkinEventResponse } from '../types/zetkin';
+import { ZetkinOrganization } from '../interfaces/ZetkinOrganization';
+
+describe('EventDetails', () => {
+    let dummyOrg : ZetkinOrganization;
+    let dummyEvent : ZetkinEvent;
+    let dummyEventResponse : ZetkinEventResponse | undefined;
+
+    beforeEach(()=> {
+        cy.fixture('dummyOrg.json')
+            .then((data : ZetkinOrganization) => {
+                dummyOrg = data;
+            });
+        cy.fixture('dummyEvents.json')
+            .then((data : {data: ZetkinEvent[]}) => {
+                dummyEvent = data.data[0];
+            });
+        cy.fixture('dummyEventResponses.json')
+            .then((data : {data: ZetkinEventResponse[]}) => {
+                dummyEventResponse = data.data[0];
+            });
+    });
+
+    it('contains data for each event', () => {
+        mountWithProviders(
+            <EventDetails
+                event={ dummyEvent }
+                onEventResponse={ () => null }
+                org={ dummyOrg }
+                response={ dummyEventResponse }
+            />,
+        );
+        cy.get('[data-test="event-title"]').should('be.visible');
+        cy.get('[data-test="org-title"]').should('be.visible');
+        cy.get('[data-test="campaign-title"]').should('be.visible');
+        cy.get('[data-test="event-dates"]').should('be.visible');
+        cy.get('[data-test="event-times"]').should('be.visible');
+        cy.get('[data-test="location"]').should('be.visible');
+        cy.get('[data-test="info-text"]').should('be.visible');
+    });
+
+    it('does not show event info if none is provided', () => {
+        dummyEvent.info_text = '';
+        mountWithProviders(
+            <EventDetails
+                event={ dummyEvent }
+                onEventResponse={ () => null }
+                org={ dummyOrg }
+                response={ dummyEventResponse }
+            />,
+        );
+        cy.get('[data-test="info-text"]').should('not.be.visible');
+    });
+
+    it('contains an activity title instead of missing event title', () => {
+        dummyEvent.title = undefined;
+        mountWithProviders(
+            <EventDetails
+                event={ dummyEvent }
+                onEventResponse={ () => null }
+                org={ dummyOrg }
+                response={ dummyEventResponse }
+            />,
+        );
+        cy.get('[data-test="event-title"]')
+            .should('contain', dummyEvent.activity.title)
+            .should('not.contain', 'undefined');
+    });
+
+    it('contains a link back to the organisation page', () => {
+        mountWithProviders(
+            <EventDetails
+                event={ dummyEvent }
+                onEventResponse={ () => null }
+                org={ dummyOrg }
+                response={ dummyEventResponse }
+            />,
+        );
+        cy.get('[data-test="org-title"]')
+            .should('have.attr', 'href', `/o/${dummyOrg.id}`);
+    });
+
+    it('contains a link back to the campaign page', () => {
+        mountWithProviders(
+            <EventDetails
+                event={ dummyEvent }
+                onEventResponse={ () => null }
+                org={ dummyOrg }
+                response={ dummyEventResponse }
+            />,
+        );
+        cy.get('[data-test="campaign-title"]')
+            .should('have.attr', 'href', `/o/${dummyOrg.id}/campaigns/${dummyEvent.campaign.id}`);
+    });
+
+    it('contains a sign-up button for the event', () => {
+        const spyOnSubmit = cy.spy();
+        mountWithProviders(
+            <EventDetails
+                event={ dummyEvent }
+                onEventResponse={ spyOnSubmit }
+                org={ dummyOrg }
+                response={ undefined }
+            />,
+        );
+
+        cy.findByText('pages.orgEvent.actions.signup')
+            .eq(0)
+            .click()
+            .then(() => {
+                expect(spyOnSubmit).to.be.calledOnce;
+            });
+    });
+
+    it('contains a cancel sign-up button for the event if there is no response', () => {
+        const spyOnSubmit = cy.spy();
+        
+        mountWithProviders(
+            <EventDetails
+                event={ dummyEvent }
+                onEventResponse={ spyOnSubmit }
+                org={ dummyOrg }
+                response={ dummyEventResponse }
+            />,
+        );
+
+        cy.findByText('pages.orgEvent.actions.undoSignup')
+            .eq(0)
+            .click()
+            .then(() => {
+                expect(spyOnSubmit).to.be.calledOnce;
+            });
+    });
+});
---
+++ src/components/EventDetails.tsx
@@ -0,0 +1,132 @@
+import Calendar from '@spectrum-icons/workflow/Calendar';
+import Flag from '@spectrum-icons/workflow/Flag';
+import Location from '@spectrum-icons/workflow/Location';
+import NextLink from 'next/link';
+import {
+    Button,
+    Divider,
+    Flex,
+    Header,
+    Heading,
+    Image,
+    Link,
+    Text,
+    View,
+} from '@adobe/react-spectrum';
+import {
+    FormattedDate,
+    FormattedTime,
+    FormattedMessage as Msg,
+} from 'react-intl';
+
+import { ZetkinEvent } from '../interfaces/ZetkinEvent';
+import { ZetkinEventResponse } from '../types/zetkin';
+import { ZetkinOrganization } from '../interfaces/ZetkinOrganization';
+
+interface EventDetailsProps {
+    event: ZetkinEvent;
+    org: ZetkinOrganization;
+    onEventResponse: (eventId: number, orgId: number, response: boolean) => void;
+    response: ZetkinEventResponse | undefined;
+}
+
+const EventDetails = ({ event, org, onEventResponse, response } : EventDetailsProps) : JSX.Element => {
+    return (
+        <>
+            <Header marginBottom="size-300">
+                <Image
+                    alt="Cover image"
+                    height="size-2000"
+                    objectFit="cover"
+                    src="/cover.jpg"
+                    width="100%"
+                />
+                <Heading
+                    data-test="event-title"
+                    level={ 1 }
+                    marginBottom="size-50">
+                    { event.title ? event.title : event.activity.title }
+                </Heading>
+                <Link>
+                    <NextLink
+                        href={ `/o/${org.id}` }>
+                        <a data-test="org-title">{ org.title }</a>
+                    </NextLink>
+                </Link>
+            </Header>
+            <Flex marginBottom="size-100">
+                <Flag marginEnd="size-100" size="S" />
+                <Link>
+                    <NextLink
+                        href={ `/o/${org.id}/campaigns/${event.campaign.id}` }>
+                        <a data-test="campaign-title">{ event.campaign.title } </a>
+                    </NextLink>
+                </Link>
+            </Flex>
+            <Flex
+                alignItems="center"
+                data-test="duration"
+                marginBottom="size-100">
+                <Calendar marginEnd="size-100" size="S" />
+                <Flex direction="column">
+                    <Text data-test="event-dates">
+                        <FormattedDate
+                            day="2-digit"
+                            month="long"
+                            value={ Date.parse(event.start_time) }
+                        />
+                        –
+                        <FormattedDate
+                            day="2-digit"
+                            month="long"
+                            value={ Date.parse(event.end_time) }
+                        />
+                    </Text>
+                    <Text data-test="event-times">
+                        <FormattedTime
+                            value={ Date.parse(event.start_time) }
+                        />
+                        –
+                        <FormattedTime
+                            value={ Date.parse(event.end_time) }
+                        />
+                    </Text>
+                </Flex>
+            </Flex>
+            <Flex marginBottom="size-300">
+                <Location marginEnd="size-100" size="S" />
+                <Text data-test="location">{ event.location.title }</Text>
+            </Flex>
+            <Divider />
+            <Text data-test="info-text" marginY="size-300">
+                { event.info_text }
+            </Text>
+            <View
+                bottom="size-200"
+                left="size-200"
+                marginTop="size-200"
+                position="absolute"
+                right="size-200">
+                { response ? (
+                    <Button
+                        data-test="event-response-button"
+                        onPress={ () => onEventResponse(event.id, org.id, true) }
+                        variant="cta"
+                        width="100%">
+                        <Msg id="pages.orgEvent.actions.undoSignup" />
+                    </Button>
+                ) : (
+                    <Button
+                        data-test="event-response-button"
+                        onPress={ () => onEventResponse(event.id, org.id, false) }
+                        variant="cta"
+                        width="100%">
+                        <Msg id="pages.orgEvent.actions.signup" />
+                    </Button>
+                ) }
+            </View>
+        </>
+    );
+};
+
+export default EventDetails;
