5fa87a632291d86b41e5139c192ce20ebeb163fc
nehahirve <neha.hirve@hyperisland.se>
2021-05-05 16:45:22 +0200

    add .js files in cypress/ and src/ to ESLint. remove extra code and lint index.js file in cypress folder

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -2,45 +2,32 @@
 require('@cypress/react/support');
 require('@testing-library/cypress/add-commands');
 
-const SESSION_ID = 'sid';
+const COOKIE_NAME = 'sid';
 
 Cypress.Commands.add('waitUntilReactRendered', (timeout = 10000) => {
     cy.window().its('__reactRendered', { timeout }).then(initialized => {
         return initialized;
     });
 });
-
-Cypress.Commands.add('waitUntilReactRendered', (timeout = 10000) => {
-    cy.window().its('__reactRendered', { timeout }).then(initialized => {
-        return initialized;
-    });
-});
-
-Cypress.Commands.add('loginFromRedirect', () => {
-    cy.get('input[aria-label="E-mail address"]').type('testadmin@example.com', {delay: 0});
-    cy.get('input[aria-label="Password"]').type('password', {delay: 0});
-    cy.get('input[aria-label="Log in"]')
-        .should('be.visible')
-        .click();
-})
 
 Cypress.Commands.add('login', () => {
-    Cypress.Cookies.preserveOnce(SESSION_ID);
+    Cypress.Cookies.preserveOnce(COOKIE_NAME);
 
-    cy.getCookie(SESSION_ID).then(cookie => {
+    cy.getCookie(COOKIE_NAME).then(cookie => {
         if (!cookie) {
-            cy.visit('/login')
-            cy.fillLoginForm()
-        } else {
-            cy.getCookie(SESSION_ID).should('have.property', 'value')
+            cy.visit('/login');
+            cy.fillLoginForm();
         }
-    })
-})
+        else {
+            cy.getCookie(COOKIE_NAME).should('have.property', 'value');
+        }
+    });
+});
 
 Cypress.Commands.add('fillLoginForm', () => {
     cy.get('input[aria-label="E-mail address"]').type('testadmin@example.com');
-            cy.get('input[aria-label="Password"]').type('password');
-            cy.get('input[aria-label="Log in"]')
-                .should('be.visible')
-                .click();
-})
+    cy.get('input[aria-label="Password"]').type('password');
+    cy.get('input[aria-label="Log in"]')
+        .should('be.visible')
+        .click();
+});
