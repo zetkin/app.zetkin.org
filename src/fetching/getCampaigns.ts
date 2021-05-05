060b24ae73a25183b9554a80dc982cb1343c0cf1
nehahirve <neha.hirve@hyperisland.se>
2021-05-05 11:54:58 +0200

    refactor ZetkinCampaign interface

--- src/fetching/getCampaigns.ts
+++ src/fetching/getCampaigns.ts
@@ -1,7 +1,8 @@
 import apiUrl from '../utils/apiUrl';
+import { ZetkinCampaign } from '../types/zetkin';
 
 export default function getCampaigns(orgId : string) {
-    return async () : Promise<{ id: number; title: string }[]> => {
+    return async () : Promise<ZetkinCampaign[]> => {
         const cRes = await fetch(apiUrl(`/orgs/${orgId}/campaigns`));
         const cData = await cRes.json();
         return cData.data;
