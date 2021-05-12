a899ba69d73886317b3fe6f1369e43685d89c9eb
kristofferlarberg <mail@kristofferlarberg.se>
2021-05-12 15:39:24 +0200

    Fix bottom menu positioning bug and add background color

--- src/components/MyHomeMenu.tsx
+++ src/components/MyHomeMenu.tsx
@@ -1,38 +1,40 @@
 import { FunctionComponent } from 'react';
 import { FormattedMessage as Msg } from 'react-intl';
 import NextLink from 'next/link';
-import { Button, Flex, Footer } from '@adobe/react-spectrum';
+import { Button, Flex, View } from '@adobe/react-spectrum';
 
 const MyHomeMenu : FunctionComponent = () => {
     return (
-        <Footer
+        <View
+            backgroundColor="gray-100"
             bottom={ 0 }
             left={ 0 }
-            position="absolute"
+            position="fixed"
             right={ 0 }>
-            <Flex justifyContent="center">
+            <Flex
+                justifyContent="center">
                 <NextLink href="/my">
-                    <Button data-testid="feed-button" margin="size-200" variant="cta">
+                    <Button data-testid="feed-button" isQuiet margin="size-200" variant="primary">
                         <Msg id="layout.my.tabs.feed"/>
                     </Button>
                 </NextLink>
                 <NextLink href="/my/todo">
-                    <Button data-testid="todo-button" margin="size-200" variant="cta">
+                    <Button data-testid="todo-button" isQuiet margin="size-200" variant="primary">
                         <Msg id="layout.my.tabs.todo"/>
                     </Button>
                 </NextLink>
                 <NextLink href="/my/orgs">
-                    <Button data-testid="orgs-button" margin="size-200" variant="cta">
+                    <Button data-testid="orgs-button" isQuiet margin="size-200" variant="primary">
                         <Msg id="layout.my.tabs.orgs"/>
                     </Button>
                 </NextLink>
                 <NextLink href="/my/settings">
-                    <Button data-testid="settings-button" margin="size-200" variant="cta">
+                    <Button data-testid="settings-button" isQuiet margin="size-200" variant="primary">
                         <Msg id="layout.my.tabs.settings"/>
                     </Button>
                 </NextLink>
             </Flex>
-        </Footer>
+        </View>
     );
 };
 
