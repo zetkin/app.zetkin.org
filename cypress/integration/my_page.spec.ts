f4506be856617bf614354f3a0fe333b7a3f32d90
kristofferlarberg <mail@kristofferlarberg.se>
2021-05-13 12:52:56 +0200

    Implement integration test for MyPage and tabs for events section

---
+++ cypress/integration/my_page.spec.ts
@@ -0,0 +1,82 @@
+import { ZetkinEvent, ZetkinMembership } from '../../src/types/zetkin';
+
+describe('/my', () => {
+    let dummyEvents : {data: ZetkinEvent[]};
+    let dummyFollowing : {data: ZetkinMembership[]};
+
+    before(() => {
+        cy.fixture('dummyEvents.json')
+            .then((data : {data: ZetkinEvent[]}) => {
+                dummyEvents = data;
+            });
+        cy.fixture('dummyFollowing.json')
+            .then((data : {data: ZetkinMembership[]}) => {
+                dummyFollowing = data;
+            });
+    });
+
+    beforeEach(() => {
+        cy.request('delete', 'http://localhost:8001/_mocks');
+    });
+
+    after(() => {
+        cy.request('delete', 'http://localhost:8001/_mocks');
+    });
+
+    it('contains events', () => {
+        cy.request('put', 'http://localhost:8001/v1/orgs/1/actions/_mocks/get', {
+            response: {
+                data: dummyEvents,
+            },
+        });
+
+        cy.request('put', 'http://localhost:8001/v1/users/me/following/_mocks/get', {
+            response: {
+                data: dummyFollowing,
+            },
+        });
+
+        cy.login();
+
+        cy.visit('/my');
+        cy.waitUntilReactRendered();
+        cy.get('a[href*="/o/1"]').should('have.length', 1);
+    });
+
+    it('contains tabs', () => {
+        cy.request('put', 'http://localhost:8001/v1/orgs/1/actions/_mocks/get', {
+            response: {
+                data: dummyEvents,
+            },
+        });
+
+        cy.request('put', 'http://localhost:8001/v1/users/me/following/_mocks/get', {
+            response: {
+                data: dummyFollowing,
+            },
+        });
+
+        cy.login();
+
+        cy.visit('/my');
+        cy.waitUntilReactRendered();
+        cy.contains('Today');
+    });
+
+    it('contains a placeholder if there is no content', () => {
+        cy.request('put', 'http://localhost:8001/v1/users/me/following/_mocks/get', {
+            response: {
+                data: [],
+            },
+        });
+
+        cy.login();
+
+        cy.visit('/my');
+        cy.contains('Sorry, there is nothing planned at the moment.');
+    });
+
+});
+
+// Hack to flag for typescript as module
+export {};
--- src/pages/my/index.tsx
+++ src/pages/my/index.tsx
@@ -1,7 +1,8 @@
 import { GetServerSideProps } from 'next';
-import { Heading } from '@adobe/react-spectrum';
 import { FormattedMessage as Msg } from 'react-intl';
 import { useQuery } from 'react-query';
+import { Content, Heading, Text } from '@adobe/react-spectrum';
+import { Item, Tabs } from '@react-spectrum/tabs';
 
 import EventList from '../../components/EventList';
 import getBookedEvents from '../../fetching/getBookedEvents';
@@ -53,19 +54,79 @@ const MyPage : PageWithLayout<MyPageProps> = (props) => {
     const bookedEventsQuery = useQuery('bookedEvents', getBookedEvents());
 
     const { eventResponses, onSignup, onUndoSignup } = useEventResponses();
+    const userEvents = userEventsQuery.data;
+
+    if (!userEvents || userEvents.length === 0) {
+        return (
+            <>
+                <Heading level={ 1 }>
+                    <Msg id="pages.my.welcome" values={{ userName: user.first_name }}/>
+                </Heading>
+                <Text>
+                    <Msg id="pages.my.placeholder"/>
+                </Text>
+            </>
+        );
+    }
+
+    const today = <Msg id="pages.my.tabs.today"/>;
+    const tomorrow = <Msg id="pages.my.tabs.tomorrow"/>;
+    const week = <Msg id="pages.my.tabs.thisWeek"/>;
+    const later = <Msg id="pages.my.tabs.later"/>;
 
     return (
         <>
             <Heading level={ 1 }>
                 <Msg id="pages.my.welcome" values={{ userName: user.first_name }}/>
             </Heading>
-            <EventList
-                bookedEvents={ bookedEventsQuery.data }
-                eventResponses={ eventResponses }
-                events={ userEventsQuery.data }
-                onSignup={ onSignup }
-                onUndoSignup={ onUndoSignup }
-            />
+            <Tabs
+                aria-label="Options for events time filtering"
+                defaultSelectedKey="today">
+                <Item key="today" title={ today }>
+                    <Content>
+                        <EventList
+                            bookedEvents={ bookedEventsQuery.data }
+                            eventResponses={ eventResponses }
+                            events={ userEvents }
+                            onSignup={ onSignup }
+                            onUndoSignup={ onUndoSignup }
+                        />
+                    </Content>
+                </Item>
+                <Item key="tomorrow" title={ tomorrow }>
+                    <Content>
+                        <EventList
+                            bookedEvents={ bookedEventsQuery.data }
+                            eventResponses={ eventResponses }
+                            events={ userEvents }
+                            onSignup={ onSignup }
+                            onUndoSignup={ onUndoSignup }
+                        />
+                    </Content>
+                </Item>
+                <Item key="week" title={ week }>
+                    <Content>
+                        <EventList
+                            bookedEvents={ bookedEventsQuery.data }
+                            eventResponses={ eventResponses }
+                            events={ userEvents }
+                            onSignup={ onSignup }
+                            onUndoSignup={ onUndoSignup }
+                        />
+                    </Content>
+                </Item>
+                <Item key="later" title={ later }>
+                    <Content>
+                        <EventList
+                            bookedEvents={ bookedEventsQuery.data }
+                            eventResponses={ eventResponses }
+                            events={ userEvents }
+                            onSignup={ onSignup }
+                            onUndoSignup={ onUndoSignup }
+                        />
+                    </Content>
+                </Item>
+            </Tabs>
         </>
     );
 };
