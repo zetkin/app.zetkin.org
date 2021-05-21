f9c829796512dc364493225c7ae182e2c502eb2a
nehahirve <neha.hirve@hyperisland.se>
2021-05-21 17:11:33 +0200

    add side bar to calendar

--- src/components/MonthCalendar.tsx
+++ src/components/MonthCalendar.tsx
@@ -29,6 +29,7 @@ const MonthCalendar = ({ month, events, year }: MonthCalendarProps): JSX.Element
         calendarRows = 6;
     }
 
+    const gridItems = calendarRows * 7;
 
     const getEventsInRange = (start: Date, end:Date) => events.filter(event => {
         return new Date(event.start_time) >= start &&
@@ -42,54 +43,111 @@ const MonthCalendar = ({ month, events, year }: MonthCalendarProps): JSX.Element
             date < end;
     };
 
-    return (
-        <div data-testid="calendar-wrapper" style={{
-            display: 'grid',
-            gap: '0.5rem',
-            gridTemplateColumns: 'repeat(7, minmax(125px, 1fr))',
-            gridTemplateRows: `repeat(${calendarRows}, minmax(125px, 1fr))`,
-            height: '100%',
-            overflow: 'scroll',
-        }}>
-            { Array.from(Array(calendarRows * 7).keys()).map((_, index) => {
-                const currentDate = new Date(new Date(firstCalendarDay).setDate(firstCalendarDay.getDate() + index));
+    const getBarPos = (currentMonth: number) => {
 
-                return (
-                    <div key={ index } data-testid={ `griditem-${index}` } style={{
-                        background: isInRange(currentDate, firstMonthDay, lastMonthDay) ? 'grey' : 'whitesmoke',
-                        position: 'relative',
+        const barUnit = 100 / gridItems;
+
+        const getGridNumber = (event: Date) => {
+            const offset = (firstMonthDay.getDay() || 7) - 2;
+            if (event.getMonth() === month) {
+                return new Date(event).getDate() + offset;
+            }
+            if (event.getMonth() < currentMonth) {
+                return (new Date(event).getDay() || 7) - 1;
+            }
+            if (event.getMonth() > currentMonth) {
+                return (new Date(event).getDay() || 7) - 3 + offset + totalDaysInMonth;
+            }
+            return 0;
+        };
+
+        const lastCalendarDay = new Date(new Date(firstCalendarDay).setDate(firstCalendarDay.getDate() + gridItems));
+
+        const calendarEvents = getEventsInRange(firstCalendarDay, lastCalendarDay);
+
+        if (calendarEvents.length === 0) return { height: 0, top: 0 };
+
+        const firstEventDate = new Date(calendarEvents[0].start_time);
+        const lastEventDate = new Date(calendarEvents[calendarEvents.length - 1].end_time);
+
+        const top = getGridNumber(new Date(firstEventDate)) * barUnit;
+        const height = (getGridNumber(new Date(lastEventDate)) * barUnit) - top;
+
+        return { height, top };
+    };
+
+    return (
+        <div style={{
+            display: 'flex',
+            height: '100%',
+        }}>
+            <div
+                style={{
+                    marginRight: '0.5rem',
+                    position: 'relative',
+                    width: '1rem',
+                }}>
+                <div
+                    data-testid="calendar-bar"
+                    style={{
+                        backgroundColor: 'lightgray',
+                        bottom: '2%',
+                        height: `${getBarPos(month).height}%`,
+                        position: 'absolute',
+                        top: `${getBarPos(month).top + 2}%`,
+                        width: '100%',
                     }}>
-                        <div style={{
-                            top: 0,
+                </div>
+            </div>
+            <div data-testid="calendar-wrapper" style={{
+                display: 'grid',
+                flexGrow: 1,
+                gap: '0.5rem',
+                gridTemplateColumns: 'repeat(7, minmax(125px, 1fr))',
+                gridTemplateRows: `repeat(${calendarRows}, minmax(125px, 1fr))`,
+                height: '100%',
+                overflow: 'scroll',
+            }}>
+                { Array.from(Array(gridItems).keys()).map((_, index) => {
+                    const currentDate = new Date(new Date(firstCalendarDay).setDate(firstCalendarDay.getDate() + index));
+
+                    return (
+                        <div key={ index } data-testid={ `griditem-${index}` } style={{
+                            background: isInRange(currentDate, firstMonthDay, lastMonthDay) ? 'grey' : 'whitesmoke',
+                            position: 'relative',
                         }}>
-                            <FormattedDate
-                                day="2-digit"
-                                value={ currentDate }
-                            />
+                            <div style={{
+                                top: 0,
+                            }}>
+                                <FormattedDate
+                                    day="2-digit"
+                                    value={ currentDate }
+                                />
+                            </div>
+                            <ul data-testid={ `day-${index}-events` } style={{
+                                listStyle: 'none',
+                                margin: 0,
+                                padding:0 ,
+                                width: '100%',
+                            }}>
+                                { getEventsInRange(currentDate, new Date (new Date(currentDate).setDate(currentDate.getDate() + 1))).map(event => (
+                                    <li
+                                        key={ event.id } data-testid={ `event-${event.id}` } style={{
+                                            alignItems: 'center',
+                                            background: 'lightgray',
+                                            display: 'flex',
+                                            justifyContent: 'center',
+                                            margin: '0.5rem 0',
+                                            padding: '0 1rem',
+                                            width: '100%',
+                                        }}>{ `event with id ${event.id}` }
+                                    </li>
+                                )) }
+                            </ul>
                         </div>
-                        <ul data-testid={ `day-${index}-events` } style={{
-                            listStyle: 'none',
-                            margin: 0,
-                            padding:0 ,
-                            width: '100%',
-                        }}>
-                            { getEventsInRange(currentDate, new Date (new Date(currentDate).setDate(currentDate.getDate() + 1))).map(event => (
-                                <li
-                                    key={ event.id } data-testid={ `event-${event.id}` } style={{
-                                        alignItems: 'center',
-                                        background: 'lightgray',
-                                        display: 'flex',
-                                        justifyContent: 'center',
-                                        margin: '0.5rem 0',
-                                        padding: '0 1rem',
-                                        width: '100%',
-                                    }}>{ `event with id ${event.id}` }
-                                </li>
-                            )) }
-                        </ul>
-                    </div>
-                );
-            }) }
+                    );
+                }) }
+            </div>
         </div>
     );
 };
