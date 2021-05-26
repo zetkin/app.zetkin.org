c28a82f986b012fa9565b48663fee0a4f8134ab6
kristofferlarberg <mail@kristofferlarberg.se>
2021-05-26 12:52:36 +0200

    Refactor getEvent to include organization, userBooked and userResponse

--- src/fetching/getEvent.ts
+++ src/fetching/getEvent.ts
@@ -1,16 +1,46 @@
 import { defaultFetch } from '.';
-import { ZetkinEvent } from '../types/zetkin';
+import { ZetkinEvent, ZetkinEventResponse } from '../types/zetkin';
 
 export default function getEvent(orgId : string, eventId : string, fetch = defaultFetch) {
     return async () : Promise<ZetkinEvent> => {
         const cRes = await fetch(`/orgs/${orgId}/campaigns`);
         const cData = await cRes.json();
 
+        const oRes = await fetch(`/orgs/${orgId}`);
+        const oData = await oRes.json();
+
+        const bookedRes = await fetch('/users/me/actions');
+        const bookedData = await bookedRes.json();
+
+        const rRes = await fetch('/users/me/action_responses');
+        const rData = await rRes.json();
+
+        const org = {
+            id: oData.data.id,
+            title: oData.data.title,
+        };
+
         for (const obj of cData.data) {
             const eventsRes = await fetch(`/orgs/${orgId}/campaigns/${obj.id}/actions`);
             const campaignEvents = await eventsRes.json();
-            const eventData = campaignEvents.data.find((event : ZetkinEvent) => event.id.toString() === eventId);
-            if (eventData) {
+
+            const campaignEvent = campaignEvents.data?.find((event : ZetkinEvent) =>
+                event.id.toString() === eventId);
+
+            if (campaignEvent) {
+                const isBookedEvent = bookedData.data.some((booked : ZetkinEvent) =>
+                    booked.id === campaignEvent.id);
+
+                const hasEventResponse = rData.data.some((response : ZetkinEventResponse) =>
+                    response.action_id === campaignEvent.id);
+
+                const eventData = {
+                    ...campaignEvent,
+                    organization: org,
+                    userBooked: isBookedEvent,
+                    userResponse: hasEventResponse,
+                };
+
                 return eventData;
             }
         }
