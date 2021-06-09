b3b7082dd70cddd48cc9ce369b815020b3b8ac4f
nehahirve <neha.hirve@hyperisland.se>
2021-06-09 11:52:55 +0200

    transition organize dashboard to MUI

---
+++ src/theme.ts
@@ -0,0 +1,22 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import { red } from '@material-ui/core/colors';
+
+// Create a theme instance.
+const theme = createMuiTheme({
+    palette: {
+        background: {
+            default: '#fff',
+        },
+        error: {
+            main: red.A400,
+        },
+        primary: {
+            main: '#553cd6',
+        },
+        secondary: {
+            main: '#19857b',
+        },
+    },
+});
+
+export default theme;
