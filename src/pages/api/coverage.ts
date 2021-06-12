0b9a349521c5913ea1f6f3d9c198a97a6b72b2af
104170kjbe <kjersti.berg@frende.no>
2021-06-12 12:27:42 +0200

    Return 404 for api/coverage in production

--- src/pages/api/coverage.ts
+++ src/pages/api/coverage.ts
@@ -1,11 +1,14 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextApiRequest, NextApiResponse } from "next";
 
 /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-const globalAny : any = global;
+const globalAny: any = global;
 
-export default (req : NextApiRequest, res : NextApiResponse) : void => {
-    // TODO: Disable this in production
-    res.status(200).json({
-        coverage: globalAny.__coverage__ || null,
-    });
+export default (req: NextApiRequest, res: NextApiResponse): void => {
+    if (process.env.NODE_ENV === "production") {
+        res.status(404);
+    } else {
+        res.status(200).json({
+            coverage: globalAny.__coverage__ || null,
+        });
+    }
 };
