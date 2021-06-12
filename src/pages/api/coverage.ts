8c93200b50dea9e2ea430b536fb743fc74365f0b
104170kjbe <kjersti.berg@frende.no>
2021-06-12 12:35:53 +0200

    Use single quotes, as per standard

--- src/pages/api/coverage.ts
+++ src/pages/api/coverage.ts
@@ -1,10 +1,10 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from 'next';
 
 /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
 const globalAny: any = global;
 
 export default (req: NextApiRequest, res: NextApiResponse): void => {
-    if (process.env.NODE_ENV === "production") {
+    if (process.env.NODE_ENV === 'production') {
         res.status(404);
     } else {
         res.status(200).json({
