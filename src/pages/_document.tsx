3be6acfb7026d70daff83eb9000d4065e74ef24f
nehahirve <neha.hirve@hyperisland.se>
2021-06-15 11:39:41 +0200

    remove comment, replace with link

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,6 +3,8 @@ import { ServerStyleSheets } from '@material-ui/core/styles';
 import theme from '../theme';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 
+// boilerplate page taken from https://github.com/mui-org/material-ui/tree/master/examples/nextjs
+
 export default class MyDocument extends Document {
     render():JSX.Element {
         return (
@@ -24,32 +26,7 @@ export default class MyDocument extends Document {
     }
 }
 
-// `getInitialProps` belongs to `_document` (instead of `_app`),
-// it's compatible with server-side generation (SSG).
 MyDocument.getInitialProps = async (ctx) => {
-    // Resolution order
-    //
-    // On the server:
-    // 1. app.getInitialProps
-    // 2. page.getInitialProps
-    // 3. document.getInitialProps
-    // 4. app.render
-    // 5. page.render
-    // 6. document.render
-    //
-    // On the server with error:
-    // 1. document.getInitialProps
-    // 2. app.render
-    // 3. page.render
-    // 4. document.render
-    //
-    // On the client
-    // 1. app.getInitialProps
-    // 2. page.getInitialProps
-    // 3. app.render
-    // 4. page.render
-
-    // Render app and page and get the context of the page with collected side effects.
     const sheets = new ServerStyleSheets();
     const originalRenderPage = ctx.renderPage;
 
@@ -62,7 +39,6 @@ MyDocument.getInitialProps = async (ctx) => {
 
     return {
         ...initialProps,
-        // Styles fragment is rendered after the app and page rendering finish.
         styles: [...Children.toArray(initialProps.styles), sheets.getStyleElement()],
     };
 };
