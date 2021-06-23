b245662b760a1eaad7ee15c781930f96d1bd2ee2
River H <eribloodlust@grrlz.net>
2021-06-23 17:52:27 +0100

    use function for useDebounce

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,21 +1,26 @@
-import { useCallback, useRef, useEffect } from 'react';
-import debounce from 'lodash.debounce';
+import { useCallback, useRef, useEffect } from "react";
+import debounce from "lodash.debounce";
 
-type DefaultCallbackArgs = Array<any>
-type CallbackFn<Args extends DefaultCallbackArgs> =(...args: Args) => Promise<void>
+type DefaultCallbackArgs = Array<any>;
+type CallbackFn<Args extends DefaultCallbackArgs> = (
+    ...args: Args
+) => Promise<void>;
 
 /**
  * Runs the callback function only if the delay time has passed.
  */
-export default <Args extends DefaultCallbackArgs>(callback: CallbackFn<Args>, delay: number) => {
-  // Memoizing the callback because if it's an arrow function
-  // it would be different on each render
-  const memoizedCallback = useCallback(callback, []);
-  const debouncedFn = useRef(debounce(memoizedCallback, delay));
+export default function useDebounce<Args extends DefaultCallbackArgs>(
+    callback: CallbackFn<Args>,
+    delay: number
+) {
+    // Memoizing the callback because if it's an arrow function
+    // it would be different on each render
+    const memoizedCallback = useCallback(callback, []);
+    const debouncedFn = useRef(debounce(memoizedCallback, delay));
 
-  useEffect(() => {
-    debouncedFn.current = debounce(memoizedCallback, delay);
-  }, [memoizedCallback, debouncedFn, delay]);
+    useEffect(() => {
+        debouncedFn.current = debounce(memoizedCallback, delay);
+    }, [memoizedCallback, debouncedFn, delay]);
 
-  return debouncedFn.current;
-};
+    return debouncedFn.current;
+}
