9dc641456c017ffe3e0e048a646c2adf08595359
River H <eribloodlust@grrlz.net>
2021-07-06 13:56:54 +0100

    add typing for overriding SpeedDial styling

--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,13 @@
+import { Overrides as CoreOverrides } from '@material-ui/core/styles/overrides';
 import { createMuiTheme } from '@material-ui/core/styles';
+import { CSSProperties } from '@material-ui/styles';
+import { SpeedDialClassKey } from '@material-ui/lab';
 import { grey, red } from '@material-ui/core/colors';
+
+interface Overrides extends CoreOverrides {
+    // Define additional lab components here as needed....
+    MuiSpeedDial?: Partial<Record<SpeedDialClassKey, CSSProperties | (() => CSSProperties)>> | undefined;
+}
 
 // Create a theme instance.
 const theme = createMuiTheme({
@@ -27,7 +35,7 @@ const theme = createMuiTheme({
                 textTransform: 'none',
             },
         },
-    },
+    } as Overrides,
     palette: {
         background: {
             default: '#fff',
