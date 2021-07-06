53ae4570d0174c9bbf864b789082f49641fd078d
River H <eribloodlust@grrlz.net>
2021-07-06 12:05:58 +0100

    remove orgId from DashboardPeople

--- src/components/DashboardPeople.tsx
+++ src/components/DashboardPeople.tsx
@@ -2,18 +2,12 @@ import { FormattedMessage as Msg } from 'react-intl';
 import { Box, Typography } from '@material-ui/core';
 
 
-interface DashboardPeopleProps {
-    orgId: string;
-}
-
-const DashboardPeople = ({ orgId } : DashboardPeopleProps) : JSX.Element => {
-
+const DashboardPeople = () : JSX.Element => {
     return (
         <Box border={ 1 } m={ 2 } p={ 2 }>
             <Typography component="h2" variant="body1">
                 <Msg id="pages.organize.people.title"/>
             </Typography>
-            { orgId }
         </Box>
     );
 };
