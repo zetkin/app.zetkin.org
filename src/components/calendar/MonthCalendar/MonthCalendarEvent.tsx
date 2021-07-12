12fb23dad4ce8d699d016d82f8069e284b0183ed
nehahirve <neha.hirve@hyperisland.se>
2021-07-12 15:25:59 +0200

    handle multiday events in month view

--- src/components/calendar/MonthCalendar/MonthCalendarEvent.tsx
+++ src/components/calendar/MonthCalendar/MonthCalendarEvent.tsx
@@ -2,6 +2,7 @@ import { FormattedTime } from 'react-intl';
 import { getContrastColor } from '../../../utils/colorUtils';
 import { grey } from '@material-ui/core/colors';
 import NextLink from 'next/link';
+import { ArrowBackIos, ArrowForwardIos } from '@material-ui/icons';
 import { Box,  Link, Typography } from '@material-ui/core';
 import { useEffect, useRef } from 'react';
 
@@ -31,8 +32,13 @@ const MonthCalendarEvent = ({ baseHref, index, startOfDay, campaign, event, onLo
     }, [onLoad]);
 
     const startTime = getNaiveDate(event.start_time);
+    const endTime = getNaiveDate(event.end_time);
+
+    const endOfDay = new Date(new Date(startOfDay)
+        .setDate(startOfDay.getDate() + 1));
 
     const startsBeforeToday = startTime <= startOfDay;
+    const endsAfterToday = endTime >= endOfDay;
 
     return (
         <li key={ event.id }>
@@ -40,17 +46,20 @@ const MonthCalendarEvent = ({ baseHref, index, startOfDay, campaign, event, onLo
                 <Link underline="none">
                     <Box
                         { ...( index === 0 && { ref: listItem } ) }
+                        alignItems="center"
                         bgcolor={ campaign?.color || DEFAULT_COLOR }
                         color={ getContrastColor(campaign?.color || DEFAULT_COLOR) }
                         data-testid={ `event-${event.id}` }
-                        display={ isVisible ? 'block' : 'none' }
+                        display={ isVisible ? 'flex' : 'none' }
                         mt={ 0.5 }
                         px={ 0.5 }
                         width={ 1 }>
+                        { startsBeforeToday && <ArrowBackIos fontSize="inherit"/> }
                         <Typography noWrap={ true } variant="body2">
                             <FormattedTime value={ new Date(startsBeforeToday? startOfDay: startTime) }/>{ ` - ` }
                             { event.title || event.activity.title }
                         </Typography>
+                        { endsAfterToday && <ArrowForwardIos fontSize="inherit"/> }
                     </Box>
                 </Link>
             </NextLink>
