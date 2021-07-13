dd629b2ba6f6c8b488327e4209dde1d38621acc6
nehahirve <neha.hirve@hyperisland.se>
2021-07-13 09:35:20 +0200

    add create campaign action to speedDial

---
+++ src/components/ZetkinSpeedDial/actions/createCampaign.tsx
@@ -0,0 +1,41 @@
+/* eslint-disable react/display-name */
+import { ACTIONS } from '../constants';
+import CampaignForm from '../../CampaignForm';
+import { Flag } from '@material-ui/icons';
+import postCampaign from '../../../fetching/postCampaign';
+import { useRouter } from 'next/router';
+import { ActionConfig, DialogContentBaseProps } from './types';
+import { useMutation, useQueryClient } from 'react-query';
+
+
+const DialogContent: React.FunctionComponent<DialogContentBaseProps> = ({ closeDialog }) => {
+    const queryClient = useQueryClient();
+    const router = useRouter();
+    const { orgId } = router.query as {orgId: string};
+    const campaignMutation = useMutation(postCampaign(orgId), {
+        onSettled: () => queryClient.invalidateQueries('campaigns'),
+    });
+
+    const handleCreateCampaignFormSubmit = (data: Record<string,unknown>) => {
+        campaignMutation.mutate(data);
+        closeDialog();
+    };
+
+    return (<CampaignForm
+        onCancel={ closeDialog }
+        onSubmit={ handleCreateCampaignFormSubmit }
+    />
+    );
+};
+
+const config = {
+    icon: <Flag />,
+    key: ACTIONS.CREATE_CAMPAIGN,
+    name: 'misc.speedDial.createCampaign',
+    urlKey: 'create-campaign',
+} as ActionConfig;
+
+export {
+    config,
+    DialogContent,
+};
