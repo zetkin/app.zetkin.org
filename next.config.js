22581488ae448c1fb206b97b99c061c774d0dbf3
nehahirve <neha.hirve@hyperisland.se>
2021-07-13 17:14:05 +0200

    redirect calendar paths

--- next.config.js
+++ next.config.js
@@ -9,5 +9,19 @@ const withTM = require('next-transpile-modules')([
 module.exports = withPlugins([withCSS, withTM], {
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     return config;
-  },
+    },
+    async redirects() {
+        return [
+            {
+                source: '/:prevPath*/calendar/events',
+                destination: '/:prevPath*/calendar', 
+                permanent: false,
+            },
+            {
+                source: '/:prevPath*/calendar/tasks',
+                destination: '/:prevPath*/calendar', 
+                permanent: false,
+            },
+        ]
+      },
 });
