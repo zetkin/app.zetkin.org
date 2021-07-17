97d766b1d0d1ab21610c718bd10bb42755764846
River H <eribloodlust@grrlz.net>
2021-07-17 19:50:28 +0100

    move events query in to event list component

--- src/components/organize/EventList/index.tsx
+++ src/components/organize/EventList/index.tsx
@@ -1,22 +1,28 @@
 import { List } from '@material-ui/core';
 import { useIntl } from 'react-intl';
+import { useQuery } from 'react-query';
+import { useRouter } from 'next/router';
 
 import EventListItem from './EventListItem';
-import { ZetkinEvent } from '../../../types/zetkin';
+import getCampaignEvents from '../../../fetching/getCampaignEvents';
 
 interface EventListProps {
-    events: ZetkinEvent[];
     hrefBase: string;
 }
 
-const EventList = ({ events, hrefBase }: EventListProps): JSX.Element => {
+const EventList = ({ hrefBase }: EventListProps): JSX.Element => {
     const intl = useIntl();
+    const { campId, orgId } = useRouter().query;
+    const { data: events } = useQuery(
+        ['campaignEvents', orgId, campId],
+        getCampaignEvents(orgId as string, campId as string),
+    );
 
     return (
         <List aria-label={ intl.formatMessage({
             id: 'pages.organizeCampaigns.events',
-        }) } disablePadding>
-            { events.map(event => (
+        }) }>
+            { events && events.map(event => (
                 <EventListItem key={ event.id } event={ event } hrefBase={ hrefBase } />
             )) }
         </List>
