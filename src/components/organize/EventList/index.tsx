325626aea25b5d4a0fa827fa9e675c985ec1c75f
River H <eribloodlust@grrlz.net>
2021-07-18 11:41:52 +0100

    move queries back to page

--- src/components/organize/EventList/index.tsx
+++ src/components/organize/EventList/index.tsx
@@ -1,29 +1,22 @@
 import { useIntl } from 'react-intl';
-import { useQuery } from 'react-query';
-import { useRouter } from 'next/router';
 import { Card, List } from '@material-ui/core';
 
 import EventListItem from './EventListItem';
-import getCampaignEvents from '../../../fetching/getCampaignEvents';
+import { ZetkinEvent } from '../../../types/zetkin';
 
 interface EventListProps {
     hrefBase: string;
+    events: ZetkinEvent[];
 }
 
-const EventList = ({ hrefBase }: EventListProps): JSX.Element => {
+const EventList = ({ hrefBase, events }: EventListProps): JSX.Element => {
     const intl = useIntl();
-    const { campId, orgId } = useRouter().query;
-    const { data: events } = useQuery(
-        ['campaignEvents', orgId, campId],
-        getCampaignEvents(orgId as string, campId as string),
-    );
-
     return (
         <Card>
             <List aria-label={ intl.formatMessage({
                 id: 'pages.organizeCampaigns.events',
             }) }>
-                { events && events.map(event => (
+                { events.map(event => (
                     <EventListItem key={ event.id } event={ event } hrefBase={ hrefBase } />
                 )) }
             </List>
