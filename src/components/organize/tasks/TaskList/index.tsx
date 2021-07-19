0019e5167d30fdc88f678d721b8f26058c159618
River H <eribloodlust@grrlz.net>
2021-07-19 10:57:41 +0100

    sort tasks by status

--- src/components/organize/tasks/TaskList/index.tsx
+++ src/components/organize/tasks/TaskList/index.tsx
@@ -3,20 +3,37 @@ import { Card, Divider, List } from '@material-ui/core';
 
 import TaskListItem from './TaskListItem';
 import { ZetkinTask } from '../../../../types/zetkin';
+import getTaskStatus, { TASK_STATUS } from '../../../../utils/getTaskStatus';
+
+const TASK_STATUS_ORDER: {[key in TASK_STATUS]: number} = {
+    [TASK_STATUS.DRAFT]: 0,
+    [TASK_STATUS.ACTIVE]: 1,
+    [TASK_STATUS.SCHEDULED]: 2,
+    [TASK_STATUS.CLOSED]: 3,
+};
+
+const sortTasksByStatus = (firstTask: ZetkinTask, secondTask: ZetkinTask): number => {
+    const firstTaskStatus = getTaskStatus(firstTask);
+    const secondTaskStatus = getTaskStatus(secondTask);
+    return TASK_STATUS_ORDER[firstTaskStatus] - TASK_STATUS_ORDER[secondTaskStatus];
+};
 
 interface TaskListProps {
     hrefBase: string;
     tasks: ZetkinTask[];
 }
 
+
 const TaskList = ({ hrefBase, tasks }: TaskListProps): JSX.Element => {
     const intl = useIntl();
+
+    const tasksOrderedByStatus = tasks.sort(sortTasksByStatus);
 
     return (
         <Card>
             <List
                 aria-label={ intl.formatMessage({ id: 'pages.organizeCampaigns.tasks' }) }>
-                { tasks.map((task, index) => (
+                { tasksOrderedByStatus.map((task, index) => (
                     <>
                         <TaskListItem key={ task.id } hrefBase={ hrefBase } task={ task } />
                         {
