038127efea2051dca9c9426c23cac03918355578
River H <eribloodlust@grrlz.net>
2021-07-20 14:39:25 +0100

    remove unneeded hooks and fetch handlers

--- src/fetching/deleteUserFollowing.ts
+++
@@ -1,7 +0,0 @@
-import apiUrl from '../utils/apiUrl';
-
-export default async function deleteUserFollowing(orgId : number) : Promise<void> {
-    await fetch(apiUrl(`/users/me/following/${orgId}`), {
-        method: 'DELETE',
-    });
-}
--- src/fetching/getSurvey.ts
+++
@@ -1,9 +0,0 @@
-import apiUrl from '../utils/apiUrl';
-
-export default function getSurvey(orgId : string, surId : string) {
-    return async () : Promise<{ title: string }> => {
-        const sIdRes = await fetch(apiUrl(`/orgs/${orgId}/surveys/${surId}`));
-        const sIdData = await sIdRes.json();
-        return sIdData.data;
-    };
-}
