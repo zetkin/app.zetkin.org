dfe3601beb2058a38237b7066760ff1068d18582
River H <eribloodlust@grrlz.net>
2021-07-28 11:27:49 +0100

    localisation and label for publish button

--- src/components/organize/tasks/TaskActionButtons/PublishButton.tsx
+++ src/components/organize/tasks/TaskActionButtons/PublishButton.tsx
@@ -1,10 +1,11 @@
 import { Button } from '@material-ui/core';
 import dayjs from 'dayjs';
-// import { FormattedMessage as Msg } from 'react-intl';
+import { FormattedMessage as Msg } from 'react-intl';
 import { useMutation, useQueryClient } from 'react-query';
 
 import patchTask from 'fetching/tasks/patchTask';
 import { ZetkinTask } from 'types/zetkin';
+import getTaskStatus, { TASK_STATUS } from 'utils/getTaskStatus';
 
 interface PublishButtonProps {
     task: ZetkinTask;
@@ -12,19 +13,11 @@ interface PublishButtonProps {
 
 const PublishButton: React.FunctionComponent<PublishButtonProps> = ({ task }) => {
     const queryClient = useQueryClient();
-
-    const { published, deadline, expires } = task;
-    const now = dayjs();
-    const publishedDate = dayjs(published);
-    const deadlineDate = dayjs(deadline);
-    const expiryDate = dayjs(expires);
+    const taskStatus = getTaskStatus(task);
 
     const patchTaskMutation = useMutation(patchTask(task.organization.id, task.id), {
         onSettled: () => queryClient.invalidateQueries('task'),
     });
-
-    // Publish Button is active if published doesn't exist or has not passed, and the deadline or expiry haven't been passed
-    const canBePublished = now.isBefore(deadlineDate) && now.isBefore(expiryDate) && (!published || now.isBefore(publishedDate));
 
     const publishTask = () => {
         patchTaskMutation.mutate({
@@ -33,7 +26,18 @@ const PublishButton: React.FunctionComponent<PublishButtonProps> = ({ task }) =>
     };
 
     return (
-        <Button color="primary" disabled={ !canBePublished } onClick={ publishTask } variant="contained">Publish</Button>
+        <Button
+            color="primary"
+            disabled={ taskStatus !== TASK_STATUS.DRAFT && taskStatus !== TASK_STATUS.SCHEDULED }
+            onClick={ publishTask } variant="contained">
+            { taskStatus === TASK_STATUS.DRAFT || taskStatus === TASK_STATUS.SCHEDULED ? (
+            // If draft or scheduled
+                <Msg id="misc.tasks.publishButton.publish" />
+            ) : (
+            // If other status
+                <Msg id={ `misc.tasks.publishButton.${taskStatus}` } />
+            ) }
+        </Button>
     );
 };
 
