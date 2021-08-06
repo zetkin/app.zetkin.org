3abdb60ff03f0038f5abe2a34b4e080fc2c9d8d0
nehahirve <neha.hirve@hyperisland.se>
2021-08-06 16:45:01 +0200

    add button to edit or view smart search query

--- src/pages/organize/[orgId]/campaigns/[campId]/calendar/tasks/[taskId]/assignees.tsx
+++ src/pages/organize/[orgId]/campaigns/[campId]/calendar/tasks/[taskId]/assignees.tsx
@@ -1,20 +1,25 @@
-import { Button } from '@material-ui/core';
 import { GetServerSideProps } from 'next';
+import { FormattedMessage as Msg } from 'react-intl';
 import { useQuery } from 'react-query';
 import { useRouter } from 'next/router';
+import { Alert, AlertTitle } from '@material-ui/lab';
+import { Box, Link , Typography } from '@material-ui/core';
 
 import getOrg from 'fetching/getOrg';
 import getTask from 'fetching/tasks/getTask';
+import getTaskAssignments from 'fetching/tasks/getTaskAssignments';
 import { PageWithLayout } from 'types';
 import { scaffold } from 'utils/next';
+import SingleTaskLayout from 'components/layout/organize/SingleTaskLayout';
 import SmartSearchDialog from 'components/smartSearch/SmartSearchDialog';
 import { useState } from 'react';
 import { ZetkinTask } from 'types/zetkin';
+import getTaskStatus, { TASK_STATUS } from 'utils/getTaskStatus';
 
 const scaffoldOptions = {
     authLevelRequired: 2,
     localeScope: [
-        'layout.organize', 'misc.breadcrumbs', 'misc.smartSearch',
+        'layout.organize', 'misc.breadcrumbs', 'misc.smartSearch', 'pages.assignees', 'misc.tasks',
     ],
 };
 
@@ -45,33 +50,84 @@ export const getServerSideProps : GetServerSideProps = scaffold(async (ctx) => {
     };
 }, scaffoldOptions);
 
+enum QUERY_STATUS {
+    EDITABLE='editable',
+    PUBLISHED='published',
+    PUBLISHING='publishing',
+}
+
 const TaskAssigneesPage: PageWithLayout = () => {
     const { taskId, orgId } = useRouter().query;
     const taskQuery = useQuery(['task', orgId, taskId], getTask(orgId as string, taskId as string));
+    const assignmentsQuery = useQuery(['assignments', orgId, taskId], getTaskAssignments(
+        orgId as string, taskId as string,
+    ));
     const task = taskQuery?.data;
-
+    const assignments = assignmentsQuery?.data;
     const query = task?.target;
 
     const [dialogOpen, setDialogOpen] = useState(false);
 
     const handleDialogClose = () => setDialogOpen(false);
 
+    const taskStatus = task ? getTaskStatus(task) : undefined;
+
+    const getQueryStatus = () => {
+        let queryStatus = QUERY_STATUS.PUBLISHED;
+        if (taskStatus === TASK_STATUS.DRAFT || taskStatus === TASK_STATUS.SCHEDULED) {
+            queryStatus = QUERY_STATUS.EDITABLE;
+        }
+        // we don't want 'publishing' state to appear on page load while the data is being fetched
+        else if (assignments && !assignments.length) {
+            queryStatus = QUERY_STATUS.PUBLISHING;
+        }
+        return queryStatus;
+    };
+
+    const queryStatus = getQueryStatus();
+
+    const readOnly = queryStatus === QUERY_STATUS.PUBLISHING ||
+        queryStatus === QUERY_STATUS.PUBLISHED;
+
     return (
         <>
-            <Button color="primary" onClick={ () => setDialogOpen(true) } variant="contained">
-                Edit
-            </Button>
+            <Box p={ 2 }>
+                <Alert severity={
+                    queryStatus === QUERY_STATUS.EDITABLE ?
+                        'warning' : queryStatus === QUERY_STATUS.PUBLISHING ?
+                            'info' : 'success' }>
+                    <AlertTitle>
+                        <Msg id={ `pages.assignees.taskStates.${queryStatus}` }/>
+                    </AlertTitle>
+                    <Link
+                        color="inherit"
+                        component="button"
+                        onClick={ () => setDialogOpen(true) }
+                        underline="always">
+                        <Typography align="left">
+                            { readOnly ?
+                                <Msg id="pages.assignees.links.readOnly"/> :
+                                <Msg id="pages.assignees.links.edit"/>
+                            }
+                        </Typography>
+                    </Link>
+                </Alert>
+            </Box>
             { dialogOpen &&
-            <SmartSearchDialog onDialogClose={ handleDialogClose } query={ query }/> }
+            <SmartSearchDialog
+                onDialogClose={ handleDialogClose }
+                query={ query }
+                readOnly={ readOnly }
+            /> }
         </>
     );
 };
 
 TaskAssigneesPage.getLayout = function getLayout(page) {
     return (
-        <>
+        <SingleTaskLayout>
             { page }
-        </>
+        </SingleTaskLayout>
     );
 };
 
