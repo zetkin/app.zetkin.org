50c9ff822e9a41b42795606b2701b6be149b91f7
River H <eribloodlust@grrlz.net>
2021-08-09 10:30:38 +0100

    fix line break

--- src/components/smartSearch/filters/SubQuery/index.tsx
+++ src/components/smartSearch/filters/SubQuery/index.tsx
@@ -12,7 +12,8 @@ import StyledSelect from '../../inputs/StyledSelect';
 import useSmartSearchFilter from 'hooks/useSmartSearchFilter';
 import {
     NewSmartSearchFilter,
-    OPERATION, QUERY_TYPE,
+    OPERATION,
+    QUERY_TYPE,
     SmartSearchFilterWithId,
     SubQueryFilterConfig,
     ZetkinQuery,
