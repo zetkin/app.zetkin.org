5265b8e85e42eddfd819a7a42f805ef8accff68a
River H <eribloodlust@grrlz.net>
2021-08-10 12:39:40 +0100

    publish button only active if task has fields and target filters

--- src/components/organize/tasks/TaskActionButtons/PublishButton.tsx
+++ src/components/organize/tasks/TaskActionButtons/PublishButton.tsx
@@ -1,7 +1,7 @@
-import { Button } from '@material-ui/core';
 import dayjs from 'dayjs';
-import { FormattedMessage as Msg } from 'react-intl';
 import { useState } from 'react';
+import { Button, Tooltip } from '@material-ui/core';
+import { FormattedMessage as Msg, useIntl } from 'react-intl';
 import { useMutation, useQueryClient } from 'react-query';
 
 import patchTask from 'fetching/tasks/patchTask';
@@ -10,15 +10,36 @@ import { ZetkinTask } from 'types/zetkin';
 import getTaskStatus, { TASK_STATUS } from 'utils/getTaskStatus';
 
 import PublishTaskForm from '../forms/PublishTaskForm';
+import validateTaskConfig from 'utils/validateTaskConfig';
+
+const getTooltipContents = (taskStatus: TASK_STATUS, isTaskConfigValid: boolean, hasAssignees: boolean): string | null => {
+    if (taskStatus === TASK_STATUS.ACTIVE || taskStatus === TASK_STATUS.CLOSED) {
+        return  'misc.tasks.publishButton.tooltip.alreadyPublished';
+    }
+
+    if (!isTaskConfigValid && !hasAssignees) {
+        return 'misc.tasks.publishButton.tooltip.missingFieldsAndAssignees';
+    }
+
+    if (!isTaskConfigValid) {
+        return 'misc.tasks.publishButton.tooltip.missingFields';
+    }
+
+    if (!hasAssignees) {
+        return 'misc.tasks.publishButton.tooltip.missingAssignees';
+    }
+
+    return null;
+};
 
 interface PublishButtonProps {
     task: ZetkinTask;
 }
 
 const PublishButton: React.FunctionComponent<PublishButtonProps> = ({ task }) => {
-    const [dialogOpen, setDialogOpen] = useState(false);
+    const intl = useIntl();
     const queryClient = useQueryClient();
-    const taskStatus = getTaskStatus(task);
+    const [dialogOpen, setDialogOpen] = useState(false);
 
     const patchTaskMutation = useMutation(patchTask(task.organization.id, task.id), {
         onSettled: () => queryClient.invalidateQueries('task'),
@@ -31,21 +52,33 @@ const PublishButton: React.FunctionComponent<PublishButtonProps> = ({ task }) =>
         setDialogOpen(false);
     };
 
+    const taskStatus = getTaskStatus(task);
+    const isTaskConfigValid = validateTaskConfig(task);
+    const hasAssignees = task.target.filter_spec.length > 0;
+
+    const isEnabled = isTaskConfigValid && hasAssignees &&
+        (
+            taskStatus === TASK_STATUS.DRAFT ||
+            taskStatus === TASK_STATUS.SCHEDULED
+        );
+
+    const tooltipContents = getTooltipContents(taskStatus, isTaskConfigValid, hasAssignees);
+
     return (
         <>
-            <Button
-                color="primary"
-                disabled={ taskStatus !== TASK_STATUS.DRAFT && taskStatus !== TASK_STATUS.SCHEDULED }
-                onClick={ () => setDialogOpen(true) }
-                variant="contained">
-                { taskStatus === TASK_STATUS.DRAFT || taskStatus === TASK_STATUS.SCHEDULED ? (
-                // If draft or scheduled
-                    <Msg id="misc.tasks.publishButton.publish" />
-                ) : (
-                // If other status
-                    <Msg id={ `misc.tasks.publishButton.${taskStatus}` } />
-                ) }
-            </Button>
+            <Tooltip
+                arrow
+                title={ tooltipContents ? intl.formatMessage({ id: tooltipContents }) : '' }>
+                <span>
+                    <Button
+                        color="primary"
+                        disabled={ !isEnabled }
+                        onClick={ () => setDialogOpen(true) }
+                        variant="contained">
+                        <Msg id="misc.tasks.publishButton.publish" />
+                    </Button>
+                </span>
+            </Tooltip>
             <ZetkinDialog
                 onClose={ () => {
                     setDialogOpen(false);
