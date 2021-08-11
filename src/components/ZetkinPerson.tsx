cea1bbdde8af0458f5a5a80c98b7457f0e766f38
River H <eribloodlust@grrlz.net>
2021-08-11 09:49:49 +0100

    add avatar

--- src/components/ZetkinPerson.tsx
+++ src/components/ZetkinPerson.tsx
@@ -1,12 +1,16 @@
+import { useRouter } from 'next/router';
 import { Avatar, Box, Typography } from '@material-ui/core';
 
 const ZetkinPerson: React.FunctionComponent<{
   person: {first_name: string; id: number; last_name: string };
   subtitle?: string;
 }> = ({ person, subtitle  }) => {
+    const router = useRouter();
+    const { orgId } = router.query;
+
     return (
         <Box display="flex">
-            <Avatar />
+            <Avatar src={ orgId ? `/api/orgs/${orgId}/people/${person.id}/avatar` : '' } />
             <Box alignItems="start" display="flex" flexDirection="column" justifyContent="center" ml={ 1 }>
                 <Typography variant="body1">{ person.first_name } { person.last_name }</Typography>
                 { subtitle && (
