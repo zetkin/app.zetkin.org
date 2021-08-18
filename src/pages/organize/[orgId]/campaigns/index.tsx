3d4b90151e52b3fc6f9403d18984e45d588569e4
River H <eribloodlust@grrlz.net>
2021-08-18 10:14:32 +0100

    remove filterable projects list from the single campaign page

--- src/pages/organize/[orgId]/campaigns/index.tsx
+++ src/pages/organize/[orgId]/campaigns/index.tsx
@@ -1,10 +1,8 @@
 import { GetServerSideProps } from 'next';
-import { grey } from '@material-ui/core/colors';
-import NextLink from 'next/link';
+
+import { Box } from '@material-ui/core';
+import { useIntl } from 'react-intl';
 import { useQuery } from 'react-query';
-import { useState } from 'react';
-import { Box, Checkbox, FormControl, FormControlLabel, FormGroup, FormLabel, Link, List, ListItem, Typography } from '@material-ui/core';
-import { FormattedMessage as Msg, useIntl } from 'react-intl';
 
 import AllCampaignsLayout from '../../../../components/layout/organize/AllCampaignsLayout';
 import CampaignCard from '../../../../components/CamapignCard';
@@ -83,41 +81,11 @@ const AllCampaignsSummaryPage: PageWithLayout<AllCampaignsSummaryPageProps> = ({
     const campaignsQuery = useQuery(['campaigns', orgId], getCampaigns(orgId));
     const upcomingEventsQuery = useQuery(['upcomingEvents', orgId], getUpcomingEvents(orgId));
     const eventsQuery = useQuery(['events', orgId], getEvents(orgId));
-    const surveysQuery = useQuery(['surveys', orgId], getSurveys(orgId));
-    const callsQuery = useQuery(['calls', orgId], getAllCallAssignments(orgId));
-    const canvassesQuery = useQuery(['canvasses', orgId], getAllCanvassAssignments(orgId));
-
-    const [filters, setFilters] = useState({
-        callAssignments: false,
-        canvasses: false,
-        standalones: false,
-        surveys: false,
-    });
 
     const campaigns = campaignsQuery.data || [];
 
     const upcomingEvents = upcomingEventsQuery.data || [];
     const events = eventsQuery.data || [];
-    const surveys = surveysQuery.data?.map(s => ({ data: s, type: 'survey' })) || [];
-    const callAssignments = callsQuery.data?.map(c => ({ data: c, type: 'call_assignment' })) || [];
-    const canvasses = canvassesQuery.data?.map(c => ({ data: c, type: 'canvass_assignment' })) || [];
-    const standalones = events.filter(e => !e.campaign.id).map(s => ({ data: s, type: 'standalone_event' }));
-
-    let unsortedProjects = [
-        ... filters.surveys ? [...surveys] : [],
-        ... filters.canvasses ? [...canvasses] : [],
-        ... filters.callAssignments ? [...callAssignments] : [],
-        ... filters.standalones ? [...standalones] : [],
-    ];
-
-    // if no filters are applied show all unsorted items
-    if (Object.values(filters).every(v => !v)) {
-        unsortedProjects = [...surveys, ...canvasses, ...callAssignments, ...standalones];
-    }
-
-    const handleFilterBoxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setFilters({ ...filters, [event.target.name]: event.target.checked });
-    };
 
     return (
         <>
@@ -129,59 +97,6 @@ const AllCampaignsSummaryPage: PageWithLayout<AllCampaignsSummaryPageProps> = ({
                         )) }
                     </Box>
                 </ZetkinSection>
-                <Box m={ 1 } p={ 1 }>
-                    <Typography variant="h5">
-                        <Msg id="pages.organizeAllCampaigns.unsorted"/>
-                    </Typography>
-                </Box>
-                <Box alignItems="start" display="flex" justifyContent="flex-start" m={ 1 } p={ 1 } pt={ 0 }>
-                    <Box flexGrow={ 0 } width={ 0.5 }>
-                        <List disablePadding={ true }>
-                            { unsortedProjects.map(item => (
-                                <ListItem key={ `${item.type}-${item.data.id}` } style={{ background: grey[200], height: '4rem', margin: '1rem 0', paddingLeft:'0.5rem' }}>
-                                    <NextLink href="/" passHref>
-                                        <Link color="inherit" variant="subtitle2">
-                                            { `${item.type} ID: ${item.data.id}` }
-                                        </Link>
-                                    </NextLink>
-                                </ListItem>
-                            )) }
-                        </List>
-                    </Box>
-                    <Box border={ 1 } borderColor={ grey[400] } flexGrow={ 0 } m={ 3 } mt={ 2 } p={ 2 }>
-                        <FormControl component="fieldset">
-                            <FormLabel component="legend">
-                                <Typography color="textPrimary" variant="subtitle1">
-                                    <Msg id="pages.organizeAllCampaigns.filter.filter" />
-                                </Typography>
-                            </FormLabel>
-                            <FormGroup>
-                                <FormControlLabel
-                                    control={ <Checkbox checked={ filters.standalones } color="primary" name="standalones" onChange={ handleFilterBoxChange }/> }
-                                    label={ intl.formatMessage(
-                                        { id: 'pages.organizeAllCampaigns.filter.standalones' }) }
-                                />
-                                <FormControlLabel
-                                    control={ <Checkbox checked={ filters.surveys } color="primary" name="surveys" onChange={ handleFilterBoxChange } /> }
-                                    label={ intl.formatMessage(
-                                        { id: 'pages.organizeAllCampaigns.filter.surveys' }) }
-                                />
-                                <FormControlLabel
-                                    control={ <Checkbox checked={ filters.callAssignments } color="primary" name="callAssignments" onChange={ handleFilterBoxChange } /> }
-                                    label={ intl.formatMessage(
-                                        { id: 'pages.organizeAllCampaigns.filter.calls' }) }
-                                />
-                                <FormControlLabel
-                                    control={ <Checkbox checked={ filters.canvasses } color="primary"
-                                        name="canvasses" onChange={ handleFilterBoxChange }
-                                    /> }
-                                    label={ intl.formatMessage(
-                                        { id: 'pages.organizeAllCampaigns.filter.canvasses' }) }
-                                />
-                            </FormGroup>
-                        </FormControl>
-                    </Box>
-                </Box>
             </Box>
             <ZetkinSpeedDial actions={ [ACTIONS.CREATE_EVENT, ACTIONS.CREATE_CAMPAIGN] } />
         </>
