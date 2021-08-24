90a3f14134599b1f725b93dd90b0d11ea7a8a535
River H <eribloodlust@grrlz.net>
2021-08-24 11:08:36 +0100

    add icons to task list items based on task type

--- src/components/organize/tasks/TaskList/TaskListItem.tsx
+++ src/components/organize/tasks/TaskList/TaskListItem.tsx
@@ -1,17 +1,27 @@
 import Link from 'next/link';
-import { Box, ListItem, ListItemText, Typography } from '@material-ui/core';
+import { Box, ListItem, ListItemIcon, ListItemText, Typography } from '@material-ui/core';
+import { FormatListNumbered, Link as LinkIcon, NaturePeople, Share, VideoLibrary } from '@material-ui/icons';
 
 import getTaskStatus from 'utils/getTaskStatus';
+import { TASK_TYPE } from 'types/tasks';
 import { ZetkinTask } from 'types/zetkin';
 
 import TaskStatusChip from '../TaskStatusChip';
 import TaskStatusText from '../TaskStatusText';
 
+const TASK_TYPE_ICONS: {[key in TASK_TYPE]: React.ReactNode} = {
+    [TASK_TYPE.OFFLINE]: <NaturePeople />,
+    [TASK_TYPE.COLLECT_DEMOGRAPHICS]: <FormatListNumbered />,
+    [TASK_TYPE.SHARE_LINK]: <Share />,
+    [TASK_TYPE.SHARE_IMAGE]: <Share />,
+    [TASK_TYPE.VISIT_LINK]: <LinkIcon />,
+    [TASK_TYPE.WATCH_VIDEO]: <VideoLibrary />,
+};
+
 interface TaskListItemProps {
     task: ZetkinTask;
     hrefBase: string;
 }
-
 
 const TaskListItem = ({ task, hrefBase }: TaskListItemProps): JSX.Element => {
     const { id, title } = task;
@@ -22,6 +32,9 @@ const TaskListItem = ({ task, hrefBase }: TaskListItemProps): JSX.Element => {
             href={ hrefBase + `/calendar/tasks/${id}` }
             passHref>
             <ListItem button component="a">
+                <ListItemIcon>
+                    { TASK_TYPE_ICONS[task.type] }
+                </ListItemIcon>
                 <ListItemText>
                     { /* Title and Chip */ }
                     <Box alignItems="center" display="flex">
