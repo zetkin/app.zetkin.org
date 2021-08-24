283d5b09f49e35d83396fcfb3ee4c1ea7a91afa1
nehahirve <neha.hirve@hyperisland.se>
2021-08-24 16:55:40 +0200

    set up mvp redirects

--- next.config.js
+++ next.config.js
@@ -11,6 +11,38 @@ module.exports = {
                 destination: '/:prevPath*/calendar', 
                 permanent: false,
             },
+            // redirects to Gen2 for MVP August 2021
+            {
+                source: '/organize/:orgId(\\d{1,})',
+                destination: 'https://organize.zetk.in/?org=:orgId', 
+                permanent: false,
+            }, 
+            {
+                source: '/organize/:orgId(\\d{1,})/areas',
+                destination: 'https://organize.zetk.in/maps?org=:orgId', 
+                permanent: false,
+            },
+            {
+                source: '/organize/:orgId(\\d{1,})/people',
+                destination: 'https://organize.zetk.in/people?org=:orgId', 
+                permanent: false,
+            }, 
+            {
+                source: '/organize/:orgId(\\d{1,})/campaigns/calendar/events/:eventId(\\d{1,})',
+                destination: 'https://organize.zetk.in/campaigns/action::eventId?org=:orgId', 
+                permanent: false,
+            }, 
+            {
+                source: '/organize/:orgId(\\d{1,})/campaigns/:campId(\\d{1,})/calendar/events/:eventId(\\d{1,})',
+                destination: 'https://organize.zetk.in/campaigns/action::eventId?org=:orgId', 
+                permanent: false,
+            },
+            // all paths with /o redirected to Gen2
+            {
+                source: '/o/:path*',
+                destination: 'https://zetk.in/o/:path*', 
+                permanent: false,
+            }, 
         ]
     },
 };
