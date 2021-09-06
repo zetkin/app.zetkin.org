2222de8009895ca95f21ea86e7aaef67d0c14e2c
River H <eribloodlust@grrlz.net>
2021-09-06 10:04:42 +0100

    copy .env.test to .env.production whille tests are running, delete file when tearing down tests

--- e2e/globalSetup.ts
+++ e2e/globalSetup.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 /* eslint-disable no-console */
 
+import fs from 'fs';
 import { nextBuild } from 'next/dist/cli/next-build';
 import path from 'path';
 
@@ -9,6 +10,15 @@ async function globalSetup() {
     process.env.NODE_ENV = 'production';
     process.env.PLAYWRIGHT = '1';
 
+    // Copy env file .env.test to .env.production
+    fs.copyFile(path.join(__dirname, '..', '.env.test'), path.join(__dirname, '..', '.env.production'), (err) => {
+        if (err) {
+            console.error('Error copying env.test to env.production');
+            throw err;
+        }
+    });
+
+    // Skip build if flag is set to 1
     if (process.env.SKIP_BUILD === '1') {
         console.log('skipping build: SKIP_BUILD is set');
     }
--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,7 @@ import { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
     globalSetup: './e2e/globalSetup.ts',
+    globalTeardown: './e2e/globalTeardown.ts',
 };
 
 export default config;
