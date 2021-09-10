afd966b665ffacef8013337854841d022090a3b1
River H <eribloodlust@grrlz.net>
2021-09-10 10:24:54 +0100

    refactor getEvents request, remove getEvent, remove unused requests in all campaigns page

--- src/pages/organize/[orgId]/campaigns/index.tsx
+++ src/pages/organize/[orgId]/campaigns/index.tsx
@@ -7,14 +7,9 @@ import { useQuery } from 'react-query';
 
 import AllCampaignsLayout from '../../../../components/layout/organize/AllCampaignsLayout';
 import CampaignCard from 'components/organize/campaigns/CamapignCard';
-import getActivities from '../../../../fetching/getActivities';
-import getAllCallAssignments from '../../../../fetching/getAllCallAssignments';
-import getAllCanvassAssignments from '../../../../fetching/getAllCanvassAssignments';
 import getCampaigns from '../../../../fetching/getCampaigns';
 import getEvents from '../../../../fetching/getEvents';
-import getLocations from '../../../../fetching/getLocations';
 import getOrg from '../../../../fetching/getOrg';
-import getSurveys from '../../../../fetching/getSurveys';
 import getUpcomingEvents from '../../../../fetching/getUpcomingEvents';
 import { PageWithLayout } from '../../../../types';
 import { scaffold } from '../../../../utils/next';
@@ -44,22 +39,12 @@ export const getServerSideProps : GetServerSideProps = scaffold(async (ctx) => {
     await ctx.queryClient.prefetchQuery(['events', orgId], getEvents(orgId as string, ctx.apiFetch));
     const eventsState = ctx.queryClient.getQueryState(['events', orgId]);
 
-    await ctx.queryClient.prefetchQuery(['surveys', orgId], getSurveys(orgId as string, ctx.apiFetch));
-    const surveysState = ctx.queryClient.getQueryState(['surveys', orgId]);
-
-    await ctx.queryClient.prefetchQuery(['calls', orgId], getAllCallAssignments(orgId as string, ctx.apiFetch));
-    const allCallAssignmentsState = ctx.queryClient.getQueryState(['calls', orgId]);
-
-    await ctx.queryClient.prefetchQuery(['canvasses', orgId], getAllCanvassAssignments(orgId as string, ctx.apiFetch));
-    const canvassesState = ctx.queryClient.getQueryState(['canvasses', orgId]);
-
-    await ctx.queryClient.prefetchQuery(['activities', orgId], getActivities(orgId as string, ctx.apiFetch));
-    const activitiesState = ctx.queryClient.getQueryState(['activities', orgId]);
-
-    await ctx.queryClient.prefetchQuery(['locations', orgId], getLocations(orgId as string, ctx.apiFetch));
-    const locationsState = ctx.queryClient.getQueryState(['locations', orgId]);
-
-    if (orgState?.status === 'success' && campaignsState?.status === 'success' && eventsState?.status === 'success'  && allCallAssignmentsState?.status === 'success' && canvassesState?.status === 'success' && surveysState?.status === 'success' && upcomingEventsState?.status === 'success' && activitiesState?.status === 'success' && locationsState?.status === 'success') {
+    if (
+        orgState?.status === 'success' &&
+        campaignsState?.status === 'success' &&
+        eventsState?.status === 'success' &&
+        upcomingEventsState?.status === 'success'
+    ) {
         return {
             props: {
                 orgId,
@@ -84,7 +69,6 @@ const AllCampaignsSummaryPage: PageWithLayout<AllCampaignsSummaryPageProps> = ({
     const eventsQuery = useQuery(['events', orgId], getEvents(orgId));
 
     const campaigns = campaignsQuery.data || [];
-
     const upcomingEvents = upcomingEventsQuery.data || [];
     const events = eventsQuery.data || [];
 
