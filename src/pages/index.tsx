dd01deb3108fa3baaad195c4d875be2a12987165
River H <eribloodlust@grrlz.net>
2021-09-22 12:52:21 +0100

    remove NEXT_PUBLIC_USE_TLS and replace everywhere with ZETKIN_USE_TLS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,7 +24,7 @@ export const getServerSideProps : GetServerSideProps = scaffold(async (context) 
     const code = query?.code;
 
     if (code) {
-        const protocol = stringToBool(process.env.NEXT_PUBLIC_APP_USE_TLS)? 'https' : 'http';
+        const protocol = stringToBool(process.env.ZETKIN_USE_TLS)? 'https' : 'http';
         const host = process.env.ZETKIN_APP_HOST;
 
         let destination = '/';
--- src/utils/apiFetch.ts
+++ src/utils/apiFetch.ts
@@ -1,11 +1,13 @@
-import apiUrl from './apiUrl';
 import { IncomingHttpHeaders } from 'node:http';
+import { stringToBool } from './stringUtils';
 
 export type ApiFetch = (path: string, init?: RequestInit) => Promise<Response>;
 
 export const createApiFetch = (headers: IncomingHttpHeaders): ApiFetch => {
     return (path,init) => {
-        return fetch(apiUrl(path), {
+        const protocol = stringToBool(process.env.ZETKIN_USE_TLS) ? 'https' : 'http';
+        const apiUrl = `${protocol}://${process.env.ZETKIN_APP_HOST}/api${path}`;
+        return fetch(apiUrl, {
             ...init,
             headers: {
                 cookie: headers.cookie ||'',
