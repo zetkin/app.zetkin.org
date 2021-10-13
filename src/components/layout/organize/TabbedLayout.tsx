b78328ca359bebaa20dce2900d5a8c059f28fc85
River H <eribloodlust@grrlz.net>
2021-10-13 13:24:04 +0100

    improve padding top of entire page when on mobile, rename header class, use semantic elements, improve html inside header

--- src/components/layout/organize/TabbedLayout.tsx
+++ src/components/layout/organize/TabbedLayout.tsx
@@ -7,7 +7,13 @@ import BreadcrumbTrail from '../../BreadcrumbTrail';
 import OrganizeSidebar from 'components/organize/OrganizeSidebar';
 
 const useStyles = makeStyles((theme) => ({
-    header: {
+    root: {
+        [theme.breakpoints.down('xs')]: {
+            paddingTop: '3.5rem',
+        },
+    },
+    titleGrid: {
+        alignItems: 'center',
         display: 'grid',
         gap: '1rem',
         gridTemplateColumns:'1fr auto',
@@ -15,11 +21,6 @@ const useStyles = makeStyles((theme) => ({
         width: '100%',
         [theme.breakpoints.down('sm')]: {
             gridTemplateColumns: '1fr',
-        },
-    },
-    root: {
-        [theme.breakpoints.down('xs')]: {
-            paddingTop: '4rem',
         },
     },
 }));
@@ -66,22 +67,19 @@ const TabbedLayout: FunctionComponent<TabbedLayoutProps> = ({
             <OrganizeSidebar />
             <Box display="flex" flexDirection="column" height="100vh" overflow="auto" position="relative" width={ 1 }>
                 <Box display={ fixedHeight ? 'flex' :'block' } flexDirection="column" height={ fixedHeight ? 1 : 'auto' }>
-                    { /* Page Header */ }
                     <Box component="header" flexGrow={ 0 } flexShrink={ 0 }>
-                        { /* Breadcrumbs and Title */ }
-                        <Box p={ 3 }>
+                        <Box mb={ 2 } pt={ 3 } px={ 3 }>
                             <BreadcrumbTrail/>
                             { /* Title, subtitle, and action buttons */ }
-                            <Box alignItems="center" className={ classes.header } mt={ 3 }>
+                            <Box className={ classes.titleGrid } mt={ 2 }>
                                 <Box overflow="hidden">
-                                    <Typography component="h1" data-testid="page-title" noWrap variant="h2">
+                                    <Typography component="h1" data-testid="page-title" noWrap variant="h3">
                                         { title }
                                     </Typography>
                                     <Typography component="h2" variant="h5">
                                         { subtitle }
                                     </Typography>
                                 </Box>
-                                { /* Action Buttons */ }
                                 <Box>
                                     { actionButtons }
                                 </Box>
@@ -102,7 +100,7 @@ const TabbedLayout: FunctionComponent<TabbedLayoutProps> = ({
                         </Tabs>
                     </Box>
                     { /* Page Content */ }
-                    <Box flexGrow={ 1 } minHeight={ 0 } p={ fixedHeight ? 0 : 3 } position="relative" role="tabpanel">
+                    <Box component="main" flexGrow={ 1 } minHeight={ 0 } p={ fixedHeight ? 0 : 3 } position="relative" role="tabpanel">
                         { children }
                     </Box>
                 </Box>
