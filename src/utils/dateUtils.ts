882a05b41d64247ec9b0fd276eeec86e40603396
ziggi <ziggi.willpower@gmail.com>
2021-10-18 15:15:19 +0200

    delete getNaiveDate function

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -1,16 +1,5 @@
 import { Dayjs } from 'dayjs';
 
-export const getNaiveDate = (dateString: string): Date => {
-    const date = new Date(dateString);
-    return new Date(
-        date.getUTCFullYear(),
-        date.getUTCMonth(),
-        date.getUTCDate(),
-        date.getUTCHours(),
-        date.getUTCMinutes(),
-    );
-};
-
 export const getNewDateWithOffset = (date: Date, offset: number ):Date => {
     return new Date(new Date(date).setDate(date.getDate() + offset));
 };
