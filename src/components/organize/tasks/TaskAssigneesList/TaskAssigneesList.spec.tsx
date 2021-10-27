513025e5499157d23631f2e176abdeb8e4569bfb
ziggi <ziggi.willpower@gmail.com>
2021-10-27 17:06:03 +0200

    make mockAssignedTask and use it in test

--- src/components/organize/tasks/TaskAssigneesList/TaskAssigneesList.spec.tsx
+++ src/components/organize/tasks/TaskAssigneesList/TaskAssigneesList.spec.tsx
@@ -1,38 +1,23 @@
+import mockAssignedTask from 'test-utils/mocks/mockAssignedTask';
 import { render } from 'test-utils';
 import singletonRouter from 'next/router';
 import TaskAssigneesList from '.';
-import {  ASSIGNED_STATUS, ZetkinAssignedTask } from 'types/tasks';
-
-const tasks : ZetkinAssignedTask[] = [
-    {
-        assigned: '2021-08-20T12:55:01.469207',
-        assignee: {
-            first_name: 'Dolly',
-            id: 2,
-            last_name: 'Parton',
-        },
-        completed: null,
-        id: 1,
-        status: ASSIGNED_STATUS.COMPLETED,
-        task: {
-            deadline: '2021-012-20T12:55:01.469207',
-            expires: '2021-012-20T12:55:01.469207',
-            id: 1,
-            title: 'Fake title',
-        },
-    },
-];
 
 jest.mock('next/dist/client/router', () => require('next-router-mock'));
 
 describe('TaskAssigneesList', () => {
     it('renders a list of assignees.', async () => {
+        const task = mockAssignedTask();
+        const task2 = mockAssignedTask({
+            id: 2,
+        });
+
         singletonRouter.query = {
             orgId: '1',
         };
-        const { findAllByTestId } = render(<TaskAssigneesList assignedTasks={ tasks } />);
+        const { findAllByTestId } = render(<TaskAssigneesList assignedTasks={ [task, task2] } />);
         const assignees = await findAllByTestId('task-assignee');
-        expect(assignees.length).toEqual(1);
+        expect(assignees.length).toEqual(2);
     });
     it.skip('correctly sorts cards.', () => {
         //todo
