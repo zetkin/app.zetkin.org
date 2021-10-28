de84ec114377f6cd038344d5e00d4161fdf7a4ad
ziggi <ziggi.willpower@gmail.com>
2021-10-28 18:07:42 +0200

    refactor to place router query in beforeEach()

--- src/components/organize/tasks/TaskAssigneesList/TaskAssigneesList.spec.tsx
+++ src/components/organize/tasks/TaskAssigneesList/TaskAssigneesList.spec.tsx
@@ -5,6 +5,12 @@ import TaskAssigneesList from '.';
 
 jest.mock('next/dist/client/router', () => require('next-router-mock'));
 
+beforeEach(() => {
+    singletonRouter.query = {
+        orgId: '1',
+    };
+});
+
 describe('TaskAssigneesList', () => {
     it('renders a list of assignees.', async () => {
         const task = mockAssignedTask();
@@ -12,9 +18,6 @@ describe('TaskAssigneesList', () => {
             id: 2,
         });
 
-        singletonRouter.query = {
-            orgId: '1',
-        };
         const { findAllByTestId } = render(<TaskAssigneesList assignedTasks={ [task, task2] } />);
         const assignees = await findAllByTestId('task-assignee');
         expect(assignees.length).toEqual(2);
