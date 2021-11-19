da95c14482688c5b9d03a2ed264edc44a268f00b
Richard Olsson <r@richardolsson.se>
2021-11-19 18:46:56 +0100

    Add avatars to view table

--- src/components/ZetkinViewTable/index.tsx
+++ src/components/ZetkinViewTable/index.tsx
@@ -1,4 +1,5 @@
-import { DataGridPro } from '@mui/x-data-grid-pro';
+import { Person } from '@material-ui/icons';
+import { DataGridPro, GridColDef, GridRenderCellParams } from '@mui/x-data-grid-pro';
 
 import { ZetkinViewColumn, ZetkinViewRow } from 'types/zetkin';
 
@@ -9,11 +10,42 @@ interface ZetkinViewTableProps {
 }
 
 const ZetkinViewTable = ({ columns, rows }: ZetkinViewTableProps): JSX.Element => {
-    const gridColumns = columns.map((col, index) => ({
-        field: index.toString(),
-        headerName: col.title,
-        minWidth: 200,
-    }));
+    const avatarColumn : GridColDef = {
+        disableColumnMenu: true,
+        disableExport: true,
+        disableReorder: true,
+        field: 'id',
+        filterable: false,
+        headerName: ' ',
+        renderCell: (params) => {
+            const url = `/api/orgs/1/people/${params.value}/avatar`;
+            return (
+                <img
+                    alt="Avatar"
+                    src={ url }
+                    style={{
+                        maxHeight: '100%',
+                        maxWidth: '100%',
+                    }}
+                />
+            );
+        },
+        renderHeader: () => {
+            return <Person/>;
+        },
+        resizable: false,
+        sortable: false,
+        width: 50,
+    };
+
+    const gridColumns = [
+        avatarColumn,
+        ...columns.map((col, index) => ({
+            field: index.toString(),
+            headerName: col.title,
+            minWidth: 200,
+        }))
+    ];
 
     const gridRows = rows.map(row => ({
         id: row.id,
