84f02fc74f3cb1346abd881b4bd11ad7e509092e
River H <eribloodlust@grrlz.net>
2021-11-23 13:32:13 +0000

    update moxy, add moxy logging to playwright fixture, update columns

--- playwright/mockData/orgs/KPD/people/views/AllMembers/columns.ts
+++ playwright/mockData/orgs/KPD/people/views/AllMembers/columns.ts
@@ -2,16 +2,22 @@ import { ZetkinViewColumn } from '../../../../../../../src/types/zetkin';
 
 const AllMembersColumns: ZetkinViewColumn[] = [
     {
+        config: { field: 'first_name' },
         id: 1,
         title: 'First name',
+        type: 'person_field',
     },
     {
+        config: { field: 'last_name' },
         id: 2,
         title: 'Last name',
+        type: 'person_field',
     },
     {
+        config: { field: 'active' },
         id: 3,
         title: 'Active',
+        type: 'custom_field',
     },
 ];
 
