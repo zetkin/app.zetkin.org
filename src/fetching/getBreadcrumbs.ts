ebb0ec2db91c02f78822e0569a263a98c3bdb0b5
Ev Sc <everin.m.scott@gmail.com>
2021-11-30 10:21:24 +0100

    ADD: Error handler for breadcrumbs api route

--- src/fetching/getBreadcrumbs.ts
+++ src/fetching/getBreadcrumbs.ts
@@ -1,11 +1,15 @@
+import APIError from 'utils/apiError';
 import { Breadcrumb } from '../types';
 import { defaultFetch } from '.';
 
 export default function getBreadcrumbs(pathname: string, queryString: string, fetch  = defaultFetch) {
     return async () : Promise<Breadcrumb[]> => {
-        const bRes = await fetch(`/breadcrumbs?pathname=${pathname}&${queryString}`);
+        const url = `/breadcrumbs?pathname=${pathname}&${queryString}`;
+        const bRes = await fetch(url);
+        if (!bRes.ok) {
+            throw new APIError('GET', url);
+        }
         const bData = await bRes.json();
         return bData.breadcrumbs;
     };
 }
-
