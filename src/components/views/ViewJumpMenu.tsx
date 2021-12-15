e189d602f1e32728a891fa2044477ee45e13bcc7
Richard Olsson <r@richardolsson.se>
2021-12-15 21:12:56 +0100

    Implement auto-complete to filter views in jump menu

--- src/components/views/ViewJumpMenu.tsx
+++ src/components/views/ViewJumpMenu.tsx
@@ -1,8 +1,9 @@
 import { ExpandMore } from '@material-ui/icons';
+import { useAutocomplete } from '@material-ui/lab';
 import { useQuery } from 'react-query';
 import { useRouter } from 'next/router';
+import { Box, IconButton, MenuItem, Popover, TextField } from '@material-ui/core';
 import { FunctionComponent, useState } from 'react';
-import { IconButton, Menu, MenuItem } from '@material-ui/core';
 
 import getViews from 'fetching/views/getViews';
 import ZetkinQuery from 'components/ZetkinQuery';
@@ -17,40 +18,64 @@ const ViewJumpMenu : FunctionComponent<ViewJumpMenuProps> = ({ onViewSelect }) =
     const { orgId, viewId } = router.query;
     const viewsQuery = useQuery(['views', orgId], getViews(orgId as string));
     const [jumpMenuAnchor, setJumpMenuAnchor] = useState<Element | null>(null);
+    const {
+        getInputProps,
+        getListboxProps,
+        getRootProps,
+        groupedOptions,
+        inputValue,
+    } = useAutocomplete({
+        getOptionLabel: option => option.title,
+        options: viewsQuery.data || [],
+    });
 
     return (
         <>
             <IconButton onClick={ (ev) => setJumpMenuAnchor(ev.target as Element) }>
                 <ExpandMore/>
             </IconButton>
-            <Menu
+            <Popover
                 anchorEl={ jumpMenuAnchor }
                 onClose={ () => setJumpMenuAnchor(null) }
                 open={ !!jumpMenuAnchor }
                 PaperProps={{
                     style: {
+                        display: 'flex',
+                        flexDirection: 'column',
                         maxHeight: '40vh',
+                        width: '30ch',
                     },
                 }}>
                 <ZetkinQuery
                     queries={{ viewsQuery }}>
                     { ({ queries: { viewsQuery } }) => {
-                        return viewsQuery.data.map(view => {
-                            if (view.id.toString() != viewId as string) {
-                                return (
-                                    <MenuItem key={ view.id }
-                                        onClick={ () => {
-                                            setJumpMenuAnchor(null);
-                                            onViewSelect(view);
-                                        } }>
-                                        { view.title }
-                                    </MenuItem>
-                                );
-                            }
-                        });
+                        const options = inputValue.length? groupedOptions : viewsQuery.data;
+
+                        return (
+                            <>
+                                <Box { ...getRootProps() }>
+                                    <TextField { ...getInputProps() } />
+                                </Box>
+                                <Box { ...getListboxProps() } style={{ overflowY: 'scroll' }}>
+                                    { options.map(view => {
+                                        if (view.id.toString() != viewId as string) {
+                                            return (
+                                                <MenuItem key={ view.id }
+                                                    onClick={ () => {
+                                                        setJumpMenuAnchor(null);
+                                                        onViewSelect(view);
+                                                    } }>
+                                                    { view.title }
+                                                </MenuItem>
+                                            );
+                                        }
+                                    }) }
+                                </Box>
+                            </>
+                        );
                     } }
                 </ZetkinQuery>
-            </Menu>
+            </Popover>
         </>
     );
 };
