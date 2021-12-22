1bb3f07333e27ed5bac78b981ce8eed306244f43
Richard Olsson <r@richardolsson.se>
2021-12-22 19:47:21 +0100

    Add ellipsis menu item to convert dynamic view to static

--- src/components/layout/organize/SingleViewLayout.tsx
+++ src/components/layout/organize/SingleViewLayout.tsx
@@ -1,8 +1,9 @@
 import { Box } from '@material-ui/core';
+import { defaultFetch } from 'fetching';
 import { useIntl } from 'react-intl';
-import { useQuery } from 'react-query';
 import { useRouter } from 'next/router';
 import { FunctionComponent, useState } from 'react';
+import { useMutation, useQuery, useQueryClient } from 'react-query';
 
 import { EllipsisMenuProps } from './EllipsisMenu';
 import getView from 'fetching/views/getView';
@@ -14,11 +15,25 @@ import ViewSmartSearchDialog from 'components/views/ViewSmartSearchDialog';
 const SingleViewLayout: FunctionComponent = ({ children }) => {
     const intl = useIntl();
     const router = useRouter();
+    const queryClient = useQueryClient();
+
     const { orgId, viewId } = router.query;
     const [queryDialogOpen, setQueryDialogOpen] = useState(false);
     const viewQuery = useQuery(['view', viewId ], getView(orgId as string, viewId as string));
 
     const view = viewQuery.data;
+
+    // TODO: Create mutation using new factory pattern
+    const deleteQueryMutation = useMutation(async () => {
+        await defaultFetch(`/orgs/${orgId}/people/views/${view?.id}/content_query`, {
+            method: 'DELETE',
+        });
+    }, {
+        onSettled: () => {
+            queryClient.invalidateQueries(['view', view?.id.toString(), 'rows']);
+            queryClient.invalidateQueries(['view', view?.id.toString()]);
+        },
+    });
 
     const title = (
         <Box>
@@ -33,6 +48,10 @@ const SingleViewLayout: FunctionComponent = ({ children }) => {
         ellipsisMenu.push({
             label: intl.formatMessage({ id: 'pages.people.views.layout.ellipsisMenu.editQuery' }),
             onSelect: () => setQueryDialogOpen(true),
+        });
+        ellipsisMenu.push({
+            label: intl.formatMessage({ id: 'pages.people.views.layout.ellipsisMenu.makeStatic' }),
+            onSelect: () => deleteQueryMutation.mutate(),
         });
     }
 
