f5f81e4e0d60cfdab8ee68824a342f03fac75578
Richard Olsson <r@richardolsson.se>
2021-12-22 19:12:29 +0100

    Add ellipsis menu item to edit view Smart Search query

--- src/components/layout/organize/SingleViewLayout.tsx
+++ src/components/layout/organize/SingleViewLayout.tsx
@@ -1,16 +1,21 @@
 import { Box } from '@material-ui/core';
-import { FunctionComponent } from 'react';
+import { useIntl } from 'react-intl';
 import { useQuery } from 'react-query';
 import { useRouter } from 'next/router';
+import { FunctionComponent, useState } from 'react';
 
+import { EllipsisMenuProps } from './EllipsisMenu';
 import getView from 'fetching/views/getView';
 import TabbedLayout from './TabbedLayout';
 import ViewJumpMenu from 'components/views/ViewJumpMenu';
+import ViewSmartSearchDialog from 'components/views/ViewSmartSearchDialog';
 
 
 const SingleViewLayout: FunctionComponent = ({ children }) => {
+    const intl = useIntl();
     const router = useRouter();
     const { orgId, viewId } = router.query;
+    const [queryDialogOpen, setQueryDialogOpen] = useState(false);
     const viewQuery = useQuery(['view', viewId ], getView(orgId as string, viewId as string));
 
     const view = viewQuery.data;
@@ -22,17 +27,36 @@ const SingleViewLayout: FunctionComponent = ({ children }) => {
         </Box>
     );
 
+    const ellipsisMenu: EllipsisMenuProps['items'] = [];
+
+    if (view?.content_query) {
+        ellipsisMenu.push({
+            label: intl.formatMessage({ id: 'pages.people.views.layout.ellipsisMenu.editQuery' }),
+            onSelect: () => setQueryDialogOpen(true),
+        });
+    }
+
     return (
-        <TabbedLayout
-            baseHref={ `/organize/${orgId}/people/views/${viewId}` }
-            defaultTab="/"
-            fixedHeight={ true }
-            tabs={ [
-                { href: `/`, messageId: 'layout.organize.view.tabs.view' },
-            ] }
-            title={ title }>
-            { children }
-        </TabbedLayout>
+        <>
+            <TabbedLayout
+                baseHref={ `/organize/${orgId}/people/views/${viewId}` }
+                defaultTab="/"
+                ellipsisMenu={ ellipsisMenu }
+                fixedHeight={ true }
+                tabs={ [
+                    { href: `/`, messageId: 'layout.organize.view.tabs.view' },
+                ] }
+                title={ title }>
+                { children }
+            </TabbedLayout>
+            { queryDialogOpen && view && (
+                <ViewSmartSearchDialog
+                    onDialogClose={ () => setQueryDialogOpen(false) }
+                    orgId={ orgId as string }
+                    view={ view }
+                />
+            ) }
+        </>
     );
 };
 
