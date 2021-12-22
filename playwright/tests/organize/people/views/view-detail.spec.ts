5a60302643888d89c8268e38d4fd30b56110580f
Richard Olsson <r@richardolsson.se>
2021-12-22 22:21:43 +0100

    Add integration test for adding person to an empty view

--- playwright/tests/organize/people/views/view-detail.spec.ts
+++ playwright/tests/organize/people/views/view-detail.spec.ts
@@ -6,6 +6,7 @@ import AllMembersColumns from '../../../../mockData/orgs/KPD/people/views/AllMem
 import AllMembersRows from '../../../../mockData/orgs/KPD/people/views/AllMembers/rows';
 import KPD from '../../../../mockData/orgs/KPD';
 import NewView from '../../../../mockData/orgs/KPD/people/views/NewView';
+import RosaLuxemburg from '../../../../mockData/users/RosaLuxemburg';
 
 test.describe('View detail page', () => {
 
@@ -101,5 +102,87 @@ test.describe('View detail page', () => {
         await removeViewMock();
         await removeRowsMock();
         await removeColsMock();
+    });
+
+    test('add person to empty view', async ({ page, appUri, moxy }) => {
+        const removeViewsMock = await moxy.setMock('/orgs/1/people/views', 'get', {
+            data: {
+                data: [ AllMembers, NewView ],
+            },
+            status: 200,
+        });
+        const removeViewMock = await moxy.setMock('/orgs/1/people/views/1', 'get', {
+            data: {
+                data: AllMembers,
+            },
+            status: 200,
+        });
+        const removeRowsMock = await moxy.setMock('/orgs/1/people/views/1/rows', 'get', {
+            data: {
+                data: [],
+            },
+            status: 200,
+        });
+        const removeColsMock = await moxy.setMock('/orgs/1/people/views/1/columns', 'get', {
+            data: {
+                data: AllMembersColumns,
+            },
+            status: 200,
+        });
+        const removePeopleSearchMock = await moxy.setMock('/orgs/1/search/person', 'post', {
+            data: {
+                data: [RosaLuxemburg],
+            },
+        });
+        const removePutRowMock = await moxy.setMock('/orgs/1/people/views/1/rows/1', 'put', {
+            data: {
+                data: {
+                    content: [
+                        RosaLuxemburg.first_name,
+                        RosaLuxemburg.last_name,
+                        false,
+                    ],
+                    id: RosaLuxemburg.id,
+                },
+            },
+            status: 201,
+        });
+        const removeDeleteQueryMock = await moxy.setMock('/orgs/1/people/views/1/content_query', 'delete', {
+            status: 204,
+        });
+
+        await page.goto(appUri + '/organize/1/people/views/1');
+
+        // Add person statically
+        await page.click('[name=person]');
+        await page.fill('[name=person]', 'Rosa');
+        await page.click('text="Rosa Luxemburg"');
+        await page.waitForTimeout(200);
+
+        // Make sure the row was added
+        expect((await moxy.logRequests()).log.find(req =>
+            req.method === 'PUT' &&
+            req.path === '/v1/orgs/1/people/views/1/rows/1',
+        )).toBeTruthy();
+
+        // Make sure previous content query was deleted
+        expect((await moxy.logRequests()).log.find(req =>
+            req.method === 'DELETE' &&
+            req.path === '/v1/orgs/1/people/views/1/content_query',
+        )).toBeTruthy();
+
+        // Make sure rows are fetched anew
+        expect((await moxy.logRequests()).log.filter(req =>
+            req.method === 'GET' &&
+            req.path === '/v1/orgs/1/people/views/1/rows',
+        ).length).toBeGreaterThan(1);
+
+        await removeViewsMock();
+        await removeViewMock();
+        await removeRowsMock();
+        await removeColsMock();
+        await removePeopleSearchMock();
+        await removePutRowMock();
+        await removeDeleteQueryMock();
     });
 });
