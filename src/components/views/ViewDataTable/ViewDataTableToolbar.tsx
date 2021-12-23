1d3568b6f804cf47224ef1837db8d103c25a3a9d
Richard Olsson <r@richardolsson.se>
2021-12-23 22:21:54 +0100

    Add toolbar button to create view from selection

--- src/components/views/ViewDataTable/ViewDataTableToolbar.tsx
+++ src/components/views/ViewDataTable/ViewDataTableToolbar.tsx
@@ -1,16 +1,28 @@
-import { Add } from '@material-ui/icons';
 import { FormattedMessage } from 'react-intl';
+import { Add, Launch } from '@material-ui/icons';
 import { Box, Button } from '@material-ui/core';
+
 
 export interface ViewDataTableToolbarProps {
     onColumnCreate: () => void;
+    onViewCreate: () => void;
+    selection: number[];
 }
 
-const ViewDataTableToolbar: React.FunctionComponent<ViewDataTableToolbarProps> = ({ onColumnCreate }) => {
+const ViewDataTableToolbar: React.FunctionComponent<ViewDataTableToolbarProps> = ({
+    onColumnCreate,
+    onViewCreate,
+    selection,
+}) => {
     return (
         <Box display="flex" justifyContent="flex-end">
-            <Button data-testid="create-column-button" onClick={ onColumnCreate }>
-                <Add /><FormattedMessage id="misc.views.createColumn" />
+            { !!selection.length && (
+                <Button onClick={ onViewCreate } startIcon={ <Launch/> }>
+                    <FormattedMessage id="misc.views.createFromSelection" />
+                </Button>
+            ) }
+            <Button data-testid="create-column-button" onClick={ onColumnCreate } startIcon={ <Add /> }>
+                <FormattedMessage id="misc.views.createColumn" />
             </Button>
         </Box>
     );
