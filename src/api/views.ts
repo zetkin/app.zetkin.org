a19b741f98d514d0cff78fb3156bafbabe9721fa
djbusstop <eribloodlust@grrlz.net>
2022-01-03 12:36:00 +0000

    create viewResource with delete handler

--- src/api/views.ts
+++ src/api/views.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { CreateNewViewReqBody } from 'pages/api/views/createNew';
 import { ZetkinView } from 'types/views';
-import { createPrefetch, createUseMutation, createUseQuery } from './utils/resourceHookFactories';
+import { createPrefetch, createUseMutation, createUseMutationDelete, createUseQuery } from './utils/resourceHookFactories';
 
 export const viewsResource = (orgId: string) => {
     const key = ['views', orgId];
@@ -11,5 +11,15 @@ export const viewsResource = (orgId: string) => {
         prefetch: createPrefetch<ZetkinView[]>(key, url),
         useCreate: createUseMutation<CreateNewViewReqBody, ZetkinView>(key, `/views/createNew?orgId=${orgId}`),
         useQuery: createUseQuery<ZetkinView[]>(key, url),
+    };
+};
+
+
+export const viewResource = (orgId: string, viewId: string) => {
+    const key = ['view', orgId, viewId];
+    const url = `/orgs/${orgId}/people/views/${viewId}`;
+
+    return {
+        useDelete: createUseMutationDelete(key, url),
     };
 };
