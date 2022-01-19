bfc3da93e6acb7f0f3dd654b1a2c3ba01c9ac2af
Ev Sc <everin.m.scott@gmail.com>
2022-01-19 22:53:34 +0100

    Add GridToolbarFilter to custom toolbar

--- src/components/views/ViewDataTable/ViewDataTableToolbar.tsx
+++ src/components/views/ViewDataTable/ViewDataTableToolbar.tsx
@@ -1,6 +1,7 @@
 import { FormattedMessage } from 'react-intl';
+import { GridToolbarFilterButton } from '@mui/x-data-grid-pro';
 import { Add, Launch } from '@material-ui/icons';
-import { Box, Button } from '@material-ui/core';
+import { Box, Button, makeStyles } from '@material-ui/core';
 
 
 export interface ViewDataTableToolbarProps {
@@ -9,13 +10,23 @@ export interface ViewDataTableToolbarProps {
     selection: number[];
 }
 
+const useStyles = makeStyles({
+    main: {
+        '& > *': {
+            margin: '0 4px',
+        },
+    },
+});
+
 const ViewDataTableToolbar: React.FunctionComponent<ViewDataTableToolbarProps> = ({
     onColumnCreate,
     onViewCreate,
     selection,
 }) => {
+    const classes = useStyles();
+
     return (
-        <Box display="flex" justifyContent="flex-end">
+        <Box className={ classes.main } display="flex" justifyContent="flex-end">
             { !!selection.length && (
                 <Button
                     data-testid="ViewDataTableToolbar-createFromSelection"
@@ -24,6 +35,7 @@ const ViewDataTableToolbar: React.FunctionComponent<ViewDataTableToolbarProps> =
                     <FormattedMessage id="misc.views.createFromSelection" />
                 </Button>
             ) }
+            <GridToolbarFilterButton componentsProps={{ button: { color: 'default', size: 'medium'  } }} />
             <Button
                 data-testid="ViewDataTableToolbar-createColumn"
                 onClick={ onColumnCreate }
