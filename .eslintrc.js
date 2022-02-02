613f05523df38bed7ea8c2aa4d14975d6fceb5b0
Ev Sc <everin.m.scott@gmail.com>
2022-02-02 19:32:18 +0100

    Add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,9 @@ module.exports = {
     es6: true,
   },
   parserOptions: { ecmaVersion: 8 }, // to enable features such as async/await
-  ignorePatterns: ['node_modules/*', '.next/*', '.out/*', '!.prettierrc.js', ], // We don't want to lint generated files nor node_modules, but we want to lint .prettierrc.js (ignored by default by eslint)
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  // We don't want to lint generated files nor node_modules, but we want to lint .prettierrc.js (ignored by default by eslint)
+  ignorePatterns: ['node_modules/*', '.next/*', '.out/*', '!.prettierrc.js', ],
+  extends: ['prettier'],
   settings: { react: { version: 'detect' } },
   overrides: [
     // This configuration will apply only to TypeScript files
@@ -19,86 +20,34 @@ module.exports = {
         es6: true,
       },
       extends: [
-        'eslint:recommended',
         'plugin:@typescript-eslint/recommended', // TypeScript rules
         'plugin:react/recommended', // React rules
         'plugin:react-hooks/recommended', // React hooks rules
         'plugin:jsx-a11y/recommended', // Accessibility rules
+        'prettier',
       ],
       plugins: [
           'no-switch-statements',
       ],
       rules: {
         '@typescript-eslint/no-unused-vars': ['error'],
-        '@typescript-eslint/member-delimiter-style': ['error', {
-          'multiline': {
-            'delimiter': 'semi',
-            'requireLast': true,
-          },
-          'singleline': {
-            'delimiter': 'semi',
-            'requireLast': false,
-          },
-        }],
         '@typescript-eslint/member-ordering': ['error', {
           'default': { 'memberTypes': 'never', 'order': 'alphabetically' },
           'interfaces': ['signature', 'method', 'constructor', 'field'],
         }],
-
-        'brace-style': ['error', 'stroustrup'],
-        'comma-dangle': ['error', 'always-multiline'],
-        'quotes': ['error', 'single', { 'allowTemplateLiterals': true, 'avoidEscape': true }],
-        'indent': ['error', 4],
         'jsx-a11y/anchor-is-valid': 'off',
-        'jsx-quotes': ['error', 'prefer-double'],
-        'keyword-spacing': ['error', {}],
         'no-console': 'error',
         'no-switch-statements/no-switch': 'error',
-        'no-trailing-spaces': 'error',
-        'object-curly-spacing': ['error', 'always'],
-        'padding-line-between-statements': ['error',
-            // imports go together, but last import must be followed by newline
-            { blankLine: 'always', prev: 'import', next: '*' },
-            { blankLine: 'any', prev: 'import', next: 'import' },
-
-            { blankLine: 'always', prev: 'export', next: '*' },
-            { blankLine: 'always', prev: '*', next: 'export' },
-
-            { blankLine: 'always', prev: 'function', next: '*' },
-            { blankLine: 'always', prev: '*', next: 'function' },
-        ],
         'prefer-const': ['error', {}],
-        'react/jsx-closing-bracket-location': ['error', {
-            'nonEmpty': 'after-props',
-            'selfClosing': 'line-aligned',
-        }],
-        'react/jsx-closing-tag-location': 'error',
-        'react/jsx-curly-brace-presence': 'error',
-        'react/jsx-curly-spacing': ['error', {
-            'children': true,
-            'spacing': {
-                'objectLiterals': 'never',
-            },
-            'when': 'always',
-        }],
-        'react/jsx-equals-spacing': 'error',
         'react/jsx-handler-names': ['error', {
             'eventHandlerPrefix': 'on',
             'eventHandlerPropPrefix': 'on',
         }],
-        'react/jsx-indent': 'error',
-        'react/jsx-indent-props': 'error',
         'react/jsx-no-target-blank': 'error',
-        'react/jsx-props-no-multi-spaces': 'error',
         'react/jsx-sort-props': ['error', {
           'ignoreCase': true,
           'reservedFirst': true,
         }],
-        'react/jsx-tag-spacing': ['error', {
-            'beforeClosing': 'never',
-            'beforeSelfClosing': 'allow',
-        }],
-        'react/jsx-wrap-multilines': 'error',
         'react/no-deprecated': 'error',
         'react/no-typos': 'error',
         'react/no-unknown-property': 'error',
@@ -109,7 +58,7 @@ module.exports = {
         'react/prefer-stateless-function': 'error',
         'react/prop-types': 'off',
         'react/react-in-jsx-scope': 'off',
-        'semi': ['error', 'always'],
+        'react-hooks/exhaustive-deps': 'off',
         'sort-imports': ['error', {
             'ignoreCase': true,
             'allowSeparatedGroups': true,
@@ -117,7 +66,6 @@ module.exports = {
         }],
         'sort-keys': 'error',
         'sort-vars': 'error',
-        'space-before-blocks': ['error', 'always'],
       },
     },
   ],
