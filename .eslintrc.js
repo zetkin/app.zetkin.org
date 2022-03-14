eec442e35c7e0b720a940403cdd66e1811b43600
djbusstop <eribloodlust@grrlz.net>
2022-03-14 17:16:17 +0000

    add eslint rules for braces requiring multiple lines and curly braces after if statements

--- .eslintrc.js
+++ .eslintrc.js
@@ -41,6 +41,8 @@ module.exports = {
             interfaces: ['signature', 'method', 'constructor', 'field'],
           },
         ],
+        curly: 'error',
+        'brace-style': 'error',
         'jsx-a11y/anchor-is-valid': 'off',
         'no-console': 'error',
         'no-switch-statements/no-switch': 'error',
