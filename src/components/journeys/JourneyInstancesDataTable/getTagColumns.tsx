eba896b708d9f4272ba7008d756b7b5b7474e536
Ev Sc <everin.m.scott@gmail.com>
2022-03-31 07:56:57 +0200

    Get tag column values

--- src/components/journeys/JourneyInstancesDataTable/getTagColumns.tsx
+++ src/components/journeys/JourneyInstancesDataTable/getTagColumns.tsx
@@ -1,6 +1,7 @@
 import { ColumnNames } from './getColumns';
 import { GridColDef } from '@mui/x-data-grid-pro';
 import { TagMetadata } from 'pages/api/organize/[orgId]/journeys/[journeyId]/getTagMetadata';
+import { ZetkinJourneyInstance } from '../../../types/zetkin';
 
 const getTagColumns = (
   tagMetadata: TagMetadata,
@@ -10,16 +11,31 @@ const getTagColumns = (
   const groupColumns: GridColDef[] = groups.map((group) => ({
     field: `tagGroup${group?.id}`,
     headerName: group?.title,
+    valueGetter: (params) => {
+      const rowTags = params.row.tags as ZetkinJourneyInstance['tags'];
+      const groupsTags = rowTags.filter((tag) => tag.group?.id === group?.id);
+      return groupsTags.map((tag) => tag.title).join(', ');
+    },
   }));
 
   const valueColumns: GridColDef[] = valueTags.map((tag) => ({
     field: `valueTag${tag.id}`,
     headerName: tag.title,
+    valueGetter: (params) => {
+      const rowTags = params.row.tags as ZetkinJourneyInstance['tags'];
+      const valueTags = rowTags.filter((tag) => 'value' in tag);
+      return valueTags.map((tag) => tag.value).join('');
+    },
   }));
 
   const freeTagColumn: GridColDef = {
     field: 'tagsFree',
     headerName: columnNames['tagsFree'],
+    valueGetter: (params) => {
+      const rowTags = params.row.tags as ZetkinJourneyInstance['tags'];
+      const freeTags = rowTags.filter((tag) => !tag.group && !('value' in tag));
+      return freeTags.map((tag) => tag.title).join(', ');
+    },
   };
 
   return groupColumns.concat(valueColumns).concat(freeTagColumn);
