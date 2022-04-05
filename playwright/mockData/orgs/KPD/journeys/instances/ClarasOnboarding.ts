a29602fbca2395e28d6b9c4a4b79eeee726d17ca
ziggi <ziggi.willpower@gmail.com>
2022-04-05 16:45:04 +0200

    Make api resource for journey instance with dummy data

---
+++ playwright/mockData/orgs/KPD/journeys/instances/ClarasOnboarding.ts
@@ -0,0 +1,46 @@
+import ActivistTag from '../../tags/Activist';
+import ClaraZetkin from '../../people/ClaraZetkin';
+import MemberOnboarding from '../MemberOnboarding';
+import PlaysGuitarTag from '../../tags/PlaysGuitar';
+import { ZetkinJourneyInstance } from 'types/zetkin';
+
+const ClarasOnboarding: ZetkinJourneyInstance = {
+  assigned_to: [ClaraZetkin],
+  created_at: '2022-02-01T14:53:15',
+  id: 1,
+  journey: {
+    id: MemberOnboarding.id,
+    title: MemberOnboarding.singular_name,
+  },
+  milestones: [
+    {
+      deadline: '2022-03-06T14:53:15',
+      status: 'completed',
+      title: 'Sign up for newsletter',
+    },
+    {
+      deadline: '2022-05-23T12:53:15',
+      status: 'unfinished',
+      title: 'Go to open meeting',
+    },
+    {
+      deadline: '2022-07-11T12:00:00',
+      status: 'unfinished',
+      title: 'Fill out member survey',
+    },
+  ],
+  next_milestone: {
+    deadline: '2022-05-23T12:53:15',
+    status: 'unfinished',
+    title: 'Go to open meeting',
+  },
+  people: [ClaraZetkin],
+  status: 'open',
+  summary:
+    'Nullam sagittis neque augue, vitae lacinia purus auctor eget. Mauris bibendum, dui id fringilla dictum, velit ligula efficitur ligula, vitae aliquet mauris augue vel ante. Mauris vitae ante molestie, pharetra risus vel, suscipit metus. Sed cursus quis elit id aliquam. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla vitae arcu a neque luctus sodales non id eros. Duis a porta turpis. Vestibulum dignissim tempor consectetur. Maecenas volutpat orci in nulla malesuada sollicitudin. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus euismod, purus vel tempus bibendum, purus lorem pulvinar lacus, ac vulputate massa nunc vitae leo. Vivamus suscipit, ex a euismod pretium, ante odio placerat urna, id sollicitudin sem metus eu purus. Nulla id ipsum interdum, pharetra eros vitae, suscipit sapien. Nunc sed massa rhoncus, varius tellus a, lobortis lacus. Fusce vel tincidunt lectus, sit amet facilisis nisl. ',
+  tags: [ActivistTag, PlaysGuitarTag],
+  title: "Clara's onboarding",
+  updated_at: '2022-03-21T12:53:15',
+};
+
+export default ClarasOnboarding;
--- src/api/journeys.ts
+++ src/api/journeys.ts
@@ -3,6 +3,7 @@ import { ScaffoldedContext } from 'utils/next';
 import { createPrefetch, createUseQuery } from './utils/resourceHookFactories';
 import { ZetkinJourney, ZetkinJourneyInstance } from 'types/zetkin';
 
+import ClarasOnboarding from '../../playwright/mockData/orgs/KPD/journeys/instances/ClarasOnboarding';
 import MarxistTraining from '../../playwright/mockData/orgs/KPD/journeys/MarxistTraining';
 import { TagMetadata } from '../pages/api/organize/[orgId]/journeys/[journeyId]/getTagMetadata';
 
@@ -42,5 +43,34 @@ export const journeyInstancesResource = (orgId: string, journeyId: string) => {
       journeyInstances: ZetkinJourneyInstance[];
       tagMetadata: TagMetadata;
     }>(key, url),
+  };
+};
+
+//TODO: update type when dummy data is removed
+interface JourneyInstanceResource {
+  prefetch: (ctx?: ScaffoldedContext | undefined) => Promise<{
+    ctx: ScaffoldedContext | undefined;
+    state: { status: string };
+  }>;
+  useQuery: () => { data: ZetkinJourneyInstance; key: unknown; url: string };
+}
+
+//TODO: remove dummy data
+export const journeyInstanceResource = (
+  orgId: string,
+  journeyId: string,
+  instanceId: string
+): JourneyInstanceResource => {
+  const key = ['journeyInstance', orgId, journeyId, instanceId];
+  const url = `/organize/${orgId}/journeys/${journeyId}/instances/${instanceId}`;
+
+  return {
+    //prefetch: createPrefetch<ZetkinJourneyInstance>(key,url),
+    prefetch: async (ctx?: ScaffoldedContext) => ({
+      ctx,
+      state: { status: 'success' },
+    }),
+    //useQuery: createUseQuery<ZetkinJourneyInstance>(key, url),
+    useQuery: () => ({ data: ClarasOnboarding, key, url }),
   };
 };
