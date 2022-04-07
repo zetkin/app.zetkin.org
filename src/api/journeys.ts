311fb04fa078b5452d2a4e305a42e4bccba006ec
Ev Sc <everin.m.scott@gmail.com>
2022-04-07 13:29:17 +0200

    Remove dummy data

--- src/api/journeys.ts
+++ src/api/journeys.ts
@@ -1,9 +1,7 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-import { ScaffoldedContext } from 'utils/next';
 import { createPrefetch, createUseQuery } from './utils/resourceHookFactories';
 import { ZetkinJourney, ZetkinJourneyInstance } from 'types/zetkin';
 
-import MarxistTraining from '../../playwright/mockData/orgs/KPD/journeys/MarxistTraining';
 import { TagMetadata } from '../utils/getTagMetadata';
 
 export const journeysResource = (orgId: string) => {
@@ -16,19 +14,13 @@ export const journeysResource = (orgId: string) => {
   };
 };
 
-// TODO: delete dummy data
 export const journeyResource = (orgId: string, journeyId: string) => {
   const key = ['journey', orgId, journeyId];
   const url = `/orgs/${orgId}/journeys/${journeyId}`;
 
   return {
-    // prefetch: createPrefetch<ZetkinJourney>(key, url),
-    prefetch: async (ctx?: ScaffoldedContext) => ({
-      ctx,
-      state: { status: 'success' },
-    }),
-    // useQuery: createUseQuery<ZetkinJourney>(key, url),
-    useQuery: () => ({ data: MarxistTraining, key, url }),
+    prefetch: createPrefetch<ZetkinJourney>(key, url),
+    useQuery: createUseQuery<ZetkinJourney>(key, url),
   };
 };
 
