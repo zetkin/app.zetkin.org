fa9442d1f3a01ff34560cc8bad0a3da089650380
djbusstop <eribloodlust@grrlz.net>
2022-04-08 11:16:41 +0100

    add remove function and start testing

--- src/components/organize/TagsManager/TagChip.tsx
+++ src/components/organize/TagsManager/TagChip.tsx
@@ -1,18 +1,38 @@
+import { HighlightOff } from '@material-ui/icons';
+import { useState } from 'react';
 import { Box, Tooltip } from '@material-ui/core';
 
 import { ZetkinTag } from 'types/zetkin';
 
-const TagChip: React.FunctionComponent<{ tag: ZetkinTag }> = ({ tag }) => {
+const TagChip: React.FunctionComponent<{
+  onClickRemove?: (tag: ZetkinTag) => void;
+  tag: ZetkinTag;
+}> = ({ onClickRemove, tag }) => {
+  const [hover, setHover] = useState(false);
   return (
     <Tooltip arrow title={tag.description}>
       <Box
+        alignItems="center"
         bgcolor={tag.color || '#e1e1e1'}
         borderRadius="18px"
+        display="flex"
         fontSize={13}
-        px={2}
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+        px={hover ? 1.4 : 2}
         py={0.7}
       >
         {tag.title}
+        {hover && onClickRemove && (
+          <HighlightOff
+            data-testid={`TagChip-removeButton-${tag.id}`}
+            fontSize="small"
+            onClick={() => onClickRemove(tag)}
+            style={{
+              cursor: 'pointer',
+            }}
+          />
+        )}
       </Box>
     </Tooltip>
   );
--- src/components/organize/TagsManager/index.tsx
+++ src/components/organize/TagsManager/index.tsx
@@ -14,8 +14,9 @@ import TagsList from './TagsList';
 const TagsManager: React.FunctionComponent<{
   appliedTags: ZetkinTag[];
   availableTags: ZetkinTag[];
+  onRemove: (tag: ZetkinTag) => void;
   onSelect: (tag: ZetkinTag) => void;
-}> = ({ availableTags, appliedTags, onSelect }) => {
+}> = ({ availableTags, appliedTags, onRemove, onSelect }) => {
   const intl = useIntl();
 
   const [addTagButton, setAddTagButton] = useState<HTMLElement | null>(null);
@@ -33,7 +34,11 @@ const TagsManager: React.FunctionComponent<{
     >
       <Box>
         {appliedTags.length > 0 ? (
-          <TagsList isGrouped={isGrouped} tags={appliedTags} />
+          <TagsList
+            isGrouped={isGrouped}
+            onRemove={onRemove}
+            tags={appliedTags}
+          />
         ) : (
           // If no tags
           <Typography>
