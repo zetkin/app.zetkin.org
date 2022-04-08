6286fc544fc2cf4b6930d3d7411ebe5fccb178f9
ziggi <ziggi.willpower@gmail.com>
2022-04-08 19:27:37 +0200

    Update mock milestone.

--- src/utils/testing/mocks/mockMilestone.ts
+++ src/utils/testing/mocks/mockMilestone.ts
@@ -4,7 +4,7 @@ import { ZetkinJourneyMilestone } from 'types/zetkin';
 const milestone: ZetkinJourneyMilestone = {
   deadline: '2022-06-18T00:29:12+02:00',
   status: 'unfinished',
-  title: 'Sing karaoke',
+  title: 'perform lip sync',
 };
 
 const mockMilestone = (
