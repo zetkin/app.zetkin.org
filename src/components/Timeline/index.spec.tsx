1b9b55f362d8e8f150d0c60068208f35620c2ad8
Ev Sc <everin.m.scott@gmail.com>
2022-04-22 00:07:55 +0200

    Extend test to cover expansion button

--- src/components/Timeline/index.spec.tsx
+++ src/components/Timeline/index.spec.tsx
@@ -1,8 +1,15 @@
+import 'next-router-mock/dynamic-routes';
 import dayjs from 'dayjs';
+import mockRouter from 'next-router-mock';
 
 import mockUpdate from 'utils/testing/mocks/mockUpdate';
-import { render } from 'utils/testing';
+import { fireEvent, render } from 'utils/testing';
 import Timeline, { SHOW_INITIALLY, TimelineProps } from '.';
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+mockRouter.registerPaths(['/organize/[orgId]/people/[id]']);
+jest.mock('next/dist/client/router', () => require('next-router-mock'));
 
 const NUM_UPDATES = 10;
 
@@ -16,10 +23,22 @@ const props: TimelineProps = {
   ),
 };
 
-describe('EditTextInPlace', () => {
-  it('displays the correct number of updates', () => {
+describe('Timeline', () => {
+  beforeEach(() => {
+    mockRouter.setCurrentUrl('/initial');
+  });
+
+  it('displays the correct number of updates initially', () => {
     const { getAllByLabelText } = render(<Timeline {...props} />);
     const updates = getAllByLabelText('timeline update');
     expect(updates.length).toEqual(SHOW_INITIALLY);
+  });
+
+  it('expands to show all updates', () => {
+    const { getAllByLabelText, getByText } = render(<Timeline {...props} />);
+    const showMore = getByText('misc.timeline.expand');
+    fireEvent.click(showMore);
+    const updates = getAllByLabelText('timeline update');
+    expect(updates.length).toEqual(NUM_UPDATES);
   });
 });
