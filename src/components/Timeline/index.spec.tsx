89e342a8648efc74201aa1d379bab5ab2039b722
Ev Sc <everin.m.scott@gmail.com>
2022-04-21 21:15:53 +0200

    Simplify test

--- src/components/Timeline/index.spec.tsx
+++ src/components/Timeline/index.spec.tsx
@@ -1,21 +1,18 @@
+import dayjs from 'dayjs';
+
 import mockUpdate from 'utils/testing/mocks/mockUpdate';
 import { render } from 'utils/testing';
 import Timeline, { SHOW_INITIALLY, TimelineProps } from '.';
-import dayjs from 'dayjs';
 
 const NUM_UPDATES = 10;
 
-const hoursAgo: number[] = Array.from({ length: NUM_UPDATES })
-  .reduce(
-    (acc: number[], val, i: number) =>
-      acc.concat(i > 1 ? acc[i - 1] + acc[i - 2] : i),
-    []
-  )
-  .slice(2);
-
 const props: TimelineProps = {
-  updates: hoursAgo.map((hours) =>
-    mockUpdate({ created_at: dayjs().subtract(hours, 'hours').format() })
+  updates: Array.from(Array(NUM_UPDATES).keys()).map(() =>
+    mockUpdate({
+      created_at: dayjs()
+        .subtract(Math.random() * 100, 'hours')
+        .format(),
+    })
   ),
 };
 
