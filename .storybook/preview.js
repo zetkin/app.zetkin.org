4124d095b5760ea1406bf6d55e3744e5c898763b
Richard Olsson <r@richardolsson.se>
2022-04-22 17:26:45 +0200

    Add Storybook middleware and provider to load messages from YAML files

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -5,19 +5,37 @@ import { QueryClientProvider, QueryClient } from 'react-query';
 import { RouterContext } from 'next/dist/shared/lib/router-context'; // next 11.1
 import theme from '../src/theme';
 import mockPerson from '../src/utils/testing/mocks/mockPerson';
+import { useEffect, useState } from 'react';
 
 const queryClient = new QueryClient();
+
+const AsyncIntlProvider = (props) => {
+  const [messages, setMessages] = useState({});
+
+  // Load localized messages on first render
+  useEffect(() => {
+    async function loadMessages() {
+      const res = await fetch('/l10n_messages');
+      const data = await res.json();
+      setMessages(data.messages);
+    }
+
+    loadMessages();
+  }, []);
+
+  return (
+    <IntlProvider defaultLocale="en" locale="en" messages={messages}>
+      {props.children}
+    </IntlProvider>
+  );
+};
 
 export const decorators = [
   muiTheme([theme]),
   (story) => (
     <QueryClientProvider client={queryClient}>{story()}</QueryClientProvider>
   ),
-  (story) => (
-    <IntlProvider defaultLocale="en" locale="en" messages={{}}>
-      {story()}
-    </IntlProvider>
-  ),
+  (story) => <AsyncIntlProvider>{story()}</AsyncIntlProvider>,
   withMock,
 ];
 
