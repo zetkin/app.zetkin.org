eca403a0103c3eb4106d8c1f42406cbd9a1915a9
Ev Sc <everin.m.scott@gmail.com>
2022-04-27 10:01:07 +0200

    Remove legacy logic

--- src/components/Timeline/updates/TimelineAssigned.tsx
+++ src/components/Timeline/updates/TimelineAssigned.tsx
@@ -7,9 +7,6 @@ import { ZetkinPerson as ZetkinPersonType } from 'types/zetkin';
 import ZetkinRelativeTime from 'components/ZetkinRelativeTime';
 import { ZetkinUpdateAssignee } from 'types/updates';
 
-const getPersonName = (person: Partial<ZetkinPersonType>) =>
-  person.first_name + ' ' + person.last_name;
-
 interface Props {
   update: ZetkinUpdateAssignee;
 }
@@ -45,35 +42,27 @@ const TimelineAssigned: React.FunctionComponent<Props> = ({ update }) => {
   }
 
   function renderActionText() {
-    const assignee = update.details?.assignee;
+    const assignee = update.details.assignee;
     const actor = update.actor;
-    let values = {};
-    if (assignee) {
-      values = {
-        ...values,
-        assignee: (
-          <PersonHoverCard
-            BoxProps={{ style: { display: 'inline-flex' } }}
-            personId={assignee.id}
-          >
-            <b>{getPersonName(assignee)}</b>
-          </PersonHoverCard>
-        ),
-      };
-    }
-    values = {
-      ...values,
-      actor: (
-        <PersonHoverCard
-          BoxProps={{ style: { display: 'inline-flex' } }}
-          personId={actor.id}
-        >
-          <b>{getPersonName(actor)}</b>
-        </PersonHoverCard>
-      ),
-    };
     return (
-      <FormattedMessage id={`misc.updates.${update.type}`} values={values} />
+      <FormattedMessage
+        id={`misc.updates.${update.type}`}
+        values={{
+          actor: renderPerson(actor),
+          assignee: renderPerson(assignee),
+        }}
+      />
+    );
+  }
+
+  function renderPerson(person: Partial<ZetkinPersonType>) {
+    return (
+      <PersonHoverCard
+        BoxProps={{ style: { display: 'inline-flex' } }}
+        personId={Number(person.id)}
+      >
+        <b>{person.first_name + ' ' + person.last_name}</b>
+      </PersonHoverCard>
     );
   }
 };
