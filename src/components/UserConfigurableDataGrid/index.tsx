1c4161ad7ba22954bfe8b6ecbe8b20102d1cebfe
Richard Olsson <r@richardolsson.se>
2022-05-13 22:20:07 +0200

    Create DataGrid replacement component that uses new hook to store config

---
+++ src/components/UserConfigurableDataGrid/index.tsx
@@ -0,0 +1,43 @@
+import { DataGridPro, DataGridProProps } from '@mui/x-data-grid-pro';
+
+import useConfigurableDataGridColumns from './useConfigurableDataGridColumns';
+
+type UserConfigurableDataGridProps = DataGridProProps & {
+  storageKey: string;
+};
+
+const UserConfigurableDataGrid: React.FC<UserConfigurableDataGridProps> = ({
+  storageKey,
+  ...gridProps
+}) => {
+  const { columns, setColumnOrder, setColumnWidth } =
+    useConfigurableDataGridColumns(storageKey, gridProps.columns);
+
+  return (
+    <DataGridPro
+      {...gridProps}
+      columns={columns}
+      onColumnOrderChange={(params, event, details) => {
+        // Subtract one for selection column, if it's visible
+        const targetIndex = gridProps.checkboxSelection
+          ? params.targetIndex - 1
+          : params.targetIndex;
+
+        setColumnOrder(params.colDef.field, targetIndex);
+
+        if (gridProps.onColumnOrderChange) {
+          gridProps.onColumnOrderChange(params, event, details);
+        }
+      }}
+      onColumnResize={(params, event, details) => {
+        setColumnWidth(params.colDef.field, params.width);
+
+        if (gridProps.onColumnResize) {
+          gridProps.onColumnResize(params, event, details);
+        }
+      }}
+    />
+  );
+};
+
+export default UserConfigurableDataGrid;
