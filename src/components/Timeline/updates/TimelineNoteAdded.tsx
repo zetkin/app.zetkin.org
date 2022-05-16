95391caad99a400d01699d494a1dba4c79a8d653
Ev Sc <everin.m.scott@gmail.com>
2022-05-16 23:45:51 +0200

    Show added note updates component

---
+++ src/components/Timeline/updates/TimelineNoteAdded.tsx
@@ -0,0 +1,37 @@
+import { createEditor } from 'slate';
+import { FormattedMessage } from 'react-intl';
+import { Typography } from '@material-ui/core';
+import { Editable, Slate, withReact } from 'slate-react';
+import React, { useMemo } from 'react';
+
+import UpdateContainer from './elements/UpdateContainer';
+import ZetkinPersonLink from 'components/ZetkinPersonLink';
+import { ZetkinUpdateJourneyInstanceAddNote } from 'types/updates';
+
+interface Props {
+  update: ZetkinUpdateJourneyInstanceAddNote;
+}
+
+const TimelineNoteAdded: React.FC<Props> = ({ update }) => {
+  const editor = useMemo(() => withReact(createEditor()), []);
+
+  return (
+    <UpdateContainer
+      headerContent={
+        <FormattedMessage
+          id="misc.updates.journeyinstance.addnote"
+          values={{ actor: <ZetkinPersonLink person={update.actor} /> }}
+        />
+      }
+      update={update}
+    >
+      <Typography variant="body1">
+        <Slate editor={editor} value={JSON.parse(update.details.note.text)}>
+          <Editable readOnly />
+        </Slate>
+      </Typography>
+    </UpdateContainer>
+  );
+};
+
+export default TimelineNoteAdded;
