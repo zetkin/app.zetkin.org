f7fdaa753ccfe84ddac182552c03c2223c3b6e25
Ev Sc <everin.m.scott@gmail.com>
2022-05-16 09:38:12 +0200

    Conform to linting rules + extract TextElement component

--- src/components/Timeline/TextEditor/index.tsx
+++ src/components/Timeline/TextEditor/index.tsx
@@ -1,5 +1,4 @@
 import { makeStyles } from '@material-ui/styles';
-import { Property } from 'csstype';
 import { withHistory } from 'slate-history';
 import { Box, Collapse } from '@material-ui/core';
 import { createEditor, Descendant } from 'slate';
@@ -7,7 +6,7 @@ import { Editable, Slate, withReact } from 'slate-react';
 import React, { Attributes, useCallback, useMemo, useState } from 'react';
 
 import './types';
-import LinkComponent from './LinkComponent';
+import TextElement from './TextElement';
 import theme from 'theme';
 import Toolbar from './Toolbar';
 import { keyDownHandler, withInlines } from './helpers';
@@ -39,7 +38,7 @@ const TextEditor: React.FunctionComponent<{ initialValue?: Descendant[] }> = ({
 }) => {
   const [active, setActive] = useState<boolean>(false);
   const classes = useStyles({ active });
-  const renderElement = useCallback((props) => <Element {...props} />, []);
+  const renderElement = useCallback((props) => <TextElement {...props} />, []);
   const renderLeaf = useCallback((props) => <Leaf {...props} />, []);
   const editor = useMemo(
     () => withInlines(withHistory(withReact(createEditor()))),
@@ -68,63 +67,6 @@ const TextEditor: React.FunctionComponent<{ initialValue?: Descendant[] }> = ({
       </Slate>
     </Box>
   );
-};
-
-const Element: React.FunctionComponent<{
-  attributes: Attributes;
-  element: { align: Property.TextAlign; [key: string]: string };
-}> = ({ attributes, children, element }) => {
-  const style = {
-    margin: '0 0 12px 0',
-    textAlign: element.align,
-  };
-  // eslint-disable-next-line no-switch-statements/no-switch
-  switch (element.type) {
-    case 'block-quote':
-      return (
-        <blockquote style={style} {...attributes}>
-          {children}
-        </blockquote>
-      );
-    case 'bulleted-list':
-      return (
-        <ul style={style} {...attributes}>
-          {children}
-        </ul>
-      );
-    case 'heading-one':
-      return (
-        <h1 style={style} {...attributes}>
-          {children}
-        </h1>
-      );
-    case 'heading-two':
-      return (
-        <h2 style={style} {...attributes}>
-          {children}
-        </h2>
-      );
-    case 'list-item':
-      return (
-        <li style={style} {...attributes}>
-          {children}
-        </li>
-      );
-    case 'numbered-list':
-      return (
-        <ol style={style} {...attributes}>
-          {children}
-        </ol>
-      );
-    case 'link':
-      return <LinkComponent {...{ attributes, children, element }} />;
-    default:
-      return (
-        <p style={style} {...attributes}>
-          {children}
-        </p>
-      );
-  }
 };
 
 const Leaf: React.FunctionComponent<{
