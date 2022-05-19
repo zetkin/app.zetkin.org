a27577a48d232b116b5ca36233a7e8d4d9b15fa5
Richard Olsson <r@richardolsson.se>
2022-05-19 20:07:39 +0200

    Configure playwright to reduce risk of timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,6 +4,7 @@ const config: PlaywrightTestConfig = {
     globalSetup: './playwright/globalSetup.ts',
     globalTeardown: './playwright/globalTeardown.ts',
     testDir: './playwright',
+    timeout: 60000,
     workers: parseInt(process.env.PLAYWRIGHT_WORKERS as string) || 4,
 };
 
