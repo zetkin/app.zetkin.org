cace616e2b7ad63ec3644aca3a29adb5b751e0cf
Richard Olsson <r@richardolsson.se>
2022-05-19 15:43:10 +0200

    Refactor TagSelect to move list in separate component

--- src/components/organize/TagManager/components/TagSelect.tsx
+++ src/components/organize/TagManager/components/TagSelect.tsx
@@ -76,84 +76,15 @@ const TagSelect: React.FunctionComponent<{
         variant="outlined"
       />
       {/* Options */}
-      <List
-        {...getListboxProps()}
-        style={{ maxHeight: '400px', overflowY: 'scroll' }}
-      >
-        {groupedFilteredTags.map((group) => {
-          // Groups
-          return (
-            <List
-              key={group.title}
-              subheader={
-                <ListSubheader disableSticky>{group.title}</ListSubheader>
-              }
-              title={group.title}
-            >
-              {/* Tags */}
-              {group.tags.map((tag) => {
-                return (
-                  <ListItem key={tag.id} dense={!disableEditTags}>
-                    <Box
-                      alignItems="center"
-                      display="flex"
-                      justifyContent="space-between"
-                      width="100%"
-                    >
-                      <TagChip
-                        disabled={disabledTags
-                          .map((disabledTags) => disabledTags.id)
-                          .includes(tag.id)}
-                        onClick={() => onSelect(tag)}
-                        tag={tag}
-                      />
-                      {/* Edit tag button, only show if enabled (it's enabled by default) */}
-                      {!disableEditTags && (
-                        <IconButton
-                          data-testid={`TagManager-TagSelect-editTag-${tag.id}`}
-                          onClick={(e) => {
-                            e.stopPropagation();
-                            setTagToEdit(tag);
-                          }}
-                        >
-                          <EditIcon fontSize="small" />
-                        </IconButton>
-                      )}
-                    </Box>
-                  </ListItem>
-                );
-              })}
-            </List>
-          );
-        })}
-        <ListItem
-          button
-          data-testid="TagManager-TagSelect-createTagOption"
-          dense
-          onClick={() =>
-            setTagToEdit({
-              title: inputValue,
-            })
-          }
-        >
-          <Add />
-          {inputValue ? (
-            <FormattedMessage
-              id="misc.tags.tagManager.createNamedTag"
-              values={{
-                b: (...chunks: string[]) => (
-                  <>
-                    &nbsp;<b>{chunks}</b>
-                  </>
-                ),
-                name: inputValue,
-              }}
-            />
-          ) : (
-            <FormattedMessage id="misc.tags.tagManager.createTag" />
-          )}
-        </ListItem>
-      </List>
+      <TagSelectList
+        disabledTags={disabledTags}
+        disableEditTags={!!disableEditTags}
+        groupedTags={groupedFilteredTags}
+        inputValue={inputValue}
+        listProps={getListboxProps()}
+        onEdit={(tag) => setTagToEdit(tag)}
+        onSelect={onSelect}
+      />
       <TagDialog
         groups={groups}
         onClose={() => setTagToEdit(undefined)}
@@ -170,6 +101,102 @@ const TagSelect: React.FunctionComponent<{
         tag={tagToEdit}
       />
     </Box>
+  );
+};
+
+const TagSelectList: React.FC<{
+  disableEditTags: boolean;
+  disabledTags: ZetkinTag[];
+  groupedTags: { id: number | 'ungrouped'; tags: ZetkinTag[]; title: string }[];
+  inputValue: string;
+  listProps: Record<string, unknown>;
+  onEdit: (tag: ZetkinTag | Pick<ZetkinTag, 'title'>) => void;
+  onSelect: (tag: ZetkinTag) => void;
+}> = ({
+  disableEditTags,
+  disabledTags,
+  groupedTags,
+  inputValue,
+  listProps,
+  onEdit,
+  onSelect,
+}) => {
+  return (
+    <List {...listProps} style={{ maxHeight: '400px', overflowY: 'scroll' }}>
+      {groupedTags.map((group) => {
+        // Groups
+        return (
+          <List
+            key={group.title}
+            subheader={
+              <ListSubheader disableSticky>{group.title}</ListSubheader>
+            }
+            title={group.title}
+          >
+            {/* Tags */}
+            {group.tags.map((tag) => {
+              return (
+                <ListItem key={tag.id} dense={!disableEditTags}>
+                  <Box
+                    alignItems="center"
+                    display="flex"
+                    justifyContent="space-between"
+                    width="100%"
+                  >
+                    <TagChip
+                      disabled={disabledTags
+                        .map((disabledTags) => disabledTags.id)
+                        .includes(tag.id)}
+                      onClick={() => onSelect(tag)}
+                      tag={tag}
+                    />
+                    {/* Edit tag button, only show if enabled (it's enabled by default) */}
+                    {!disableEditTags && (
+                      <IconButton
+                        data-testid={`TagManager-TagSelect-editTag-${tag.id}`}
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          onEdit(tag);
+                        }}
+                      >
+                        <EditIcon fontSize="small" />
+                      </IconButton>
+                    )}
+                  </Box>
+                </ListItem>
+              );
+            })}
+          </List>
+        );
+      })}
+      <ListItem
+        button
+        data-testid="TagManager-TagSelect-createTagOption"
+        dense
+        onClick={() =>
+          onEdit({
+            title: inputValue,
+          })
+        }
+      >
+        <Add />
+        {inputValue ? (
+          <FormattedMessage
+            id="misc.tags.tagManager.createNamedTag"
+            values={{
+              b: (...chunks: string[]) => (
+                <>
+                  &nbsp;<b>{chunks}</b>
+                </>
+              ),
+              name: inputValue,
+            }}
+          />
+        ) : (
+          <FormattedMessage id="misc.tags.tagManager.createTag" />
+        )}
+      </ListItem>
+    </List>
   );
 };
 
