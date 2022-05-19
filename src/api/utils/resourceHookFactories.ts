48069b762212641b8d408104380b4df59e678351
Richard Olsson <r@richardolsson.se>
2022-05-19 17:37:01 +0200

    Create resource hook factory for put requests with body

--- src/api/utils/resourceHookFactories.ts
+++ src/api/utils/resourceHookFactories.ts
@@ -10,6 +10,7 @@ import {
   UseQueryResult,
 } from 'react-query';
 
+import APIError from 'utils/apiError';
 import { defaultFetch } from 'fetching';
 import handleResponseData from './handleResponseData';
 import { makeUseMutationOptions } from './makeUseMutationOptions';
@@ -107,6 +108,38 @@ export const createUseMutationPut = (
       handler,
       makeUseMutationOptions(queryClient, props.key, props.mutationOptions)
     );
+  };
+};
+
+export const createUseMutationPutWithBody = <
+  Input extends { id: number }
+>(props: {
+  key: string[];
+  url: string;
+}): (() => UseMutationResult<null, unknown, Input, unknown>) => {
+  const handler = async (resource: Input): Promise<null> => {
+    const { id, ...body } = resource;
+    const requestOptions: RequestInit = {
+      method: 'PUT',
+    };
+    if (body) {
+      requestOptions.body = JSON.stringify(body);
+      requestOptions.headers = {
+        'Content-Type': 'application/json',
+      };
+    }
+
+    const res = await defaultFetch(`${props.url}/${id}`, requestOptions);
+
+    if (!res.ok) {
+      throw new APIError('PUT', res.url);
+    }
+    return null;
+  };
+
+  return () => {
+    const queryClient = useQueryClient();
+    return useMutation(handler, makeUseMutationOptions(queryClient, props.key));
   };
 };
 
