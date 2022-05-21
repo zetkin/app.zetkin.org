8816ea7f651f5077ca340fde7a8b71b846981e39
djbusstop <eribloodlust@grrlz.net>
2022-05-21 09:56:59 +0100

    refactor index page to show only open instances

--- src/pages/organize/[orgId]/journeys/[journeyId]/index.tsx
+++ src/pages/organize/[orgId]/journeys/[journeyId]/index.tsx
@@ -1,18 +1,15 @@
-import { Add } from '@material-ui/icons';
 import { GetServerSideProps } from 'next';
 import Head from 'next/head';
-import NextLink from 'next/link';
-import { Fab, makeStyles } from '@material-ui/core';
 
 import AllJourneyInstancesLayout from 'layout/organize/AllJourneyInstancesLayout';
 import getOrg from 'fetching/getOrg';
+import JourneyInstanceCreateFab from 'components/journeys/JourneyInstanceCreateFab';
 import JourneyInstancesDataTable from 'components/journeys/JourneyInstancesDataTable';
 import { PageWithLayout } from 'types';
 import { scaffold } from 'utils/next';
-import { TagMetadata } from 'utils/getTagMetadata';
+import { ZetkinJourney } from 'types/zetkin';
 import ZetkinQuery from 'components/ZetkinQuery';
 import { journeyInstancesResource, journeyResource } from 'api/journeys';
-import { ZetkinJourney, ZetkinJourneyInstance } from 'types/zetkin';
 
 const scaffoldOptions = {
   authLevelRequired: 2,
@@ -50,26 +47,13 @@ export const getServerSideProps: GetServerSideProps = scaffold(async (ctx) => {
   }
 }, scaffoldOptions);
 
-type JourneyInstancesOverviewPageProps = {
+type OpenJourneyInstancesPageProps = {
   journeyId: string;
   orgId: string;
 };
 
-interface JourneyInstancesData {
-  journeyInstances: ZetkinJourneyInstance[];
-  tagMetadata: TagMetadata;
-}
-
-const useStyles = makeStyles((theme) => ({
-  fab: {
-    bottom: theme.spacing(10),
-    position: 'fixed',
-    right: theme.spacing(4),
-  },
-}));
-
-const JourneyInstancesOverviewPage: PageWithLayout<
-  JourneyInstancesOverviewPageProps
+const OpenJourneyInstancesPage: PageWithLayout<
+  OpenJourneyInstancesPageProps
 > = ({ orgId, journeyId }) => {
   const journeyQuery = journeyResource(orgId, journeyId).useQuery();
   const journeyInstancesQuery = journeyInstancesResource(
@@ -78,35 +62,35 @@ const JourneyInstancesOverviewPage: PageWithLayout<
   ).useQuery();
   const journey = journeyQuery.data as ZetkinJourney;
 
-  const classes = useStyles();
-
   return (
     <>
       <Head>
         <title>{journey.plural_label}</title>
       </Head>
       <ZetkinQuery queries={{ journeyInstancesQuery }}>
-        <JourneyInstancesDataTable
-          {...(journeyInstancesQuery.data as JourneyInstancesData)}
-        />
+        {({ queries: { journeyInstancesQuery } }) => {
+          const openJourneyInstances =
+            journeyInstancesQuery.data.journeyInstances.filter(
+              (journeyInstance) => journeyInstance.closed == null
+            );
+
+          return (
+            <JourneyInstancesDataTable
+              journeyInstances={openJourneyInstances}
+              tagMetadata={journeyInstancesQuery.data.tagMetadata}
+            />
+          );
+        }}
       </ZetkinQuery>
-      <NextLink href={`/organize/${orgId}/journeys/${journeyId}/new`} passHref>
-        <Fab
-          className={classes.fab}
-          color="primary"
-          data-testid="JourneyInstanceOverviewPage-addFab"
-        >
-          <Add />
-        </Fab>
-      </NextLink>
+      <JourneyInstanceCreateFab />
     </>
   );
 };
 
-JourneyInstancesOverviewPage.getLayout = function getLayout(page) {
+OpenJourneyInstancesPage.getLayout = function getLayout(page) {
   return (
     <AllJourneyInstancesLayout fixedHeight>{page}</AllJourneyInstancesLayout>
   );
 };
 
-export default JourneyInstancesOverviewPage;
+export default OpenJourneyInstancesPage;
