63ca293a77167fe4db46d5eeca8b020963b9178d
ziggi <ziggi.willpower@gmail.com>
2022-05-24 10:39:26 +0200

    Conditionally set time to "now" if provided time is in the future.

--- src/components/ZetkinRelativeTime.tsx
+++ src/components/ZetkinRelativeTime.tsx
@@ -7,18 +7,24 @@ import ZetkinDateTime from './ZetkinDateTime';
 interface ZetkinRelativeTimeProps {
   convertToLocal?: boolean;
   datetime: string; // iso datetime string
+  forcePast?: boolean;
 }
 
 const ZetkinRelativeTime: React.FunctionComponent<ZetkinRelativeTimeProps> = ({
   convertToLocal,
   datetime,
+  forcePast,
 }) => {
   const now = dayjs();
   const absoluteDatetime = dayjs(
     convertToLocal ? new Date(datetime + 'Z') : datetime
   );
 
-  const difference: number = absoluteDatetime.unix() - now.unix();
+  //if forcePast is set and datetime is in the future, set time to "now"
+  const difference: number =
+    forcePast && absoluteDatetime.unix() - now.unix() > 0
+      ? 0
+      : absoluteDatetime.unix() - now.unix();
 
   if (isNaN(difference)) {
     return null;
