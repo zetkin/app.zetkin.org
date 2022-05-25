8b8240f6f257d41c84eb62919fe151197ac2575f
djbusstop <eribloodlust@grrlz.net>
2022-05-25 10:09:08 +0100

    add redirect for people views

--- next.config.js
+++ next.config.js
@@ -1,43 +1,52 @@
 module.exports = {
-    async redirects() {
-        return [
-            {
-                source: '/:prevPath*/calendar/events',
-                destination: '/:prevPath*/calendar',
-                permanent: false,
-            },
-            {
-                source: '/:prevPath*/calendar/tasks',
-                destination: '/:prevPath*/calendar',
-                permanent: false,
-            },
-            // redirects to Gen2 for MVP August 2021
-            {
-                source: '/organize/:orgId(\\d{1,})',
-                destination: 'https://organize.zetk.in/?org=:orgId',
-                permanent: false,
-            },
-            {
-                source: '/organize/:orgId(\\d{1,})/areas',
-                destination: 'https://organize.zetk.in/maps?org=:orgId',
-                permanent: false,
-            },
-            {
-                source: '/organize/:orgId(\\d{1,})/campaigns/calendar/events/:eventId(\\d{1,})',
-                destination: 'https://organize.zetk.in/campaign/action%3A:eventId?org=:orgId',
-                permanent: false,
-            },
-            {
-                source: '/organize/:orgId(\\d{1,})/campaigns/:campId(\\d{1,})/calendar/events/:eventId(\\d{1,})',
-                destination: 'https://organize.zetk.in/campaign/action%3A:eventId?org=:orgId',
-                permanent: false,
-            },
-            // all paths with /o redirected to Gen2
-            {
-                source: '/o/:path*',
-                destination: 'https://zetk.in/o/:path*',
-                permanent: false,
-            },
-        ]
-    },
+  async redirects() {
+    return [
+      {
+        source: '/:prevPath*/calendar/events',
+        destination: '/:prevPath*/calendar',
+        permanent: false,
+      },
+      {
+        source: '/:prevPath*/calendar/tasks',
+        destination: '/:prevPath*/calendar',
+        permanent: false,
+      },
+      {
+        source: `/:prevPath*/people/views`,
+        destination: '/:prevPath*/people',
+        permanent: true,
+      },
+      // redirects to Gen2 for MVP August 2021
+      {
+        source: '/organize/:orgId(\\d{1,})',
+        destination: 'https://organize.zetk.in/?org=:orgId',
+        permanent: false,
+      },
+      {
+        source: '/organize/:orgId(\\d{1,})/areas',
+        destination: 'https://organize.zetk.in/maps?org=:orgId',
+        permanent: false,
+      },
+      {
+        source:
+          '/organize/:orgId(\\d{1,})/campaigns/calendar/events/:eventId(\\d{1,})',
+        destination:
+          'https://organize.zetk.in/campaign/action%3A:eventId?org=:orgId',
+        permanent: false,
+      },
+      {
+        source:
+          '/organize/:orgId(\\d{1,})/campaigns/:campId(\\d{1,})/calendar/events/:eventId(\\d{1,})',
+        destination:
+          'https://organize.zetk.in/campaign/action%3A:eventId?org=:orgId',
+        permanent: false,
+      },
+      // all paths with /o redirected to Gen2
+      {
+        source: '/o/:path*',
+        destination: 'https://zetk.in/o/:path*',
+        permanent: false,
+      },
+    ];
+  },
 };
