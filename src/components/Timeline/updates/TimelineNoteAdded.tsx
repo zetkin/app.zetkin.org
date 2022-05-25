72b6ca3e82a1166ce4f7b874fcacb11aca1ed397
djbusstop <eribloodlust@grrlz.net>
2022-05-25 17:08:19 +0100

    replace slate with marked for renedering notes in the timeline

--- src/components/Timeline/updates/TimelineNoteAdded.tsx
+++ src/components/Timeline/updates/TimelineNoteAdded.tsx
@@ -1,12 +1,7 @@
 import { FormattedMessage } from 'react-intl';
+import { marked } from 'marked';
 import { Typography } from '@material-ui/core';
-import { createEditor, Descendant } from 'slate';
-import { Editable, Slate, withReact } from 'slate-react';
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
 
-import { Leaf } from '../TextEditor';
-import { markdownToSlate } from '../TextEditor/helpers';
-import TextElement from '../TextEditor/TextElement';
 import UpdateContainer from './elements/UpdateContainer';
 import ZetkinPersonLink from 'components/ZetkinPersonLink';
 import { ZetkinUpdateJourneyInstanceAddNote } from 'types/updates';
@@ -16,21 +11,6 @@ interface Props {
 }
 
 const TimelineNoteAdded: React.FC<Props> = ({ update }) => {
-  const editor = useMemo(() => withReact(createEditor()), []);
-  const [value, setValue] = useState<Descendant[]>();
-  const renderLeaf = useCallback((props) => <Leaf {...props} />, []);
-  const renderElement = useCallback((props) => <TextElement {...props} />, []);
-
-  useEffect(() => {
-    markdownToSlate(update.details.note.text).then((slateArray) =>
-      setValue(slateArray as Descendant[])
-    );
-  }, [update]);
-
-  if (!value) {
-    return null;
-  }
-
   return (
     <UpdateContainer
       headerContent={
@@ -42,13 +22,11 @@ const TimelineNoteAdded: React.FC<Props> = ({ update }) => {
       update={update}
     >
       <Typography variant="body1">
-        <Slate editor={editor} value={value || []}>
-          <Editable
-            readOnly
-            renderElement={renderElement}
-            renderLeaf={renderLeaf}
-          />
-        </Slate>
+        <div
+          dangerouslySetInnerHTML={{
+            __html: marked(update.details.note.text),
+          }}
+        />
       </Typography>
     </UpdateContainer>
   );
