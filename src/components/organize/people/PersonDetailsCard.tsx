f2f34049aa1805cb531d03b7096d5504e32b5330
djbusstop <eribloodlust@grrlz.net>
2022-05-27 16:03:05 +0100

    PersonDetailsCard refactored to show all desired fields

--- src/components/organize/people/PersonDetailsCard.tsx
+++ src/components/organize/people/PersonDetailsCard.tsx
@@ -1,45 +1,92 @@
-import {
-  Divider,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-} from '@material-ui/core';
-import { LocationCity, Mail, Phone } from '@material-ui/icons';
+import NextLink from 'next/link';
+import { useIntl } from 'react-intl';
+import { Link, ListItem, ListItemText } from '@material-ui/core';
 
 import PersonCard from './PersonCard';
-import { ZetkinPerson } from 'types/zetkin';
+import ZetkinDate from 'components/ZetkinDate';
+import ZetkinList from 'components/ZetkinList';
+import { ZetkinCustomField, ZetkinPerson } from 'types/zetkin';
 
-const PersonDetailsCard: React.FunctionComponent<{ person: ZetkinPerson }> = ({
-  person,
-}) => {
-  const details = [
-    { icon: <Phone />, value: person.phone },
-    { icon: <Mail />, value: person.email },
-    {
-      icon: <LocationCity />,
-      value: [person.street_address, person.city]
-        .filter((item) => !!item)
-        .join(', '),
-    },
-  ].filter((detail) => !!detail.value);
+const nativeFieldsToDisplay = [
+  'first_name',
+  'last_name',
+  'email',
+  'phone',
+  'alt_phone',
+  'street_address',
+  'city',
+  'zip_code',
+  'country',
+  'gender',
+];
+
+const PersonDetailsCard: React.FunctionComponent<{
+  customFields: ZetkinCustomField[];
+  person: ZetkinPerson;
+}> = ({ customFields, person }) => {
+  const intl = useIntl();
+
+  const nativeFields = nativeFieldsToDisplay.map((field) => ({
+    title: intl.formatMessage({
+      id: 'misc.person.fields.' + field,
+    }),
+    value:
+      field === 'gender' && person.gender
+        ? intl.formatMessage({
+            id: 'misc.person.genders.' + (person.gender || 'unknown'),
+          })
+        : person[field],
+  }));
+
+  const customFieldsConfig = customFields
+    .filter((field) => field.type !== 'json')
+    .map((field) => {
+      let value: string | React.ReactNode = person[field.slug];
+      if (value && field.type === 'date') {
+        value = <ZetkinDate datetime={value as string} />;
+      } else if (value && field.type === 'url') {
+        value = (
+          <NextLink href={value as string} passHref>
+            <Link>{value}</Link>
+          </NextLink>
+        );
+      }
+
+      return {
+        title: field.title,
+        value,
+      };
+    });
+
+  const allFields = [...nativeFields, ...customFieldsConfig].sort(
+    // Sort so that items with values are shown first
+    (first, second) => {
+      if (first.value && !second.value) {
+        return -1;
+      }
+      if (!first.value && second.value) {
+        return 1;
+      }
+      return 0;
+    }
+  );
 
   return (
-    <PersonCard
-      onClickEdit={() => null}
-      titleId="pages.people.person.details.title"
-    >
-      <List disablePadding>
-        {details.map((detail, idx) => (
-          <div key={idx}>
-            <ListItem>
-              <ListItemIcon>{detail.icon}</ListItemIcon>
-              <ListItemText primary={detail.value} />
-            </ListItem>
-            <Divider style={{ marginLeft: 72 }} />
-          </div>
+    <PersonCard titleId="pages.people.person.details.title">
+      <ZetkinList
+        initialLength={allFields.filter((field) => !!field.value).length}
+        showMoreStep={allFields.filter((field) => !field.value).length}
+        showMoreText="Show empty..."
+      >
+        {allFields.map((detail, idx) => (
+          <ListItem key={idx} divider>
+            <ListItemText
+              primary={detail.value || '-'}
+              secondary={detail.title}
+            />
+          </ListItem>
         ))}
-      </List>
+      </ZetkinList>
     </PersonCard>
   );
 };
