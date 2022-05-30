83a725492f955f6b5a977c757717c55052d0c2a3
djbusstop <eribloodlust@grrlz.net>
2022-05-30 16:04:02 +0100

    clickable card refactored to always have an action area, there is no case anymore where there is a non-clickable card

--- src/components/views/SuggestedViews/ClickableCard.tsx
+++ src/components/views/SuggestedViews/ClickableCard.tsx
@@ -1,39 +1,21 @@
 import { ReactEventHandler } from 'react';
-import { Card, CardActionArea, makeStyles } from '@material-ui/core';
+import { Box, Card, CardActionArea } from '@material-ui/core';
 
 interface ZetkinCardProps {
   onClick?: ReactEventHandler;
   value?: string | number;
 }
 
-const useStyles = makeStyles((theme) => ({
-  card: ({ hasClickHandler }: { hasClickHandler: boolean }) => ({
-    padding: theme.spacing(hasClickHandler ? 0 : 2),
-  }),
-  cardActionArea: {
-    padding: theme.spacing(2),
-  },
-}));
-
 const ClickableCard: React.FunctionComponent<ZetkinCardProps> = ({
   children,
   onClick,
   value,
 }) => {
-  const classes = useStyles({ hasClickHandler: !!onClick });
-
   return (
-    <Card className={classes.card}>
-      {!onClick && children}
-      {onClick && (
-        <CardActionArea
-          className={classes.cardActionArea}
-          onClick={onClick}
-          value={value}
-        >
-          {children}
-        </CardActionArea>
-      )}
+    <Card>
+      <CardActionArea onClick={onClick} value={value}>
+        <Box p={2}>{children}</Box>
+      </CardActionArea>
     </Card>
   );
 };
