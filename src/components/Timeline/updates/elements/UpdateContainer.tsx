69d5922acb580ae4279b09009a4870b373305f75
djbusstop <eribloodlust@grrlz.net>
2022-06-20 12:32:24 +0100

    `UpdateContainer` takes an action button as a prop

--- src/components/Timeline/updates/elements/UpdateContainer.tsx
+++ src/components/Timeline/updates/elements/UpdateContainer.tsx
@@ -3,12 +3,14 @@ import UpdateHeader from './UpdateHeader';
 import { ZetkinUpdate } from 'types/updates';
 
 interface UpdateContainerProps {
+  actionButton?: React.ReactNode;
   headerContent: React.ReactNode;
   update: ZetkinUpdate;
 }
 
 const UpdateContainer: React.FC<UpdateContainerProps> = ({
   children,
+  actionButton,
   headerContent,
   update,
 }) => {
@@ -19,11 +21,12 @@ const UpdateContainer: React.FC<UpdateContainerProps> = ({
         display: 'grid',
         gap: 10,
         gridAutoRows: 'auto',
-        gridTemplateColumns: 'auto 1fr',
+        gridTemplateColumns: 'auto 1fr auto',
       }}
     >
       <TimelineActor actor={update.actor} size={32} />
       <UpdateHeader timestamp={update.timestamp}>{headerContent}</UpdateHeader>
+      {actionButton || null}
       {children && (
         <>
           <div style={{ gridColumn: '2 / end' }}>{children}</div>
