c119869b02d14b119c9402a5f7bc147294261e39
River <river.honer@bbc.co.uk>
2022-10-14 12:46:06 +0100

    update container can have action buttons

--- src/zui/ZUITimeline/updates/elements/UpdateContainer.tsx
+++ src/zui/ZUITimeline/updates/elements/UpdateContainer.tsx
@@ -3,12 +3,14 @@ import UpdateHeader from './UpdateHeader';
 import { ZetkinUpdate } from 'zui/ZUITimeline/types';
 
 interface UpdateContainerProps {
+  actionButton?: React.ReactNode;
   children?: React.ReactNode;
   headerContent: React.ReactNode;
   update: ZetkinUpdate;
 }
 
 const UpdateContainer: React.FC<UpdateContainerProps> = ({
+  actionButton,
   children,
   headerContent,
   update,
@@ -20,11 +22,12 @@ const UpdateContainer: React.FC<UpdateContainerProps> = ({
         display: 'grid',
         gap: 10,
         gridAutoRows: 'auto',
-        gridTemplateColumns: 'auto 1fr',
+        gridTemplateColumns: 'auto 1fr auto',
       }}
     >
       <TimelineActor actor={update.actor} size={32} />
       <UpdateHeader timestamp={update.timestamp}>{headerContent}</UpdateHeader>
+      {actionButton || null}
       {children && (
         <>
           <div style={{ gridColumn: '2 / end', overflowWrap: 'anywhere' }}>
