987bf0d43e6f0406252e1c5b809ce186f1c78642
ziggi <ziggi.willpower@gmail.com>
2022-11-16 18:43:38 +0100

    Display an "-" insted of number if targeting is not done.

--- src/features/callAssignments/components/StatusCardItem.tsx
+++ src/features/callAssignments/components/StatusCardItem.tsx
@@ -2,7 +2,7 @@ import { Box, ListItem, Typography } from '@material-ui/core';
 
 interface StatusCardItemProps {
   title: string;
-  value: number;
+  value: number | undefined;
 }
 
 const StatusCardItem = ({ title, value }: StatusCardItemProps) => {
@@ -13,7 +13,9 @@ const StatusCardItem = ({ title, value }: StatusCardItemProps) => {
           <Typography color="secondary" variant="h5">
             {title}
           </Typography>
-          <Typography variant="h3">{value}</Typography>
+          <Typography variant="h3">
+            {value != undefined ? value : '-'}
+          </Typography>
         </Box>
       </Box>
     </ListItem>
