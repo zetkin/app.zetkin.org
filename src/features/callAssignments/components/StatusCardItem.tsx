ef04f1f71a905106a338479390ad8a7acf6b4207
ziggi <ziggi.willpower@gmail.com>
2022-11-17 12:03:30 +0100

    Make card items take optional "cta" element.

--- src/features/callAssignments/components/StatusCardItem.tsx
+++ src/features/callAssignments/components/StatusCardItem.tsx
@@ -1,14 +1,15 @@
 import { Box, ListItem, Typography } from '@material-ui/core';
 
 interface StatusCardItemProps {
+  action?: JSX.Element;
   title: string;
   value: number | undefined;
 }
 
-const StatusCardItem = ({ title, value }: StatusCardItemProps) => {
+const StatusCardItem = ({ action, title, value }: StatusCardItemProps) => {
   return (
     <ListItem>
-      <Box display="flex">
+      <Box display="flex" justifyContent="space-between" width="100%">
         <Box display="flex" flexDirection="column">
           <Typography color="secondary" variant="h5">
             {title}
@@ -17,6 +18,7 @@ const StatusCardItem = ({ title, value }: StatusCardItemProps) => {
             {value != undefined ? value : '-'}
           </Typography>
         </Box>
+        {action && action}
       </Box>
     </ListItem>
   );
