de9a2dc803e740d5d8a85e95a1d9a99790784ca9
Richard Olsson <r@richardolsson.se>
2022-12-09 19:54:19 +0100

    Add label to ZUIDateRangePicker that toggles the picker when clicked

--- src/zui/ZUIDateRangePicker/ZUIDateRangePicker/index.tsx
+++ src/zui/ZUIDateRangePicker/ZUIDateRangePicker/index.tsx
@@ -1,26 +1,44 @@
 import { Clear } from '@mui/icons-material';
 import {
   Box,
+  ClickAwayListener,
   IconButton,
   InputAdornment,
   Paper,
   Popper,
   TextField,
+  Typography,
 } from '@mui/material';
 import { DateRange, StaticDateRangePicker } from '@mui/x-date-pickers-pro';
 import dayjs, { Dayjs } from 'dayjs';
-import React, { FC, useCallback, useEffect, useState } from 'react';
+import { IntlShape, useIntl } from 'react-intl';
+import React, { FC, MouseEvent, useEffect, useState } from 'react';
+
+const rangeStr = (intl: IntlShape, values: DateRange<Dayjs>): string => {
+  const [start, end] = values;
+
+  if (start && end) {
+    return intl.formatMessage(
+      { id: 'misc.dateRange.finite' },
+      { end: end.toDate(), start: start.toDate() }
+    );
+  } else if (start) {
+    return intl.formatMessage(
+      { id: 'misc.dateRange.indefinite' },
+      { start: start.toDate() }
+    );
+  } else {
+    return intl.formatMessage({ id: 'misc.dateRange.draft' });
+  }
+};
 
 const ZUIDateRangePicker: FC = () => {
-  const [anchorEl, setAnchorEl] = useState<HTMLDivElement | null>(null);
+  const intl = useIntl();
+  const [anchorEl, setAnchorEl] = useState<HTMLSpanElement | null>(null);
   const [value, setValue] = useState<DateRange<Dayjs>>([
     dayjs('1857-07-05'),
     dayjs('1933-06-20'),
   ]);
-
-  const callback = useCallback((elem: HTMLDivElement) => {
-    setAnchorEl(elem);
-  }, []);
 
   // Calculate duration to use when shifting the entire range,
   // which happens when setting a start date that precedes the
@@ -30,53 +48,67 @@ const ZUIDateRangePicker: FC = () => {
 
   return (
     <>
-      <span ref={callback}>Label</span>
-      <Popper anchorEl={anchorEl} open={true}>
-        <Paper elevation={2}>
-          <Box
-            alignItems="stretch"
-            display="flex"
-            flexDirection="column"
-            paddingTop={2}
-          >
-            <Box marginX={2} marginY={1}>
-              <DateTextField
-                label="Start"
-                onChange={(date) => {
-                  let endDate = value[1];
-                  if (date && endDate?.isBefore(date)) {
-                    endDate = date.add(duration, 'day');
-                  }
-                  setValue([date, endDate]);
-                }}
-                value={value[0]}
+      <Typography
+        component="span"
+        onClick={(ev: MouseEvent<HTMLSpanElement>) => {
+          ev.stopPropagation();
+          setAnchorEl(ev.currentTarget);
+        }}
+      >
+        {rangeStr(intl, value)}
+      </Typography>
+      <ClickAwayListener
+        onClickAway={() => {
+          setAnchorEl(null);
+        }}
+      >
+        <Popper anchorEl={anchorEl} open={!!anchorEl}>
+          <Paper elevation={2}>
+            <Box
+              alignItems="stretch"
+              display="flex"
+              flexDirection="column"
+              paddingTop={2}
+            >
+              <Box marginX={2} marginY={1}>
+                <DateTextField
+                  label="Start"
+                  onChange={(date) => {
+                    let endDate = value[1];
+                    if (date && endDate?.isBefore(date)) {
+                      endDate = date.add(duration, 'day');
+                    }
+                    setValue([date, endDate]);
+                  }}
+                  value={value[0]}
+                />
+              </Box>
+              <Box marginX={2} marginY={1}>
+                <DateTextField
+                  label="End"
+                  onChange={(date) => {
+                    let startDate = value[0];
+                    if (date && startDate?.isAfter(date)) {
+                      startDate = date?.subtract(duration, 'day');
+                    }
+                    setValue([startDate, date]);
+                  }}
+                  value={value[1]}
+                />
+              </Box>
+              <StaticDateRangePicker
+                calendars={1}
+                displayStaticWrapperAs="desktop"
+                label="date range"
+                minDate={dayjs('1800-01-01')}
+                onChange={(newValue: DateRange<Dayjs>) => setValue(newValue)}
+                renderInput={() => <></>}
+                value={value}
               />
             </Box>
-            <Box marginX={2} marginY={1}>
-              <DateTextField
-                label="End"
-                onChange={(date) => {
-                  let startDate = value[0];
-                  if (date && startDate?.isAfter(date)) {
-                    startDate = date?.subtract(duration, 'day');
-                  }
-                  setValue([startDate, date]);
-                }}
-                value={value[1]}
-              />
-            </Box>
-            <StaticDateRangePicker
-              calendars={1}
-              displayStaticWrapperAs="desktop"
-              label="date range"
-              minDate={dayjs('1800-01-01')}
-              onChange={(newValue: DateRange<Dayjs>) => setValue(newValue)}
-              renderInput={() => <></>}
-              value={value}
-            />
-          </Box>
-        </Paper>
-      </Popper>
+          </Paper>
+        </Popper>
+      </ClickAwayListener>
     </>
   );
 };
