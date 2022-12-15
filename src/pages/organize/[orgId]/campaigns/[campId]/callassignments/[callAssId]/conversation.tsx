d7b8443168ae3b5c5b48618ca35e1861308c02ec
ziggi <ziggi.willpower@gmail.com>
2022-12-15 16:51:53 +0100

    Add basic text editor with submit button.

--- src/pages/organize/[orgId]/campaigns/[campId]/callassignments/[callAssId]/conversation.tsx
+++ src/pages/organize/[orgId]/campaigns/[campId]/callassignments/[callAssId]/conversation.tsx
@@ -1,10 +1,15 @@
 import { GetServerSideProps } from 'next';
+import { FormattedMessage as Msg } from 'react-intl';
+import { useState } from 'react';
+import { Box, Button, Paper, Typography } from '@mui/material';
 
 import CallAssignmentLayout from 'features/callAssignments/layout/CallAssignmentLayout';
 import CallAssignmentModel from 'features/callAssignments/models/CallAssignmentModel';
 import { PageWithLayout } from 'utils/types';
+
 import { scaffold } from 'utils/next';
 import useModel from 'core/useModel';
+import ZUITextEditor from 'zui/ZUITextEditor';
 
 export const getServerSideProps: GetServerSideProps = scaffold(
   async (ctx) => {
@@ -42,10 +47,42 @@ const ConversationPage: PageWithLayout<ConversationPageProps> = ({
     (store) =>
       new CallAssignmentModel(store, parseInt(orgId), parseInt(assignmentId))
   );
-
   const future = model.getData();
 
-  return <>{future.data?.title}</>;
+  const [instructions, setInstructions] = useState(
+    future.data?.instructions || ''
+  );
+
+  const onChange = (markdown: string) => {
+    setInstructions(markdown);
+  };
+
+  return (
+    <Paper>
+      <Box padding={2}>
+        <Typography variant="h4">Caller instructions</Typography>
+        <form
+          onSubmit={(evt) => {
+            evt.preventDefault();
+            model.setCallerInstructions(instructions);
+          }}
+        >
+          <Box marginBottom={2} marginTop={4}>
+            <ZUITextEditor
+              initialValue={future.data?.instructions}
+              onChange={onChange}
+              placeholder={'Add instructions for your callers'}
+            />
+          </Box>
+          <Box display="flex" justifyContent="flex-end">
+            <Button color="primary" type="submit" variant="contained">
+              <Msg id="misc.formDialog.submit" />
+            </Button>
+          </Box>
+        </form>
+      </Box>
+    </Paper>
+  );
 };
 
 ConversationPage.getLayout = function getLayout(page, props) {
