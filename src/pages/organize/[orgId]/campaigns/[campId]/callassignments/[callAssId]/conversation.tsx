f46201982456fc750f5a192bf154449612da01a6
ziggi <ziggi.willpower@gmail.com>
2023-01-03 14:46:41 +0100

    Use confirm dialog when reverting.

--- src/pages/organize/[orgId]/campaigns/[campId]/callassignments/[callAssId]/conversation.tsx
+++ src/pages/organize/[orgId]/campaigns/[campId]/callassignments/[callAssId]/conversation.tsx
@@ -1,14 +1,15 @@
 import { GetServerSideProps } from 'next';
 import { Link } from '@material-ui/core';
-import { useState } from 'react';
 import { Box, Button, Paper, Typography } from '@mui/material';
 import { FormattedMessage as Msg, useIntl } from 'react-intl';
+import { useContext, useState } from 'react';
 
 import CallAssignmentLayout from 'features/callAssignments/layout/CallAssignmentLayout';
 import CallerInstructionsModel from 'features/callAssignments/models/CallerInstructionsModel';
 import { PageWithLayout } from 'utils/types';
 import { scaffold } from 'utils/next';
 import useModel from 'core/useModel';
+import { ZUIConfirmDialogContext } from 'zui/ZUIConfirmDialogProvider';
 import ZUITextEditor from 'zui/ZUITextEditor';
 
 export const getServerSideProps: GetServerSideProps = scaffold(
@@ -44,6 +45,8 @@ const ConversationPage: PageWithLayout<ConversationPageProps> = ({
   orgId,
 }) => {
   const intl = useIntl();
+  const { showConfirmDialog } = useContext(ZUIConfirmDialogContext);
+
   const model = useModel(
     (store) =>
       new CallerInstructionsModel(
@@ -105,9 +108,16 @@ const ConversationPage: PageWithLayout<ConversationPageProps> = ({
                   <Link
                     component={Typography}
                     onClick={() => {
-                      model.revert();
-                      //Force Slate to re-mount
-                      setKey((current) => current + 1);
+                      showConfirmDialog({
+                        onSubmit: () => {
+                          model.revert();
+                          //Force Slate to re-mount
+                          setKey((current) => current + 1);
+                        },
+                        warningText: intl.formatMessage({
+                          id: 'pages.organizeCallAssignment.conversation.confirm',
+                        }),
+                      });
                     }}
                     sx={{
                       cursor: 'pointer',
