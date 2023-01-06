331b5e2fd9a0ae489273fc94a77eff8e543d1c9c
River <river.honer@bbc.co.uk>
2023-01-06 15:39:41 +0000

    test for redirect

--- integrationTesting/tests/organize/views/redirect.spec.ts
+++ integrationTesting/tests/organize/views/redirect.spec.ts
@@ -1,7 +1,11 @@
 import { expect } from '@playwright/test';
 import test from '../../../fixtures/next';
 
+import AllMembers from '../../../mockData/orgs/KPD/people/views/AllMembers';
+import AllMembersColumns from '../../../mockData/orgs/KPD/people/views/AllMembers/columns';
+import AllMembersRows from '../../../mockData/orgs/KPD/people/views/AllMembers/rows';
 import KPD from '../../../mockData/orgs/KPD';
+import NewView from '../../../mockData/orgs/KPD/people/views/NewView';
 
 test('Navigating to /organize/:orgId/people/views redirects to views page', async ({
   login,
@@ -14,6 +18,34 @@ test('Navigating to /organize/:orgId/people/views redirects to views page', asyn
   moxy.setZetkinApiMock('/orgs/1/people/views', 'get', []);
 
   await page.goto(appUri + '/organize/1/people/views');
+
+  // Expect to have been redirected
+  expect(page.url()).not.toEqual(appUri + '/organize/1/people/views');
+  expect(page.url()).toEqual(appUri + '/organize/1/people');
+
+  moxy.teardown();
+});
+
+test('Clicking the "Views" breadcrumb redirects to views page', async ({
+  login,
+  page,
+  appUri,
+  moxy,
+}) => {
+  login();
+  moxy.setZetkinApiMock('/orgs/1', 'get', KPD);
+  moxy.setZetkinApiMock('/orgs/1/people/views', 'get', [AllMembers, NewView]);
+  moxy.setZetkinApiMock('/orgs/1/people/views/1', 'get', AllMembers);
+  moxy.setZetkinApiMock('/orgs/1/people/views/1/rows', 'get', AllMembersRows);
+  moxy.setZetkinApiMock(
+    '/orgs/1/people/views/1/columns',
+    'get',
+    AllMembersColumns
+  );
+
+  await page.goto(appUri + '/organize/1/people/views/1');
+
+  await page.click('Views');
 
   // Expect to have been redirected
   expect(page.url()).not.toEqual(appUri + '/organize/1/people/views');
