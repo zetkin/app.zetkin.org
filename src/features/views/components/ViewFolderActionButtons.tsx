3c95d247b62c700f8942e4bdbcf4dda42711207b
Richard Olsson <r@richardolsson.se>
2023-01-15 10:40:32 +0100

    Hook up action button to create new folder

--- src/features/views/components/ViewFolderActionButtons.tsx
+++ src/features/views/components/ViewFolderActionButtons.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
-import { FormattedMessage } from 'react-intl';
 import { Box, Button } from '@mui/material';
 import { FolderOutlined, InsertDriveFileOutlined } from '@mui/icons-material';
+import { FormattedMessage, useIntl } from 'react-intl';
 
 import ViewBrowserModel from '../models/ViewBrowserModel';
 
@@ -14,9 +14,19 @@ const ViewFolderActionButtons: FC<ViewFolderActionButtonsProps> = ({
   folderId,
   model,
 }) => {
+  const intl = useIntl();
   return (
     <Box display="flex" gap={1}>
-      <Button endIcon={<FolderOutlined />} variant="outlined">
+      <Button
+        endIcon={<FolderOutlined />}
+        onClick={() => {
+          model.createFolder(
+            intl.formatMessage({ id: 'misc.views.newFolderTitle' }),
+            folderId || undefined
+          );
+        }}
+        variant="outlined"
+      >
         <FormattedMessage id="pages.people.views.actions.createFolder" />
       </Button>
       <Button
