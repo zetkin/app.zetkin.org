f0fe0cfd351aaf76a4bb11d984ee71fe72420b77
Richard Olsson <r@richardolsson.se>
2023-01-18 06:22:34 +0100

    Add pretty animations to indicate drag/drop state in view browser

--- src/features/views/components/ViewBrowser/BrowserDragLayer.tsx
+++ src/features/views/components/ViewBrowser/BrowserDragLayer.tsx
@@ -1,12 +1,20 @@
 import { FC } from 'react';
 import { makeStyles } from '@mui/styles';
-import { Box, Paper } from '@mui/material';
-import { useDragLayer, XYCoord } from 'react-dnd';
+import { Box, Paper, Theme } from '@mui/material';
+import { useDragDropManager, useDragLayer, XYCoord } from 'react-dnd';
 
 import BrowserItemIcon from './BrowserItemIcon';
 import { ViewBrowserItem } from 'features/views/models/ViewBrowserModel';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles<Theme, CollectedProps>((theme) => ({
+  '@keyframes dragPreview-in': {
+    '0%': {
+      transform: 'scale(0.3) rotate(0)',
+    },
+    '100%': {
+      transform: 'scale(1.1) rotate(5deg)',
+    },
+  },
   dragLayer: {
     left: 0,
     pointerEvens: 'none',
@@ -15,30 +23,49 @@ const useStyles = makeStyles({
     zIndex: 9999,
   },
   dragPreview: {
+    animation: `$dragPreview-in 400ms ${theme.transitions.easing.easeInOut}`,
     backgroundColor: 'white',
+    boxShadow: (props) =>
+      props.canDrop
+        ? '0 6px 6px rgba(0,0,0,0.2)'
+        : '0 14px 14px rgba(0,0,0,0.2)',
     padding: 10,
     pointerEvents: 'none',
     position: 'absolute',
+    transform: (props) =>
+      props.canDrop ? 'scale(1) rotate(0)' : 'scale(1.1) rotate(5deg)',
+    transition: 'transform 200ms, box-shadow 200ms',
     whiteSpace: 'nowrap',
   },
-});
+}));
 
 interface CollectedProps {
+  canDrop: boolean;
   currentOffset: XYCoord | null;
   isDragging: boolean;
   item: ViewBrowserItem;
 }
 
 const BrowserDragLayer: FC = () => {
-  const styles = useStyles();
+  const mgr = useDragDropManager();
+  const dragProps = useDragLayer<CollectedProps, ViewBrowserItem>((monitor) => {
+    // This is a bit of a hack, because react-dnd does not have a canDrop()
+    // method on the monitor passed to useDragLayer(). Instead, we go into
+    // the internals of react-dnd and check that there are target IDs under
+    // the cursor, and that we can drop onto any of them.
+    const mgrMonitor = mgr.getMonitor();
+    const targetIds = mgrMonitor.getTargetIds();
+    const canDrop = targetIds.some((id) => mgrMonitor.canDropOnTarget(id));
 
-  const dragProps = useDragLayer<CollectedProps, ViewBrowserItem>(
-    (monitor) => ({
+    return {
+      canDrop: canDrop,
       currentOffset: monitor.getSourceClientOffset(),
       isDragging: monitor.isDragging(),
       item: monitor.getItem(),
-    })
-  );
+    };
+  });
+
+  const styles = useStyles(dragProps);
 
   const { isDragging, item, currentOffset } = dragProps;
 
@@ -47,7 +74,6 @@ const BrowserDragLayer: FC = () => {
       <Box className={styles.dragLayer}>
         <Paper
           className={styles.dragPreview}
-          elevation={5}
           style={{
             left: currentOffset.x,
             top: currentOffset.y,
