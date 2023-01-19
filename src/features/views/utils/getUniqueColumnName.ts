e5c67a949d2a92f23667889100e027dae792a5ad
rebecarubio <rebecarubiolopez@gmail.com>
2023-01-19 22:36:17 +0100

    refactor and clean function to increment column name

--- src/features/views/utils/getUniqueColumnName.ts
+++ src/features/views/utils/getUniqueColumnName.ts
@@ -5,31 +5,15 @@ export default function getUniqueColumnName(
   columns: ZetkinViewColumn[]
 ) {
   let title = defaultTitle;
-
+  let count = 2;
   let foundNewTitle = false;
   while (!foundNewTitle) {
-    let count = 2;
     const titleIsAlreadyIn = columns.some((column) => column.title === title);
-    //const firstColumnTitle = isNaN(+title.slice(title.length - 1));
-    // make title and 1 variables
-    // make if statements variables
-    //write test
-    //create utils folder > create modul getUniqueColumnName +
     if (titleIsAlreadyIn) {
-      /*        if (firstColumnTitle) {
-        title += ' ' + 2;
-        console.log('if');
-      } else {
-        const oldNumericSuffix = title.split(defaultTitle);
-        title = defaultTitle + (+oldNumericSuffix[0] + 1).toString();
-        console.log('else');
-      } */
-      const oldNumericSuffix = title.split(defaultTitle);
-      title = defaultTitle + ' ' + (+oldNumericSuffix[0] + count).toString();
+      title = defaultTitle + ' ' + count.toString();
     } else {
       foundNewTitle = true;
     }
-
     count++;
   }
   return title;
