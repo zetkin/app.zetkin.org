063805b5d0cd2a490dab961b450296f4fe054e0e
Richard Olsson <r@richardolsson.se>
2023-01-19 15:05:23 +0100

    Add UI for adding to the view access list

--- src/features/views/components/ShareViewDialog/ShareViewDialogShareTab/index.tsx
+++ src/features/views/components/ShareViewDialog/ShareViewDialogShareTab/index.tsx
@@ -1,7 +1,10 @@
 import { Box } from '@mui/material';
-import { FC } from 'react';
+import { useIntl } from 'react-intl';
+import { FC, useRef } from 'react';
 
+import { MUIOnlyPersonSelect } from 'zui/ZUIPersonSelect';
 import ViewSharingModel from 'features/views/models/ViewSharingModel';
+import { ZetkinPerson } from 'utils/types/zetkin';
 import ZUIAccessList from 'zui/ZUIAccessList';
 import ZUIFuture from 'zui/ZUIFuture';
 
@@ -12,11 +15,45 @@ interface ShareViewDialogShareTabProps {
 const ShareViewDialogShareTab: FC<ShareViewDialogShareTabProps> = ({
   model,
 }) => {
+  const intl = useIntl();
+  const selectInputRef = useRef<HTMLInputElement>();
+
   const accessFuture = model.getAccessList();
+
   return (
     <Box>
       <ZUIFuture future={accessFuture}>
-        {(data) => <ZUIAccessList list={data} orgId={model.orgId} />}
+        {(accessList) => (
+          <>
+            <ZUIAccessList list={accessList} orgId={model.orgId} />
+            <MUIOnlyPersonSelect
+              getOptionDisabled={(person) =>
+                accessList.some((item) => item.person.id == person.id)
+              }
+              getOptionExtraLabel={(person) => {
+                const accessItem = accessList.find(
+                  (item) => item.person.id == person.id
+                );
+                if (accessItem) {
+                  return intl.formatMessage({
+                    id: `zui.accessList.levels.${accessItem.level}`,
+                  });
+                }
+                return '';
+              }}
+              inputRef={selectInputRef}
+              onChange={function (person: ZetkinPerson): void {
+                model.grantAccess(person.id, 'readonly');
+
+                // Blur and re-focus input to reset, so that user can type again to
+                // add another person, without taking their hands off the keyboard.
+                selectInputRef?.current?.blur();
+                selectInputRef?.current?.focus();
+              }}
+              selectedPerson={null}
+            />
+          </>
+        )}
       </ZUIFuture>
     </Box>
   );
