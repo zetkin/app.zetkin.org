31558ac85d0066add54ced012acd02955296b76a
Richard Olsson <r@richardolsson.se>
2023-01-20 06:27:28 +0100

    Add support for different size ZUIAvatars

--- src/zui/ZUIAvatar/index.tsx
+++ src/zui/ZUIAvatar/index.tsx
@@ -4,10 +4,20 @@ import { FC } from 'react';
 interface ZUIAvatarProps {
   orgId: number;
   personId: number;
+  size?: 'sm' | 'md';
 }
+const SIZES = {
+  md: 40,
+  sm: 30,
+};
 
-const ZUIAvatar: FC<ZUIAvatarProps> = ({ orgId, personId }) => {
-  return <Avatar src={`/api/orgs/${orgId}/people/${personId}/avatar`} />;
+const ZUIAvatar: FC<ZUIAvatarProps> = ({ orgId, personId, size = 'md' }) => {
+  return (
+    <Avatar
+      src={`/api/orgs/${orgId}/people/${personId}/avatar`}
+      style={{ height: SIZES[size], width: SIZES[size] }}
+    />
+  );
 };
 
 export default ZUIAvatar;
