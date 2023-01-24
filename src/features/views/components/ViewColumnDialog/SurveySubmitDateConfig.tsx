d1b3f152fa4542fe53163bec407684406d9a649c
ziggi <ziggi.willpower@gmail.com>
2023-01-24 18:53:35 +0100

    Render message instead of select if org does not have any surveys.

--- src/features/views/components/ViewColumnDialog/SurveySubmitDateConfig.tsx
+++ src/features/views/components/ViewColumnDialog/SurveySubmitDateConfig.tsx
@@ -1,7 +1,13 @@
-import { useIntl } from 'react-intl';
 import { useQuery } from 'react-query';
 import { useRouter } from 'next/router';
-import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  Typography,
+} from '@mui/material';
+import { FormattedMessage as Msg, useIntl } from 'react-intl';
 
 import getSurveys from 'features/smartSearch/fetching/getSurveys';
 import { COLUMN_TYPE, SelectedViewColumn } from '../types';
@@ -24,37 +30,46 @@ const SurveySubmitDateConfig = ({
   const surveys = surveysQuery.data;
 
   return (
-    <FormControl sx={{ width: 300 }}>
-      <InputLabel>
-        {intl.formatMessage({
-          id: 'misc.views.columnDialog.editor.fieldLabels.survey',
-        })}
-      </InputLabel>
-      <Select
-        onChange={(evt) => {
-          if (!evt.target.value) {
-            return;
-          }
-          const surveyId = evt.target.value as number;
-          onOutputConfigured([
-            {
-              config: {
-                survey_id: surveyId,
-              },
-              title:
-                surveys?.find((survey) => survey.id === surveyId)?.title || '',
-              type: COLUMN_TYPE.SURVEY_SUBMITTED,
-            },
-          ]);
-        }}
-      >
-        {surveys?.map((survey) => (
-          <MenuItem key={survey.id} value={survey.id}>
-            {survey.title}
-          </MenuItem>
-        ))}
-      </Select>
-    </FormControl>
+    <>
+      {!surveys || surveys.length > 0 ? (
+        <FormControl sx={{ width: 300 }}>
+          <InputLabel>
+            {intl.formatMessage({
+              id: 'misc.views.columnDialog.editor.fieldLabels.survey',
+            })}
+          </InputLabel>
+          <Select
+            onChange={(evt) => {
+              if (!evt.target.value) {
+                return;
+              }
+              const surveyId = evt.target.value as number;
+              onOutputConfigured([
+                {
+                  config: {
+                    survey_id: surveyId,
+                  },
+                  title:
+                    surveys?.find((survey) => survey.id === surveyId)?.title ||
+                    '',
+                  type: COLUMN_TYPE.SURVEY_SUBMITTED,
+                },
+              ]);
+            }}
+          >
+            {surveys?.map((survey) => (
+              <MenuItem key={survey.id} value={survey.id}>
+                {survey.title}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      ) : (
+        <Typography>
+          <Msg id="misc.views.columnDialog.choices.surveySubmitDate.noSurveys" />
+        </Typography>
+      )}
+    </>
   );
 };
 
