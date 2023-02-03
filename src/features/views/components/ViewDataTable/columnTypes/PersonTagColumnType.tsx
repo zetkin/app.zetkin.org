b8fabc510e0221c64b55b336d500e7887d637909
Richard Olsson <r@richardolsson.se>
2023-02-03 12:47:37 +0100

    Add UI in person_tag view cell to toggle tags

--- src/features/views/components/ViewDataTable/columnTypes/PersonTagColumnType.tsx
+++ src/features/views/components/ViewDataTable/columnTypes/PersonTagColumnType.tsx
@@ -1,14 +1,16 @@
+import { Box } from '@mui/material';
 import { FC } from 'react';
-import { GridColDef } from '@mui/x-data-grid-pro';
+import { makeStyles } from '@mui/styles';
 import { useRouter } from 'next/router';
+import { GridColDef, GridRenderCellParams } from '@mui/x-data-grid-pro';
 
 import { IColumnType } from '.';
-import { PersonTagViewColumn } from '../../types';
 import TagChip from 'features/tags/components/TagManager/components/TagChip';
 import TagModel from 'features/tags/models/TagModel';
 import useModel from 'core/useModel';
 import { ZetkinTag } from 'utils/types/zetkin';
 import ZUIFuture from 'zui/ZUIFuture';
+import { PersonTagViewColumn, ZetkinViewRow } from '../../types';
 
 export default class PersonTagColumnType implements IColumnType {
   cellToString(cell: ZetkinTag): string {
@@ -17,31 +19,75 @@ export default class PersonTagColumnType implements IColumnType {
 
   getColDef(column: PersonTagViewColumn): Omit<GridColDef, 'field'> {
     return {
-      renderCell: (params) => {
-        return <Cell cell={params.value} tagId={column.config.tag_id} />;
+      align: 'center',
+      headerAlign: 'center',
+      renderCell: (params: GridRenderCellParams<ZetkinTag, ZetkinViewRow>) => {
+        return (
+          <Cell
+            cell={params.value}
+            personId={params.row.id}
+            tagId={column.config.tag_id}
+          />
+        );
       },
     };
   }
 }
 
-const Cell: FC<{ cell: ZetkinTag | null; tagId: number }> = ({
-  cell,
-  tagId,
-}) => {
+const useStyles = makeStyles(() => ({
+  ghost: {
+    pointerEvents: 'none',
+  },
+  ghostContainer: {
+    '&:hover': {
+      opacity: 0.3,
+    },
+    cursor: 'pointer',
+    opacity: 0,
+    transition: 'opacity 0.1s',
+  },
+}));
+
+const Cell: FC<{
+  cell: ZetkinTag | undefined;
+  personId: number;
+  tagId: number;
+}> = ({ cell, personId, tagId }) => {
   // TODO: Find a way to share a model between cells in a column
   const query = useRouter().query;
   const orgId = parseInt(query.orgId as string);
   const model = useModel((env) => new TagModel(env, orgId, tagId));
+  const styles = useStyles();
 
-  if (!cell) {
-    return null;
-  } else if (cell.value) {
+  if (cell?.value) {
     return <>{cell.value}</>;
   } else {
     return (
       <ZUIFuture future={model.getTag()}>
         {(tag) => {
-          return <TagChip tag={tag} />;
+          if (cell) {
+            return (
+              <TagChip
+                onDelete={() => {
+                  model.removeFromPerson(personId);
+                }}
+                tag={tag}
+              />
+            );
+          } else {
+            return (
+              <Box
+                className={styles.ghostContainer}
+                onClick={() => {
+                  model.assignToPerson(personId);
+                }}
+              >
+                <Box className={styles.ghost}>
+                  <TagChip tag={tag} />
+                </Box>
+              </Box>
+            );
+          }
         }}
       </ZUIFuture>
     );
