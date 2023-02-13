cb9d131152c225a0835c9d3d0defacd31056bd4a
ziggi <ziggi.willpower@gmail.com>
2023-02-13 12:15:29 +0100

    Add action buttons to survey layout.

--- src/features/surveys/models/SurveyDataModel.ts
+++ src/features/surveys/models/SurveyDataModel.ts
@@ -4,6 +4,14 @@ import { ModelBase } from 'core/models';
 import SurveysRepo from '../repos/SurveysRepo';
 import { ZetkinSurvey } from 'utils/types/zetkin';
 
+export enum SurveyState {
+  UNPUBLISHED = 'unpublished',
+  DRAFT = 'draft',
+  PUBLISHED = 'published',
+  SCHEDULED = 'scheduled',
+  UNKNOWN = 'unknown',
+}
+
 export default class SurveyDataModel extends ModelBase {
   private _orgId: number;
   private _repo: SurveysRepo;
@@ -22,5 +30,33 @@ export default class SurveyDataModel extends ModelBase {
 
   setTitle(title: string) {
     this._repo.updateSurvey(this._orgId, this._surveyId, { title });
+  }
+
+  get state(): SurveyState {
+    const { data } = this.getData();
+    if (!data) {
+      return SurveyState.UNKNOWN;
+    }
+
+    if (data.published) {
+      const publishDate = new Date(data.published);
+      const now = new Date();
+
+      if (publishDate > now) {
+        return SurveyState.SCHEDULED;
+      } else {
+        if (data.expires) {
+          const expiryDate = new Date(data.expires);
+
+          if (expiryDate < now) {
+            return SurveyState.UNPUBLISHED;
+          }
+        }
+
+        return SurveyState.PUBLISHED;
+      }
+    } else {
+      return SurveyState.DRAFT;
+    }
   }
 }
