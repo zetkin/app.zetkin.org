ad13964c4a334d1ad94dd7ec60eb4399bd30eb01
Richard Olsson <r@richardolsson.se>
2023-02-14 14:31:53 +0100

    Migrate deleteFolder to new RPC framework

--- src/core/rpc/index.ts
+++ src/core/rpc/index.ts
@@ -1,7 +1,10 @@
 import { RPCRouter } from './router';
 
+import { deleteFolderRouteDef } from 'features/views/rpc/deleteFolder';
+
 export function createRPCRouter() {
   const rpcRouter = new RPCRouter();
+  rpcRouter.register(deleteFolderRouteDef);
 
   return rpcRouter;
 }
--- src/features/views/repos/ViewsRepo.ts
+++ src/features/views/repos/ViewsRepo.ts
@@ -1,5 +1,5 @@
 import { CreateNewViewReqBody } from 'pages/api/views/createNew';
-import { DeleteFolderReport } from 'pages/api/views/deleteFolder';
+import deleteFolder from '../rpc/deleteFolder';
 import Environment from 'core/env/Environment';
 import IApiClient from 'core/api/client/IApiClient';
 import shouldLoad from 'core/caching/shouldLoad';
@@ -86,10 +86,7 @@ export default class ViewsRepo {
   }
 
   async deleteFolder(orgId: number, folderId: number): Promise<void> {
-    const report = await this._apiClient.post<DeleteFolderReport>(
-      `/api/views/deleteFolder?orgId=${orgId}&folderId=${folderId}`,
-      {}
-    );
+    const report = await this._apiClient.rpc(deleteFolder, { folderId, orgId });
     this._store.dispatch(folderDeleted(report));
   }
 
