95668514f2edd5a6e76fa713c274eab0d41738cb
rebecarubio <rebecarubiolopez@gmail.com>
2023-02-17 12:03:52 +0100

    add  Input fields for question and description

--- src/features/surveys/components/SurveyEditor/blocks/OpenQuestionBlock.tsx
+++ src/features/surveys/components/SurveyEditor/blocks/OpenQuestionBlock.tsx
@@ -1,5 +1,6 @@
-import { Box } from '@mui/material';
 import { FC } from 'react';
+import { TextField } from '@mui/material';
+import { useIntl } from 'react-intl';
 import { ZetkinTextQuestion } from 'utils/types/zetkin';
 
 interface OpenQuestionBlockProps {
@@ -7,7 +8,27 @@ interface OpenQuestionBlockProps {
 }
 
 const OpenQuestionBlock: FC<OpenQuestionBlockProps> = ({ question }) => {
-  return <Box>{JSON.stringify(question)}</Box>;
+  const intl = useIntl();
+  return (
+    <>
+      <TextField
+        defaultValue={question.question}
+        fullWidth
+        label={intl.formatMessage({
+          id: 'pages.organizeSurvey.openQuestion.title',
+        })}
+        margin="normal"
+      />
+      <TextField
+        defaultValue={question.description}
+        fullWidth
+        label={intl.formatMessage({
+          id: 'pages.organizeSurvey.openQuestion.description',
+        })}
+        margin="normal"
+      />
+    </>
+  );
 };
 
 export default OpenQuestionBlock;
