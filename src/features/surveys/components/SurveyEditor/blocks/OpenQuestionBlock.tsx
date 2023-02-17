303fcb0f254ff2aa42c322804130073719c4b64c
rebecarubio <rebecarubiolopez@gmail.com>
2023-02-17 15:56:00 +0100

    add dropdown to chose text field type

--- src/features/surveys/components/SurveyEditor/blocks/OpenQuestionBlock.tsx
+++ src/features/surveys/components/SurveyEditor/blocks/OpenQuestionBlock.tsx
@@ -1,13 +1,33 @@
+import AbcIcon from '@mui/icons-material/Abc';
 import { FC } from 'react';
-import { TextField } from '@mui/material';
+
+import { makeStyles } from '@mui/styles';
+import { FormattedMessage as Msg } from 'react-intl';
+
+import SortIcon from '@mui/icons-material/Sort';
 import { useIntl } from 'react-intl';
 import { ZetkinTextQuestion } from 'utils/types/zetkin';
+import { MenuItem, TextField } from '@mui/material';
 
 interface OpenQuestionBlockProps {
   question: ZetkinTextQuestion;
 }
 
+enum fieldType {
+  multiLine = 'multiLine',
+  singleLine = 'singleLine',
+}
+
+const useStyles = makeStyles({
+  fieldType: {
+    alignItems: 'center',
+    display: 'flex',
+  },
+});
+
 const OpenQuestionBlock: FC<OpenQuestionBlockProps> = ({ question }) => {
+  const styles = useStyles();
+
   const intl = useIntl();
   return (
     <>
@@ -27,6 +47,33 @@ const OpenQuestionBlock: FC<OpenQuestionBlockProps> = ({ question }) => {
         })}
         margin="normal"
       />
+      <TextField
+        fullWidth
+        label={intl.formatMessage({
+          id: 'pages.organizeSurvey.openQuestion.textFieldType',
+        })}
+        margin="normal"
+        select
+        sx={{ alignItems: 'center', display: 'flex' }}
+        value={fieldType}
+      >
+        <MenuItem
+          key={fieldType.singleLine}
+          className={styles.fieldType}
+          value={fieldType.singleLine}
+        >
+          <AbcIcon sx={{ marginRight: '10px' }} />
+          <Msg id="pages.organizeSurvey.openQuestion.singleLine" />
+        </MenuItem>
+        <MenuItem
+          key={fieldType.multiLine}
+          className={styles.fieldType}
+          value={fieldType.multiLine}
+        >
+          <SortIcon sx={{ marginRight: '10px' }} />
+          <Msg id="pages.organizeSurvey.openQuestion.multiLine" />
+        </MenuItem>
+      </TextField>
     </>
   );
 };
