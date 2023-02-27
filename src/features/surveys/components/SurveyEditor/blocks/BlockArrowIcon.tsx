a06103a52d859a6906c7fa9e4d10e5d6abc2c41f
kaulfield23 <kaulfield23@gmail.com>
2023-02-27 11:51:49 +0100

    Try to implements function to arrows. It only checks up/down click so far

--- src/features/surveys/components/SurveyEditor/blocks/BlockArrowIcon.tsx
+++ src/features/surveys/components/SurveyEditor/blocks/BlockArrowIcon.tsx
@@ -1,6 +1,7 @@
 import { Box } from '@mui/material';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+// import { ClickAwayListener } from '@material-ui/core';
 
 type BlockArrowIconProps = {
   blockIndex: number;
@@ -13,17 +14,32 @@ const BlockArrowIcon = ({
   blockLength,
   blockIsOnEdit,
 }: BlockArrowIconProps) => {
+  // const handleArrow = (direction: string): undefined => {
+  // console.log(direction);
+  //   return;
+  // };
+
+  // const handleClick = () => {
+  //   console.log('i'm out');
+  // };
+  // when you click the arrows, inEditMode should be true still.
   return (
     <>
       {blockIsOnEdit && (
+        // <ClickAwayListener onClickAway={handleClick}>
         <Box display="flex" flexDirection="column">
           {blockIndex !== 0 && (
+            // <Box onClick={() => handleArrow('up')}>
             <KeyboardArrowUpIcon sx={{ margin: '1em 1em 1em 0' }} />
+            // </Box>
           )}
           {blockIndex !== blockLength && (
+            // <Box onClick={() => handleArrow('down')}>
             <KeyboardArrowDownIcon sx={{ margin: '1em 1em 1em 0' }} />
+            // </Box>
           )}
         </Box>
+        // </ClickAwayListener>
       )}
     </>
   );
