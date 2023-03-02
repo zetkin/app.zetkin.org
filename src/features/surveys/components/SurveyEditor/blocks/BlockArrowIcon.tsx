ebaa17c18df2ba941a57d17930e4c74c5f1c5dab
kaulfield23 <kaulfield23@gmail.com>
2023-03-02 15:05:56 +0100

    Add stopPropagation to each arrows

--- src/features/surveys/components/SurveyEditor/blocks/BlockArrowIcon.tsx
+++ src/features/surveys/components/SurveyEditor/blocks/BlockArrowIcon.tsx
@@ -1,7 +1,6 @@
 import { Box } from '@mui/material';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-// import { ClickAwayListener } from '@material-ui/core';
 
 type BlockArrowIconProps = {
   blockIndex: number;
@@ -14,32 +13,21 @@ const BlockArrowIcon = ({
   blockLength,
   blockIsOnEdit,
 }: BlockArrowIconProps) => {
-  // const handleArrow = (direction: string): undefined => {
-  // console.log(direction);
-  //   return;
-  // };
-
-  // const handleClick = () => {
-  //   console.log('i'm out');
-  // };
-  // when you click the arrows, inEditMode should be true still.
   return (
     <>
       {blockIsOnEdit && (
-        // <ClickAwayListener onClickAway={handleClick}>
         <Box display="flex" flexDirection="column">
           {blockIndex !== 0 && (
-            // <Box onClick={() => handleArrow('up')}>
-            <KeyboardArrowUpIcon sx={{ margin: '1em 1em 1em 0' }} />
-            // </Box>
+            <Box onClick={(e) => e.stopPropagation()}>
+              <KeyboardArrowUpIcon sx={{ margin: '1em 1em 1em 0' }} />
+            </Box>
           )}
           {blockIndex !== blockLength && (
-            // <Box onClick={() => handleArrow('down')}>
-            <KeyboardArrowDownIcon sx={{ margin: '1em 1em 1em 0' }} />
-            // </Box>
+            <Box onClick={(e) => e.stopPropagation()}>
+              <KeyboardArrowDownIcon sx={{ margin: '1em 1em 1em 0' }} />
+            </Box>
           )}
         </Box>
-        // </ClickAwayListener>
       )}
     </>
   );
