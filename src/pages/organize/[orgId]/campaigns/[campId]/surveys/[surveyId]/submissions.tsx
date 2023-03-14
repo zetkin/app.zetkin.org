43f0e7d4444333b2ff3f9e184464aec8d82a5e76
Rebe R <36491300+rebecarubio@users.noreply.github.com>
2023-03-14 20:09:01 +0100

    add surveysuborgsCard to submissions page, add localized strings

--- src/pages/organize/[orgId]/campaigns/[campId]/surveys/[surveyId]/submissions.tsx
+++ src/pages/organize/[orgId]/campaigns/[campId]/surveys/[surveyId]/submissions.tsx
@@ -1,9 +1,11 @@
 import { GetServerSideProps } from 'next';
+import { Grid } from '@mui/material';
 import { PageWithLayout } from 'utils/types';
 import { scaffold } from 'utils/next';
 import SurveyLayout from 'features/surveys/layout/SurveyLayout';
 import SurveySubmissionsList from 'features/surveys/components/SurveySubmissionsList';
 import SurveySubmissionsModel from 'features/surveys/models/SurveySubmissionsModel';
+import SurveySuborgsCard from 'features/surveys/components/SurveySuborgsCard';
 import useModel from 'core/useModel';
 import ZUIFuture from 'zui/ZUIFuture';
 
@@ -43,7 +45,19 @@ const SubmissionsPage: PageWithLayout<SubmissionsPageProps> = ({
   return (
     <ZUIFuture future={subsModel.getSubmissions()}>
       {(data) => {
-        return <SurveySubmissionsList submissions={data} />;
+        return (
+          <Grid container spacing={2}>
+            <Grid item md={8} sm={12} xs={12}>
+              <SurveySubmissionsList submissions={data} />
+            </Grid>
+            <Grid item md={4} sm={12} xs={12}>
+              <SurveySuborgsCard
+                orgId={parseInt(orgId)}
+                surveyId={parseInt(surveyId)}
+              />
+            </Grid>
+          </Grid>
+        );
       }}
     </ZUIFuture>
   );
