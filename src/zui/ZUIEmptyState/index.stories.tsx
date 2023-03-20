daa0ba0c22bc5fb211792a212ec37ecac8f4869b
ziggi <ziggi.willpower@gmail.com>
2023-03-20 11:33:22 +0100

    Update storybook story.

--- src/zui/ZUIEmptyState/index.stories.tsx
+++ src/zui/ZUIEmptyState/index.stories.tsx
@@ -6,8 +6,24 @@ export default {
   title: 'ZUIEmptyState',
 } as ComponentMeta<typeof ZUIEmptyState>;
 
-const Template: ComponentStory<typeof ZUIEmptyState> = () => {
-  return <ZUIEmptyState message="This is empty!" />;
+const Template: ComponentStory<typeof ZUIEmptyState> = (args) => {
+  return (
+    <ZUIEmptyState
+      href={args.href}
+      linkMessage={args.linkMessage}
+      message={args.message}
+    />
+  );
 };
 
 export const basic = Template.bind({});
+basic.args = {
+  message: 'This is empty!!',
+};
+
+export const withLink = Template.bind({});
+withLink.args = {
+  href: '/',
+  linkMessage: 'But you might find it here!',
+  message: 'This page is empty',
+};
