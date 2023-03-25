d330a595fd6643e5c33811a7c99dc985fc7337e1
Richard Olsson <r@richardolsson.se>
2023-03-25 08:45:03 +0100

    Improve some strings

--- src/features/campaigns/l10n/messageIds.ts
+++ src/features/campaigns/l10n/messageIds.ts
@@ -75,7 +75,7 @@ export default makeMessages('feat.campaigns', {
       submitButton: m('Confirm deletion'),
       title: m('Delete project'),
       warning: m(
-        'Are you sure you want to delete this campiagn? This action is permanent.'
+        'Are you sure you want to delete this project? This action is permanent.'
       ),
     },
     description: m('Description'),
@@ -105,7 +105,7 @@ export default makeMessages('feat.campaigns', {
   indefinite: m('Indefinite timeline'),
   layout: {
     activities: m('Activities'),
-    allCampaigns: m('All Projects'),
+    allCampaigns: m('All Projects & Activities'),
     archive: m('Archive'),
     calendar: m('Calendar'),
     insights: m('Insights'),
