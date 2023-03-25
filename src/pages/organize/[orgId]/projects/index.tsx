70885a6d3d993354bdae5efd7e081373a39cee2e
Richard Olsson <r@richardolsson.se>
2023-03-25 08:41:01 +0100

    Add ActivitiesOverview for all activities on global project home

--- src/pages/organize/[orgId]/projects/index.tsx
+++ src/pages/organize/[orgId]/projects/index.tsx
@@ -1,9 +1,9 @@
 import { GetServerSideProps } from 'next';
-
-import { Grid } from '@mui/material';
 import Head from 'next/head';
 import { useQuery } from 'react-query';
+import { Box, Grid, Typography } from '@mui/material';
 
+import ActivitiesOverview from 'features/campaigns/components/ActivitiesOverview';
 import AllCampaignsLayout from 'features/campaigns/layout/AllCampaignsLayout';
 import CampaignCard from 'features/campaigns/components/CampaignCard';
 import getCampaigns from 'features/campaigns/fetching/getCampaigns';
@@ -12,8 +12,7 @@ import getOrg from 'utils/fetching/getOrg';
 import getUpcomingEvents from 'features/events/fetching/getUpcomingEvents';
 import { PageWithLayout } from 'utils/types';
 import { scaffold } from 'utils/next';
-import { useMessages } from 'core/i18n';
-import ZUISection from 'zui/ZUISection';
+import { Msg, useMessages } from 'core/i18n';
 
 import messageIds from 'features/campaigns/l10n/messageIds';
 
@@ -95,17 +94,22 @@ const AllCampaignsSummaryPage: PageWithLayout<AllCampaignsSummaryPageProps> = ({
       <Head>
         <title>{messages.layout.allCampaigns()}</title>
       </Head>
-      <ZUISection title={messages.all.heading()}>
-        <Grid container gap={5}>
+      <ActivitiesOverview orgId={parseInt(orgId)} />
+      <Box mt={4}>
+        <Typography mb={2} variant="h4">
+          <Msg id={messageIds.all.heading} />
+        </Typography>
+
+        <Grid container spacing={2}>
           {campaigns.map((campaign) => {
             return (
-              <Grid key={campaign.id} item lg={3} md={5} xs={12}>
+              <Grid key={campaign.id} item lg={3} md={4} xs={12}>
                 <CampaignCard campaign={campaign} events={events} />
               </Grid>
             );
           })}
         </Grid>
-      </ZUISection>
+      </Box>
     </>
   );
 };
