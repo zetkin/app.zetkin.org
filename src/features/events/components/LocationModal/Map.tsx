073a541af68f95a297a6ccea5d1b41e9f4ec98f1
ziggi <ziggi.willpower@gmail.com>
2023-04-03 18:18:54 +0200

    Opens info box when selecting location, either by clicking or choosing in autoselect.

--- src/features/events/components/LocationModal/Map.tsx
+++ src/features/events/components/LocationModal/Map.tsx
@@ -1,13 +1,14 @@
 import 'leaflet/dist/leaflet.css';
+import { Close } from '@mui/icons-material';
 import Fuse from 'fuse.js';
-import { Autocomplete, Box, TextField } from '@mui/material';
+import { Autocomplete, Box, TextField, Typography } from '@mui/material';
 import { FC, useState } from 'react';
 import { MapContainer, Marker, TileLayer, useMap } from 'react-leaflet';
 
 import messageIds from 'features/events/l10n/messageIds';
 import { useMessages } from 'core/i18n';
 import { ZetkinLocation } from 'utils/types/zetkin';
-import { icon, latLngBounds, Map as MapType } from 'leaflet';
+import { icon, latLng, latLngBounds, Map as MapType } from 'leaflet';
 
 const selectedIcon = icon({
   iconAnchor: [12, 32],
@@ -54,63 +55,125 @@ const Map: FC<MapProps> = ({ locations }) => {
     ? fuse.search(searchString).map((fuseResult) => fuseResult.item)
     : locations;
 
+  const selectedLocation = locations.find(
+    (location) => location.id === selectedLocationId
+  );
+
   return (
     <MapContainer bounds={bounds} style={{ height: '100%', width: '100%' }}>
-      <Box
-        sx={{
-          display: 'flex',
-          justifyContent: 'flex-end',
-          padding: 2,
-          position: 'relative',
-          width: '100%',
-          zIndex: 10000,
-        }}
-      >
-        <Autocomplete
-          onChange={(ev) => {
-            const event = ev.target as HTMLLIElement;
-            const text = event.textContent;
-            setSearchString(text || '');
-          }}
-          options={locations.map((location) => location.title)}
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              label={messages.locationModal.searchBox()}
-              onChange={(ev) => setSearchString(ev.target.value)}
+      <MapProvider>
+        {(map) => (
+          <>
+            <Box
               sx={{
-                backgroundColor: 'white',
-                borderRadius: '5px',
+                display: 'flex',
+                justifyContent: 'flex-end',
+                padding: 2,
+                position: 'relative',
+                width: '100%',
+                zIndex: 10000,
               }}
-            />
-          )}
-          sx={{ minWidth: '200px', width: '25%' }}
-        />
-      </Box>
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {filteredLocations.map((location) => {
-        return (
-          <MapProvider key={location.id}>
-            {(map) => (
-              <Marker
-                eventHandlers={{
-                  click: (evt) => {
-                    map.setView(evt.latlng, 17);
-                    setSelectedLocationId(location.id);
-                  },
+            >
+              <Box
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  minWidth: '200px',
+                  width: '30%',
                 }}
-                icon={
-                  selectedLocationId === location.id ? selectedIcon : basicIcon
-                }
-                position={[location.lat, location.lng]}
-              />
-            )}
-          </MapProvider>
-        );
-      })}
+              >
+                {!selectedLocation && (
+                  <Autocomplete
+                    onChange={(ev, value) => {
+                      const location = locations.find(
+                        (location) => location.title === value
+                      );
+                      if (!location?.lat || !location?.lng) {
+                        return;
+                      }
+                      map.setView(latLng(location.lat, location.lng), 17);
+                    }}
+                    onInputChange={(ev, value) => {
+                      setSearchString(value || '');
+                      setSelectedLocationId(
+                        locations.find((location) => location.title === value)
+                          ?.id || undefined
+                      );
+                    }}
+                    options={locations.map((location) => location.title)}
+                    renderInput={(params) => (
+                      <TextField
+                        {...params}
+                        label={messages.locationModal.searchBox()}
+                        onChange={(ev) => setSearchString(ev.target.value)}
+                        sx={{
+                          backgroundColor: 'white',
+                          borderRadius: '5px',
+                        }}
+                      />
+                    )}
+                  />
+                )}
+                {selectedLocation && (
+                  <Box
+                    bgcolor="white"
+                    padding={2}
+                    sx={{
+                      cursor: 'default',
+                      display: 'flex',
+                      flexDirection: 'column',
+                    }}
+                  >
+                    <Box display="flex" justifyContent="space-between">
+                      <Typography variant="h5">
+                        {selectedLocation?.title}
+                      </Typography>
+                      <Close
+                        color="secondary"
+                        onClick={() => {
+                          setSearchString('');
+                          setSelectedLocationId(undefined);
+                        }}
+                        sx={{
+                          cursor: 'pointer',
+                        }}
+                      />
+                    </Box>
+                    {selectedLocation?.info_text && (
+                      <Typography color="secondary">
+                        {selectedLocation.info_text}
+                      </Typography>
+                    )}
+                  </Box>
+                )}
+              </Box>
+            </Box>
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            {filteredLocations.map((location) => {
+              return (
+                <Marker
+                  key={location.id}
+                  eventHandlers={{
+                    click: (evt) => {
+                      map.setView(evt.latlng, 17);
+                      setSelectedLocationId(location.id);
+                    },
+                  }}
+                  icon={
+                    selectedLocationId === location.id
+                      ? selectedIcon
+                      : basicIcon
+                  }
+                  position={[location.lat, location.lng]}
+                />
+              );
+            })}
+          </>
+        )}
+      </MapProvider>
     </MapContainer>
   );
 };
