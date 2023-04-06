ea33c7baf885d6699bb5fd1cd3ff8fff0b1f50cd
Richard Olsson <r@richardolsson.se>
2023-04-06 14:55:14 +0200

    Extract time span from EventLayout to new ZUITimeSpan component

--- src/features/events/layout/EventLayout.tsx
+++ src/features/events/layout/EventLayout.tsx
@@ -1,6 +1,5 @@
 import { Box } from '@mui/material';
 import EventIcon from '@mui/icons-material/Event';
-import { FormattedDate } from 'react-intl';
 import PeopleIcon from '@mui/icons-material/People';
 import TabbedLayout from 'utils/layout/TabbedLayout';
 
@@ -12,6 +11,7 @@ import ZUIEditTextinPlace from 'zui/ZUIEditTextInPlace';
 import ZUIFuture from 'zui/ZUIFuture';
 import { ZUIIconLabelProps } from 'zui/ZUIIconLabel';
 import ZUIIconLabelRow from 'zui/ZUIIconLabelRow';
+import ZUITimeSpan from 'zui/ZUITimeSpan';
 import { Msg, useMessages } from 'core/i18n';
 
 interface EventLayoutProps {
@@ -33,15 +33,6 @@ const EventLayout: React.FC<EventLayoutProps> = ({
     (env) => new EventDataModel(env, parseInt(orgId), parseInt(eventId))
   );
 
-  const showTimeOnly = (value: Date) => {
-    return value.toLocaleTimeString('en-Us', {
-      hour: '2-digit',
-      hour12: false,
-      minute: '2-digit',
-      timeZone: 'UTC',
-    });
-  };
-
   return (
     <TabbedLayout
       baseHref={`/organize/${orgId}/projects/${campaignId}/events/${eventId}`}
@@ -53,116 +44,15 @@ const EventLayout: React.FC<EventLayoutProps> = ({
             <ZUIFuture future={model.getData()}>
               {(data) => {
                 const startDate = new Date(data.start_time);
-                const startTime = showTimeOnly(startDate);
 
                 const endDate = new Date(data.end_time);
-                const endTime = showTimeOnly(endDate);
-
-                const isToday =
-                  startDate.toDateString() === new Date().toDateString();
-                const endsOnSameDay =
-                  startDate.toDateString() === endDate.toDateString();
-                const endsOnToday =
-                  endDate.toDateString() === new Date().toDateString();
 
                 const labels: ZUIIconLabelProps[] = [];
 
                 if (startDate && endDate) {
                   labels.push({
                     icon: <EventIcon />,
-                    label: (
-                      <>
-                        {isToday && (
-                          <>
-                            {endsOnSameDay && (
-                              <Msg
-                                id={messageIds.stats.singleDayToday}
-                                values={{ end: endTime, start: startTime }}
-                              />
-                            )}
-                            {!endsOnSameDay && (
-                              <Msg
-                                id={messageIds.stats.multiDayToday}
-                                values={{
-                                  end: endTime,
-                                  endDate: (
-                                    <FormattedDate
-                                      day="numeric"
-                                      month="long"
-                                      value={data.end_time}
-                                    />
-                                  ),
-                                  start: startTime,
-                                }}
-                              />
-                            )}
-                          </>
-                        )}
-                        {!isToday && (
-                          <>
-                            {endsOnSameDay && (
-                              <Msg
-                                id={messageIds.stats.singleDay}
-                                values={{
-                                  date: (
-                                    <FormattedDate
-                                      day="numeric"
-                                      month="long"
-                                      value={data.start_time}
-                                    />
-                                  ),
-                                  end: endTime,
-                                  start: startTime,
-                                }}
-                              />
-                            )}
-                            {!endsOnSameDay && (
-                              <>
-                                {endsOnToday && (
-                                  <Msg
-                                    id={messageIds.stats.multiDayEndsToday}
-                                    values={{
-                                      end: endTime,
-                                      start: startTime,
-                                      startDate: (
-                                        <FormattedDate
-                                          day="numeric"
-                                          month="long"
-                                          value={data.start_time}
-                                        />
-                                      ),
-                                    }}
-                                  />
-                                )}
-                                {!endsOnToday && (
-                                  <Msg
-                                    id={messageIds.stats.multiDay}
-                                    values={{
-                                      end: endTime,
-                                      endDate: (
-                                        <FormattedDate
-                                          day="numeric"
-                                          month="long"
-                                          value={data.end_time}
-                                        />
-                                      ),
-                                      start: startTime,
-                                      startDate: (
-                                        <FormattedDate
-                                          day="numeric"
-                                          month="long"
-                                          value={data.start_time}
-                                        />
-                                      ),
-                                    }}
-                                  />
-                                )}
-                              </>
-                            )}
-                          </>
-                        )}
-                      </>
-                    ),
+                    label: <ZUITimeSpan end={endDate} start={startDate} />,
                   });
                   if (data.num_participants_available) {
                     labels.push({
