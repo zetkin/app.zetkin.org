3590e4f61d3cb46e1f08548d96103b58b3989775
KraftKatten <kraftj@student.chalmers.se>
2023-04-22 17:55:34 +0200

    Added weekday and day of month to week view

--- src/features/calendar/components/CalendarWeekView/index.tsx
+++ src/features/calendar/components/CalendarWeekView/index.tsx
@@ -1,6 +1,8 @@
 import _ from 'lodash';
 import dayjs, { Dayjs } from 'dayjs';
 import { Grid, TextField } from '@mui/material';
+import theme from 'theme';
+import { Box, fontWeight } from '@mui/system';
 
 export interface DayHeaderProps {
   date: Dayjs;
@@ -9,10 +11,36 @@ export interface DayHeaderProps {
 
 const DayHeader = ({ date, focused }: DayHeaderProps) => {
   return (
-    <>
-      <Grid item>{date.format('ddd')}</Grid>
-      <Grid item>{date.format('D')}</Grid>
-    </>
+    <Grid container xs={12/7} marginTop={2} padding={2} >
+      <Grid item xs={4} sx={
+        {
+          color: theme.palette.statusColors.gray,
+          fontWeight: "bold"
+        }
+      }>
+        {date.format('ddd')}
+      </Grid>
+      <Grid item xs={4} sx={
+          {
+            textAlign: "center", 
+          }
+        }>
+        <Box sx={
+          {
+            backgroundColor: focused ? theme.palette.statusColors.blue : "",
+            color: focused ? "white" : "inherit", 
+            textAlign: "center",
+            width: "30px",
+            height: "30px",
+            borderRadius: "50%",
+          }
+        }> 
+        {date.format('D')}
+        </Box>
+          
+        </Grid>
+      <Grid item xs={4} ></Grid>
+    </Grid>
   );
 };
 
