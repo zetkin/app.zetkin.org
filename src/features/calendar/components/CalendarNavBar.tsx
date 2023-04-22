658f804a5c509f7833952e76c9b2329f14f686d8
River <river@someserver.de>
2023-04-22 10:40:15 +0100

    add event handlers for changing focus date and selected time scale

--- src/features/calendar/components/CalendarNavBar.tsx
+++ src/features/calendar/components/CalendarNavBar.tsx
@@ -2,18 +2,24 @@ import { Box } from '@mui/system';
 import { ArrowBack, ArrowForward } from '@mui/icons-material';
 import { Button, ButtonGroup, IconButton } from '@mui/material';
 
-// import { TimeScale } from '.';
+import { TimeScale } from '.';
 
 export interface CalendarNavBarProps {
   focusDate: Date;
   onChangeFocusDate: (date: Date) => void;
-  // onChangeTimeScale: () => void;
-  // onStepBackward: () => void;
-  // onStepForward: () => void;
-  // timeScale: TimeScale;
+  onChangeTimeScale: (timeScale: TimeScale) => void;
+  onStepBackward: () => void;
+  onStepForward: () => void;
+  timeScale: TimeScale;
 }
 
-const CalendarNavBar = ({ onChangeFocusDate }: CalendarNavBarProps) => {
+const CalendarNavBar = ({
+  onChangeFocusDate,
+  onChangeTimeScale,
+  onStepBackward,
+  onStepForward,
+  timeScale,
+}: CalendarNavBarProps) => {
   return (
     <Box display="flex" justifyContent="space-between">
       <Box>
@@ -24,18 +30,33 @@ const CalendarNavBar = ({ onChangeFocusDate }: CalendarNavBarProps) => {
         >
           Today
         </Button>
-        <IconButton>
+        <IconButton onClick={onStepBackward}>
           <ArrowBack />
         </IconButton>
-        <IconButton>
+        <IconButton onClick={onStepForward}>
           <ArrowForward />
         </IconButton>
       </Box>
 
       <ButtonGroup>
-        <Button>Day</Button>
-        <Button>Week</Button>
-        <Button>Month</Button>
+        <Button
+          onClick={() => onChangeTimeScale(TimeScale.DAY)}
+          variant={timeScale === TimeScale.DAY ? 'contained' : 'outlined'}
+        >
+          Day
+        </Button>
+        <Button
+          onClick={() => onChangeTimeScale(TimeScale.WEEK)}
+          variant={timeScale === TimeScale.WEEK ? 'contained' : 'outlined'}
+        >
+          Week
+        </Button>
+        <Button
+          onClick={() => onChangeTimeScale(TimeScale.MONTH)}
+          variant={timeScale === TimeScale.MONTH ? 'contained' : 'outlined'}
+        >
+          Month
+        </Button>
       </ButtonGroup>
     </Box>
   );
--- src/features/calendar/components/index.tsx
+++ src/features/calendar/components/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 
 import CalendarNavBar from './CalendarNavBar';
+import dayjs from 'dayjs';
 
 export enum TimeScale {
   DAY = 'day',
@@ -10,14 +11,9 @@ export enum TimeScale {
 
 const Calendar = () => {
   const [focusDate, setFocusDate] = useState<Date>(new Date());
-  //   const [selectedTimeScale, setSelectedTimeScale] = useState<TimeScale>(
-  //     TimeScale.MONTH
-  //   );
-
-  //   onChangeTimeScale={() => {}}
-  //         onStepBackward={() => {}}
-  //         onStepForward={() => {}}
-  //   timeScale={selectedTimeScale}
+  const [selectedTimeScale, setSelectedTimeScale] = useState<TimeScale>(
+    TimeScale.MONTH
+  );
 
   return (
     <>
@@ -26,7 +22,23 @@ const Calendar = () => {
         onChangeFocusDate={(date) => {
           setFocusDate(date);
         }}
+        onChangeTimeScale={(timeScale) => {
+          setSelectedTimeScale(timeScale);
+        }}
+        onStepBackward={() => {
+          setFocusDate(
+            dayjs(focusDate).subtract(1, selectedTimeScale).toDate()
+          );
+        }}
+        onStepForward={() => {
+          setFocusDate(dayjs(focusDate).add(1, selectedTimeScale).toDate());
+        }}
+        timeScale={selectedTimeScale}
       />
+
+      {selectedTimeScale === TimeScale.DAY && <h2>Day</h2>}
+      {selectedTimeScale === TimeScale.WEEK && <h2>Week</h2>}
+      {selectedTimeScale === TimeScale.MONTH && <h2>Month</h2>}
     </>
   );
 };
