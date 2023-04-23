543bdd6769e65446e40ec3009df14d702cff50e5
Samuel Skånberg <samuel.skanberg@etc.se>
2023-04-23 11:52:18 +0200

    Show all future dates

--- src/features/calendar/components/CalendarDayView/index.tsx
+++ src/features/calendar/components/CalendarDayView/index.tsx
@@ -5,7 +5,6 @@ import { useRouter } from 'next/router';
 import ZUIFuture from 'zui/ZUIFuture';
 import CalendarDayItem from './CalendarDayItem';
 import { dateIsEqualOrBefore, isSameDate } from 'utils/dateUtils';
-import { ZetkinActivity, ZetkinEvent } from 'utils/types/zetkin';
 import { DayInfo } from './types';
 
 export interface CalendarDayViewProps {
@@ -63,17 +62,8 @@ const CalendarDayView = ({
             }
           }
         }
-        console.log("activitiesByDate");
-        console.log(activitiesByDate);
         const todayAndFutureEventsDates = Object.keys(activitiesByDate).filter(d => dateIsEqualOrBefore(new Date(d), new Date(focusDate))).sort();
-        console.log("todayAndFutureEventsDates");
-        console.log(todayAndFutureEventsDates);
 
-
-        const events = data.filter(a => a.kind == ACTIVITIES.EVENT).map(activity => activity.data) as ZetkinEvent[];
-        const laterEvents = events.filter(a => a.start_time != null && dateIsEqualOrBefore(new Date(a.start_time), new Date(focusDate)));
-        const laterEventsSameDay = laterEvents.length > 0 ?
-          laterEvents.filter(a => isSameDate(new Date(a.start_time), new Date(laterEvents[0].start_time))) : [];
         return (
           <Box
             sx={{
@@ -81,26 +71,9 @@ const CalendarDayView = ({
               marginTop: '1em',
             }}
           >
-            {console.log("focusdate: "+focusDate.toISOString())}
-            {console.log("laterEvents length: "+laterEvents.length)}
-            {console.log("laterEvents[0].data.start_time: "+String((laterEvents.length > 0 ? laterEvents[0].start_time: "-")))}
-            {console.log("laterEvents[1].data.start_time: "+String((laterEvents.length > 1 ? laterEvents[1].start_time: "-")))}
-
-
-            {console.log("laterEventsSameDay.length: "+laterEventsSameDay.length)}
-            {console.log("laterEventsSameDay[0].data.start_time: "+String((laterEventsSameDay.length > 0 ? laterEventsSameDay[0].start_time: "-")))}
-            {console.log("laterEventsSameDay[1].data.start_time: "+String((laterEventsSameDay.length > 1 ? laterEventsSameDay[1].start_time: "-")))}
-
-            
-            {/* { laterEventsSameDay.length > 0 && 
-              <CalendarDayItem focusDate={new Date(focusDate)} date={new Date(laterEventsSameDay[0].start_time)} events={laterEventsSameDay} />
-            } */}
-            { todayAndFutureEventsDates.length > 0 && 
-              <CalendarDayItem focusDate={new Date(focusDate)} date={new Date(todayAndFutureEventsDates[0])} dayInfo={activitiesByDate[todayAndFutureEventsDates[0]]} />
-            }
-            {/* { laterEvents.length > 1 && 
-              <CalendarDayItem focusDate={new Date(focusDate)} date={new Date(laterEvents[1].data.start_time)} />
-            } */}
+            { todayAndFutureEventsDates.map((d, i) => (
+              <CalendarDayItem focusDate={new Date(focusDate)} date={new Date(d)} dayInfo={activitiesByDate[d]} />
+            ))}
           </Box>
         );
       }}
---
+++ src/features/calendar/components/CalendarDayView/types.ts
@@ -0,0 +1,8 @@
+import { ZetkinActivity, ZetkinEvent } from 'utils/types/zetkin';
+import CampaignActivitiesModel, { ACTIVITIES, CampaignActivity } from 'features/campaigns/models/CampaignActivitiesModel';
+
+export interface DayInfo {
+  events: ZetkinEvent[];
+  activities_starts: CampaignActivity[];
+  activities_ends: CampaignActivity[];
+}
