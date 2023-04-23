b1735b6f8c6ed1172b2875e4afb48d0880d001b4
Carl <carlagh0501@gmail.com>
2023-04-23 12:07:07 +0200

    fixed issue 1255

--- src/zui/ZUIPersonGridCell.tsx
+++ src/zui/ZUIPersonGridCell.tsx
@@ -1,26 +1,46 @@
-import { FC } from 'react';
 import { Person } from '@mui/icons-material';
 import { useRouter } from 'next/router';
-import { Avatar, Box, SxProps } from '@mui/material';
+import { FC, forwardRef } from 'react';
+import { Avatar, Box, SxProps, Tooltip } from '@mui/material';
 
-import ZUIAvatar from 'zui/ZUIAvatar';
+import ZetkinPerson from 'utils/types/zetkin';
+
+//this is basicly a copy of ZUIAvatar
+interface ZUICellAvatarProps {
+  orgId: number;
+  person: ZetkinPerson;
+}
+
+const ZUICellAvatar: FC<ZUICellAvatarProps> = ({ orgId, person }) => {
+  const personId = person.id;
+  return (
+    <Tooltip title={person.first_name + ' ' + person.last_name}>
+      <Avatar
+        src={`/api/orgs/${orgId}/people/${personId}/avatar`}
+        style={{ height: 40, width: 40 }}
+      />
+    </Tooltip>
+  );
+};
 
 const ZUIPersonGridCell: FC<{
   onClick?: () => void;
-  personId: number | null;
   sx?: SxProps;
-}> = ({ personId, onClick, sx }) => {
+  person: ZetkinPerson | null;
+}> = ({ onClick, sx, person }) => {
   const query = useRouter().query;
   const orgId = parseInt(query.orgId as string);
 
   return (
     <Box onClick={onClick} sx={sx}>
-      {personId ? (
-        <ZUIAvatar orgId={orgId} personId={personId} />
+      {person ? (
+        <ZUICellAvatar orgId={orgId} person={person} />
       ) : (
-        <Avatar>
-          <Person />
-        </Avatar>
+        <Tooltip title={'no person assigned'}>
+          <Avatar>
+            <Person />
+          </Avatar>
+        </Tooltip>
       )}
     </Box>
   );
