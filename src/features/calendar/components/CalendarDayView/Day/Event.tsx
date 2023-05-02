9ce3abfecd749ac383819f5854d83b5cbb915721
djbusstop <eribloodlust@grrlz.net>
2023-05-02 11:28:31 +0100

    add status and icons to Event

--- src/features/calendar/components/CalendarDayView/Day/Event.tsx
+++ src/features/calendar/components/CalendarDayView/Day/Event.tsx
@@ -1,7 +1,15 @@
 import { FormattedTime } from 'react-intl';
 import { Box, Typography } from '@mui/material';
-import { PlaceOutlined, Schedule } from '@mui/icons-material';
+import {
+  EmojiPeople,
+  FaceRetouchingOff,
+  MailOutline,
+  People,
+  PlaceOutlined,
+  Schedule,
+} from '@mui/icons-material';
 
+import theme from 'theme';
 import { ZetkinEvent } from 'utils/types/zetkin';
 
 const Event = ({ event }: { event: ZetkinEvent }) => {
@@ -9,27 +17,50 @@ const Event = ({ event }: { event: ZetkinEvent }) => {
     <Box
       display="flex"
       flexDirection="row"
+      justifyContent="space-between"
       padding={2}
       sx={{ backgroundColor: 'white', borderRadius: '3px' }}
       width="100%"
     >
-      <Box display="flex" gap={2.2}>
+      <Box alignItems="center" display="flex" gap={2.2}>
+        {/* Status */}
+        <Box
+          sx={{
+            backgroundColor: 'green',
+            borderRadius: '50%',
+            height: '10px',
+            width: '10px',
+          }}
+        />
+        {/* Title */}
         <Typography>{event.title || event.activity.title}</Typography>
         {/* Time */}
-        <Box display="flex">
-          <Schedule />
-          <Typography>
+        <Typography color={theme.palette.secondary.main}>
+          <Box alignItems="center" display="flex" gap={0.5}>
+            <Schedule />
             <FormattedTime value={event.start_time} /> -
             <FormattedTime value={event.end_time} />
-          </Typography>
-        </Box>
+          </Box>
+        </Typography>
         {/* Location */}
         {event.location && (
-          <Box display="flex">
-            <PlaceOutlined />
-            <Typography>{event.location?.title}</Typography>
-          </Box>
+          <Typography color={theme.palette.secondary.main}>
+            <Box alignItems="center" display="flex" gap={0.5}>
+              <PlaceOutlined />
+              {event.location?.title}
+            </Box>
+          </Typography>
         )}
+      </Box>
+      {/* Icons */}
+      <Box alignItems="center" display="flex" gap={1}>
+        <EmojiPeople color="error" />
+        <FaceRetouchingOff color="error" />
+        <MailOutline color="error" />
+        <People />
+        <Typography>
+          {event.num_participants_available}/{event.num_participants_required}
+        </Typography>
       </Box>
     </Box>
   );
