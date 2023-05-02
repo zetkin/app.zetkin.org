e8302308644880f4344df3a8f8f8eef24dd95f3a
djbusstop <eribloodlust@grrlz.net>
2023-05-02 12:41:00 +0100

    move future back in to day view

--- src/features/calendar/components/CalendarDayView/index.tsx
+++ src/features/calendar/components/CalendarDayView/index.tsx
@@ -1,35 +1,48 @@
 import Box from '@mui/material/Box';
+import { useRouter } from 'next/router';
 
-import { CampaignActivity } from 'features/campaigns/models/CampaignActivitiesModel';
+import CampaignActivitiesModel from 'features/campaigns/models/CampaignActivitiesModel';
 import { dateIsEqualOrBefore } from 'utils/dateUtils';
 import Day from './Day';
 import groupActivitiesByDate from './utils';
 import PreviousDayPrompt from './PreviousDayPrompt';
+import useModel from 'core/useModel';
+import ZUIFuture from 'zui/ZUIFuture';
 
 export interface CalendarDayViewProps {
-  activities: CampaignActivity[];
   focusDate: Date;
 }
 
-const CalendarDayView = ({ activities, focusDate }: CalendarDayViewProps) => {
-  const activitiesByDate = groupActivitiesByDate(activities);
-  const todayAndFutureActivitiesDates = Object.keys(activitiesByDate)
-    .filter((d) => dateIsEqualOrBefore(new Date(d), new Date(focusDate)))
-    .sort();
-
+const CalendarDayView = ({ focusDate }: CalendarDayViewProps) => {
+  const { orgId } = useRouter().query;
+  const model = useModel(
+    (env) => new CampaignActivitiesModel(env, parseInt(orgId as string))
+  );
   return (
-    <Box display="flex" flexDirection="column" gap={2}>
-      <PreviousDayPrompt
-        onClickShowMore={() => {
-          return;
-        }}
-      />
-      {/* List of days with events */}
-      {todayAndFutureActivitiesDates.map((date, index) => {
-        const daysEvents = activitiesByDate[date];
-        return <Day key={index} date={new Date(date)} dayInfo={daysEvents} />;
-      })}
-    </Box>
+    <ZUIFuture future={model.getAllActivities()}>
+      {(data) => {
+        const activitiesByDate = groupActivitiesByDate(data);
+        const todayAndFutureActivitiesDates = Object.keys(activitiesByDate)
+          .filter((d) => dateIsEqualOrBefore(new Date(d), new Date(focusDate)))
+          .sort();
+        return (
+          <Box display="flex" flexDirection="column" gap={2}>
+            <PreviousDayPrompt
+              onClickShowMore={() => {
+                return;
+              }}
+            />
+            {/* List of days with events */}
+            {todayAndFutureActivitiesDates.map((date, index) => {
+              const daysEvents = activitiesByDate[date];
+              return (
+                <Day key={index} date={new Date(date)} dayInfo={daysEvents} />
+              );
+            })}
+          </Box>
+        );
+      }}
+    </ZUIFuture>
   );
 };
 
--- src/features/calendar/components/index.tsx
+++ src/features/calendar/components/index.tsx
@@ -1,15 +1,11 @@
 import { Box } from '@mui/system';
 import dayjs from 'dayjs';
-import { useRouter } from 'next/router';
 import { useState } from 'react';
 
 import CalendarDayView from './CalendarDayView';
 import CalendarMonthView from './CalendarMonthView';
 import CalendarNavBar from './CalendarNavBar';
 import CalendarWeekView from './CalendarWeekView';
-import CampaignActivitiesModel from 'features/campaigns/models/CampaignActivitiesModel';
-import useModel from 'core/useModel';
-import ZUIFuture from 'zui/ZUIFuture';
 
 export enum TimeScale {
   DAY = 'day',
@@ -23,63 +19,45 @@ const Calendar = () => {
     TimeScale.MONTH
   );
 
-  const { orgId } = useRouter().query;
-  const model = useModel(
-    (env) => new CampaignActivitiesModel(env, parseInt(orgId as string))
-  );
-
   return (
-    <ZUIFuture future={model.getAllActivities()}>
-      {(data) => {
-        return (
-          <Box
-            display="flex"
-            flexDirection="column"
-            height={'100%'}
-            padding={2}
-          >
-            <CalendarNavBar
-              focusDate={focusDate}
-              onChangeFocusDate={(date) => {
-                setFocusDate(date);
-              }}
-              onChangeTimeScale={(timeScale) => {
-                setSelectedTimeScale(timeScale);
-              }}
-              onStepBackward={() => {
-                setFocusDate(
-                  dayjs(focusDate).subtract(1, selectedTimeScale).toDate()
-                );
-              }}
-              onStepForward={() => {
-                setFocusDate(
-                  dayjs(focusDate).add(1, selectedTimeScale).toDate()
-                );
-              }}
-              timeScale={selectedTimeScale}
-            />
+    <Box display="flex" flexDirection="column" height={'100%'} padding={2}>
+      <CalendarNavBar
+        focusDate={focusDate}
+        onChangeFocusDate={(date) => {
+          setFocusDate(date);
+        }}
+        onChangeTimeScale={(timeScale) => {
+          setSelectedTimeScale(timeScale);
+        }}
+        onStepBackward={() => {
+          setFocusDate(
+            dayjs(focusDate).subtract(1, selectedTimeScale).toDate()
+          );
+        }}
+        onStepForward={() => {
+          setFocusDate(dayjs(focusDate).add(1, selectedTimeScale).toDate());
+        }}
+        timeScale={selectedTimeScale}
+      />
 
-            <Box
-              display="flex"
-              flexDirection="column"
-              flexGrow={1}
-              marginTop={2}
-              overflow="auto"
-            >
-              {selectedTimeScale === TimeScale.DAY && (
-                <CalendarDayView activities={data} focusDate={focusDate} />
-              )}
-              {selectedTimeScale === TimeScale.WEEK && (
-                <CalendarWeekView focusDate={focusDate} />
-              )}
-              {selectedTimeScale === TimeScale.MONTH && (
-                <CalendarMonthView focusDate={focusDate} />
-              )}
-            </Box>
-          </Box>
-        );
-      }}
-    </ZUIFuture>
+      <Box
+        display="flex"
+        flexDirection="column"
+        flexGrow={1}
+        marginTop={2}
+        overflow="auto"
+      >
+        {selectedTimeScale === TimeScale.DAY && (
+          <CalendarDayView focusDate={focusDate} />
+        )}
+        {selectedTimeScale === TimeScale.WEEK && (
+          <CalendarWeekView focusDate={focusDate} />
+        )}
+        {selectedTimeScale === TimeScale.MONTH && (
+          <CalendarMonthView focusDate={focusDate} />
+        )}
+      </Box>
+    </Box>
   );
 };
 
