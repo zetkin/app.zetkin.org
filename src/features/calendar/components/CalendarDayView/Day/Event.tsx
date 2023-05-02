388953483267072632bac89664750d4cf032c1e2
djbusstop <eribloodlust@grrlz.net>
2023-05-02 11:18:16 +0100

    building up Event component

--- src/features/calendar/components/CalendarDayView/Day/Event.tsx
+++ src/features/calendar/components/CalendarDayView/Day/Event.tsx
@@ -1,4 +1,7 @@
-import { Box } from '@mui/material';
+import { FormattedTime } from 'react-intl';
+import { Box, Typography } from '@mui/material';
+import { PlaceOutlined, Schedule } from '@mui/icons-material';
+
 import { ZetkinEvent } from 'utils/types/zetkin';
 
 const Event = ({ event }: { event: ZetkinEvent }) => {
@@ -6,11 +9,28 @@ const Event = ({ event }: { event: ZetkinEvent }) => {
     <Box
       display="flex"
       flexDirection="row"
-      padding={1}
+      padding={2}
       sx={{ backgroundColor: 'white', borderRadius: '3px' }}
       width="100%"
     >
-      Event
+      <Box display="flex" gap={2.2}>
+        <Typography>{event.title || event.activity.title}</Typography>
+        {/* Time */}
+        <Box display="flex">
+          <Schedule />
+          <Typography>
+            <FormattedTime value={event.start_time} /> -
+            <FormattedTime value={event.end_time} />
+          </Typography>
+        </Box>
+        {/* Location */}
+        {event.location && (
+          <Box display="flex">
+            <PlaceOutlined />
+            <Typography>{event.location?.title}</Typography>
+          </Box>
+        )}
+      </Box>
     </Box>
   );
 };
