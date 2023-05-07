456b43eaaef4f7062bc7cc7157821cdacb956258
djbusstop <eribloodlust@grrlz.net>
2023-05-07 18:08:58 +0100

    remove util

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -74,10 +74,11 @@ export function isValidDate(date: Date): boolean {
     return false;
   }
 }
-export function dateIsBefore(first: Date, second: Date): boolean {
-  return first.toISOString().slice(0, 10) > second.toISOString().slice(0, 10);
+
+export function dateIsAfter(first: Date, second: Date): boolean {
+  return first.toISOString().slice(0, 10) < second.toISOString().slice(0, 10);
 }
 
-export function dateIsEqualOrBefore(first: Date, second: Date): boolean {
-  return first.toISOString().slice(0, 10) >= second.toISOString().slice(0, 10);
+export function dateIsBefore(first: Date, second: Date): boolean {
+  return first.toISOString().slice(0, 10) > second.toISOString().slice(0, 10);
 }
