1418917d393b0e5ef0c836c489ddc5aa48f6e8b9
djbusstop <eribloodlust@grrlz.net>
2023-05-07 14:11:53 +0100

    remove unused

--- src/features/calendar/components/CalendarDayView/types.ts
+++
@@ -1,8 +0,0 @@
-import { CampaignActivity } from 'features/campaigns/models/CampaignActivitiesModel';
-import { ZetkinEvent } from 'utils/types/zetkin';
-
-export interface DayInfo {
-  events: ZetkinEvent[];
-  activities_starts: CampaignActivity[];
-  activities_ends: CampaignActivity[];
-}
