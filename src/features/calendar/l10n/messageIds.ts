73fef7ad2375be97d345b9236a35ecf74525252a
ziggi <ziggi.willpower@gmail.com>
2023-05-10 13:59:09 +0200

    Create Arbitrary Cluster.

--- src/features/calendar/l10n/messageIds.ts
+++ src/features/calendar/l10n/messageIds.ts
@@ -1,6 +1,26 @@
 import { m, makeMessages } from 'core/i18n';
 
 export default makeMessages('feat.calendar', {
+  event: {
+    differentLocations: m<{ numLocations: number }>(
+      '{numLocations} different locations'
+    ),
+    events: m('Events'),
+    noContactSelected: m('No contact selected'),
+    remindersNotSent: m<{ numNotSent: number }>(
+      '{numNotSent} Reminders not sent'
+    ),
+    unbookedSignups: m<{ numUnbooked: number }>(
+      '{numUnbooked} Unbooked signups'
+    ),
+    underbooked: m<{ numUnderbooked: number }>(
+      '{numUnderbooked} Underbooked events'
+    ),
+    withSignups: m<{ numWithSignups: number }>('{numWithSignups} With signups'),
+    withoutContact: m<{ numWithoutContact: number }>(
+      '{numWithoutContact} Without contact'
+    ),
+  },
   moreEvents: m<{ numEvents: number }>(
     '{numEvents, plural, one {# more event} other {# more events}}'
   ),
