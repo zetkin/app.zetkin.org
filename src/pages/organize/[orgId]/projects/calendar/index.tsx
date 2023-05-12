49979cf854b1fb01d2424a245f415ba6a74ae400
Richard Olsson <r@richardolsson.se>
2023-05-12 08:33:38 +0200

    Avoid unnecessary SSR on calendar pages

--- src/pages/organize/[orgId]/projects/calendar/index.tsx
+++ src/pages/organize/[orgId]/projects/calendar/index.tsx
@@ -3,15 +3,13 @@ import Head from 'next/head';
 
 import AllCampaignsLayout from 'features/campaigns/layout/AllCampaignsLayout';
 import Calendar from 'features/calendar/components';
-import getCampaigns from 'features/campaigns/fetching/getCampaigns';
-import getEvents from 'features/events/fetching/getEvents';
 import getOrg from 'utils/fetching/getOrg';
 import { PageWithLayout } from 'utils/types';
 import { scaffold } from 'utils/next';
-import { tasksResource } from 'features/tasks/api/tasks';
 import { useMessages } from 'core/i18n';
 
 import messageIds from 'features/campaigns/l10n/messageIds';
+import useServerSide from 'core/useServerSide';
 
 const scaffoldOptions = {
   authLevelRequired: 2,
@@ -33,27 +31,7 @@ export const getServerSideProps: GetServerSideProps = scaffold(async (ctx) => {
   );
   const orgState = ctx.queryClient.getQueryState(['org', orgId]);
 
-  await ctx.queryClient.prefetchQuery(
-    ['events', orgId],
-    getEvents(orgId as string, ctx.apiFetch)
-  );
-  const eventsState = ctx.queryClient.getQueryState(['events', orgId]);
-
-  await ctx.queryClient.prefetchQuery(
-    ['campaigns', orgId],
-    getCampaigns(orgId as string, ctx.apiFetch)
-  );
-  const campaignsState = ctx.queryClient.getQueryState(['campaigns', orgId]);
-
-  const { prefetch: prefetchTasks } = tasksResource(orgId as string);
-  const { state: tasksState } = await prefetchTasks(ctx);
-
-  if (
-    orgState?.status === 'success' &&
-    eventsState?.status === 'success' &&
-    campaignsState?.status === 'success' &&
-    tasksState?.status === 'success'
-  ) {
+  if (orgState?.status === 'success') {
     return {
       props: {
         orgId,
@@ -80,6 +58,11 @@ const AllCampaignsCalendarPage: PageWithLayout<
   // const events = eventsQuery.data || [];
   // const tasks = tasksQuery.data || [];
   // const campaigns = campaignsQuery.data || [];
+
+  const isOnServer = useServerSide();
+  if (isOnServer) {
+    return null;
+  }
 
   return (
     <>
