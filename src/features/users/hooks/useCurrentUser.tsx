4bcf8bea30173f191413790a592ac6719535b3b0
Rebe R <36491300+rebecarubio@users.noreply.github.com>
2023-05-25 12:54:42 +0200

    refactor user hook to save user in the store and move it to the features folder

---
+++ src/features/users/hooks/useCurrentUser.tsx
@@ -0,0 +1,22 @@
+import { RootState } from 'core/store';
+import shouldLoad from 'core/caching/shouldLoad';
+import { useApiClient } from 'core/hooks';
+import { ZetkinUser } from 'utils/types/zetkin';
+import { useDispatch, useSelector } from 'react-redux';
+import { userLoad, userLoaded } from '../store/store';
+
+const useCurrentUser = () => {
+  const apiClient = useApiClient();
+  const dispatch = useDispatch();
+  const userState = useSelector((state: RootState) => state.users);
+
+  if (shouldLoad(userState.userItem)) {
+    dispatch(userLoad());
+    apiClient.get<ZetkinUser>(`/api/users/me`).then((user) => {
+      dispatch(userLoaded(user));
+    });
+  }
+  return userState.userItem.data;
+};
+
+export default useCurrentUser;
