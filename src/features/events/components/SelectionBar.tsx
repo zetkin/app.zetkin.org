f4e4e7b582cdc5c1dde652576cc09dc3573cbe00
kaulfield23 <kaulfield23@gmail.com>
2023-05-29 15:33:21 +0200

    resize the selection bar and center it

--- src/features/events/components/SelectionBar.tsx
+++ src/features/events/components/SelectionBar.tsx
@@ -21,53 +21,60 @@ const SelectionBar = () => {
   };
 
   return (
-    <Box sx={{ position: 'fixed', zIndex: 200 }}>
+    <Box
+      sx={{
+        position: 'relative',
+        zIndex: 200,
+      }}
+    >
       {selectedEvents.length > 0 && (
-        <Paper
-          elevation={3}
-          sx={{
-            backgroundColor: 'white',
-            borderRadius: '5px',
-            bottom: 15,
-            padding: 2,
-            position: 'fixed',
-            width: 'calc(100% - 65px)',
-          }}
-        >
-          <Box display="flex" justifyContent="space-between">
-            <Box alignItems="center" display="flex">
-              <CheckBoxOutlined color="primary" />
-              <Typography color="primary" sx={{ px: 0.4 }}>
-                {selectedEvents.length}
-              </Typography>
-              <Button color="primary" onClick={handleDeselect} sx={{ mr: 1 }}>
-                <Msg id={messageIds.selectionBar.deselect} />
-              </Button>
-              <Divider orientation="vertical" variant="fullWidth" />
-              <Button
-                color="primary"
-                sx={{ borderRadius: '5px', ml: 1.5 }}
-                variant="outlined"
+        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+          <Paper
+            elevation={3}
+            sx={{
+              backgroundColor: 'white',
+              borderRadius: '5px',
+              bottom: 15,
+              padding: 2,
+              position: 'fixed',
+              width: 'calc(100% - 97px)',
+            }}
+          >
+            <Box display="flex" justifyContent="space-between">
+              <Box alignItems="center" display="flex">
+                <CheckBoxOutlined color="primary" />
+                <Typography color="primary" sx={{ px: 0.4 }}>
+                  {selectedEvents.length}
+                </Typography>
+                <Button color="primary" onClick={handleDeselect} sx={{ mr: 1 }}>
+                  <Msg id={messageIds.selectionBar.deselect} />
+                </Button>
+                <Divider orientation="vertical" variant="fullWidth" />
+                <Button
+                  color="primary"
+                  sx={{ borderRadius: '5px', ml: 1.5 }}
+                  variant="outlined"
+                >
+                  <Msg id={messageIds.selectionBar.editEvents} />
+                </Button>
+              </Box>
+              <Box
+                alignItems="center"
+                display="flex"
+                gap={1}
+                justifyContent="center"
               >
-                <Msg id={messageIds.selectionBar.editEvents} />
-              </Button>
+                <Button endIcon={<ArrowDropDown />} variant="outlined">
+                  <Msg id={messageIds.selectionBar.move} />
+                </Button>
+                <Button endIcon={<ArrowDropDown />} variant="outlined">
+                  <Msg id={messageIds.selectionBar.copy} />
+                </Button>
+                <ZUIEllipsisMenu items={[]} />
+              </Box>
             </Box>
-            <Box
-              alignItems="center"
-              display="flex"
-              gap={1}
-              justifyContent="center"
-            >
-              <Button endIcon={<ArrowDropDown />} variant="outlined">
-                <Msg id={messageIds.selectionBar.move} />
-              </Button>
-              <Button endIcon={<ArrowDropDown />} variant="outlined">
-                <Msg id={messageIds.selectionBar.copy} />
-              </Button>
-              <ZUIEllipsisMenu items={[]} />
-            </Box>
-          </Box>
-        </Paper>
+          </Paper>
+        </Box>
       )}
     </Box>
   );
