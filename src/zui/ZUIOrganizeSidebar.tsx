4b6ca6f7888f2da582b4c841246e377096896634
djbusstop <eribloodlust@grrlz.net>
2023-06-10 16:10:02 +0100

    Links no longer duplicated for handling tooltips

--- src/zui/ZUIOrganizeSidebar.tsx
+++ src/zui/ZUIOrganizeSidebar.tsx
@@ -279,129 +279,68 @@ const ZUIOrganizeSidebar = (): JSX.Element => {
                 });
 
                 return (
-                  <>
-                    {/* Add tooltip if menu is collapsed */}
-                    {!open && (
-                      <NextLink
-                        key={item.name}
-                        href={`/organize/${orgId}/${item.name}`}
-                        passHref
+                  <NextLink
+                    key={item.name}
+                    href={`/organize/${orgId}/${item.name}`}
+                    passHref
+                  >
+                    <Tooltip
+                      placement="right"
+                      title={
+                        open ? undefined : messages.organizeSidebar[item.name]()
+                      }
+                    >
+                      <ListItemButton
+                        disableGutters
+                        sx={{
+                          '&:hover': {
+                            background: theme.palette.grey[100],
+                            pointer: 'cursor',
+                          },
+                          backgroundColor: selected
+                            ? theme.palette.grey[200]
+                            : 'transparent',
+                          borderRadius: '3px',
+                          my: 0.5,
+                          py: open ? 1.25 : 1.5,
+                          transition: theme.transitions.create(
+                            [
+                              'padding-top',
+                              'padding-bottom',
+                              'background-color',
+                            ],
+                            {
+                              duration:
+                                theme.transitions.duration.leavingScreen,
+                              easing: theme.transitions.easing.sharp,
+                            }
+                          ),
+                        }}
                       >
-                        <Tooltip
-                          placement="right"
-                          title={messages.organizeSidebar[item.name]()}
-                        >
-                          <ListItemButton
-                            disableGutters
-                            sx={{
-                              '&:hover': {
-                                background: theme.palette.grey[100],
-                                pointer: 'cursor',
-                              },
-                              backgroundColor: selected
-                                ? theme.palette.grey[200]
-                                : 'transparent',
-                              borderRadius: '3px',
-                              my: 0.5,
-                              py: open ? 1.25 : 1.5,
-                              transition: theme.transitions.create(
-                                [
-                                  'padding-top',
-                                  'padding-bottom',
-                                  'background-color',
-                                ],
-                                {
-                                  duration:
-                                    theme.transitions.duration.leavingScreen,
-                                  easing: theme.transitions.easing.sharp,
-                                }
-                              ),
-                            }}
-                          >
-                            <ListItemIcon
-                              sx={{
-                                alignItems: 'center',
-                                justifyContent: 'center',
-                                minWidth: '48px',
-                                width: '48px',
-                              }}
-                            >
-                              {icon}
-                            </ListItemIcon>
-                            <Typography
-                              sx={{
-                                alignItems: 'center',
-                                display: open ? 'block' : 'none',
-                                fontWeight: key.startsWith('/' + item.name)
-                                  ? 700
-                                  : 'normal',
-                              }}
-                            >
-                              {messages.organizeSidebar[item.name]()}
-                            </Typography>
-                          </ListItemButton>
-                        </Tooltip>
-                      </NextLink>
-                    )}
-                    {/* Don't add tooltip if menu isn't collapsed */}
-                    {open && (
-                      <NextLink
-                        key={item.name}
-                        href={`/organize/${orgId}/${item.name}`}
-                        passHref
-                      >
-                        <ListItemButton
-                          disableGutters
+                        <ListItemIcon
                           sx={{
-                            '&:hover': {
-                              background: theme.palette.grey[100],
-                              pointer: 'cursor',
-                            },
-                            backgroundColor: selected
-                              ? theme.palette.grey[200]
-                              : 'transparent',
-                            borderRadius: '3px',
-                            my: 0.5,
-                            py: open ? 1.25 : 1.5,
-                            transition: theme.transitions.create(
-                              [
-                                'padding-top',
-                                'padding-bottom',
-                                'background-color',
-                              ],
-                              {
-                                duration:
-                                  theme.transitions.duration.leavingScreen,
-                                easing: theme.transitions.easing.sharp,
-                              }
-                            ),
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            minWidth: '48px',
+                            width: '48px',
                           }}
                         >
-                          <ListItemIcon
-                            sx={{
-                              alignItems: 'center',
-                              justifyContent: 'center',
-                              minWidth: '48px',
-                              width: '48px',
-                            }}
-                          >
-                            {icon}
-                          </ListItemIcon>
-                          <Typography
-                            sx={{
-                              alignItems: 'center',
-                              display: open ? 'block' : 'none',
-                              fontWeight: key.startsWith('/' + item.name)
-                                ? 700
-                                : 'normal',
-                            }}
-                          >
-                            {messages.organizeSidebar[item.name]()}
-                          </Typography>
-                        </ListItemButton>
-                      </NextLink>
-                    )}
-                  </>
+                          {icon}
+                        </ListItemIcon>
+                        <Typography
+                          sx={{
+                            alignItems: 'center',
+                            display: open ? 'block' : 'none',
+                            fontWeight: key.startsWith('/' + item.name)
+                              ? 700
+                              : 'normal',
+                          }}
+                        >
+                          {messages.organizeSidebar[item.name]()}
+                        </Typography>
+                      </ListItemButton>
+                    </Tooltip>
+                  </NextLink>
                 );
               })}
             </List>
