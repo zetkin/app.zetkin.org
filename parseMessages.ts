9aa324a79216d05989e2eff46cf24f5e5ec326e7
Richard Olsson <r@richardolsson.se>
2023-06-30 08:45:11 +0200

    Read typescript AST to figure out param types for messages

--- parseMessages.ts
+++ parseMessages.ts
@@ -15,7 +15,101 @@ if (file) {
   if (makeMessagesCallNode) {
     const messageMap = evalMessages(makeMessagesCallNode, file);
     console.log(messageMap);
+
+    const ir = inspectMessages(makeMessagesCallNode);
+    console.log(ir);
   }
+}
+
+type TypeID = string;
+
+type MessageData = {
+  id: string;
+  defaultMessage: string;
+  params: {
+    name: string;
+    types: TypeID[];
+  }[];
+};
+
+function inspectMessages(node: ts.CallExpression): MessageData[] {
+  function typeIdFromNode(typeNode?: ts.Node): string {
+    if (typeNode?.kind == ts.SyntaxKind.BooleanKeyword) {
+      return 'boolean';
+    } else if (typeNode?.kind == ts.SyntaxKind.NumberKeyword) {
+      return 'number';
+    } else if (typeNode?.kind == ts.SyntaxKind.StringKeyword) {
+      return 'string';
+    } else if (typeNode?.kind == ts.SyntaxKind.NullKeyword) {
+      return 'null';
+    } else if (typeNode?.kind == ts.SyntaxKind.UndefinedKeyword) {
+      return 'undefined';
+    } else if (typeNode?.kind == ts.SyntaxKind.TypeReference) {
+      const refNode = typeNode as ts.TypeReferenceNode;
+      const idNode = refNode.typeName as ts.Identifier;
+      return idNode.text;
+    } else {
+      return 'unknown';
+    }
+  }
+
+  function traverse(
+    prefix: string,
+    curNode: ts.Node,
+    output: MessageData[] = []
+  ): MessageData[] {
+    if (curNode.kind == ts.SyntaxKind.ObjectLiteralExpression) {
+      const objNode = curNode as ts.ObjectLiteralExpression;
+      objNode.properties.forEach((propNode) => {
+        const assignmentNode = propNode as ts.PropertyAssignment;
+        const nameNode = assignmentNode.name as ts.Identifier;
+        const id = `${prefix}.${nameNode.escapedText}`;
+
+        if (
+          assignmentNode.initializer.kind ==
+          ts.SyntaxKind.ObjectLiteralExpression
+        ) {
+          traverse(id, assignmentNode.initializer, output);
+        } else if (
+          assignmentNode.initializer.kind == ts.SyntaxKind.CallExpression
+        ) {
+          const callNode = assignmentNode.initializer as ts.CallExpression;
+          const argNode = callNode.arguments[0] as ts.StringLiteral;
+
+          output.push({
+            defaultMessage: argNode.text,
+            id: id,
+            params:
+              callNode.typeArguments?.map((typeArg) => {
+                const typeNode = typeArg as ts.TypeLiteralNode;
+                const memberNode = typeNode.members[0] as ts.PropertySignature;
+                const typeIdNode = memberNode.name as ts.Identifier;
+                if (memberNode.type?.kind == ts.SyntaxKind.UnionType) {
+                  const unionNode = memberNode.type as ts.UnionTypeNode;
+                  return {
+                    name: typeIdNode.text,
+                    types: unionNode.types.map((typeNode) =>
+                      typeIdFromNode(typeNode)
+                    ),
+                  };
+                } else {
+                  return {
+                    name: typeIdNode.text,
+                    types: [typeIdFromNode(memberNode.type)],
+                  };
+                }
+              }) ?? [],
+          });
+        }
+      });
+    }
+    return output;
+  }
+
+  const prefixArg = node.arguments[0] as ts.StringLiteral;
+  const output = traverse(prefixArg.text, node.arguments[1]);
+
+  return output;
 }
 
 function findMakeMessages(curNode: ts.Node): ts.CallExpression | undefined {
