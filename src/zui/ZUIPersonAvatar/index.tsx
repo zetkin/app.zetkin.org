68f9db2c7fea73c35a9c49924acaf13e4caaff09
Rebe R <36491300+rebecarubio@users.noreply.github.com>
2023-09-07 14:53:50 +0200

    refactor ZUIPersonAvatar to use ZUIAvatar

--- src/zui/ZUIPersonAvatar/index.tsx
+++ src/zui/ZUIPersonAvatar/index.tsx
@@ -1,37 +1,23 @@
 /* eslint-disable react/display-name */
-import { Avatar } from '@mui/material';
-import { forwardRef } from 'react';
+import ZUIAvatar from 'zui/ZUIAvatar';
 
 interface ZUIPersonAvatarProps {
   orgId: number;
   personId: number;
-  isUserAvatar?: boolean;
   size?: 'sm' | 'md' | 'lg';
 }
-const SIZES = {
-  lg: 50,
-  md: 40,
-  sm: 30,
-};
 
-const ZUIPersonAvatar = forwardRef<HTMLDivElement, ZUIPersonAvatarProps>(
-  (
-    { orgId, personId, isUserAvatar = false, size = 'md', ...restProps },
-    ref
-  ) => {
-    return (
-      <Avatar
-        ref={ref}
-        {...restProps}
-        src={
-          isUserAvatar
-            ? `/api/users/${personId}/avatar`
-            : `/api/orgs/${orgId}/people/${personId}/avatar`
-        }
-        style={{ height: SIZES[size], width: SIZES[size] }}
-      />
-    );
-  }
-);
+const ZUIPersonAvatar: React.FC<ZUIPersonAvatarProps> = ({
+  orgId,
+  personId,
+  size = 'md',
+}) => {
+  return (
+    <ZUIAvatar
+      url={`/api/orgs/${orgId}/people/${personId}/avatar`}
+      size={size}
+    />
+  );
+};
 
 export default ZUIPersonAvatar;
