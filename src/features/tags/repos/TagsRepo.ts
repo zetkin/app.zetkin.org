9623fe6f51eba46766ae141a956c1ce9da3bd339
kaulfield23 <kaulfield23@gmail.com>
2023-09-28 12:00:06 +0200

    remove getTag in tagsRepo

--- src/features/tags/repos/TagsRepo.ts
+++ src/features/tags/repos/TagsRepo.ts
@@ -1,10 +1,9 @@
 import Environment from 'core/env/Environment';
 import IApiClient from 'core/api/client/IApiClient';
-import { IFuture } from 'core/caching/futures';
-import { loadItemIfNecessary } from 'core/caching/cacheUtils';
+
 import { Store } from 'core/store';
 import { ZetkinTag } from 'utils/types/zetkin';
-import { tagAssigned, tagLoad, tagLoaded, tagUnassigned } from '../store';
+import { tagAssigned, tagUnassigned } from '../store';
 
 export default class TagsRepo {
   private _apiClient: IApiClient;
@@ -27,19 +26,6 @@ export default class TagsRepo {
   constructor(env: Environment) {
     this._store = env.store;
     this._apiClient = env.apiClient;
-  }
-
-  getTag(orgId: number, tagId: number): IFuture<ZetkinTag> {
-    const state = this._store.getState();
-    const item = state.tags.tagList.items.find((item) => item.id == tagId);
-    return loadItemIfNecessary(item, this._store, {
-      actionOnLoad: () => tagLoad(tagId),
-      actionOnSuccess: (tag) => tagLoaded(tag),
-      loader: () =>
-        this._apiClient.get<ZetkinTag>(
-          `/api/orgs/${orgId}/people/tags/${tagId}`
-        ),
-    });
   }
 
   async removeTagFromPerson(orgId: number, personId: number, tagId: number) {
