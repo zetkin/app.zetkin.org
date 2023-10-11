0a6918fbcd3b91940b2229e208b5350ef261f1a2
Richard Olsson <r@richardolsson.se>
2023-10-11 15:49:58 +0200

    Add submission loading to useSurveySubmission

--- src/features/surveys/hooks/useSurveySubmission.ts
+++ src/features/surveys/hooks/useSurveySubmission.ts
@@ -1,11 +1,18 @@
-import { surveySubmissionUpdate, surveySubmissionUpdated } from '../store';
-import { useApiClient, useAppDispatch } from 'core/hooks';
+import { loadItemIfNecessary } from 'core/caching/cacheUtils';
+import { futureToObject, IFuture } from 'core/caching/futures';
+import {
+  submissionLoad,
+  submissionLoaded,
+  surveySubmissionUpdate,
+  surveySubmissionUpdated,
+} from '../store';
+import { useApiClient, useAppDispatch, useAppSelector } from 'core/hooks';
 import {
   ZetkinSurveySubmission,
   ZetkinSurveySubmissionPatchBody,
 } from 'utils/types/zetkin';
 
-type UseSurveySubmissionReturn = {
+type UseSurveySubmissionReturn = IFuture<ZetkinSurveySubmission> & {
   setRespondentId: (id: number | null) => void;
 };
 
@@ -16,7 +23,19 @@ export default function useSurveySubmission(
   const apiClient = useApiClient();
   const dispatch = useAppDispatch();
 
+  const submissionItem = useAppSelector((state) =>
+    state.surveys.submissionList.items.find((item) => item.id == submissionId)
+  );
+
+  const future = loadItemIfNecessary(submissionItem, dispatch, {
+    actionOnLoad: () => submissionLoad(submissionId),
+    actionOnSuccess: (data) => submissionLoaded(data),
+    loader: () =>
+      apiClient.get(`/api/orgs/${orgId}/survey_submissions/${submissionId}`),
+  });
+
   return {
+    ...futureToObject(future),
     setRespondentId(id) {
       dispatch(surveySubmissionUpdate([submissionId, ['respondent_id']]));
       apiClient
