168c3754a21e09fc58327e5722d9351cf248058f
Richard Olsson <r@richardolsson.se>
2023-10-13 12:11:02 +0200

    Create useSurveyState() hook

---
+++ src/features/surveys/hooks/useSurveyState.ts
@@ -0,0 +1,41 @@
+import useSurvey from './useSurvey';
+
+export enum SurveyState {
+  UNPUBLISHED = 'unpublished',
+  DRAFT = 'draft',
+  PUBLISHED = 'published',
+  SCHEDULED = 'scheduled',
+  UNKNOWN = 'unknown',
+}
+
+export default function useSurveyState(
+  orgId: number,
+  surveyId: number
+): SurveyState {
+  const { data } = useSurvey(orgId, surveyId);
+
+  if (!data) {
+    return SurveyState.UNKNOWN;
+  }
+
+  if (data.published) {
+    const publishDate = new Date(data.published);
+    const now = new Date();
+
+    if (publishDate > now) {
+      return SurveyState.SCHEDULED;
+    } else {
+      if (data.expires) {
+        const expiryDate = new Date(data.expires);
+
+        if (expiryDate < now) {
+          return SurveyState.UNPUBLISHED;
+        }
+      }
+
+      return SurveyState.PUBLISHED;
+    }
+  } else {
+    return SurveyState.DRAFT;
+  }
+}
