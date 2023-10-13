e489d643ae4ab35d1de68b78435022b1fe227590
Richard Olsson <r@richardolsson.se>
2023-10-13 17:01:45 +0200

    Add updateSurvey() mutation to useSurveyMutations()

--- src/features/surveys/hooks/useSurveyMutations.ts
+++ src/features/surveys/hooks/useSurveyMutations.ts
@@ -1,12 +1,20 @@
 import {
   ELEMENT_TYPE,
   ZetkinOptionsQuestion,
+  ZetkinSurvey,
   ZetkinSurveyElement,
+  ZetkinSurveyExtended,
   ZetkinSurveyOption,
   ZetkinSurveyTextElement,
   ZetkinTextQuestion,
 } from 'utils/types/zetkin';
-import { elementAdded, elementDeleted, elementUpdated } from '../store';
+import {
+  elementAdded,
+  elementDeleted,
+  elementUpdated,
+  surveyUpdate,
+  surveyUpdated,
+} from '../store';
 import { useApiClient, useAppDispatch } from 'core/hooks';
 
 type ZetkinSurveyTextQuestionElementPostBody = {
@@ -56,6 +64,8 @@ type ZetkinSurveyTextElementPatchBody = {
   };
 };
 
+type ZetkinSurveyPatchBody = Partial<Omit<ZetkinSurvey, 'id'>>;
+
 type UseSurveyEditingReturn = {
   addElement: (data: ZetkinSurveyElementPostBody) => Promise<void>;
   deleteElement: (elemId: number) => Promise<void>;
@@ -63,6 +73,7 @@ type UseSurveyEditingReturn = {
     elemId: number,
     data: ZetkinSurveyElementPatchBody
   ) => Promise<void>;
+  updateSurvey: (data: ZetkinSurveyPatchBody) => Promise<void>;
 };
 
 export default function useSurveyMutations(
@@ -72,30 +83,50 @@ export default function useSurveyMutations(
   const apiClient = useApiClient();
   const dispatch = useAppDispatch();
 
+  async function updateSurvey(data: ZetkinSurveyPatchBody) {
+    dispatch(surveyUpdate([surveyId, Object.keys(data)]));
+    apiClient
+      .patch<ZetkinSurveyExtended>(
+        `/api/orgs/${orgId}/surveys/${surveyId}`,
+        data
+      )
+      .then((survey) => {
+        dispatch(surveyUpdated(survey));
+      });
+  }
+
+  async function addElement(data: ZetkinSurveyElementPostBody) {
+    apiClient
+      .post<ZetkinSurveyElement, ZetkinSurveyElementPostBody>(
+        `/api/orgs/${orgId}/surveys/${surveyId}/elements`,
+        data
+      )
+      .then((newElement) => {
+        dispatch(elementAdded([surveyId, newElement]));
+        return newElement;
+      });
+  }
+  async function deleteElement(elemId: number) {
+    await apiClient.delete(
+      `/api/orgs/${orgId}/surveys/${surveyId}/elements/${elemId}`
+    );
+    dispatch(elementDeleted([surveyId, elemId]));
+  }
+  async function updateElement(
+    elemId: number,
+    data: ZetkinSurveyElementPatchBody
+  ) {
+    const element = await apiClient.patch<
+      ZetkinSurveyElement,
+      ZetkinSurveyElementPatchBody
+    >(`/api/orgs/${orgId}/surveys/${surveyId}/elements/${elemId}`, data);
+    dispatch(elementUpdated([surveyId, elemId, element]));
+  }
+
   return {
-    async addElement(data) {
-      apiClient
-        .post<ZetkinSurveyElement, ZetkinSurveyElementPostBody>(
-          `/api/orgs/${orgId}/surveys/${surveyId}/elements`,
-          data
-        )
-        .then((newElement) => {
-          dispatch(elementAdded([surveyId, newElement]));
-          return newElement;
-        });
-    },
-    async deleteElement(elemId) {
-      await apiClient.delete(
-        `/api/orgs/${orgId}/surveys/${surveyId}/elements/${elemId}`
-      );
-      dispatch(elementDeleted([surveyId, elemId]));
-    },
-    async updateElement(elemId, data) {
-      const element = await apiClient.patch<
-        ZetkinSurveyElement,
-        ZetkinSurveyElementPatchBody
-      >(`/api/orgs/${orgId}/surveys/${surveyId}/elements/${elemId}`, data);
-      dispatch(elementUpdated([surveyId, elemId, element]));
-    },
+    addElement,
+    deleteElement,
+    updateElement,
+    updateSurvey,
   };
 }
