916cfc90d61fa1c8110f6b0a9b192756e173b20e
Richard Olsson <r@richardolsson.se>
2023-10-30 14:33:02 +0100

    Create usePersonTags() and store logic for loading tags on person

---
+++ src/features/profile/hooks/usePersonTags.ts
@@ -0,0 +1,19 @@
+import { loadListIfNecessary } from 'core/caching/cacheUtils';
+import { ZetkinTag } from 'utils/types/zetkin';
+import { personTagsLoad, personTagsLoaded } from '../store';
+import { useApiClient, useAppDispatch, useAppSelector } from 'core/hooks';
+
+export default function usePersonTags(orgId: number, personId: number) {
+  const apiClient = useApiClient();
+  const dispatch = useAppDispatch();
+  const tagList = useAppSelector(
+    (state) => state.profiles.tagsByPersonId[personId]
+  );
+
+  return loadListIfNecessary(tagList, dispatch, {
+    actionOnLoad: () => personTagsLoad(personId),
+    actionOnSuccess: (tags) => personTagsLoaded([personId, tags]),
+    loader: () =>
+      apiClient.get<ZetkinTag[]>(`/api/orgs/${orgId}/people/${personId}/tags`),
+  });
+}
