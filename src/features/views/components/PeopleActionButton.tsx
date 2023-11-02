2c0d1a906fc6454e7c9b16c38744fb6ab01cbf19
Rebe R <36491300+rebecarubio@users.noreply.github.com>
2023-11-02 17:35:13 +0100

    create Dialog to display uploadFile component

--- src/features/views/components/PeopleActionButton.tsx
+++ src/features/views/components/PeopleActionButton.tsx
@@ -1,4 +1,7 @@
-import { FC } from 'react';
+import { Box } from '@mui/system';
+import CloseIcon from '@mui/icons-material/Close';
+import { Dialog, DialogTitle, IconButton } from '@mui/material';
+import { FC, useState } from 'react';
 import {
   FolderOutlined,
   InsertDriveFileOutlined,
@@ -6,6 +9,7 @@ import {
 } from '@mui/icons-material';
 
 import messageIds from '../l10n/messageIds';
+import UploadFile from '../../import/components/UploadFile';
 import { useMessages } from 'core/i18n';
 import ViewBrowserModel from '../models/ViewBrowserModel';
 import ZUIButtonMenu from 'zui/ZUIButtonMenu';
@@ -20,31 +24,61 @@ const PeopleActionButton: FC<PeopleActionButtonProps> = ({
   model,
 }) => {
   const messages = useMessages(messageIds);
+  const [showDialog, setShowDialog] = useState(false);
+
+  const handleClickOpen = () => {
+    setShowDialog(true);
+  };
+
+  const handleClose = () => {
+    setShowDialog(false);
+  };
+
   return (
-    <ZUIButtonMenu
-      items={[
-        {
-          icon: <InsertDriveFileOutlined />,
-          label: messages.actions.createView(),
-          onClick: () => {
-            model.createView(folderId || undefined);
+    <Box>
+      <ZUIButtonMenu
+        items={[
+          {
+            icon: <InsertDriveFileOutlined />,
+            label: messages.actions.createView(),
+            onClick: () => {
+              model.createView(folderId || undefined);
+            },
           },
-        },
-        {
-          icon: <FolderOutlined />,
-          label: messages.actions.createFolder(),
-          onClick: () => {
-            model.createView(folderId || undefined);
+          {
+            icon: <FolderOutlined />,
+            label: messages.actions.createFolder(),
+            onClick: () => {
+              model.createView(folderId || undefined);
+            },
           },
-        },
-        {
-          icon: <UploadFileOutlined />,
-          label: messages.actions.importPeople(),
-          onClick: () => {},
-        },
-      ]}
-      label={messages.actions.create()}
-    />
+          {
+            icon: <UploadFileOutlined />,
+            label: messages.actions.importPeople(),
+            onClick: handleClickOpen,
+          },
+        ]}
+        label={messages.actions.create()}
+      />
+      <Dialog onClose={handleClose} open={showDialog}>
+        <DialogTitle sx={{ fontSize: 32 }}>
+          {messages.actions.importPeople()}
+        </DialogTitle>
+        <IconButton
+          aria-label="close"
+          onClick={handleClose}
+          sx={{
+            color: (theme) => theme.palette.grey[500],
+            position: 'absolute',
+            right: 8,
+            top: 8,
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+        <UploadFile />
+      </Dialog>
+    </Box>
   );
 };
 
