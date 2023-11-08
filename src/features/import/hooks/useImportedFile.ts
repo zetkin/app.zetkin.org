3733e1191ad3c6d4f41fe01c62c19994638e3893
Rebe R <36491300+rebecarubio@users.noreply.github.com>
2023-11-08 10:39:45 +0100

    update useImportedFile hook to use the parsed functions, save the parsed file in the store and add loading logic

--- src/features/import/hooks/useImportedFile.ts
+++ src/features/import/hooks/useImportedFile.ts
@@ -1,24 +1,50 @@
-import { parseCSVFile, parseExcelFile } from '../utils/parseFile';
+import { addFiles } from '../store';
+import { parseExcelFile } from '../utils/parseFile';
+import { useState } from 'react';
+import { ImportedFile, parseCSVFile } from '../utils/parseFile';
+import { useAppDispatch, useAppSelector } from 'core/hooks';
 
-export default function useImportedFile() {
-  function importData(file: File) {
+export default function useFileImport() {
+  const [loading, setLoading] = useState(false);
+  const [parsedFile, setParsedFile] = useState<ImportedFile>({
+    sheets: [],
+    title: '',
+  });
+  const files = useAppSelector((state) => state.import.files);
+  const dispatch = useAppDispatch();
+
+  function parseData(file: File) {
+    setLoading(true);
     if (
       file.type === 'text/comma-separated-values' ||
       file.type === 'text/csv' ||
       file.type === 'application/csv'
     ) {
-      return parseCSVFile(file);
+      parseCSVFile(file).then((res) => {
+        saveData(res);
+        setParsedFile(res);
+        setLoading(false);
+      });
     } else if (
       file.type === 'application/vnd.ms-excel' ||
       file.type ===
         'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' ||
       file.type === 'application/xls'
     ) {
-      return parseExcelFile(file);
+      parseExcelFile(file).then((res) => {
+        saveData(res);
+        setParsedFile(res);
+        setLoading(false);
+      });
     } else {
       return null;
     }
   }
 
-  return { importData };
+  function saveData(data: ImportedFile) {
+    dispatch(addFiles(data));
+  }
+
+
+  return { checkIfFileIsAlreadyInStore, loading, parseData };
 }
