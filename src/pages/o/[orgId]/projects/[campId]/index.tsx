78414d5b0f01f789f471c99a6a81d38c5e7d4b66
Richard Olsson <r@richardolsson.se>
2023-11-10 10:24:07 +0100

    Create boilerplate for activist portal pages

---
+++ src/pages/o/[orgId]/projects/[campId]/index.tsx
@@ -0,0 +1,33 @@
+import { FC } from 'react';
+import { scaffold } from 'utils/next';
+
+const scaffoldOptions = {
+  allowNonOfficials: true,
+  authLevelRequired: 1,
+};
+
+export const getServerSideProps = scaffold(async (ctx) => {
+  const { campId, orgId } = ctx.params!;
+
+  return {
+    props: {
+      campId,
+      orgId,
+    },
+  };
+}, scaffoldOptions);
+
+type PageProps = {
+  campId: string;
+  orgId: string;
+};
+
+const Page: FC<PageProps> = ({ orgId, campId }) => {
+  return (
+    <h1>
+      Page for org {orgId}, project {campId}
+    </h1>
+  );
+};
+
+export default Page;
---
+++ src/pages/o/[orgId]/surveys/[surveyId]/index.tsx
@@ -0,0 +1,33 @@
+import { FC } from 'react';
+import { scaffold } from 'utils/next';
+
+const scaffoldOptions = {
+  allowNonOfficials: true,
+  authLevelRequired: 1,
+};
+
+export const getServerSideProps = scaffold(async (ctx) => {
+  const { surveyId, orgId } = ctx.params!;
+
+  return {
+    props: {
+      orgId,
+      surveyId,
+    },
+  };
+}, scaffoldOptions);
+
+type PageProps = {
+  orgId: string;
+  surveyId: string;
+};
+
+const Page: FC<PageProps> = ({ orgId, surveyId }) => {
+  return (
+    <h1>
+      Page for org {orgId}, survey {surveyId}
+    </h1>
+  );
+};
+
+export default Page;
