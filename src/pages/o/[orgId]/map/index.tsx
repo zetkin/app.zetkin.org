3be08cff6773e4452af3cc40cbe2012e7f649609
awarn <anton.warnhag@gmail.com>
2023-11-11 18:50:56 +0100

    Add filters to activist map

--- src/pages/o/[orgId]/map/index.tsx
+++ src/pages/o/[orgId]/map/index.tsx
@@ -1,4 +1,3 @@
-import { Box } from '@mui/material';
 import dynamic from 'next/dynamic';
 import groupEventsByLocation from 'features/events/components/ActivistMap/groupEventsByLocation';
 import { LatLngLiteral } from 'leaflet';
@@ -9,10 +8,30 @@ import { Theme } from '@mui/system';
 import useEventActivities from 'features/campaigns/hooks/useEventActivities';
 import { ZetkinLocation } from 'utils/types/zetkin';
 import { ACTIVITIES, EventActivity } from 'features/campaigns/types';
+import { Box, Checkbox, FormControlLabel, TextField } from '@mui/material';
 import { FC, useState } from 'react';
 
 const useStyles = makeStyles<Theme>(() => ({
-  overlay: {
+  filterLabel: {
+    margin: 0,
+  },
+  filterOverlay: {
+    alignItems: 'flex-start',
+    background: '#fff',
+    border: '1px solid rgba(0, 0, 0, 0.23)',
+    borderRadius: 4,
+    display: 'flex',
+    flexFlow: 'column',
+    justifyContent: 'flex-end',
+    justifySelf: 'flex-end',
+    margin: 2,
+    padding: 8,
+    position: 'absolute',
+    right: 32,
+    top: 100,
+    zIndex: 1000,
+  },
+  searchOverlay: {
     display: 'flex',
     justifyContent: 'flex-end',
     justifySelf: 'flex-end',
@@ -54,6 +73,8 @@ const Page: FC<PageProps> = ({ orgId }) => {
   const [searchString, setSearchString] = useState('');
   const [, setSelectedLocation] = useState<ZetkinLocation | undefined>();
   const [center, setCenter] = useState<LatLngLiteral | undefined>();
+  const [filterNeedParticipants, setFilterNeedParticipants] = useState(false);
+  const [filterString, setFilterString] = useState('');
 
   const { data: activities } = useEventActivities(parseInt(orgId));
 
@@ -62,7 +83,21 @@ const Page: FC<PageProps> = ({ orgId }) => {
       (activity) => activity.kind === ACTIVITIES.EVENT
     ) as EventActivity[];
 
-    const locationsWithEvents = groupEventsByLocation(events);
+    const filteredEvents = events.filter((event) => {
+      return (
+        (filterNeedParticipants
+          ? event.data.num_participants_available <
+            event.data.num_participants_required
+          : true) &&
+        (filterString
+          ? !!event.data.activity?.title
+              .toLowerCase()
+              .match(filterString.toLowerCase())
+          : true)
+      );
+    });
+
+    const locationsWithEvents = groupEventsByLocation(filteredEvents);
     const locations = locationsWithEvents.map(
       (locationGroup) => locationGroup.location
     );
@@ -74,7 +109,7 @@ const Page: FC<PageProps> = ({ orgId }) => {
           locationsWithEvents={locationsWithEvents}
         />
 
-        <Box className={classes.overlay}>
+        <Box className={classes.searchOverlay}>
           <LocationSearch
             onChange={(value: ZetkinLocation) => {
               setSearchString(searchString);
@@ -96,6 +131,31 @@ const Page: FC<PageProps> = ({ orgId }) => {
             onInputChange={(value) => setSearchString(value || '')}
             onTextFieldChange={(value) => setSearchString(value)}
             options={locations}
+          />
+        </Box>
+
+        <Box className={classes.filterOverlay}>
+          <div className={classes.filterLabel}>
+            <TextField
+              label={'Filter'}
+              onChange={(e) => {
+                setFilterString(e.target.value);
+              }}
+            />
+          </div>
+
+          <FormControlLabel
+            className={classes.filterLabel}
+            control={
+              <Checkbox
+                checked={filterNeedParticipants}
+                onChange={() => {
+                  setFilterNeedParticipants(!filterNeedParticipants);
+                }}
+                value={'needparticipants'}
+              />
+            }
+            label={'Needs participants'}
           />
         </Box>
       </>
