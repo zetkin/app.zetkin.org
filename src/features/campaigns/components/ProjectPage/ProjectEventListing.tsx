4501dd39ea5a86dac29890d1c6f306b3996b67bf
Karl Kristiansen <karlkristians@gmail.com>
2023-11-11 16:45:03 +0100

    POC: Adding theme color

--- src/features/campaigns/components/ProjectPage/ProjectEventListing.tsx
+++ src/features/campaigns/components/ProjectPage/ProjectEventListing.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import useEventsFromDateRange from 'features/events/hooks/useEventsFromDateRange';
 import { Card, CardActions, Typography, CardContent, CardMedia, Button } from '@mui/material';
+import theme from '/src/theme';
 
 type EventListingProps = {
   orgId: number;
@@ -21,16 +22,16 @@ const EventListing: FC<EventListingProps> = ({ orgId, campId }) => {
           return (
             <Card sx={{ maxWidth: 345 }}>
               <CardMedia
-                // component="img"
-                // alt="green iguana"
-                // height="140"
-                // image="/static/images/cards/contemplative-reptile.jpg"
+                component="img"
+                alt="green iguana"
+                height="140"
+                image="/static/images/cards/contemplative-reptile.jpg"
               />
               <CardContent>
                 <Typography gutterBottom variant="h5" component="div">
                   {data.data.title}
                 </Typography>
-                <Typography variant="body2" color="text.secondary">
+                <Typography variant="body2" color={theme.palette.secondary.main}>
                   {data.data.info_text}
                 </Typography>
               </CardContent>
