0351c00cd4a5ef24f767c1a75906f801575bf8d6
Karl Kristiansen <karlkristians@gmail.com>
2023-11-11 16:41:35 +0100

    Add in the parts of card component from MUI that we need, and POC a few of the variables

--- src/features/campaigns/components/ProjectPage/ProjectEventListing.tsx
+++ src/features/campaigns/components/ProjectPage/ProjectEventListing.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 import useEventsFromDateRange from 'features/events/hooks/useEventsFromDateRange';
-import { Card, Typography } from '@mui/material';
+import { Card, CardActions, Typography, CardContent, CardMedia, Button } from '@mui/material';
 
 type EventListingProps = {
   orgId: number;
@@ -19,8 +19,25 @@ const EventListing: FC<EventListingProps> = ({ orgId, campId }) => {
       {eventsFromDateRange &&
         eventsFromDateRange.map((data) => {
           return (
-            <Card>
-              <Typography>{data.data.info_text}</Typography>
+            <Card sx={{ maxWidth: 345 }}>
+              <CardMedia
+                // component="img"
+                // alt="green iguana"
+                // height="140"
+                // image="/static/images/cards/contemplative-reptile.jpg"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {data.data.title}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {data.data.info_text}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Share</Button>
+                <Button size="small">Learn More</Button>
+              </CardActions>
             </Card>
           );
         })}
