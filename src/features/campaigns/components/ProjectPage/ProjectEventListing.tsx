cd2ccd187efff3ab7298def02ed63c3ee602cee7
Karl Kristiansen <karlkristians@gmail.com>
2023-11-11 15:13:35 +0100

    Using ZUIFuture, but failing to return data

--- src/features/campaigns/components/ProjectPage/ProjectEventListing.tsx
+++ src/features/campaigns/components/ProjectPage/ProjectEventListing.tsx
@@ -1,19 +1,26 @@
 import { FC } from 'react';
 import { ZetkinEvent } from 'utils/types/zetkin';
+import ZUIFuture from 'zui/ZUIFuture';
 import useEventsFromDateRange from 'features/events/hooks/useEventsFromDateRange';
-import useCampaignEvents from 'features/campaigns/hooks/useCampaignEvents';
 
 type EventListingProps = {
     orgId: number;  
     campId: number;
-    data: ZetkinEvent;
 }
 
-const EventListing: FC<EventListingProps> = ({orgId, campId, data}) => { 
-    const activitiesDateRange = useEventsFromDateRange(new Date(), new Date(Date.now() + 60 * 24 * 60 * 60 * 1000)); // TODO: Smarter date handling. We dont just want to do next 60 days.
-    console.log("Events with date range", activitiesDateRange)
+const EventListing: FC<EventListingProps> = ({orgId, campId}) => { 
+    const eventsFromDateRange = useEventsFromDateRange(new Date(), new Date(Date.now() + 60 * 24 * 60 * 60 * 1000)); // TODO: Smarter date handling. We dont just want to do next 60 days.
 
-    return <h1>{'Hello'}</h1>;
+    return (
+        <>
+            <p>{'Hello hehe'}</p>
+            <ZUIFuture future={eventsFromDateRange}>
+                {(data) => {
+                    <p>{data}</p> // Not working
+                }}
+            </ZUIFuture>
+        </>
+    )
 };
 
-  export default EventListing;
+export default EventListing;
