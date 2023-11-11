593b01340a013fc822333835d6f39859fa34ef9c
Karl Kristiansen <karlkristians@gmail.com>
2023-11-11 18:58:58 +0100

    First try

--- src/features/campaigns/components/ProjectPage/ProjectEventListing.tsx
+++ src/features/campaigns/components/ProjectPage/ProjectEventListing.tsx
@@ -19,60 +19,40 @@ const EventListing: FC<EventListingProps> = ({ orgId, campId }) => {
     (e0, e1) =>
       new Date(e0.data.start_time).getTime() - new Date(e1.data.start_time).getTime()
   );
-  
+
   return (
-    <>
-      {sortedEventsFromDateRange &&
-        sortedEventsFromDateRange.map((data) => {
-          return (
-            <Card sx={{marginTop: '2rem'}}>
-              <CardMedia
-                component="img"
-                alt="green iguana"
-                height="180"
-                image="https://zetkin.org/assets/img/hero.jpg" // TODO: Add dynamic images
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  {data.data.title}
-                </Typography>
-                <Typography variant="body2" color={theme.palette.secondary.main}>
-                  {data.data.info_text}
-                </Typography>
-              </CardContent>
-              <CardActions style={{justifyContent: 'space-between'}}>
-                <Button size="small" href={`/o/${data.data.organization.id}/projects/${data.data.id}`}>Read more</Button>
-                <Button variant="contained" size="small">Count me in!</Button>
-              </CardActions>
-            </Card>
-          );
-        })}
-        {/*
-        {eventsFromDateRange &&
-        eventsFromDateRange.map((data) => {
-          return (
-            <Card sx={{marginTop: '2rem'}}>
-              <CardMedia
-                component="img"
-                alt="green iguana"
-                height="180"
-                image="https://zetkin.org/assets/img/hero.jpg" // TODO: Add dynamic images
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  {data.data.title}
-                </Typography>
-                <Typography variant="body2" color={theme.palette.secondary.main}>
-                  {data.data.info_text}
-                </Typography>
-              </CardContent>
-              <CardActions style={{justifyContent: 'space-between'}}>
-                <Button size="small" href={`/o/${data.data.organization.id}/projects/${data.data.id}`}>Read more</Button>
-                <Button variant="contained" size="small">Count me in!</Button>
-              </CardActions>
-            </Card>
-          );
-        })}*/}
+    <>    
+    {sortedEventsFromDateRange &&
+      sortedEventsFromDateRange.map((data) => {
+        // TODO: Instead of always using the en-US .toLocaleString, update to match the user settings.
+        const startTime = new Date(data.data.start_time).toLocaleString('sv-SE', { hour: 'numeric', minute: 'numeric'});
+
+        return (
+          <Card sx={{marginTop: '2rem'}}>
+            <CardMedia
+              component="img"
+              alt={data.data.title}
+              height="140"
+              image="https://zetkin.org/assets/img/hero.jpg" // TODO: Add dynamic images
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {data.data.title}
+              </Typography>
+              <Typography sx={{color: theme.palette.secondary.main, fontSize: '.7rem'}} gutterBottom variant="p" component="div">
+                  {startTime} - {data.data.location.title}
+              </Typography>
+              <Typography variant="body2" color={theme.palette.secondary.main}>
+                {data.data.info_text}
+              </Typography>
+            </CardContent>
+            <CardActions style={{justifyContent: 'space-between'}}>
+              <Button size="small" href={`/o/${data.data.organization.id}/projects/${data.data.id}`}>Read more</Button>
+              <Button variant="contained" size="small">Count me in!</Button>
+            </CardActions>
+          </Card>
+        );
+      })}
     </>
   );
 };
