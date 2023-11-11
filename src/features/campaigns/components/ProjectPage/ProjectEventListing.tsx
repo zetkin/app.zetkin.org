75e55ba1cd8c13657147f33d131321489c6c3502
Rebe R <36491300+rebecarubio@users.noreply.github.com>
2023-11-11 16:32:47 +0100

    add mapping of array

--- src/features/campaigns/components/ProjectPage/ProjectEventListing.tsx
+++ src/features/campaigns/components/ProjectPage/ProjectEventListing.tsx
@@ -1,24 +1,31 @@
 import { FC } from 'react';
-import EventCard from './EventCard';
 import useEventsFromDateRange from 'features/events/hooks/useEventsFromDateRange';
+import { Card, Typography } from '@mui/material';
 
 type EventListingProps = {
-    orgId: number;  
-    campId: number;
-}
+  orgId: number;
+  campId: number;
+};
 
-const EventListing: FC<EventListingProps> = ({orgId, campId}) => { 
-    const eventsFromDateRange = useEventsFromDateRange(new Date(), new Date(Date.now() + 60 * 24 * 60 * 60 * 1000)); // TODO: Smarter date handling. We dont just want to do next 60 days.
-    console.log("Events with date range", eventsFromDateRange)
+const EventListing: FC<EventListingProps> = ({ orgId, campId }) => {
+  const eventsFromDateRange = useEventsFromDateRange(
+    new Date(),
+    new Date(Date.now() + 60 * 24 * 60 * 60 * 1000)
+  ); // TODO: Smarter date handling. We dont just want to do next 60 days.
+  console.log('Events with date range', eventsFromDateRange);
 
-    return (
-        <>
-            <h1>{eventsFromDateRange[0].data.title}</h1>
-            {eventsFromDateRange.data.map((event) => {
-                <EventCard data={event}></EventCard>
-            })}
-        </>
-    )
+  return (
+    <>
+      {eventsFromDateRange &&
+        eventsFromDateRange.map((data) => {
+          return (
+            <Card>
+              <Typography>{data.data.info_text}</Typography>
+            </Card>
+          );
+        })}
+    </>
+  );
 };
 
 export default EventListing;
