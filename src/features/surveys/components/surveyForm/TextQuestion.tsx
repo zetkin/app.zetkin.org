efa7609c4cd858b75c38502d52d179bd78f29de5
xuan tran <xuandungdkl@gmail.com>
2023-11-12 12:14:10 +0100

    Form styling

--- src/features/surveys/components/surveyForm/TextQuestion.tsx
+++ src/features/surveys/components/surveyForm/TextQuestion.tsx
@@ -6,11 +6,21 @@ const OptionsQuestion: FC<{ element: ZetkinSurveyTextQuestionElement }> = ({
   element,
 }) => {
   return (
-    <FormControl>
-      <FormLabel id="demo-radio-buttons-group-label">
+    <FormControl sx={{ width: '100%' }}>
+      <FormLabel
+        id="demo-radio-buttons-group-label"
+        style={{
+          color: 'black',
+          fontSize: '1.5em',
+          fontWeight: '500',
+          marginBottom: '0.5em',
+          marginTop: '0.5em',
+        }}
+      >
         {element.question.question}
       </FormLabel>
       <TextField
+        fullWidth
         id={`input-${element.id}`}
         name={`${element.id}.text`}
         type="text"
