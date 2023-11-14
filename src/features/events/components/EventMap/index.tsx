c8652bf47ae9b09dbf677eeee4e6e5151b949b71
djbusstop <eribloodlust@grrlz.net>
2023-11-14 16:20:51 +0000

    add event list

--- src/features/events/components/EventMap/index.tsx
+++ src/features/events/components/EventMap/index.tsx
@@ -3,6 +3,7 @@ import { LatLngLiteral } from 'leaflet';
 import { useState } from 'react';
 
 import { EventActivity } from 'features/campaigns/types';
+import EventSignUpList from '../EventSignUpList';
 import groupEventsByLocation from './groupEventsByLocation';
 import LocationDrawer from './LocationDrawer';
 import ZUIMap, { LocationWithData } from 'zui/ZUIMap';
@@ -16,6 +17,7 @@ const EventMap = ({
 }) => {
   const [selectedLocation, setSelectedLocation] =
     useState<LocationWithData<EventActivity>>();
+
   const locationsWithEvents = groupEventsByLocation(events);
 
   return (
@@ -23,7 +25,9 @@ const EventMap = ({
       <ZUIMap
         center={center}
         locations={locationsWithEvents}
-        onClickLocation={(location) => setSelectedLocation(location)}
+        onClickLocation={(location) => {
+          setSelectedLocation(location);
+        }}
       />
 
       <LocationDrawer
@@ -31,9 +35,11 @@ const EventMap = ({
           setSelectedLocation(undefined);
         }}
       >
-        {selectedLocation?.data.map((location) => {
-          return <div key={location.data.id}>{location.data.title}</div>;
-        })}
+        {selectedLocation && (
+          <EventSignUpList
+            events={selectedLocation?.data.map(({ data: event }) => event)}
+          />
+        )}
       </LocationDrawer>
     </Box>
   );
