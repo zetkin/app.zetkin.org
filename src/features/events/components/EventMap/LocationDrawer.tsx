c26895032f9635f77dc70257e93631bfc93931ee
djbusstop <eribloodlust@grrlz.net>
2023-11-14 16:50:52 +0000

    desktop shows from right

--- src/features/events/components/EventMap/LocationDrawer.tsx
+++ src/features/events/components/EventMap/LocationDrawer.tsx
@@ -1,13 +1,19 @@
-import { Box } from '@mui/system';
 import { PropsWithChildren } from 'react';
-import { Button, Drawer } from '@mui/material';
+import { Box, useTheme } from '@mui/system';
+import { Button, Drawer, useMediaQuery } from '@mui/material';
 
 const LocationDrawer = ({
   onClose,
   children,
 }: PropsWithChildren<{ onClose: () => void }>) => {
+  const theme = useTheme();
+  const isDesktop = useMediaQuery(theme.breakpoints.up('md'));
   return (
-    <Drawer anchor={'bottom'} onClose={onClose} open={Boolean(children)}>
+    <Drawer
+      anchor={isDesktop ? 'right' : 'bottom'}
+      onClose={onClose}
+      open={Boolean(children)}
+    >
       <Button onClick={onClose}>Close</Button>
       <Box onClick={onClose} padding={2} role="presentation">
         {children}
