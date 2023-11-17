8c94cff0cf3584b34cdd81ba5daea61cda138553
ziggi <ziggi.willpower@gmail.com>
2023-11-17 11:21:45 +0100

    Create useColumn hook to hold the function to update a column.

--- src/features/import/components/Configure/Mapping/index.tsx
+++ src/features/import/components/Configure/Mapping/index.tsx
@@ -4,10 +4,10 @@ import { Box, Divider, Typography } from '@mui/material';
 import MappingRow from './MappingRow';
 import messageIds from 'features/import/l10n/messageIds';
 import { UIDataColumn } from 'features/import/utils/types';
-import { updateColumn } from 'features/import/store';
+import useColumn from 'features/import/hooks/useColumn';
 import useColumnOptions from 'features/import/hooks/useColumnOptions';
+import { useNumericRouteParams } from 'core/hooks';
 import { Msg, useMessages } from 'core/i18n';
-import { useAppDispatch, useNumericRouteParams } from 'core/hooks';
 
 interface MappingProps {
   columns: UIDataColumn[];
@@ -25,7 +25,8 @@ const Mapping: FC<MappingProps> = ({
   const { orgId } = useNumericRouteParams();
   const messages = useMessages(messageIds);
   const fields = useColumnOptions(orgId);
-  const dispatch = useAppDispatch();
+
+  const updateColumn = useColumn();
 
   return (
     <Box
@@ -59,9 +60,7 @@ const Mapping: FC<MappingProps> = ({
               <MappingRow
                 column={column}
                 isBeingConfigured={columnIndexBeingConfigured == index}
-                onChange={(column) => {
-                  dispatch(updateColumn([index, column]));
-                }}
+                onChange={(column) => updateColumn(index, column)}
                 onConfigureStart={() => onConfigureStart(index)}
                 onDeselectColumn={onDeselectColumn}
                 options={fields}
