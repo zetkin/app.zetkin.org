a22a254a2fc201f7b88253971243410c0e894077
kaulfield23 <kaulfield23@gmail.com>
2023-11-24 16:14:04 +0100

    remove fake data

--- src/features/views/components/PeopleActionButton.tsx
+++ src/features/views/components/PeopleActionButton.tsx
@@ -7,12 +7,9 @@ import {
 } from '@mui/icons-material';
 import { FC, useState } from 'react';
 
-import { addFile } from 'features/import/store';
-import { ColumnKind } from 'features/import/utils/types';
 import Importer from 'features/import/components/Importer';
 import messageIds from '../l10n/messageIds';
 import UploadFile from 'features/import/components/UploadFile';
-import { useAppDispatch } from 'core/hooks';
 import useCreateView from '../hooks/useCreateView';
 import useFolder from '../hooks/useFolder';
 import { useMessages } from 'core/i18n';
@@ -33,7 +30,7 @@ const PeopleActionButton: FC<PeopleActionButtonProps> = ({
 
   const createView = useCreateView(orgId);
   const { createFolder } = useFolder(orgId, folderId);
-  const dispatch = useAppDispatch();
+
   return (
     <Box>
       <ZUIButtonMenu
@@ -55,132 +52,7 @@ const PeopleActionButton: FC<PeopleActionButtonProps> = ({
           {
             icon: <UploadFileOutlined />,
             label: 'importer', //messages.actions.importPeople(),
-            onClick: () => {
-              setImporterDialogOpen(true);
-              dispatch(
-                addFile({
-                  selectedSheetIndex: 0,
-                  sheets: [
-                    {
-                      columns: [
-                        {
-                          kind: ColumnKind.UNKNOWN,
-                          selected: false,
-                        },
-                        {
-                          kind: ColumnKind.UNKNOWN,
-                          selected: false,
-                        },
-                        {
-                          kind: ColumnKind.UNKNOWN,
-                          selected: false,
-                        },
-                        {
-                          kind: ColumnKind.UNKNOWN,
-                          selected: false,
-                        },
-                        {
-                          kind: ColumnKind.UNKNOWN,
-                          selected: false,
-                        },
-                      ],
-                      firstRowIsHeaders: true,
-                      rows: [
-                        {
-                          data: ['Name', 'Last name', 'Email', 'Age', 'Pet'],
-                        },
-                        {
-                          data: [
-                            'Angela',
-                            'Davies',
-                            'angela@gmail.com',
-                            34,
-                            'Rabbit',
-                          ],
-                        },
-                        {
-                          data: ['Maya', 'Angelou', 'maya@gmail.com', 66, null],
-                        },
-                        {
-                          data: ['Rosa', 'Parks', 'rosa@gmail.com', 34, 'Cat'],
-                        },
-                        {
-                          data: [
-                            'Huey',
-                            'P Newton',
-                            'huey@gmail.com',
-                            51,
-                            'Tortoise',
-                          ],
-                        },
-                        {
-                          data: ['Hey', 'P', 'huey@gmail.com', 51, 'Parrot'],
-                        },
-                        {
-                          data: [
-                            'Haeju',
-                            'P Newton',
-                            'huey@gmail.com',
-                            51,
-                            'Cow',
-                          ],
-                        },
-                        {
-                          data: [
-                            'Hello',
-                            'P Newton',
-                            'huey@gmail.com',
-                            51,
-                            'Dog',
-                          ],
-                        },
-                      ],
-                      title: 'Members',
-                    },
-                    {
-                      columns: [
-                        {
-                          kind: ColumnKind.UNKNOWN,
-                          selected: false,
-                        },
-                        {
-                          kind: ColumnKind.UNKNOWN,
-                          selected: false,
-                        },
-                        {
-                          kind: ColumnKind.UNKNOWN,
-                          selected: false,
-                        },
-                        {
-                          kind: ColumnKind.UNKNOWN,
-                          selected: false,
-                        },
-                      ],
-                      firstRowIsHeaders: true,
-                      rows: [
-                        {
-                          data: ['Name', 'Last name', 'Email', 'Age'],
-                        },
-                        {
-                          data: ['Kitty', 'Jonsson', 'kitty@gmail.com', 36],
-                        },
-                        {
-                          data: ['Lasse', 'Brandeby', 'lasse@gmail.com', 81],
-                        },
-                        {
-                          data: ['Pamela', 'Andersson', 'pamela@gmail.com', 61],
-                        },
-                        {
-                          data: ['Jane', 'Austen', 'jane@gmail.com', 102],
-                        },
-                      ],
-                      title: 'Old Members',
-                    },
-                  ],
-                  title: 'Excel file',
-                })
-              );
-            },
+            onClick: () => setImporterDialogOpen(true),
           },
           {
             icon: <UploadFileOutlined />,
