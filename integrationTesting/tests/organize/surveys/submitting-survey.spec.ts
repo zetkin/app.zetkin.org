07d37bd7f32a113d8d53c7318e06e8d7bbc7e97b
Henry Catalini Smith <henry@catalinismith.com>
2023-11-25 14:50:48 +0100

    Add survey email signature playwright test

--- integrationTesting/tests/organize/surveys/submitting-survey.spec.ts
+++ integrationTesting/tests/organize/surveys/submitting-survey.spec.ts
@@ -5,15 +5,12 @@ import KPDMembershipSurvey from '../../../mockData/orgs/KPD/surveys/MembershipSu
 import RosaLuxemburg from '../../../mockData/orgs/KPD/people/RosaLuxemburg';
 import RosaLuxemburgUser from '../../../mockData/users/RosaLuxemburgUser';
 import test from '../../../fixtures/next';
+import { ZetkinSurveyQuestionResponse } from 'utils/types/zetkin';
 
 test.describe('User submitting a survey', () => {
-  test.afterEach(({ moxy }) => {
-    moxy.teardown();
-  });
+  const apiPostPath = `/orgs/${KPDMembershipSurvey.organization.id}/surveys/${KPDMembershipSurvey.id}/submissions`;
 
-  test('submits data successfully', async ({ appUri, login, moxy, page }) => {
-    const apiPostPath = `/orgs/${KPDMembershipSurvey.organization.id}/surveys/${KPDMembershipSurvey.id}/submissions`;
-
+  test.beforeEach(async ({ appUri, login, moxy, page }) => {
     moxy.setZetkinApiMock(
       `/orgs/${KPDMembershipSurvey.organization.id}/surveys/${KPDMembershipSurvey.id}`,
       'get',
@@ -28,7 +25,7 @@ test.describe('User submitting a survey', () => {
       }
     );
 
-    const memberships = [
+    login(RosaLuxemburgUser, [
       {
         organization: KPD,
         profile: {
@@ -37,13 +34,18 @@ test.describe('User submitting a survey', () => {
         },
         role: null,
       },
-    ];
+    ]);
 
-    login(RosaLuxemburgUser, memberships);
     await page.goto(
       `${appUri}/o/${KPDMembershipSurvey.organization.id}/surveys/${KPDMembershipSurvey.id}`
     );
+  });
 
+  test.afterEach(({ moxy }) => {
+    moxy.teardown();
+  });
+
+  test('submits responses', async ({ moxy, page }) => {
     await page.click('input[name="1.options"]');
     await page.fill('input[name="2.text"]', 'Topple capitalism');
     await page.click('input[name="sig"][value="authenticated"]');
@@ -53,19 +55,52 @@ test.describe('User submitting a survey', () => {
       await page.click('data-testid=Survey-submit'),
     ]);
 
-    const reqLog = moxy.log(`/v1${apiPostPath}`);
-    expect(reqLog.length).toBe(1);
-    expect(reqLog[0].data).toMatchObject({
-      responses: [
-        {
-          options: [1],
-          question_id: KPDMembershipSurvey.elements[0].id,
-        },
-        {
-          question_id: KPDMembershipSurvey.elements[1].id,
-          response: 'Topple capitalism',
-        },
-      ],
+    const log = moxy.log(`/v1${apiPostPath}`);
+    expect(log.length).toBe(1);
+    const [request] = log;
+    const data = request.data as {
+      responses: ZetkinSurveyQuestionResponse[];
+    };
+    expect(data.responses.length).toBe(2);
+    expect(data.responses).toMatchObject([
+      {
+        options: [1],
+        question_id: KPDMembershipSurvey.elements[0].id,
+      },
+      {
+        question_id: KPDMembershipSurvey.elements[1].id,
+        response: 'Topple capitalism',
+      },
+    ]);
+  });
+
+  test('submits email signature', async ({ moxy, page }) => {
+    await page.click('input[name="1.options"]');
+    await page.fill('input[name="2.text"]', 'Topple capitalism');
+    await page.click('input[name="sig"][value="email"]');
+    await page.fill('input[name="sig.email"]', 'testuser@example.org');
+    await page.fill('input[name="sig.first_name"]', 'Test');
+    await page.fill('input[name="sig.last_name"]', 'User');
+    await page.click('data-testid=Survey-acceptTerms');
+    await Promise.all([
+      page.waitForResponse((res) => res.request().method() == 'POST'),
+      await page.click('data-testid=Survey-submit'),
+    ]);
+
+    const log = moxy.log(`/v1${apiPostPath}`);
+    expect(log.length).toBe(1);
+    const [request] = log;
+    const data = request.data as {
+      signature: {
+        email: string;
+        first_name: string;
+        last_name: string;
+      };
+    };
+    expect(data.signature).toMatchObject({
+      email: 'testuser@example.org',
+      first_name: 'Test',
+      last_name: 'User',
     });
   });
 });
