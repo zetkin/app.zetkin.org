bf5969fbd2012ac1bd000e3b49465885604f9c5e
Henry Catalini Smith <henry@catalinismith.com>
2023-11-25 13:14:23 +0100

    Get error bg color from theme

--- src/features/surveys/components/surveyForm/ErrorMessage.tsx
+++ src/features/surveys/components/surveyForm/ErrorMessage.tsx
@@ -2,10 +2,18 @@ import Box from '@mui/material/Box';
 import { FC } from 'react';
 import messageIds from 'features/surveys/l10n/messageIds';
 import { Msg } from 'core/i18n';
+import { useTheme } from '@mui/material';
 
 const ErrorMessage: FC = () => {
+  const theme = useTheme();
   return (
-    <Box bgcolor="#ee878a" mx={4} my={4} p={2} sx={{ borderRadius: '5px' }}>
+    <Box
+      bgcolor={theme.palette.error.light}
+      mx={4}
+      my={4}
+      p={2}
+      sx={{ borderRadius: '5px' }}
+    >
       <Msg id={messageIds.surveyForm.error} />
     </Box>
   );
