c5d7b08bedd3372b7838ded93317ceb7098c9670
kaulfield23 <kaulfield23@gmail.com>
2023-12-03 19:28:57 +0100

    add test for duplicated ids

--- src/features/import/utils/prepareImportOperations.spec.ts
+++ src/features/import/utils/prepareImportOperations.spec.ts
@@ -823,5 +823,82 @@ describe('prepareImportOperations()', () => {
         },
       ]);
     });
+    it('remove duplicated tagIds and orgIds', () => {
+      const configData: Sheet = {
+        columns: [
+          {
+            field: 'city',
+            kind: ColumnKind.FIELD,
+            selected: true,
+          },
+          { idField: 'ext_id', kind: ColumnKind.ID_FIELD, selected: true },
+          {
+            kind: ColumnKind.ORGANIZATION,
+            mapping: [
+              { orgId: 111, value: 1 },
+              { orgId: 333, value: 2 },
+            ],
+            selected: true,
+          },
+          {
+            kind: ColumnKind.ORGANIZATION,
+            mapping: [
+              { orgId: 111, value: 3 },
+              { orgId: 222, value: 4 },
+            ],
+            selected: true,
+          },
+          {
+            kind: ColumnKind.TAG,
+            mapping: [
+              { tagIds: [123, 100], value: 'Frontend' },
+              { tagIds: [124, 100], value: 'Backend' },
+            ],
+
+            selected: true,
+          },
+          {
+            kind: ColumnKind.TAG,
+            mapping: [
+              { tagIds: [100, 222], value: 'Cat' },
+              { tagIds: [333, 444], value: 'Dog' },
+            ],
+
+            selected: true,
+          },
+        ],
+        firstRowIsHeaders: false,
+        rows: [
+          {
+            data: ['Linköping', '123', 1, 3, 'Frontend', 'Cat'],
+          },
+          {
+            data: ['Linköping', '125', 2, 4, 'Backend', 'Dog'],
+          },
+        ],
+        title: 'My sheet',
+      };
+      const result = prepareImportOperations(configData);
+      expect(result).toEqual([
+        {
+          fields: {
+            city: 'Linköping',
+            ext_id: '123',
+          },
+          op: 'person.import',
+          organizations: [111],
+          tags: [123, 100, 222],
+        },
+        {
+          fields: {
+            city: 'Linköping',
+            ext_id: '125',
+          },
+          op: 'person.import',
+          organizations: [333, 222],
+          tags: [124, 100, 333, 444],
+        },
+      ]);
+    });
   });
 });
