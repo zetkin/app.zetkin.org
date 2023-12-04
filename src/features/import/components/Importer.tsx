c200d23fb6b9dae53117d7dbf2804f5a415b6011
ziggi <ziggi.willpower@gmail.com>
2023-12-04 16:13:18 +0100

    Remove redundant fragment.

--- src/features/import/components/Importer.tsx
+++ src/features/import/components/Importer.tsx
@@ -35,56 +35,54 @@ const Importer: FC<ImporterProps> = ({ open, onClose }) => {
       open={open}
     >
       <Box display="flex" flexDirection="column" overflow="hidden" padding={2}>
-        <>
-          {activeStep == 0 && (
-            <Upload
-              onClose={onClose}
-              onSuccess={() => {
-                setActiveStep(1);
-                setMaxWidth('lg');
-              }}
-            />
-          )}
-          {activeStep == 1 && (
-            <Configure
-              onClose={() => {
-                onClose();
-                setActiveStep(0);
-              }}
-              onRestart={() => setActiveStep(0)}
-              onValidate={async () => {
-                if (importPreview) {
-                  await importPreview();
-                }
-                setActiveStep(2);
-              }}
-            />
-          )}
-          {activeStep === 2 && (
-            <Validation
-              onClickBack={() => setActiveStep(1)}
-              onClose={() => {
-                onClose();
-                setActiveStep(0);
-              }}
-              onImportDone={() => setActiveStep(3)}
-              onImportStart={() => setMaxWidth('sm')}
-            />
-          )}
-          {activeStep === 3 && (
-            <ImportStatus
-              onClickBack={() => setActiveStep(2)}
-              onClose={() => {
-                onClose();
-                setActiveStep(0);
-              }}
-              onDone={() => {
-                onClose();
-                setActiveStep(0);
-              }}
-            />
-          )}
-        </>
+        {activeStep == 0 && (
+          <Upload
+            onClose={onClose}
+            onSuccess={() => {
+              setActiveStep(1);
+              setMaxWidth('lg');
+            }}
+          />
+        )}
+        {activeStep == 1 && (
+          <Configure
+            onClose={() => {
+              onClose();
+              setActiveStep(0);
+            }}
+            onRestart={() => setActiveStep(0)}
+            onValidate={async () => {
+              if (importPreview) {
+                await importPreview();
+              }
+              setActiveStep(2);
+            }}
+          />
+        )}
+        {activeStep === 2 && (
+          <Validation
+            onClickBack={() => setActiveStep(1)}
+            onClose={() => {
+              onClose();
+              setActiveStep(0);
+            }}
+            onImportDone={() => setActiveStep(3)}
+            onImportStart={() => setMaxWidth('sm')}
+          />
+        )}
+        {activeStep === 3 && (
+          <ImportStatus
+            onClickBack={() => setActiveStep(2)}
+            onClose={() => {
+              onClose();
+              setActiveStep(0);
+            }}
+            onDone={() => {
+              onClose();
+              setActiveStep(0);
+            }}
+          />
+        )}
       </Box>
     </Dialog>
   );
