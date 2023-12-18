44abd638f6e1f1b0d374c3bdc35c9724a2708fe2
Henry Catalini Smith <henry@catalinismith.com>
2023-12-18 07:12:52 +0100

    Apply WAI-ARIA form control grouping semantics to privacy policy

--- src/features/surveys/components/surveyForm/SurveyPrivacyPolicy.tsx
+++ src/features/surveys/components/surveyForm/SurveyPrivacyPolicy.tsx
@@ -1,8 +1,15 @@
-import Box from '@mui/material/Box';
 import { FC } from 'react';
 import messageIds from 'features/surveys/l10n/messageIds';
 import { ZetkinSurveyExtended } from 'utils/types/zetkin';
-import { Checkbox, FormControlLabel, Link, Typography } from '@mui/material';
+import {
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  FormLabel,
+  Link,
+  Typography,
+} from '@mui/material';
 import { Msg, useMessages } from 'core/i18n';
 
 export type SurveyPrivacyPolicyProps = {
@@ -16,38 +23,48 @@ const SurveyPrivacyPolicy: FC<SurveyPrivacyPolicyProps> = ({
 }) => {
   const messages = useMessages(messageIds);
   return (
-    <Box alignItems="center" component="section" sx={{ py: 2 }}>
-      <Typography fontWeight={'bold'}>
-        <Msg id={messageIds.surveyForm.terms.title} />
-      </Typography>
-
-      <FormControlLabel
-        control={
-          <Checkbox
-            defaultChecked={formData['privacy.approval'] === 'on'}
-            required
-          />
-        }
-        data-testid="Survey-acceptTerms"
-        label={<Msg id={messageIds.surveyForm.accept} />}
-        name="privacy.approval"
-      />
-      <Typography style={{ fontSize: '0.8em' }}>
-        <Msg
-          id={messageIds.surveyForm.terms.description}
-          values={{ organization: survey.organization.title }}
-        />
-      </Typography>
-      <Typography style={{ fontSize: '0.8em', marginBottom: '0.5em' }}>
-        <Link
-          href={messages.surveyForm.policy.link()}
-          rel="noreferrer"
-          target="_blank"
+    <FormControl>
+      <FormGroup aria-labelledby="privacy-policy-label">
+        <FormLabel
+          id="privacy-policy-label"
+          style={{
+            color: 'black',
+            fontSize: '1.5em',
+            fontWeight: '500',
+            marginBottom: '0.5em',
+            marginTop: '0.5em',
+          }}
         >
-          <Msg id={messageIds.surveyForm.policy.text} />
-        </Link>
-      </Typography>
-    </Box>
+          <Msg id={messageIds.surveyForm.terms.title} />
+        </FormLabel>
+        <FormControlLabel
+          control={
+            <Checkbox
+              defaultChecked={formData['privacy.approval'] === 'on'}
+              required
+            />
+          }
+          data-testid="Survey-acceptTerms"
+          label={<Msg id={messageIds.surveyForm.accept} />}
+          name="privacy.approval"
+        />
+        <Typography style={{ fontSize: '0.8em' }}>
+          <Msg
+            id={messageIds.surveyForm.terms.description}
+            values={{ organization: survey.organization.title }}
+          />
+        </Typography>
+        <Typography style={{ fontSize: '0.8em', marginBottom: '0.5em' }}>
+          <Link
+            href={messages.surveyForm.policy.link()}
+            rel="noreferrer"
+            target="_blank"
+          >
+            <Msg id={messageIds.surveyForm.policy.text} />
+          </Link>
+        </Typography>
+      </FormGroup>
+    </FormControl>
   );
 };
 
