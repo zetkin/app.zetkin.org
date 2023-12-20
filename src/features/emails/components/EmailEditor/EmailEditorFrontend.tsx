bd01b9b6f00a896aa2021432c9124739f6c06c1e
Richard Olsson <r@richardolsson.se>
2023-12-20 10:59:36 +0100

    Add barebones editor to new design tab

---
+++ src/features/emails/components/EmailEditor/EmailEditorFrontend.tsx
@@ -0,0 +1,52 @@
+import EditorJS, { EditorConfig, OutputData } from '@editorjs/editorjs';
+import { FC, useEffect, useRef } from 'react';
+
+export type EditorProps = {
+  onSave?: (data: OutputData) => void;
+};
+
+const EmailEditorFrontend: FC<EditorProps> = ({ onSave }) => {
+  const editorInstance = useRef<EditorJS | null>(null);
+
+  const saved = async () => {
+    try {
+      const savedData = await editorInstance.current?.save();
+      if (savedData && onSave) {
+        onSave(savedData);
+      }
+    } catch (error) {
+      //TODO: handle error
+    }
+  };
+
+  useEffect(() => {
+    const editorConfig: EditorConfig = {
+      // TODO: Find way to make unique IDs
+      holder: 'ClientOnlyEditor-container',
+      inlineToolbar: ['bold', 'link', 'italic'],
+      onChange: () => {
+        saved();
+      },
+      tools: {},
+    };
+
+    // Create the EditorJS instance
+    editorInstance.current = new EditorJS(editorConfig);
+
+    return () => {
+      // Cleanup when the component is unmounted
+      if (editorInstance.current) {
+        editorInstance.current.destroy();
+      }
+    };
+  }, []);
+
+  return (
+    <div
+      id="ClientOnlyEditor-container"
+      style={{ backgroundColor: 'white', border: '1px solid black' }}
+    />
+  );
+};
+
+export default EmailEditorFrontend;
