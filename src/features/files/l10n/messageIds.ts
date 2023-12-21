712bf0b73193d3b99884a839614b08692de3a535
ziggabyte <ziggi.willpower@gmail.com>
2023-12-21 17:24:13 +0100

    Create image uploader card

--- src/features/files/l10n/messageIds.ts
+++ src/features/files/l10n/messageIds.ts
@@ -1,6 +1,18 @@
 import { m, makeMessages } from 'core/i18n';
 
 export default makeMessages('feat.files', {
+  imageUpload: {
+    dialogButtons: {
+      configure: m('Configure'),
+      restart: m('Restart'),
+    },
+    instructions: m(' or drag and drop'),
+    loading: m('Loading file...'),
+    release: m('Release the file here'),
+    selectClick: m('Click to upload'),
+    types: m('JPG, PNG or GIF'),
+    unsupportedFile: m('Unsupported file format.'),
+  },
   libraryDialog: {
     title: m('Library'),
   },
