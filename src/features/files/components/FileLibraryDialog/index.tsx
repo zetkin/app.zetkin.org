3b985bea5b407b654d1a81141fedd4961638e08a
Richard Olsson <r@richardolsson.se>
2023-12-21 16:57:08 +0100

    Implement crude version of drag-and-drop file upload

--- src/features/files/components/FileLibraryDialog/index.tsx
+++ src/features/files/components/FileLibraryDialog/index.tsx
@@ -12,6 +12,7 @@ import { FC, useState } from 'react';
 import LibraryImageCard from '../LibraryImageCard';
 import messageIds from 'features/files/l10n/messageIds';
 import useFiles from 'features/files/hooks/useFiles';
+import useFileUploads from 'features/files/hooks/useFileUploads';
 import { useMessages } from 'core/i18n';
 import { ZetkinFile } from 'utils/types/zetkin';
 import ZUIDialog from 'zui/ZUIDialog';
@@ -46,6 +47,8 @@ const FileLibraryDialog: FC<Props> = ({
   const filesFuture = useFiles(orgId);
   const messages = useMessages(messageIds);
 
+  const { getDropZoneProps } = useFileUploads(orgId, { multiple: false });
+
   return (
     <ZUIDialog
       maxWidth="lg"
@@ -53,96 +56,100 @@ const FileLibraryDialog: FC<Props> = ({
       open={open}
       title={messages.libraryDialog.title()}
     >
-      <Box display="flex" gap={1} mt={1}>
-        <TextField
-          fullWidth
-          label={messages.searching.label()}
-          onChange={(ev) => setFilterText(ev.currentTarget.value)}
-          value={filterText}
-        />
-        <FormControl fullWidth>
-          <InputLabel>{messages.sorting.label()}</InputLabel>
-          <Select
-            label={messages.sorting.label()}
-            onChange={(ev) => setSorting(ev.target.value)}
-            value={sorting}
-          >
-            <MenuItem value="date">{messages.sorting.options.date()}</MenuItem>
-            <MenuItem value="originalName">
-              {messages.sorting.options.originalName()}
-            </MenuItem>
-          </Select>
-        </FormControl>
-        <FormControl disabled={!!type} fullWidth>
-          <InputLabel>{messages.typeFilter.label()}</InputLabel>
-          <Select
-            label={messages.typeFilter.label()}
-            onChange={(ev) =>
-              setFilterType(
-                ev.target.value == 'any'
-                  ? 'any'
-                  : (ev.target.value as TypeOption)
-              )
-            }
-            value={filterType}
-          >
-            <MenuItem value="any">{messages.typeFilter.anyOption()}</MenuItem>
-            {Object.keys(TYPE_OPTIONS).map((typeStr) => {
-              if (!(typeStr in TYPE_OPTIONS)) {
-                throw new Error('Unknown format');
+      <Box {...getDropZoneProps()}>
+        <Box display="flex" gap={1} mt={1}>
+          <TextField
+            fullWidth
+            label={messages.searching.label()}
+            onChange={(ev) => setFilterText(ev.currentTarget.value)}
+            value={filterText}
+          />
+          <FormControl fullWidth>
+            <InputLabel>{messages.sorting.label()}</InputLabel>
+            <Select
+              label={messages.sorting.label()}
+              onChange={(ev) => setSorting(ev.target.value)}
+              value={sorting}
+            >
+              <MenuItem value="date">
+                {messages.sorting.options.date()}
+              </MenuItem>
+              <MenuItem value="originalName">
+                {messages.sorting.options.originalName()}
+              </MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl disabled={!!type} fullWidth>
+            <InputLabel>{messages.typeFilter.label()}</InputLabel>
+            <Select
+              label={messages.typeFilter.label()}
+              onChange={(ev) =>
+                setFilterType(
+                  ev.target.value == 'any'
+                    ? 'any'
+                    : (ev.target.value as TypeOption)
+                )
               }
+              value={filterType}
+            >
+              <MenuItem value="any">{messages.typeFilter.anyOption()}</MenuItem>
+              {Object.keys(TYPE_OPTIONS).map((typeStr) => {
+                if (!(typeStr in TYPE_OPTIONS)) {
+                  throw new Error('Unknown format');
+                }
 
-              // This cast is safe because the error above would have been
-              // thrown if typeStr is not one of the allowed strings.
-              const typeKey = typeStr as TypeOption;
+                // This cast is safe because the error above would have been
+                // thrown if typeStr is not one of the allowed strings.
+                const typeKey = typeStr as TypeOption;
+
+                return (
+                  <MenuItem key={typeKey} value={typeKey}>
+                    {messages.typeFilter.options[typeKey]()}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+          </FormControl>
+        </Box>
+        <Box>
+          <ZUIFuture future={filesFuture}>
+            {(allFiles) => {
+              const filteredFiles = allFiles.filter((file) => {
+                const matchesText =
+                  !filterText ||
+                  file.original_name.toLowerCase().includes(filterText);
+
+                const matchesType =
+                  filterType == 'any' ||
+                  TYPE_OPTIONS[filterType].includes(file.mime_type);
+
+                return matchesText && matchesType;
+              });
+              const sortedFiles = filteredFiles.sort((f0, f1) => {
+                if (sorting == 'originalName') {
+                  return f0.original_name.localeCompare(f1.original_name);
+                } else {
+                  const d0 = new Date(f0.uploaded);
+                  const d1 = new Date(f1.uploaded);
+                  return d1.getTime() - d0.getTime();
+                }
+              });
 
               return (
-                <MenuItem key={typeKey} value={typeKey}>
-                  {messages.typeFilter.options[typeKey]()}
-                </MenuItem>
+                <Grid container>
+                  {sortedFiles.map((file) => (
+                    <Grid key={file.id} md={3} p={1} xs={12}>
+                      <LibraryImageCard
+                        imageFile={file}
+                        onSelectImage={() => onSelectFile(file)}
+                      />
+                    </Grid>
+                  ))}
+                </Grid>
               );
-            })}
-          </Select>
-        </FormControl>
-      </Box>
-      <Box>
-        <ZUIFuture future={filesFuture}>
-          {(allFiles) => {
-            const filteredFiles = allFiles.filter((file) => {
-              const matchesText =
-                !filterText ||
-                file.original_name.toLowerCase().includes(filterText);
-
-              const matchesType =
-                filterType == 'any' ||
-                TYPE_OPTIONS[filterType].includes(file.mime_type);
-
-              return matchesText && matchesType;
-            });
-            const sortedFiles = filteredFiles.sort((f0, f1) => {
-              if (sorting == 'originalName') {
-                return f0.original_name.localeCompare(f1.original_name);
-              } else {
-                const d0 = new Date(f0.uploaded);
-                const d1 = new Date(f1.uploaded);
-                return d1.getTime() - d0.getTime();
-              }
-            });
-
-            return (
-              <Grid container>
-                {sortedFiles.map((file) => (
-                  <Grid key={file.id} md={3} p={1} xs={12}>
-                    <LibraryImageCard
-                      imageFile={file}
-                      onSelectImage={() => onSelectFile(file)}
-                    />
-                  </Grid>
-                ))}
-              </Grid>
-            );
-          }}
-        </ZUIFuture>
+            }}
+          </ZUIFuture>
+        </Box>
       </Box>
     </ZUIDialog>
   );
