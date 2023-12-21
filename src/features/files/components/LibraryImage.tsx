6eb4b33e87152877bb1bf63798b90bf693f1bc54
ziggabyte <ziggi.willpower@gmail.com>
2023-12-21 15:54:29 +0100

    Create parent component to display LibraryImage in the gallery.

--- src/features/files/components/LibraryImage.tsx
+++ src/features/files/components/LibraryImage.tsx
@@ -1,27 +1,27 @@
-import { Box } from '@mui/material';
 import { FC } from 'react';
 import Image from 'next/image';
+import { Box, useTheme } from '@mui/material';
 
 import { ZetkinFile } from 'utils/types/zetkin';
 
 interface LibraryImageProps {
-  onSelectImage: () => void;
   imageFile: ZetkinFile;
 }
 
-const LibraryImage: FC<LibraryImageProps> = ({ imageFile, onSelectImage }) => {
+const LibraryImage: FC<LibraryImageProps> = ({ imageFile }) => {
+  const theme = useTheme();
+
   return (
     <Box
-      height={200}
-      onClick={onSelectImage}
+      border={1}
+      borderColor={theme.palette.grey[400]}
+      borderRadius={1}
       sx={{
         backgroundImage:
           'linear-gradient(45deg, #808080 25%, transparent 25%), linear-gradient(-45deg, #808080 25%, transparent 25%), linear-gradient(45deg, transparent 75%, #808080 75%), linear-gradient(-45deg, transparent 75%, #808080 75%);',
         backgroundPosition: '0 0, 0 15px, 15px -15px, -15px 0px',
         backgroundSize: '30px 30px',
-        cursor: 'pointer',
       }}
-      width={200}
     >
       <Image
         alt={imageFile.original_name}
