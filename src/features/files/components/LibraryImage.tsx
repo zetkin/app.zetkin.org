e36565aa34b5700e8ad32eede0206e9fc0bc2d1c
Richard Olsson <r@richardolsson.se>
2023-12-21 16:16:07 +0100

    Tweak styling of LibraryImage

--- src/features/files/components/LibraryImage.tsx
+++ src/features/files/components/LibraryImage.tsx
@@ -1,6 +1,6 @@
-import { FC } from 'react';
 import Image from 'next/image';
 import { Box, useTheme } from '@mui/material';
+import { FC, useState } from 'react';
 
 import { ZetkinFile } from 'utils/types/zetkin';
 
@@ -8,19 +8,29 @@ interface LibraryImageProps {
   imageFile: ZetkinFile;
 }
 
+const DEFAULT_GRID_INTENSITY = 0.02;
+
 const LibraryImage: FC<LibraryImageProps> = ({ imageFile }) => {
   const theme = useTheme();
+  const [gridIntensity, setGridIntensity] = useState(DEFAULT_GRID_INTENSITY);
 
   return (
     <Box
       border={1}
       borderColor={theme.palette.grey[400]}
       borderRadius={1}
+      onMouseEnter={() => {
+        setGridIntensity(0.15);
+      }}
+      onMouseLeave={() => {
+        setGridIntensity(DEFAULT_GRID_INTENSITY);
+      }}
       sx={{
-        backgroundImage:
-          'linear-gradient(45deg, #808080 25%, transparent 25%), linear-gradient(-45deg, #808080 25%, transparent 25%), linear-gradient(45deg, transparent 75%, #808080 75%), linear-gradient(-45deg, transparent 75%, #808080 75%);',
+        backgroundColor: '#eee',
+        backgroundImage: `linear-gradient(45deg, rgba(0,0,0,${gridIntensity}) 25%, transparent 25%), linear-gradient(-45deg, rgba(0,0,0,${gridIntensity}) 25%, transparent 25%), linear-gradient(45deg, transparent 75%, rgba(0,0,0,${gridIntensity}) 75%), linear-gradient(-45deg, transparent 75%, rgba(0,0,0,${gridIntensity}) 75%);`,
         backgroundPosition: '0 0, 0 15px, 15px -15px, -15px 0px',
         backgroundSize: '30px 30px',
+        transition: 'backgroundImage 0.2s',
       }}
     >
       <Image
