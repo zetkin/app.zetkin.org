c7105baadbc96cea41a8430040df0a0f521efe9b
ziggabyte <ziggi.willpower@gmail.com>
2024-01-12 12:58:23 +0100

    Sort imports.

--- src/features/emails/components/EmailEditor/index.tsx
+++ src/features/emails/components/EmailEditor/index.tsx
@@ -1,6 +1,7 @@
 import dynamic from 'next/dynamic';
-import { EditorProps } from './EmailEditorFrontend';
 import { FC } from 'react';
+
+import { EditorProps } from './EmailEditorFrontend';
 
 const EmailEditorFrontend = dynamic(import('./EmailEditorFrontend'), {
   ssr: false,
