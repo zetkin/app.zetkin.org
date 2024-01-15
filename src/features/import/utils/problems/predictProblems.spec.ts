2ccfcf52deb12aad709f039a077323cf63afb81e
Richard Olsson <r@richardolsson.se>
2024-01-15 11:18:17 +0100

    Create predictProblems() utility

---
+++ src/features/import/utils/problems/predictProblems.spec.ts
@@ -0,0 +1,337 @@
+import { ImportProblemKind } from './types';
+import { predictProblems } from './predictProblems';
+import { ColumnKind, Sheet } from '../types';
+import { CUSTOM_FIELD_TYPE, ZetkinCustomField } from 'utils/types/zetkin';
+
+function makeFullSheet(overrides: Partial<Sheet>): Sheet {
+  return {
+    columns: [],
+    firstRowIsHeaders: false,
+    rows: [],
+    title: '',
+    ...overrides,
+  };
+}
+
+function makeFullField(
+  overrides: Partial<ZetkinCustomField>
+): ZetkinCustomField {
+  return {
+    description: '',
+    id: 1001,
+    organization: {
+      id: 1,
+      title: 'KPD',
+    },
+    slug: 'field',
+    title: 'Field',
+    type: CUSTOM_FIELD_TYPE.TEXT,
+    ...overrides,
+  };
+}
+
+describe('predictProblem()', () => {
+  it('returns NO_IMPACT problem for empty sheet', () => {
+    const sheet: Sheet = {
+      columns: [],
+      firstRowIsHeaders: false,
+      rows: [],
+      title: '',
+    };
+    const problems = predictProblems(sheet, 'SE', []);
+
+    expect(problems).toEqual([
+      {
+        kind: ImportProblemKind.UNCONFIGURED_ID_AND_NAME,
+      },
+      {
+        kind: ImportProblemKind.NO_IMPACT,
+      },
+    ]);
+  });
+
+  it('returns NO_IMPACT when no columns are selected', () => {
+    const sheet = makeFullSheet({
+      columns: [
+        {
+          field: 'first_name',
+          kind: ColumnKind.FIELD,
+          selected: false,
+        },
+      ],
+      rows: [{ data: ['Clara'] }],
+    });
+    const problems = predictProblems(sheet, 'SE', []);
+
+    expect(problems).toEqual([
+      {
+        kind: ImportProblemKind.UNCONFIGURED_ID_AND_NAME,
+      },
+      {
+        kind: ImportProblemKind.NO_IMPACT,
+      },
+    ]);
+  });
+
+  it('finds no problems in valid sheet', () => {
+    const sheet = makeFullSheet({
+      columns: [
+        {
+          idField: 'id',
+          kind: ColumnKind.ID_FIELD,
+          selected: true,
+        },
+        {
+          idField: 'ext_id',
+          kind: ColumnKind.ID_FIELD,
+          selected: true,
+        },
+        {
+          field: 'first_name',
+          kind: ColumnKind.FIELD,
+          selected: true,
+        },
+        {
+          field: 'email',
+          kind: ColumnKind.FIELD,
+          selected: true,
+        },
+      ],
+      rows: [{ data: [1, 'a', 'Clara', 'clara@example.com'] }],
+    });
+
+    const problems = predictProblems(sheet, 'SE', []);
+    expect(problems).toEqual([]);
+  });
+
+  it('correctly validates format for email, phone, date and url', () => {
+    const sheet = makeFullSheet({
+      columns: [
+        {
+          idField: 'id',
+          kind: ColumnKind.ID_FIELD,
+          selected: true,
+        },
+        {
+          field: 'email',
+          kind: ColumnKind.FIELD,
+          selected: true,
+        },
+        {
+          field: 'phone',
+          kind: ColumnKind.FIELD,
+          selected: true,
+        },
+        {
+          field: 'alt_phone',
+          kind: ColumnKind.FIELD,
+          selected: true,
+        },
+        {
+          field: 'date',
+          kind: ColumnKind.FIELD,
+          selected: true,
+        },
+        {
+          field: 'url',
+          kind: ColumnKind.FIELD,
+          selected: true,
+        },
+      ],
+      rows: [
+        // Valid values
+        {
+          data: [
+            2,
+            'clara@example.com',
+            '+46701234567',
+            '0701234567',
+            '1857-07-05',
+            'http://zetk.in',
+          ],
+        },
+        // Invalid values
+        {
+          data: [1, 'clara at example.com', '1234', 'abc123', '15/1', 'zetkin'],
+        },
+      ],
+    });
+
+    const customFields: ZetkinCustomField[] = [
+      makeFullField({
+        slug: 'url',
+        type: CUSTOM_FIELD_TYPE.URL,
+      }),
+      makeFullField({
+        slug: 'date',
+        type: CUSTOM_FIELD_TYPE.DATE,
+      }),
+    ];
+
+    const problems = predictProblems(sheet, 'SE', customFields);
+    expect(problems).toEqual([
+      {
+        field: 'email',
+        indices: [1],
+        kind: ImportProblemKind.INVALID_FORMAT,
+      },
+      {
+        field: 'phone',
+        indices: [1],
+        kind: ImportProblemKind.INVALID_FORMAT,
+      },
+      {
+        field: 'alt_phone',
+        indices: [1],
+        kind: ImportProblemKind.INVALID_FORMAT,
+      },
+      {
+        field: 'date',
+        indices: [1],
+        kind: ImportProblemKind.INVALID_FORMAT,
+      },
+      {
+        field: 'url',
+        indices: [1],
+        kind: ImportProblemKind.INVALID_FORMAT,
+      },
+    ]);
+  });
+
+  it('aggregates related problems', () => {
+    const sheet = makeFullSheet({
+      columns: [
+        {
+          idField: 'id',
+          kind: ColumnKind.ID_FIELD,
+          selected: true,
+        },
+        {
+          field: 'email',
+          kind: ColumnKind.FIELD,
+          selected: true,
+        },
+      ],
+      rows: [
+        { data: [1, 'clara at example.com'] },
+        { data: [2, 'clara@example.com'] },
+        { data: [3, 'clara at example.com'] },
+      ],
+    });
+
+    const problems = predictProblems(sheet, 'SE', []);
+    expect(problems).toEqual([
+      {
+        field: 'email',
+        indices: [0, 2],
+        kind: ImportProblemKind.INVALID_FORMAT,
+      },
+    ]);
+  });
+
+  it('returns problem when ID or name are not configured', () => {
+    const sheet = makeFullSheet({
+      columns: [
+        {
+          field: 'first_name',
+          kind: ColumnKind.FIELD,
+          selected: true,
+        },
+      ],
+      rows: [{ data: ['Clara'] }],
+    });
+
+    const problems = predictProblems(sheet, 'SE', []);
+    expect(problems).toEqual([
+      {
+        kind: ImportProblemKind.UNCONFIGURED_ID_AND_NAME,
+      },
+    ]);
+  });
+
+  it('returns warning when there is no ID but full name is configured', () => {
+    const sheet = makeFullSheet({
+      columns: [
+        {
+          field: 'first_name',
+          kind: ColumnKind.FIELD,
+          selected: true,
+        },
+        {
+          field: 'last_name',
+          kind: ColumnKind.FIELD,
+          selected: true,
+        },
+      ],
+      rows: [{ data: ['Clara', 'Zetkin'] }],
+    });
+
+    const problems = predictProblems(sheet, 'SE', []);
+    expect(problems).toEqual([
+      {
+        kind: ImportProblemKind.UNCONFIGURED_ID,
+      },
+    ]);
+  });
+
+  it('returns no problem when ID is missing on a row that has name', () => {
+    const sheet = makeFullSheet({
+      columns: [
+        {
+          idField: 'ext_id',
+          kind: ColumnKind.ID_FIELD,
+          selected: true,
+        },
+        {
+          field: 'first_name',
+          kind: ColumnKind.FIELD,
+          selected: true,
+        },
+        {
+          field: 'first_name',
+          kind: ColumnKind.FIELD,
+          selected: true,
+        },
+      ],
+      rows: [{ data: [null, 'Clara', 'Zetkin'] }],
+    });
+
+    const problems = predictProblems(sheet, 'SE', []);
+    expect(problems).toEqual([]);
+  });
+
+  it('returns problem when ID and name are configured but missing on a row', () => {
+    const sheet = makeFullSheet({
+      columns: [
+        {
+          idField: 'ext_id',
+          kind: ColumnKind.ID_FIELD,
+          selected: true,
+        },
+        {
+          field: 'first_name',
+          kind: ColumnKind.FIELD,
+          selected: true,
+        },
+        {
+          field: 'last_name',
+          kind: ColumnKind.FIELD,
+          selected: true,
+        },
+      ],
+      rows: [
+        { data: [null, 'Clara', null] },
+        { data: [null, null, 'Zetkin'] },
+        { data: ['', null, 'Zetkin'] },
+      ],
+    });
+
+    const problems = predictProblems(sheet, 'SE', []);
+    expect(problems).toEqual([
+      {
+        indices: [0, 1, 2],
+        kind: ImportProblemKind.MISSING_ID_AND_NAME,
+      },
+    ]);
+  });
+});
---
+++ src/features/import/utils/problems/predictProblems.ts
@@ -0,0 +1,140 @@
+import isEmail from 'validator/lib/isEmail';
+import isURL from 'validator/lib/isURL';
+import { ColumnKind, Sheet } from '../types';
+import { CountryCode, isValidPhoneNumber } from 'libphonenumber-js';
+import { CUSTOM_FIELD_TYPE, ZetkinCustomField } from 'utils/types/zetkin';
+import {
+  ImportFieldProblem,
+  ImportProblem,
+  ImportProblemKind,
+  ImportRowProblem,
+} from './types';
+
+const VALIDATORS: Record<CUSTOM_FIELD_TYPE, (value: string) => boolean> = {
+  date: (value) => {
+    try {
+      return new Date(value).toISOString().slice(0, 10) == value;
+    } catch (err) {
+      return false;
+    }
+  },
+  json: () => true,
+  text: () => true,
+  url: (value) => isURL(value),
+};
+
+export function predictProblems(
+  sheet: Sheet,
+  country: CountryCode,
+  customFields: ZetkinCustomField[]
+): ImportProblem[] {
+  const problems: ImportProblem[] = [];
+  let hadImpact = false;
+
+  const problemByField: Record<string, ImportFieldProblem> = {};
+  const rowProblemByKind: Record<string, ImportRowProblem> = {};
+
+  const sheetHasId = sheet.columns.some(
+    (col) => col.kind == ColumnKind.ID_FIELD
+  );
+  const sheetHasFirstName = sheet.columns.some(
+    (col) => col.kind == ColumnKind.FIELD && col.field == 'first_name'
+  );
+  const sheetHasLastName = sheet.columns.some(
+    (col) => col.kind == ColumnKind.FIELD && col.field == 'last_name'
+  );
+
+  if (!sheetHasId) {
+    if (sheetHasFirstName && sheetHasLastName) {
+      problems.push({
+        kind: ImportProblemKind.UNCONFIGURED_ID,
+      });
+    } else {
+      problems.push({
+        kind: ImportProblemKind.UNCONFIGURED_ID_AND_NAME,
+      });
+    }
+  }
+
+  function accumulateFieldProblem(field: string, row: number) {
+    const existing = problemByField[field];
+    if (existing) {
+      existing.indices.push(row);
+    } else {
+      problemByField[field] = {
+        field: field,
+        indices: [row],
+        kind: ImportProblemKind.INVALID_FORMAT,
+      };
+    }
+  }
+
+  const customFieldsBySlug: Record<string, ZetkinCustomField> = {};
+  customFields.forEach((field) => {
+    customFieldsBySlug[field.slug] = field;
+  });
+
+  sheet.rows.forEach((row, rowIndex) => {
+    let rowHasId = false;
+    let rowHasFirstName = false;
+    let rowHasLastName = false;
+
+    sheet.columns.forEach((column, colIndex) => {
+      if (column.selected) {
+        const value = row.data[colIndex];
+
+        if (value) {
+          if (column.kind == ColumnKind.ID_FIELD) {
+            rowHasId = true;
+          } else if (column.kind == ColumnKind.FIELD) {
+            const fieldInfo = customFieldsBySlug[column.field];
+            if (fieldInfo) {
+              const validator = VALIDATORS[fieldInfo.type];
+              const valid = validator(value.toString());
+              if (!valid) {
+                accumulateFieldProblem(column.field, rowIndex);
+              }
+            } else if (column.field == 'first_name') {
+              rowHasFirstName = true;
+            } else if (column.field == 'last_name') {
+              rowHasLastName = true;
+            } else if (column.field == 'email' && !isEmail(value.toString())) {
+              accumulateFieldProblem(column.field, rowIndex);
+            } else if (column.field == 'phone' || column.field == 'alt_phone') {
+              if (!isValidPhoneNumber(value.toString(), country)) {
+                accumulateFieldProblem(column.field, rowIndex);
+              }
+            }
+          }
+          hadImpact = true;
+        }
+      }
+    });
+
+    if (sheetHasId && sheetHasFirstName && sheetHasLastName) {
+      if (!rowHasId && (!rowHasFirstName || !rowHasLastName)) {
+        if (!rowProblemByKind[ImportProblemKind.MISSING_ID_AND_NAME]) {
+          rowProblemByKind[ImportProblemKind.MISSING_ID_AND_NAME] = {
+            indices: [],
+            kind: ImportProblemKind.MISSING_ID_AND_NAME,
+          };
+        }
+
+        rowProblemByKind[ImportProblemKind.MISSING_ID_AND_NAME].indices.push(
+          rowIndex
+        );
+      }
+    }
+  });
+
+  if (!hadImpact) {
+    problems.push({
+      kind: ImportProblemKind.NO_IMPACT,
+    });
+  }
+
+  Object.values(problemByField).forEach((problem) => problems.push(problem));
+  Object.values(rowProblemByKind).forEach((problem) => problems.push(problem));
+
+  return problems;
+}
