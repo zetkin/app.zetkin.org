cc43c9a05e283d61accd3f2c2c8533d7c16c01c5
Mattias Ringström <mattettam@hotmail.se>
2024-01-21 12:30:02 +0100

    Adds sorting of name, phonenumber, email, and notified date to the booked participants table

--- src/features/events/components/ParticipantListSection.tsx
+++ src/features/events/components/ParticipantListSection.tsx
@@ -145,7 +145,6 @@ const ParticipantListSection: FC<ParticipantListSectionListProps> = ({
       field: 'name',
       flex: 1,
       headerName: messages.eventParticipantsList.columnName(),
-      hideSortIcons: true,
       minWidth: 250,
       renderCell: (params) => {
         if (params.row.person) {
@@ -188,16 +187,18 @@ const ParticipantListSection: FC<ParticipantListSectionListProps> = ({
         }
       },
       resizable: false,
-      sortable: false,
+      sortingOrder: ['asc', 'desc', null],
+      valueGetter: (params) => {
+        return `${params.row.first_name || ''} ${params.row.last_name || ''}`;
+      },
     },
     {
       disableColumnMenu: true,
       field: 'phone',
       flex: 1,
       headerName: messages.eventParticipantsList.columnPhone(),
-      hideSortIcons: true,
       resizable: false,
-      sortable: false,
+      sortingOrder: ['asc', 'desc', null],
       valueGetter: (params) => {
         if (params.row.person) {
           return params.row.person.phone;
@@ -211,9 +212,8 @@ const ParticipantListSection: FC<ParticipantListSectionListProps> = ({
       field: 'email',
       flex: 1,
       headerName: messages.eventParticipantsList.columnEmail(),
-      hideSortIcons: true,
       resizable: false,
-      sortable: false,
+      sortingOrder: ['asc', 'desc', null],
       valueGetter: (params) => {
         if (params.row.person) {
           return params.row.person.email;
@@ -227,7 +227,6 @@ const ParticipantListSection: FC<ParticipantListSectionListProps> = ({
       field: 'notified',
       flex: 1,
       headerName: messages.eventParticipantsList.columnNotified(),
-      hideSortIcons: true,
       renderCell: (params) => {
         if (params.row.person) {
           return <ZUIRelativeTime datetime={params.row.response_date} />;
@@ -236,7 +235,15 @@ const ParticipantListSection: FC<ParticipantListSectionListProps> = ({
         }
       },
       resizable: false,
-      sortable: false,
+      sortingOrder: ['asc', 'desc', null],
+      type: 'date',
+      valueGetter: (params) => {
+        if (params.row.person) {
+          return new Date(params.row.response_date);
+        } else {
+          return new Date(params.row.reminder_sent);
+        }
+      },
     },
     {
       align: 'right',
