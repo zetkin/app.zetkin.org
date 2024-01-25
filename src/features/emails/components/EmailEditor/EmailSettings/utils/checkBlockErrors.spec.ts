5df4b65272288546b0e7ea4be37aaa56117367db
ziggabyte <ziggi.willpower@gmail.com>
2024-01-25 13:49:47 +0100

    Add error for buttons with no text.

--- src/features/emails/components/EmailEditor/EmailSettings/utils/checkBlockErrors.spec.ts
+++ src/features/emails/components/EmailEditor/EmailSettings/utils/checkBlockErrors.spec.ts
@@ -1,31 +1,45 @@
 import { BLOCK_TYPES } from 'features/emails/types';
+import { ButtonData } from '../../tools/Button';
 import checkBlockErrors from './checkBlockErrors';
 
 describe('checkBlockErrors()', () => {
   describe('checks if button block has errors', () => {
-    it('returns false when there is a correct url provided', () => {
-      const hasErrors = checkBlockErrors({
-        data: { url: 'http://www.clara.com' },
+    const mockButtonData = (overrides?: Partial<ButtonData>) => {
+      return {
+        data: {
+          buttonText: 'Click me!',
+          url: 'http://www.clara.com',
+          ...overrides,
+        },
         type: BLOCK_TYPES.BUTTON,
-      });
+      };
+    };
+    it('returns false when there is a correct url provided', () => {
+      const hasErrors = checkBlockErrors(mockButtonData());
 
       expect(hasErrors).toEqual(false);
     });
 
     it('returns true when there is an incorrect url', () => {
-      const hasErrors = checkBlockErrors({
-        data: { url: 'clara' },
-        type: BLOCK_TYPES.BUTTON,
-      });
+      const hasErrors = checkBlockErrors(mockButtonData({ url: 'clara' }));
 
       expect(hasErrors).toEqual(true);
     });
 
     it('returns true when url is missing', () => {
-      const hasErrors = checkBlockErrors({
-        data: { url: undefined },
-        type: BLOCK_TYPES.BUTTON,
-      });
+      const hasErrors = checkBlockErrors(mockButtonData({ url: undefined }));
+
+      expect(hasErrors).toEqual(true);
+    });
+
+    it('returns false when a button text is provided', () => {
+      const hasErrors = checkBlockErrors(mockButtonData());
+
+      expect(hasErrors).toEqual(false);
+    });
+
+    it('returns true when the button text is missing/falsy', () => {
+      const hasErrors = checkBlockErrors(mockButtonData({ buttonText: '' }));
 
       expect(hasErrors).toEqual(true);
     });
--- src/features/emails/components/EmailEditor/EmailSettings/utils/checkBlockErrors.ts
+++ src/features/emails/components/EmailEditor/EmailSettings/utils/checkBlockErrors.ts
@@ -5,15 +5,11 @@ import { BLOCK_TYPES } from 'features/emails/types';
 
 export default function checkBlockErrors(block: OutputBlockData) {
   if (block.type === BLOCK_TYPES.BUTTON) {
-    if (!block.data.url) {
+    if (!block.data.url || !block.data.buttonText) {
       return true;
     }
 
-    if (isURL(block.data.url, { require_protocol: true })) {
-      return false;
-    } else {
-      return true;
-    }
+    return !isURL(block.data.url, { require_protocol: true });
   }
   return false;
 }
