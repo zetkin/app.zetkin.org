5574d4afde8e37e66184a13f2fd96293744a398f
ziggabyte <ziggi.willpower@gmail.com>
2024-02-05 16:42:01 +0100

    Add icons to toolbar.

--- src/features/emails/components/EmailEditor/tools/InlineLink/index.tsx
+++ src/features/emails/components/EmailEditor/tools/InlineLink/index.tsx
@@ -75,7 +75,6 @@ export default class LinkTool extends InlineToolBase implements InlineTool {
   renderButton() {
     this._button = document.createElement('button');
     this._button.type = 'button';
-    this._button.textContent = 'Link';
     this._button.classList.add(this._api.styles.inlineToolButton);
     return this._button;
   }
@@ -126,8 +125,12 @@ export default class LinkTool extends InlineToolBase implements InlineTool {
       }
 
       this._errorMessage.style.color = error ? 'red' : 'transparent';
-      this._button.textContent =
-        !this._selectedAnchor && anchors.length == 0 ? 'Link' : 'Unlink';
+
+      //switch between icons for adding and removing link
+      this._button.innerHTML =
+        !this._selectedAnchor && anchors.length == 0
+          ? '<svg width="24" height="24" viewBox="0 0 24 24"><path d="M3.9 12c0-1.71 1.39-3.1 3.1-3.1h4V7H7c-2.76 0-5 2.24-5 5s2.24 5 5 5h4v-1.9H7c-1.71 0-3.1-1.39-3.1-3.1M8 13h8v-2H8zm9-6h-4v1.9h4c1.71 0 3.1 1.39 3.1 3.1s-1.39 3.1-3.1 3.1h-4V17h4c2.76 0 5-2.24 5-5s-2.24-5-5-5"/></svg>'
+          : '<svg width="24" height="24" viewBox="0 0 24 24"><path d="M17 7h-4v1.9h4c1.71 0 3.1 1.39 3.1 3.1 0 1.43-.98 2.63-2.31 2.98l1.46 1.46C20.88 15.61 22 13.95 22 12c0-2.76-2.24-5-5-5m-1 4h-2.19l2 2H16zM2 4.27l3.11 3.11C3.29 8.12 2 9.91 2 12c0 2.76 2.24 5 5 5h4v-1.9H7c-1.71 0-3.1-1.39-3.1-3.1 0-1.59 1.21-2.9 2.76-3.07L8.73 11H8v2h2.73L13 15.27V17h1.73l4.01 4L20 19.74 3.27 3z"/></svg>';
     }
   }
 }
