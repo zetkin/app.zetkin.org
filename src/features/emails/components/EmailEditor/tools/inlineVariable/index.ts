d76322f0adcd906e333e07c7c2f78d4a071b79ba
ziggabyte <ziggi.willpower@gmail.com>
2024-02-09 12:32:27 +0100

    Add icon to tool button.

--- src/features/emails/components/EmailEditor/tools/inlineVariable/index.ts
+++ src/features/emails/components/EmailEditor/tools/inlineVariable/index.ts
@@ -27,7 +27,22 @@ export default function variableToolFactory(title: string) {
 
       this._button = document.createElement('button');
       this._button.type = 'button';
-      this._button.innerHTML = 'VAR';
+      const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+      svg.setAttributeNS(null, 'viewBox', '0 0 24 24');
+      svg.style.display = 'block';
+      svg.style.padding = '2px';
+      const path = document.createElementNS(
+        'http://www.w3.org/2000/svg',
+        'path'
+      );
+      path.setAttributeNS(
+        null,
+        'd',
+        'M4 7v2c0 .55-.45 1-1 1H2v4h1c.55 0 1 .45 1 1v2c0 1.65 1.35 3 3 3h3v-2H7c-.55 0-1-.45-1-1v-2c0-1.3-.84-2.42-2-2.83v-.34C5.16 11.42 6 10.3 6 9V7c0-.55.45-1 1-1h3V4H7C5.35 4 4 5.35 4 7m17 3c-.55 0-1-.45-1-1V7c0-1.65-1.35-3-3-3h-3v2h3c.55 0 1 .45 1 1v2c0 1.3.84 2.42 2 2.83v.34c-1.16.41-2 1.52-2 2.83v2c0 .55-.45 1-1 1h-3v2h3c1.65 0 3-1.35 3-3v-2c0-.55.45-1 1-1h1v-4z'
+      );
+      path.setAttributeNS(null, 'stroke-width', '0');
+      svg.appendChild(path);
+      this._button.appendChild(svg);
       this._button.classList.add(this._api.styles.inlineToolButton);
 
       this._list = document.createElement('ul');
