1aa6a58c8aeb5b6fd0aabfdbe1b9e73a85714f30
ziggabyte <ziggi.willpower@gmail.com>
2024-02-28 12:28:15 +0100

    Add types from @richardolsson 's backend work.

--- src/features/emails/types.ts
+++ src/features/emails/types.ts
@@ -4,3 +4,44 @@ export enum BLOCK_TYPES {
   LIBRARY_IMAGE = 'libraryImage',
   PARAGRAPH = 'paragraph',
 }
+
+export enum InlineNodeKind {
+  BOLD = 'bold',
+  ITALIC = 'italic',
+  LINK = 'link',
+  STRING = 'string',
+  VARIABLE = 'variable',
+}
+
+export type StringNode = {
+  kind: InlineNodeKind.STRING;
+  value: string;
+};
+
+export type VariableNode = {
+  kind: InlineNodeKind.VARIABLE;
+  name: string;
+};
+
+export type LinkNode = {
+  content: EmailContentInlineNode[];
+  href: string;
+  kind: InlineNodeKind.LINK;
+};
+
+export type BoldNode = {
+  content: EmailContentInlineNode[];
+  kind: InlineNodeKind.BOLD;
+};
+
+export type ItalicNode = {
+  content: EmailContentInlineNode[];
+  kind: InlineNodeKind.ITALIC;
+};
+
+export type EmailContentInlineNode =
+  | ItalicNode
+  | BoldNode
+  | LinkNode
+  | StringNode
+  | VariableNode;
