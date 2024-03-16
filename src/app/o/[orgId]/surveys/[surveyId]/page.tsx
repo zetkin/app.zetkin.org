682d865c8d72b6bc3c7d3c1503a937df408d8fd5
Henry Catalini Smith <henry@catalinismith.com>
2024-03-16 17:38:58 +0100

    Add survey metadata to HTML head

--- src/app/o/[orgId]/surveys/[surveyId]/page.tsx
+++ src/app/o/[orgId]/surveys/[surveyId]/page.tsx
@@ -1,6 +1,7 @@
 'use server';
 
 import BackendApiClient from 'core/api/client/BackendApiClient';
+import { Metadata } from 'next';
 import SurveyForm from 'features/surveys/components/surveyForm/SurveyForm';
 import { ZetkinSurveyExtended } from 'utils/types/zetkin';
 import { FC, ReactElement } from 'react';
@@ -11,6 +12,24 @@ type PageProps = {
     surveyId: string;
   };
 };
+
+export async function generateMetadata({
+  params,
+}: PageProps): Promise<Metadata> {
+  const { orgId, surveyId } = params;
+  const apiClient = new BackendApiClient({});
+  const survey = await apiClient.get<ZetkinSurveyExtended>(
+    `/api/orgs/${orgId}/surveys/${surveyId}`
+  );
+  return {
+    description: survey.info_text,
+    openGraph: {
+      description: survey.info_text,
+      title: survey.title,
+    },
+    title: survey.title,
+  };
+}
 
 // @ts-expect-error Async support missing
 const Page: FC<PageProps> = async ({ params }): Promise<ReactElement> => {
