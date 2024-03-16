181aae4f9c64dada0903869e1c434b4bd737a43a
KraftKatten <kraftj@student.chalmers.se>
2024-03-16 18:56:36 +0100

    Enabled filtering for assignee for views

--- src/features/views/components/ViewDataTable/columnTypes/LocalPersonColumnType.tsx
+++ src/features/views/components/ViewDataTable/columnTypes/LocalPersonColumnType.tsx
@@ -1,6 +1,12 @@
 import { FC } from 'react';
 import { useRouter } from 'next/router';
-import { GridCellParams, GridColDef, GridFilterOperator, GridRenderCellParams, GridRenderEditCellParams, GridValueGetterParams, useGridApiContext, } from '@mui/x-data-grid-pro';
+import {
+  GridColDef,
+  GridRenderCellParams,
+  GridRenderEditCellParams,
+  GridValueGetterParams,
+  useGridApiContext,
+} from '@mui/x-data-grid-pro';
 
 import { IColumnType } from '.';
 import { IFuture } from 'core/caching/futures';
@@ -20,21 +26,6 @@ import { useMessages } from 'core/i18n';
 
 type LocalPersonViewCell = null | ZetkinPerson;
 
-function makeEmptyFilterOperator(
-/*   messages: UseMessagesMap<typeof messageIds>
- */): GridFilterOperator {
-  return {
-    getApplyFilterFn: () => {
-      return (params: GridCellParams) => {
-        const people = params.value as ZetkinPerson[];
-        return people.length === 0;
-      };
-    },
-    label: "Is empty",
-    value: 'isEmpty',
-  };
-}
-
 export default class LocalPersonColumnType
   implements IColumnType<LocalPersonViewColumn, LocalPersonViewCell>
 {
@@ -47,45 +38,25 @@ export default class LocalPersonColumnType
     return {
       align: 'center',
       editable: true,
-      filterOperators: [
-        makeEmptyFilterOperator(
-
-
-        )
-      ],
       filterable: true,
       headerAlign: 'center',
 
-      renderCell: (params:GridRenderCellParams) => {
+      renderCell: (params: GridRenderCellParams) => {
         return <ZUIPersonGridCell person={params.row[params.field]} />;
       },
-      renderEditCell: (params:GridRenderEditCellParams) => {
-        return <EditCell cell={params.row[params.field]} column={col} row={params.row} />;
+      renderEditCell: (params: GridRenderEditCellParams) => {
+        return (
+          <EditCell
+            cell={params.row[params.field]}
+            column={col}
+            row={params.row}
+          />
+        );
       },
-       /* sortComparator: (
-        val0: LocalPersonViewCell,
-        val1: LocalPersonViewCell
-      ) => {
-
-        if (!val0 && !val1) {
-          return 0;
-        }
-        if (!val0) {
-          return 1;
-        }
-        if (!val1) {
-          return -1;
-        }
-
-        const name0 = `${val0.first_name}  ${val0.last_name}`;
-        const name1 = `${val1.first_name}  ${val1.last_name}`;
-        return name0.localeCompare(name1);
-      },  */
-
       valueGetter: (params: GridValueGetterParams) => {
         const cell = params.row[params.field];
         return this.cellToString(cell);
-      }
+      },
     };
   }
   getSearchableStrings(cell: LocalPersonViewCell): string[] {
