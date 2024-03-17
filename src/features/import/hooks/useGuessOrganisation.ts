99207d0f91de3c72ba4f17566626a085f5d9d6f3
River <river@someserver.de>
2024-03-17 10:18:11 +0100

    improve guess org logic

--- src/features/import/hooks/useGuessOrganisation.ts
+++ src/features/import/hooks/useGuessOrganisation.ts
@@ -1,8 +1,10 @@
 import Fuse from 'fuse.js';
 
-import { OrgColumn } from '../utils/types';
 import { UIDataColumn } from './useUIDataColumns';
 import useSubOrganizations from 'features/organizations/hooks/useSubOrganizations';
+import { CellData, OrgColumn } from '../utils/types';
+
+type OrgMap = { orgId: number; value: CellData };
 
 const useGuessOrganisaion = (
   orgId: number,
@@ -16,26 +18,33 @@ const useGuessOrganisaion = (
   });
 
   const guessOrg = () => {
-    // Loop through each title
-    const matchedRows = uiDataColumn.uniqueValues.map((orgTitle) => {
-      if (typeof orgTitle === 'number') {
-        return;
-      }
-      // Find orgs with most similar name
-      const results = fuse.search(orgTitle);
-      // Filter out items with a bad match
-      const goodResults = results.filter(
-        (result) => result.score && result.score < 0.25
-      );
-      // If there is a match, guess it
-      if (goodResults && goodResults.length > 0) {
-        return {
-          orgId: goodResults[0].item.id,
-          value: orgTitle,
-        };
-      }
-    });
-    uiDataColumn.selectOrgs(matchedRows.filter((value) => value !== undefined));
+    // Loop through each possible cell value
+    const matchedRows = uiDataColumn.uniqueValues.reduce(
+      (acc: OrgMap[], orgTitle: string | number) => {
+        if (typeof orgTitle === 'string') {
+          // Find orgs with most similar name
+          const results = fuse.search(orgTitle);
+          // Filter out items with a bad match
+          const goodResults = results.filter(
+            (result) => result.score && result.score < 0.25
+          );
+          // If there is a match, guess it
+          if (goodResults.length > 0) {
+            return [
+              ...acc,
+              {
+                orgId: goodResults[0].item.id,
+                value: orgTitle,
+              },
+            ];
+          }
+        }
+        return acc;
+      },
+      []
+    );
+
+    uiDataColumn.selectOrgs(matchedRows);
   };
 
   return guessOrg;
