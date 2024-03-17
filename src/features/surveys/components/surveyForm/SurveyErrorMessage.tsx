445e60f745d801cbaf9509b7f6c90e96074395c8
Henry Catalini Smith <henry@catalinismith.com>
2024-03-17 12:24:48 +0100

    Scroll to survey error on mount

--- src/features/surveys/components/surveyForm/SurveyErrorMessage.tsx
+++ src/features/surveys/components/surveyForm/SurveyErrorMessage.tsx
@@ -1,14 +1,20 @@
 import Box from '@mui/material/Box';
-import { FC } from 'react';
 import messageIds from 'features/surveys/l10n/messageIds';
 import { Msg } from 'core/i18n';
 import SurveyContainer from './SurveyContainer';
 import { useTheme } from '@mui/material';
+import { FC, useEffect, useRef } from 'react';
 
 const SurveyErrorMessage: FC = () => {
+  const element = useRef<HTMLDivElement>(null);
   const theme = useTheme();
+  useEffect(() => {
+    if (element.current) {
+      element.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, []);
   return (
-    <SurveyContainer pt={4}>
+    <SurveyContainer ref={element} pt={4} px={2}>
       <Box
         bgcolor={theme.palette.error.light}
         data-testid="Survey-error"
