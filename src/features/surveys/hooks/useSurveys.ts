714672c5af896458cdf53fb5162ec73724e798a1
KraftKatten <kraftj@student.chalmers.se>
2024-03-17 12:37:58 +0100

    Fix linting rules for useSurveys

--- src/features/surveys/hooks/useSurveys.ts
+++ src/features/surveys/hooks/useSurveys.ts
@@ -1,8 +1,8 @@
+import { IFuture } from 'core/caching/futures';
 import { loadListIfNecessary } from 'core/caching/cacheUtils';
 import { ZetkinSurvey } from 'utils/types/zetkin';
 import { surveysLoad, surveysLoaded } from 'features/surveys/store';
 import { useApiClient, useAppDispatch, useAppSelector } from 'core/hooks';
-import { IFuture } from 'core/caching/futures';
 
 export default function useSurveys(orgId: number): IFuture<ZetkinSurvey[]> {
   const apiClient = useApiClient();
