86e3be5fbee6c1c938ed2d79c274b3849b5dd6e1
ziggabyte <ziggi.willpower@gmail.com>
2024-04-09 16:28:36 +0200

    Update comments to correctly name the feature they are in.

--- src/pages/organize/[orgId]/projects/[campId]/surveys/[surveyId]/index.tsx
+++ src/pages/organize/[orgId]/projects/[campId]/surveys/[surveyId]/index.tsx
@@ -26,7 +26,7 @@ export const getServerSideProps: GetServerSideProps = scaffold(
       const client = new BackendApiClient(ctx.req.headers);
 
       if (campId) {
-        //We don't want to load the event if its project does not exist
+        //We don't want to load the survey if its project does not exist
         //If this GET of the project fails, we end up in the catch block
         //and return a 404 to the client
         await client.get(`/api/orgs/${orgId}/campaigns/${campId}`);
