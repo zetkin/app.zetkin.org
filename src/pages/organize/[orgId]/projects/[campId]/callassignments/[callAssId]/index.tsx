589147187b82e63bcc71be55bff78aa37c37acc8
ziggabyte <ziggi.willpower@gmail.com>
2024-04-12 17:15:44 +0200

    Revert changes introduced by #1888

--- src/pages/organize/[orgId]/projects/[campId]/callassignments/[callAssId]/index.tsx
+++ src/pages/organize/[orgId]/projects/[campId]/callassignments/[callAssId]/index.tsx
@@ -23,13 +23,6 @@ export const getServerSideProps: GetServerSideProps = scaffold(
     try {
       const client = new BackendApiClient(ctx.req.headers);
 
-      if (campId) {
-        //We don't want to load the call assignment if its project does not exist
-        //If this GET of the project fails, we end up in the catch block
-        //and return a 404 to the client
-        await client.get(`/api/orgs/${orgId}/campaigns/${campId}`);
-      }
-
       const data = await client.get<ZetkinCallAssignment>(
         `/api/orgs/${orgId}/call_assignments/${callAssId}`
       );
