f29727a24a94fcc9d012d528a6bf87087dd7765f
ziggabyte <ziggi.willpower@gmail.com>
2024-04-15 12:58:19 +0200

    Add redirect to project page from /emails

---
+++ src/pages/organize/[orgId]/projects/[campId]/emails/index.tsx
@@ -0,0 +1,16 @@
+import { GetServerSideProps } from 'next';
+import { scaffold } from 'utils/next';
+
+export const getServerSideProps: GetServerSideProps = scaffold(async (ctx) => {
+  const { orgId, campId } = ctx.params!;
+  return {
+    redirect: {
+      destination: `/organize/${orgId}/projects/${campId}`,
+      permanent: false,
+    },
+  };
+});
+
+export default function NotUsed(): null {
+  return null;
+}
