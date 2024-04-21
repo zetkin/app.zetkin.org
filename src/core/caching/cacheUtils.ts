c1c8b14b060d9b2538a2b6c8b02f58ef5b4e4747
Richard Olsson <r@richardolsson.se>
2024-04-21 14:08:02 +0200

    Refact loadListIfNecessary() to separate loading from checking if necessary

--- src/core/caching/cacheUtils.ts
+++ src/core/caching/cacheUtils.ts
@@ -24,29 +24,45 @@ export function loadListIfNecessary<
   }
 ): IFuture<DataType[]> {
   if (!remoteList || shouldLoad(remoteList)) {
-    dispatch(hooks.actionOnLoad());
-    const promise = hooks
-      .loader()
-      .then((val) => {
-        dispatch(hooks.actionOnSuccess(val));
-        return val;
-      })
-      .catch((err: unknown) => {
-        if (hooks.actionOnError) {
-          dispatch(hooks.actionOnError(err));
-          return null;
-        } else {
-          throw err;
-        }
-      });
-
-    return new PromiseFuture(promise);
+    return loadList(dispatch, hooks);
   }
 
   return new RemoteListFuture({
     ...remoteList,
     items: remoteList.items.filter((item) => !item.deleted),
   });
+}
+
+export function loadList<
+  DataType,
+  OnLoadPayload = void,
+  OnSuccessPayload = DataType[]
+>(
+  dispatch: AppDispatch,
+  hooks: {
+    actionOnError?: (err: unknown) => PayloadAction<unknown>;
+    actionOnLoad: () => PayloadAction<OnLoadPayload>;
+    actionOnSuccess: (items: DataType[]) => PayloadAction<OnSuccessPayload>;
+    loader: () => Promise<DataType[]>;
+  }
+): IFuture<DataType[]> {
+  dispatch(hooks.actionOnLoad());
+  const promise = hooks
+    .loader()
+    .then((val) => {
+      dispatch(hooks.actionOnSuccess(val));
+      return val;
+    })
+    .catch((err: unknown) => {
+      if (hooks.actionOnError) {
+        dispatch(hooks.actionOnError(err));
+        return null;
+      } else {
+        throw err;
+      }
+    });
+
+  return new PromiseFuture(promise);
 }
 
 export function loadItemIfNecessary<
