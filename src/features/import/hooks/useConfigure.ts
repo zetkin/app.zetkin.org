3c5d4577bd5691881bfeac3e1e38d3de7d37d54c
Rebe R <36491300+rebecarubio@users.noreply.github.com>
2024-05-14 16:18:06 +0200

    catch unexpected error and add it to the problems array

--- src/features/import/hooks/useConfigure.ts
+++ src/features/import/hooks/useConfigure.ts
@@ -6,7 +6,11 @@ import { predictProblems } from '../utils/problems/predictProblems';
 import prepareImportOperations from '../utils/prepareImportOperations';
 import useCustomFields from 'features/profile/hooks/useCustomFields';
 import useOrganization from 'features/organizations/hooks/useOrganization';
-import { ImportPreview, ZetkinPersonImportPostBody } from '../utils/types';
+import {
+  ImportPreview,
+  ImportPreviewProblemCode,
+  ZetkinPersonImportPostBody,
+} from '../utils/types';
 import { useApiClient, useAppDispatch, useAppSelector } from 'core/hooks';
 
 export default function useConfigure(orgId: number) {
@@ -61,19 +65,63 @@ export default function useConfigure(orgId: number) {
         })
       );
     } else {
-      const importOperations = prepareImportOperations(
-        configuredSheet,
-        countryCode
-      );
+      let importOperations;
+      try {
+        importOperations = prepareImportOperations(
+          configuredSheet,
+          countryCode
+        );
+      } catch (error) {
+        addUnexpectedErrorToProblems();
+      }
 
-      const previewRes = await apiClient.post<
-        ImportPreview,
-        ZetkinPersonImportPostBody
-      >(`/api/orgs/${orgId}/bulk/preview`, {
-        ops: importOperations,
-      });
-
-      dispatch(importPreviewAdd(previewRes));
+      try {
+        const previewRes = await apiClient.post<
+          ImportPreview,
+          ZetkinPersonImportPostBody
+        >(`/api/orgs/${orgId}/bulk/preview`, {
+          ops: importOperations,
+        });
+        dispatch(importPreviewAdd(previewRes));
+      } catch (error) {
+        addUnexpectedErrorToProblems();
+      }
     }
   };
+
+  function addUnexpectedErrorToProblems() {
+    dispatch(
+      importPreviewAdd({
+        problems: [
+          {
+            code: ImportPreviewProblemCode.UNEXPECTED_ERROR,
+            index: 1,
+            level: 'error',
+          },
+        ],
+        stats: {
+          person: {
+            summary: {
+              addedToOrg: {
+                byOrg: {},
+                total: 0,
+              },
+              created: {
+                total: 0,
+              },
+              tagged: {
+                byTag: {},
+                total: 0,
+              },
+              updated: {
+                byChangedField: {},
+                byInitializedField: {},
+                total: 0,
+              },
+            },
+          },
+        },
+      })
+    );
+  }
 }
