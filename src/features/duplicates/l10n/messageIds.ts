1177ff79b9a99db6e383e2fe39e3460245a83c42
Rebe R <36491300+rebecarubio@users.noreply.github.com>
2024-05-23 15:38:54 +0200

    add messages strings

---
+++ src/features/duplicates/l10n/messageIds.ts
@@ -0,0 +1,10 @@
+import { m, makeMessages } from 'core/i18n';
+
+export default makeMessages('feat.duplicates', {
+  page: {
+    noDuplicates: m('No duplicates'),
+    noDuplicatesDescription: m(
+      'Yay! all your members seem to be unique individuals.'
+    ),
+  },
+});
