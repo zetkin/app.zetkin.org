66adc2bf52830d3de802ca64bffd2618757074a3
ziggabyte <ziggi.willpower@gmail.com>
2024-05-24 14:39:06 +0200

    Gender values show as words, not letters.

--- src/features/duplicates/components/FieldSettings/FieldSettingsRow.tsx
+++ src/features/duplicates/components/FieldSettings/FieldSettingsRow.tsx
@@ -1,7 +1,9 @@
 import { FC, useState } from 'react';
 
+import messageIds from 'features/duplicates/l10n/messageIds';
 import { NATIVE_PERSON_FIELDS } from 'features/views/components/types';
 import sortValuesByFrequency from 'features/duplicates/utils/sortValuesByFrequency';
+import { useMessages } from 'core/i18n';
 import { MenuItem, Select, Typography } from '@mui/material';
 
 interface FieldSettingsRowProps {
@@ -10,23 +12,42 @@ interface FieldSettingsRowProps {
 }
 
 const FieldSettingsRow: FC<FieldSettingsRowProps> = ({ field, values }) => {
+  const messages = useMessages(messageIds);
   const [selectedValue, setSelectedValue] = useState(values[0]);
 
   const sortedValues = sortValuesByFrequency(field, values);
 
+  const getGender = (value: string) => {
+    if (!value) {
+      return messages.modal.fieldSettings.noData();
+    }
+
+    if (value === 'f') {
+      return messages.modal.fieldSettings.gender.f();
+    } else if (value === 'm') {
+      return messages.modal.fieldSettings.gender.m();
+    } else {
+      return messages.modal.fieldSettings.gender.o();
+    }
+  };
+
   return (
     <>
       {sortedValues.length === 1 && (
-        <Typography color="secondary">{sortedValues[0]}</Typography>
+        <Typography color="secondary">
+          {field === NATIVE_PERSON_FIELDS.GENDER
+            ? getGender(sortedValues[0])
+            : sortedValues[0]}
+        </Typography>
       )}
       {sortedValues.length > 1 && (
         <Select
           onChange={(event) => setSelectedValue(event.target.value)}
           value={selectedValue}
         >
-          {sortedValues.map((value) => (
-            <MenuItem key={value} value={value}>
-              {value}
+          {sortedValues.map((value, index) => (
+            <MenuItem key={index} value={value}>
+              {field === NATIVE_PERSON_FIELDS.GENDER ? getGender(value) : value}
             </MenuItem>
           ))}
         </Select>
--- src/features/duplicates/l10n/messageIds.ts
+++ src/features/duplicates/l10n/messageIds.ts
@@ -3,6 +3,14 @@ import { m, makeMessages } from 'core/i18n';
 export default makeMessages('feat.duplicates', {
   modal: {
     cancelButton: m('Cancel'),
+    fieldSettings: {
+      gender: {
+        f: m('Female'),
+        m: m('Male'),
+        o: m('Other'),
+      },
+      noData: m('No data'),
+    },
     mergeButton: m('Merge'),
     title: m('Merge duplicates'),
   },
