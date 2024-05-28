3c105061376b5e4320f1eddaea5c6c65475c27e0
Rebe R <36491300+rebecarubio@users.noreply.github.com>
2024-05-28 15:31:48 +0200

    remove dummy data and get duplicates list from hook

--- src/pages/organize/[orgId]/people/duplicates/index.tsx
+++ src/pages/organize/[orgId]/people/duplicates/index.tsx
@@ -6,10 +6,11 @@ import messageIds from 'features/duplicates/l10n/messageIds';
 import { PageWithLayout } from 'utils/types';
 import PeopleLayout from 'features/views/layout/PeopleLayout';
 import { scaffold } from 'utils/next';
-//import useDuplicates from 'features/duplicates/hooks/useDuplicates';
+import useDuplicates from 'features/duplicates/hooks/useDuplicates';
 import { useMessages } from 'core/i18n';
 import useServerSide from 'core/useServerSide';
 import { ZetkinDuplicate } from 'features/duplicates/store';
+import { useNumericRouteParams } from 'core/hooks';
 
 export const getServerSideProps: GetServerSideProps = scaffold(async () => {
   return {
@@ -19,74 +20,9 @@ export const getServerSideProps: GetServerSideProps = scaffold(async () => {
 
 const DuplicatesPage: PageWithLayout = () => {
   const onServer = useServerSide();
-  //const list = useDuplicates().data ?? [];
-  const list: ZetkinDuplicate[] = [
-    {
-      dismissed: null,
-      duplicatePersons: [
-        {
-          alt_phone: '',
-          city: 'Oakland',
-          co_address: '',
-          country: 'USA',
-          email: 'angela@blackpanthers.org',
-          ext_id: '74',
-          first_name: 'Angel',
-          gender: 'f',
-          id: 2,
-          is_user: false,
-          last_name: 'Davidsson',
-          phone: '0018493298448',
-          street_address: '45 Main Street',
-          zip_code: '34910',
-        },
-        {
-          alt_phone: '',
-          city: 'Linköping',
-          co_address: '',
-          country: 'USA',
-          email: 'haeju@blackpanthers.org',
-          ext_id: '74',
-          first_name: 'Haeju',
-          gender: 'f',
-          id: 2,
-          is_user: false,
-          last_name: 'Eom',
-          phone: '0018493298448',
-          street_address: '45 Main Street',
-          zip_code: '34910',
-        },
-        {
-          alt_phone: '',
-          city: 'Oakland',
-          co_address: '',
-          country: 'USA',
-          email: 'angela@blackpanthers.org',
-          ext_id: '74',
-          first_name: 'Angela',
-          gender: 'f',
-          id: 2,
-          is_user: false,
-          last_name: 'Davis',
-          phone: '0018493298449',
-          street_address: '45 Main Street',
-          zip_code: '34910',
-        },
-      ],
-      id: 100,
-      merged: null,
-      organization: {
-        id: 1,
-        title: 'My Organization',
-      },
-      status: 'pending',
-    },
-  ];
+  const { orgId } = useNumericRouteParams();
+  const list = useDuplicates(orgId).data ?? [];
   const messages = useMessages(messageIds);
-
-  if (onServer) {
-    return null;
-  }
 
   return (
     <>
