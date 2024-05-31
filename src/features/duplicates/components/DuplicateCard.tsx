cdbad2ad5b411bf76c7348870bb3c4593e62b4a6
Rebe R <36491300+rebecarubio@users.noreply.github.com>
2024-05-31 15:47:03 +0200

    implement Resolve button in DuplicateCard to open ConfigureModal

--- src/features/duplicates/components/DuplicateCard.tsx
+++ src/features/duplicates/components/DuplicateCard.tsx
@@ -1,7 +1,8 @@
 import theme from 'theme';
 import { Box, Button, Paper, Typography } from '@mui/material';
-import { FC, useContext } from 'react';
+import { FC, useContext, useState } from 'react';
 
+import ConfigureModal from './ConfigureModal';
 import messageIds from '../l10n/messageIds';
 import { PotentialDuplicate } from '../store';
 import useDuplicatesMutations from '../hooks/useDuplicatesMutations';
@@ -18,47 +19,57 @@ const DuplicateCard: FC<DuplicateCardProps> = ({ cluster }) => {
   const { orgId } = useNumericRouteParams();
   const { showConfirmDialog } = useContext(ZUIConfirmDialogContext);
   const { dismissDuplicate } = useDuplicatesMutations(orgId);
+  const [openModal, setOpenModal] = useState(false);
 
   return (
-    <Paper elevation={2} sx={{ mb: 1.5, p: 1.5 }}>
-      <Box display={'flex'} flexDirection={'column'} gap={1.5}>
-        <Typography
-          color={theme.palette.grey[500]}
-          sx={{ textTransform: 'uppercase' }}
-          variant="subtitle2"
-        >
-          {messages.page.possibleDuplicatesDescription({
-            numPeople: cluster.duplicates.length,
-          })}
-        </Typography>
-        <Box display={'flex'} flexWrap={'wrap'}>
-          {cluster.duplicates.map((duplicate, index) => (
-            <ZUIPersonHoverCard key={index} personId={duplicate.id}>
-              <Box component="div" sx={{ display: 'inline', pb: 1.5, pr: 4 }}>
-                <ZUIPerson
-                  key={index}
-                  id={duplicate.id}
-                  name={`${duplicate.first_name} ${duplicate.last_name}`}
-                />
-              </Box>
-            </ZUIPersonHoverCard>
-          ))}
-        </Box>
-        <Box sx={{ display: 'flex', gap: 1, justifyContent: 'right' }}>
-          <Button
-            onClick={() => {
-              showConfirmDialog({
-                onSubmit: () => dismissDuplicate(cluster.id),
-              });
-            }}
-            variant="text"
+    <>
+      <Paper elevation={2} sx={{ mb: 1.5, p: 1.5 }}>
+        <Box display={'flex'} flexDirection={'column'} gap={1.5}>
+          <Typography
+            color={theme.palette.grey[500]}
+            sx={{ textTransform: 'uppercase' }}
+            variant="subtitle2"
           >
-            {messages.page.dismiss()}
-          </Button>
-          <Button variant="outlined">{messages.page.resolve()}</Button>
+            {messages.page.possibleDuplicatesDescription({
+              numPeople: cluster.duplicates.length,
+            })}
+          </Typography>
+          <Box display={'flex'} flexWrap={'wrap'}>
+            {cluster.duplicates.map((duplicate, index) => (
+              <ZUIPersonHoverCard key={index} personId={duplicate.id}>
+                <Box component="div" sx={{ display: 'inline', pb: 1.5, pr: 4 }}>
+                  <ZUIPerson
+                    key={index}
+                    id={duplicate.id}
+                    name={`${duplicate.first_name} ${duplicate.last_name}`}
+                  />
+                </Box>
+              </ZUIPersonHoverCard>
+            ))}
+          </Box>
+          <Box sx={{ display: 'flex', gap: 1, justifyContent: 'right' }}>
+            <Button
+              onClick={() => {
+                showConfirmDialog({
+                  onSubmit: () => dismissDuplicate(cluster.id),
+                });
+              }}
+              variant="text"
+            >
+              {messages.page.dismiss()}
+            </Button>
+            <Button onClick={() => setOpenModal(true)} variant="outlined">
+              {messages.page.resolve()}
+            </Button>
+          </Box>
         </Box>
-      </Box>
-    </Paper>
+      </Paper>
+      <ConfigureModal
+        onClose={() => setOpenModal(false)}
+        open={openModal}
+        potentialDuplicate={cluster}
+      />
+    </>
   );
 };
 
