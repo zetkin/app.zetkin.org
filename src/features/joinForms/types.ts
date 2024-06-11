7e3339bb7c0e53a225798b8c17e5a0557f4431e4
Richard Olsson <r@richardolsson.se>
2024-06-11 07:13:32 +0200

    Define full type of ZetkinJoinForm

--- src/features/joinForms/types.ts
+++ src/features/joinForms/types.ts
@@ -1,4 +1,17 @@
 export type ZetkinJoinForm = {
+  description: string;
+  embeddable: boolean;
+  fields: string[];
   id: number;
+  organization: {
+    id: number;
+    title: string;
+  };
+  renderable: boolean;
+  submit_token: string;
+  tags: {
+    id: number;
+    title: string;
+  }[];
   title: string;
 };
