79bc0483705f86b8791d748ec6b63692e279899c
Richard Olsson <r@richardolsson.se>
2024-06-12 07:39:12 +0200

    Refactor JoinSubmissionTable and remove Card to get rid of double outline

--- src/features/joinForms/components/JoinSubmissionTable.tsx
+++ src/features/joinForms/components/JoinSubmissionTable.tsx
@@ -1,6 +1,6 @@
 import { DataGridPro } from '@mui/x-data-grid-pro';
 import { FC } from 'react';
-import { Box, Button, Card } from '@mui/material';
+import { Box, Button, useTheme } from '@mui/material';
 
 import messageIds from '../l10n/messageIds';
 import { ZetkinJoinSubmission } from '../types';
@@ -13,65 +13,64 @@ type Props = {
 
 const JoinSubmissionTable: FC<Props> = ({ onSelect, submissions }) => {
   const messages = useMessages(messageIds);
+  const theme = useTheme();
 
   return (
-    <Box m={2}>
-      <Card>
-        <DataGridPro
-          columns={[
-            {
-              field: 'first_name',
-              flex: 1,
-              headerName: messages.submissionList.firstName(),
-              valueGetter: (params) => params.row.person_data.first_name,
+    <Box bgcolor={theme.palette.background.paper} m={2}>
+      <DataGridPro
+        columns={[
+          {
+            field: 'first_name',
+            flex: 1,
+            headerName: messages.submissionList.firstName(),
+            valueGetter: (params) => params.row.person_data.first_name,
+          },
+          {
+            field: 'last_name',
+            flex: 1,
+            headerName: messages.submissionList.lastName(),
+            valueGetter: (params) => params.row.person_data.last_name,
+          },
+          {
+            field: 'form',
+            flex: 1,
+            headerName: messages.submissionList.form(),
+            valueGetter: (params) => params.row.form.title,
+          },
+          {
+            field: 'submitted',
+            flex: 1,
+            headerName: messages.submissionList.timestamp(),
+            type: 'dateTime',
+            valueGetter: (params) => new Date(params.row.submitted),
+          },
+          {
+            align: 'right',
+            field: 'actions',
+            flex: 1,
+            headerName: '',
+            renderCell: () => {
+              // TODO: Handle clicks
+              return (
+                <Box display="flex" gap={2}>
+                  <Button variant="text">
+                    <Msg id={messageIds.submissionList.rejectButton} />
+                  </Button>
+                  <Button variant="outlined">
+                    <Msg id={messageIds.submissionList.approveButton} />
+                  </Button>
+                </Box>
+              );
             },
-            {
-              field: 'last_name',
-              flex: 1,
-              headerName: messages.submissionList.lastName(),
-              valueGetter: (params) => params.row.person_data.last_name,
-            },
-            {
-              field: 'form',
-              flex: 1,
-              headerName: messages.submissionList.form(),
-              valueGetter: (params) => params.row.form.title,
-            },
-            {
-              field: 'submitted',
-              flex: 1,
-              headerName: messages.submissionList.timestamp(),
-              type: 'dateTime',
-              valueGetter: (params) => new Date(params.row.submitted),
-            },
-            {
-              align: 'right',
-              field: 'actions',
-              flex: 1,
-              headerName: '',
-              renderCell: () => {
-                // TODO: Handle clicks
-                return (
-                  <Box display="flex" gap={2}>
-                    <Button variant="text">
-                      <Msg id={messageIds.submissionList.rejectButton} />
-                    </Button>
-                    <Button variant="outlined">
-                      <Msg id={messageIds.submissionList.approveButton} />
-                    </Button>
-                  </Box>
-                );
-              },
-            },
-          ]}
-          hideFooter
-          isRowSelectable={() => false}
-          onRowClick={(params) => {
-            onSelect(params.row);
-          }}
-          rows={submissions}
-        />
-      </Card>
+          },
+        ]}
+        hideFooter
+        isRowSelectable={() => false}
+        onRowClick={(params) => {
+          onSelect(params.row);
+        }}
+        rows={submissions}
+      />
     </Box>
   );
 };
