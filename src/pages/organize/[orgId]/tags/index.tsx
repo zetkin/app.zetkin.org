28eb30b1ce8d2492cc0d5a039ad38553de205f8d
ziggabyte <ziggi.willpower@gmail.com>
2024-06-14 12:01:36 +0200

    Open edit tag dialog when clicking tag.

--- src/pages/organize/[orgId]/tags/index.tsx
+++ src/pages/organize/[orgId]/tags/index.tsx
@@ -1,16 +1,23 @@
 import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 import { Box, Typography, useTheme } from '@mui/material';
+import { useContext, useState } from 'react';
 
 import { groupTags } from 'features/tags/components/TagManager/utils';
 import messageIds from 'features/tags/l10n/messageIds';
 import { PageWithLayout } from 'utils/types';
 import { scaffold } from 'utils/next';
 import TagChip from 'features/tags/components/TagManager/components/TagChip';
+import TagDialog from 'features/tags/components/TagManager/components/TagDialog';
 import TagsLayout from 'features/tags/layout/TagsLayout';
+import useDeleteTag from 'features/tags/hooks/useDeleteTag';
 import { useNumericRouteParams } from 'core/hooks';
 import useServerSide from 'core/useServerSide';
+import useTagGroups from 'features/tags/hooks/useTagGroups';
+import useTagMutations from 'features/tags/hooks/useTagMutations';
 import useTags from 'features/tags/hooks/useTags';
+import { ZetkinTag } from 'utils/types/zetkin';
+import { ZUIConfirmDialogContext } from 'zui/ZUIConfirmDialogProvider';
 import { Msg, useMessages } from 'core/i18n';
 
 export const getServerSideProps: GetServerSideProps = scaffold(
@@ -30,6 +37,11 @@ const TagsPage: PageWithLayout = () => {
   const messages = useMessages(messageIds);
   const theme = useTheme();
   const tags = useTags(orgId).data;
+  const tagGroups = useTagGroups(orgId).data || [];
+  const deleteTag = useDeleteTag(orgId);
+  const { updateTag } = useTagMutations(orgId);
+  const { showConfirmDialog } = useContext(ZUIConfirmDialogContext);
+  const [tagToEdit, setTagToEdit] = useState<ZetkinTag | undefined>(undefined);
 
   const groupedTags = groupTags(
     tags || [],
@@ -82,12 +94,35 @@ const TagsPage: PageWithLayout = () => {
               </Box>
               <Box display="flex" flexWrap="wrap" style={{ gap: 4 }}>
                 {group.tags.map((tag) => (
-                  <TagChip key={tag.id} tag={tag} />
+                  <TagChip
+                    key={tag.id}
+                    onClick={() => setTagToEdit(tag)}
+                    tag={tag}
+                  />
                 ))}
               </Box>
             </Box>
           );
         })}
+        <TagDialog
+          groups={tagGroups}
+          onClose={() => setTagToEdit(undefined)}
+          onDelete={(tagId) => {
+            showConfirmDialog({
+              onSubmit: () => {
+                deleteTag(tagId);
+              },
+              warningText: messages.dialog.deleteWarning(),
+            });
+          }}
+          onSubmit={(tag) => {
+            if ('id' in tag) {
+              updateTag(tag);
+            }
+          }}
+          open={!!tagToEdit}
+          tag={tagToEdit}
+        />
       </Box>
     </>
   );
