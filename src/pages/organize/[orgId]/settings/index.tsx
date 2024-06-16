383ed70e0d4c6239ea8276d741a8586162afa5e5
kaulfield23 <kaulfield23@gmail.com>
2024-06-16 16:13:49 +0200

    place the url card on the right side of settings page

--- src/pages/organize/[orgId]/settings/index.tsx
+++ src/pages/organize/[orgId]/settings/index.tsx
@@ -1,5 +1,6 @@
 import { GetServerSideProps } from 'next';
-import { Box, Typography } from '@mui/material';
+import { OpenInNew } from '@mui/icons-material';
+import { Box, Grid, Link, Typography } from '@mui/material';
 
 import AddOfficialButton from 'features/settings/components/AddOfficialButton';
 import messageIds from 'features/settings/l10n/messageIds';
@@ -11,6 +12,8 @@ import SettingsLayout from 'features/settings/layout/SettingsLayout';
 import useNumericRouteParams from 'core/hooks/useNumericRouteParams';
 import useOfficialMemberships from 'features/settings/hooks/useOfficialMemberships';
 import useServerSide from 'core/useServerSide';
+import ZUICard from 'zui/ZUICard';
+import ZUITextfieldToClipboard from 'zui/ZUITextfieldToClipboard';
 
 export const getServerSideProps: GetServerSideProps = scaffold(
   async () => {
@@ -27,6 +30,7 @@ const SettingsPage: PageWithLayout = () => {
   const onServer = useServerSide();
   const { orgId } = useNumericRouteParams();
   const listFuture = useOfficialMemberships(orgId).data || [];
+  const publicUrl = `https://app.zetkin.org/o/${orgId}`;
 
   const adminList = listFuture.filter((user) => user.role === 'admin');
   const organizerList = listFuture.filter((user) => user.role === 'organizer');
@@ -36,52 +40,75 @@ const SettingsPage: PageWithLayout = () => {
   }
 
   return (
-    <Box>
-      <Box
-        alignItems="center"
-        display="flex"
-        justifyContent="space-between"
-        sx={{
-          marginBottom: '15px',
-          marginTop: '15px',
-        }}
-      >
-        <Typography variant="h4">
-          <Msg id={messageIds.officials.administrators.title} />
+    <Grid container spacing={2}>
+      <Grid item md={8}>
+        <Box
+          alignItems="center"
+          display="flex"
+          justifyContent="space-between"
+          sx={{
+            marginBottom: '15px',
+            marginTop: '15px',
+          }}
+        >
+          <Typography variant="h4">
+            <Msg id={messageIds.officials.administrators.title} />
+          </Typography>
+          <AddOfficialButton
+            disabledList={adminList}
+            orgId={orgId}
+            roleType="admin"
+          />
+        </Box>
+        <Typography mb={2} variant="body2">
+          <Msg id={messageIds.officials.administrators.description} />
         </Typography>
-        <AddOfficialButton
-          disabledList={adminList}
-          orgId={orgId}
-          roleType="admin"
-        />
-      </Box>
-      <Typography mb={2} variant="body2">
-        <Msg id={messageIds.officials.administrators.description} />
-      </Typography>
-      <OfficialList officialList={adminList} orgId={orgId} />
-      <Box
-        alignItems="center"
-        display="flex"
-        justifyContent="space-between"
-        sx={{
-          marginBottom: '15px',
-          marginTop: '40px',
-        }}
-      >
-        <Typography variant="h4">
-          <Msg id={messageIds.officials.organizers.title} />
+        <OfficialList officialList={adminList} orgId={orgId} />
+        <Box
+          alignItems="center"
+          display="flex"
+          justifyContent="space-between"
+          sx={{
+            marginBottom: '15px',
+            marginTop: '40px',
+          }}
+        >
+          <Typography variant="h4">
+            <Msg id={messageIds.officials.organizers.title} />
+          </Typography>
+          <AddOfficialButton
+            disabledList={organizerList}
+            orgId={orgId}
+            roleType="organizer"
+          />
+        </Box>
+        <Typography mb={2} variant="body2">
+          <Msg id={messageIds.officials.organizers.description} />
         </Typography>
-        <AddOfficialButton
-          disabledList={organizerList}
-          orgId={orgId}
-          roleType="organizer"
-        />
-      </Box>
-      <Typography mb={2} variant="body2">
-        <Msg id={messageIds.officials.organizers.description} />
-      </Typography>
-      <OfficialList officialList={organizerList} orgId={orgId} />
-    </Box>
+        <OfficialList officialList={organizerList} orgId={orgId} />
+      </Grid>
+      <Grid item md={4}>
+        <ZUICard
+          header={'Link to public organization'}
+          subheader="Connect to the organization before assigning"
+        >
+          <Box display="flex" paddingBottom={2}>
+            <ZUITextfieldToClipboard copyText={publicUrl}>
+              {publicUrl}
+            </ZUITextfieldToClipboard>
+          </Box>
+          <Link
+            display="flex"
+            href={`/o/${orgId}`}
+            sx={{ alignItems: 'center', gap: 1 }}
+            target="_blank"
+          >
+            <OpenInNew fontSize="inherit" />
+            Link to public organization
+          </Link>
+        </ZUICard>
+      </Grid>
+    </Grid>
   );
 };
 
