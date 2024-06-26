55dc91707f8943013c01b23f4ce8c716916b6c6d
WULCAN <7813515+WULCAN@users.noreply.github.com>
2024-06-26 20:44:30 +0200

    Replace storybook-addon-mock
    The addon does not seem to be compatible witb Storybook 7. See for
    example /linearlabs-workspace/storybook-addon-mock/issues/211 on
    github.com
    
    Note that this exposes another issue with our Stroybook: there are a lot
    of unhandled requests for avatars. With storybook-addon-mock, these were
    hidden. Since I only mock our API Client, these requests are not caught
    by my cave-man mock.

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,7 +6,6 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    'storybook-addon-mock/register',
   ],
   framework: '@storybook/nextjs',
   webpackFinal: async (config) => {
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,14 @@
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import dayjs from 'dayjs';
+import Environment from 'core/env/Environment';
+import FetchApiClient from 'core/api/client/FetchApiClient';
+import { EnvProvider } from 'core/env/EnvContext';
 import { IntlProvider } from 'react-intl';
 import isoWeek from 'dayjs/plugin/isoWeek';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import withMock from 'storybook-addon-mock';
 import { Provider as ReduxProvider } from 'react-redux';
 import { ThemeProvider } from '@mui/material';
+import { useRouter } from 'next/router';
 
 import theme from '../src/theme';
 import '../src/styles.css';
@@ -24,15 +27,45 @@ const I18nProvider = (props) => {
   );
 };
 
+function mockFetch(path, init) {
+  if (path === '/api/orgs/1/people/1' && init === undefined) {
+    return Promise.resolve(
+      new Response(
+        JSON.stringify({
+          data: mockPerson(),
+        })
+      )
+    );
+  } else if (path === '/api/orgs/1/people/1/tags' && init === undefined) {
+    return Promise.resolve(new Response('[]'));
+  } else {
+    return Promise.reject({ error: 'unmocked request', path, init });
+  }
+}
+
+class MockApiClient extends FetchApiClient {
+  constructor() {
+    super(mockFetch);
+  }
+}
+
 export const decorators = [
   (Story) => (
     <ThemeProvider theme={theme}>
       <Story />
     </ThemeProvider>
   ),
-  (story) => <ReduxProvider store={createStore()}>{story()}</ReduxProvider>,
+  (story) => {
+    const store = createStore();
+    const router = useRouter();
+    const env = new Environment(store, new MockApiClient(), router);
+    return (
+      <ReduxProvider store={store}>
+        <EnvProvider env={env}>{story()}</EnvProvider>
+      </ReduxProvider>
+    );
+  },
   (story) => <I18nProvider>{story()}</I18nProvider>,
-  withMock,
 ];
 
 export const parameters = {
@@ -44,22 +77,4 @@ export const parameters = {
       },
     },
   },
-  mockData: [
-    {
-      url: 'api/orgs/1/people/1',
-      method: 'GET',
-      status: 200,
-      response: {
-        data: mockPerson(),
-      },
-    },
-    {
-      url: 'api/orgs/1/people/1/tags',
-      method: 'GET',
-      status: 200,
-      response: {
-        data: [],
-      },
-    },
-  ],
 };
