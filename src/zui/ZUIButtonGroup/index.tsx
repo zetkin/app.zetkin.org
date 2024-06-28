26b08a7220eac7560e0c97fbc0f2891a1d4a627d
ziggabyte <ziggi.willpower@gmail.com>
2024-06-28 12:14:42 +0200

    Create ZUIButtonGroup + story.

---
+++ src/zui/ZUIButtonGroup/index.tsx
@@ -0,0 +1,38 @@
+import { ButtonGroup } from '@mui/material';
+import { FC, ReactNode } from 'react';
+
+interface ZUIButtonGroupProps {
+  children: ReactNode;
+  orientation?: 'horizontal' | 'vertical';
+  size?: 'large' | 'medium' | 'small';
+  type: 'primary' | 'secondary' | 'tertiary';
+}
+
+const getVariant = (type: 'primary' | 'secondary' | 'tertiary') => {
+  if (type === 'secondary') {
+    return 'outlined';
+  } else if (type === 'tertiary') {
+    return 'text';
+  } else {
+    return 'contained';
+  }
+};
+
+const ZUIButtonGroup: FC<ZUIButtonGroupProps> = ({
+  children,
+  orientation = 'horizontal',
+  size = 'medium',
+  type = 'primary',
+}) => {
+  return (
+    <ButtonGroup
+      orientation={orientation}
+      size={size}
+      variant={getVariant(type)}
+    >
+      {children}
+    </ButtonGroup>
+  );
+};
+
+export default ZUIButtonGroup;
