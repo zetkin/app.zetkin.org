2e9f23a5218d65e1c8d10e76cb85d0bd6d87ccd9
ziggabyte <ziggi.willpower@gmail.com>
2024-06-28 12:16:36 +0200

    Create ZUIStatusChip + story.

---
+++ src/zui/ZUIStatusChip/index.tsx
@@ -0,0 +1,40 @@
+import { FC } from 'react';
+import { makeStyles } from '@mui/styles';
+import { Box, Theme, Typography } from '@mui/material';
+
+import { getContrastColor } from 'utils/colorUtils';
+
+const useStyles = makeStyles<Theme, { status: ActivityStatus }>((theme) => ({
+  chip: {
+    alignItems: 'center',
+    backgroundColor: ({ status }) => theme.palette.activityStatusColors[status],
+    borderRadius: '2em',
+    color: ({ status }) =>
+      getContrastColor(theme.palette.activityStatusColors[status]),
+    display: 'inline-flex',
+    padding: '0.5em 0.7em',
+  },
+}));
+
+type ActivityStatus =
+  | 'cancelled'
+  | 'draft'
+  | 'expired'
+  | 'published'
+  | 'scheduled';
+
+interface ZUIStatusChipProps {
+  label: string;
+  status: ActivityStatus;
+}
+
+const ZUIStatusChip: FC<ZUIStatusChipProps> = ({ label, status }) => {
+  const classes = useStyles({ status });
+  return (
+    <Box className={classes.chip}>
+      <Typography sx={{ fontSize: 13 }}>{label}</Typography>
+    </Box>
+  );
+};
+
+export default ZUIStatusChip;
