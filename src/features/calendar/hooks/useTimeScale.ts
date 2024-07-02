130aa86d948f2623d3eb9d5806fb135ebd433875
river-bbc <river.honer@bbc.co.uk>
2024-07-02 16:08:26 -0700

    local scorage for time scale

--- src/features/calendar/hooks/useTimeScale.ts
+++ src/features/calendar/hooks/useTimeScale.ts
@@ -1,32 +1,36 @@
 import { useEffect, useState } from 'react';
 
 import { TimeScale } from '../components';
+import useLocalStorage from 'zui/hooks/useLocalStorage';
 
 function getTimeScale(timeScaleQueryParam: string | string[] | undefined) {
-  let timeScale = TimeScale.MONTH;
   if (
-    timeScaleQueryParam !== undefined &&
     typeof timeScaleQueryParam === 'string' &&
     Object.values(TimeScale).includes(timeScaleQueryParam as TimeScale)
   ) {
-    timeScale = timeScaleQueryParam as TimeScale;
+    return timeScaleQueryParam as TimeScale;
   }
-  return timeScale;
+  return TimeScale.MONTH;
 }
 
 export default function useTimeScale(
   timeScaleQueryParam: string | string[] | undefined
 ) {
+  const [localStorageTimeScale, setLocalStorageTimeScale] = useLocalStorage<
+    TimeScale | undefined
+  >('calendarTimeScale', undefined);
+
   const [timeScale, setTimeScale] = useState<TimeScale>(
-    getTimeScale(timeScaleQueryParam)
+    localStorageTimeScale || getTimeScale(timeScaleQueryParam)
   );
 
-  // When the time scale query param changes, update the time scale
+  // When the time scale query param changes in the URL
   useEffect(() => {
     setTimeScale(getTimeScale(timeScaleQueryParam));
   }, [timeScaleQueryParam]);
 
   const setPersistentTimeScale = (newTimeScale: TimeScale) => {
+    setLocalStorageTimeScale(newTimeScale);
     setTimeScale(newTimeScale);
   };
 
