aeb026d2a4967de0b0e2b5e3cf7ec8e427cb9d10
Richard Olsson <r@richardolsson.se>
2024-08-19 10:44:09 +0200

    Use Component Story Format 3 for ZUIDuration

--- src/zui/ZUIDuration/index.stories.tsx
+++ src/zui/ZUIDuration/index.stories.tsx
@@ -1,4 +1,4 @@
-import { Meta, StoryFn } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react';
 
 import ZUIDuration from '.';
 
@@ -7,23 +7,13 @@ export default {
   title: 'Atoms/ZUIDuration',
 } as Meta<typeof ZUIDuration>;
 
-const Template: StoryFn<typeof ZUIDuration> = (args) => (
-  <ZUIDuration
-    enableDays={args.enableDays}
-    enableHours={args.enableHours}
-    enableMilliseconds={args.enableMilliseconds}
-    enableMinutes={args.enableMinutes}
-    enableSeconds={args.enableSeconds}
-    seconds={args.seconds}
-  />
-);
-
-export const basic = Template.bind({});
-basic.args = {
-  enableDays: true,
-  enableHours: true,
-  enableMilliseconds: false,
-  enableMinutes: true,
-  enableSeconds: false,
-  seconds: 12345,
+export const Basic: StoryObj<typeof ZUIDuration> = {
+  args: {
+    enableDays: true,
+    enableHours: true,
+    enableMilliseconds: false,
+    enableMinutes: true,
+    enableSeconds: false,
+    seconds: 12345,
+  },
 };
