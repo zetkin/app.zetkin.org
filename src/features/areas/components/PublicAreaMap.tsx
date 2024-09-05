8858c435bf1fa250b855b1b3b4697f4a88d8bea9
ziggabyte <ziggi.willpower@gmail.com>
2024-09-05 14:15:47 +0200

    Render out markers on map.

--- src/features/areas/components/PublicAreaMap.tsx
+++ src/features/areas/components/PublicAreaMap.tsx
@@ -1,4 +1,4 @@
-import { FC, useRef } from 'react';
+import { FC, useRef, useState } from 'react';
 import { latLngBounds, Map } from 'leaflet';
 import { makeStyles } from '@mui/styles';
 import { Add, Remove } from '@mui/icons-material';
@@ -8,6 +8,9 @@ import {
   ButtonGroup,
   Divider,
   IconButton,
+  ToggleButton,
+  ToggleButtonGroup,
+  Typography,
   useTheme,
 } from '@mui/material';
 import { MapContainer, Polygon, TileLayer } from 'react-leaflet';
@@ -19,6 +22,16 @@ import messageIds from '../l10n/messageIds';
 import { DivIconMarker } from 'features/events/components/LocationModal/DivIconMarker';
 
 const useStyles = makeStyles((theme) => ({
+  areaNumber: {
+    alignItems: 'center',
+    backgroundColor: 'white',
+    borderRadius: '2em',
+    display: 'flex',
+    height: 30,
+    justifyContent: 'center',
+    transform: 'translate(-20%, -60%)',
+    width: 30,
+  },
   counter: {
     bottom: 15,
     display: 'flex',
@@ -28,15 +41,14 @@ const useStyles = makeStyles((theme) => ({
     width: '100%',
     zIndex: 1000,
   },
-  number: {
-    alignItems: 'center',
-    backgroundColor: 'white',
-    borderRadius: '2em',
-    display: 'flex',
-    height: 30,
-    justifyContent: 'center',
-    transform: 'translate(-20%, -60%)',
-    width: 30,
+  markerNumber: {
+    bottom: -14,
+    color: 'blue',
+    left: '100%',
+    position: 'absolute',
+    textAlign: 'center',
+    transform: 'translate(-50%)',
+    zIndex: 2000,
   },
   zoomControls: {
     backgroundColor: theme.palette.common.white,
@@ -58,6 +70,7 @@ const PublicAreaMap: FC<PublicAreaMapProps> = ({ area }) => {
   const theme = useTheme();
   const classes = useStyles();
   const mapRef = useRef<Map | null>(null);
+  const [mode, setMode] = useState<'area' | 'markers'>('area');
   const { updateArea } = useAreaMutations(area.organization.id, area.id);
 
   return (
@@ -72,6 +85,20 @@ const PublicAreaMap: FC<PublicAreaMapProps> = ({ area }) => {
         </IconButton>
       </Box>
       <Box className={classes.counter}>
+        <ToggleButtonGroup
+          color="primary"
+          exclusive
+          onChange={(ev, newMode) => {
+            if (newMode != null) {
+              setMode(newMode);
+            }
+          }}
+          sx={{ backgroundColor: theme.palette.background.default }}
+          value={mode}
+        >
+          <ToggleButton value="area">Area</ToggleButton>
+          <ToggleButton value="markers">Markers</ToggleButton>
+        </ToggleButtonGroup>
         <ButtonGroup fullWidth>
           <Button
             disabled={area.numberOfActions == 0}
@@ -111,9 +138,45 @@ const PublicAreaMap: FC<PublicAreaMapProps> = ({ area }) => {
           url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         <Polygon color={theme.palette.primary.main} positions={area.points} />
-        <DivIconMarker position={latLngBounds(area.points).getCenter()}>
-          <Box className={classes.number}>{area.numberOfActions}</Box>
-        </DivIconMarker>
+        {mode == 'area' && (
+          <DivIconMarker position={latLngBounds(area.points).getCenter()}>
+            <Box className={classes.areaNumber}>{area.numberOfActions}</Box>
+          </DivIconMarker>
+        )}
+        {mode == 'markers' && (
+          <>
+            {area.markers.map((marker, index) => (
+              <DivIconMarker
+                key={`marker-${index}`}
+                iconAnchor={[11, 33]}
+                position={{
+                  lat: marker.position.lat,
+                  lng: marker.position.lng,
+                }}
+              >
+                <Box className={classes.markerNumber}>
+                  <Typography>{marker.numberOfActions}</Typography>
+                </Box>
+                <svg fill="none" height="35" viewBox="0 0 30 40" width="25">
+                  <path
+                    d="M14 38.479C13.6358 38.0533 13.1535 37.4795
+           12.589 36.7839C11.2893 35.1826 9.55816 32.9411
+            7.82896 30.3782C6.09785 27.8124 4.38106 24.9426
+            3.1001 22.0833C1.81327 19.211 1 16.4227 1 14C1
+            6.81228 6.81228 1 14 1C21.1877 1 27 6.81228 27 14C27
+            16.4227 26.1867 19.211 24.8999 22.0833C23.6189 24.9426
+            21.9022 27.8124 20.171 30.3782C18.4418 32.9411 16.7107
+            35.1826 15.411 36.7839C14.8465 37.4795 14.3642
+            38.0533 14 38.479Z"
+                    fill="white"
+                    stroke="#ED1C55"
+                    strokeWidth="2"
+                  />
+                </svg>
+              </DivIconMarker>
+            ))}
+          </>
+        )}
       </MapContainer>
     </>
   );
