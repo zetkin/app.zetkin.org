da0993daa0bb6731d43af09d9aa700998dca1b77
ziggabyte <ziggi.willpower@gmail.com>
2024-09-18 11:28:01 +0200

    Endpoint to POST a canvass session. made by @richardolsson :)

--- src/app/beta/orgs/[orgId]/canvassassignments/[canvassAssId]/sessions/route.ts
+++ src/app/beta/orgs/[orgId]/canvassassignments/[canvassAssId]/sessions/route.ts
@@ -67,3 +67,63 @@ export async function GET(request: NextRequest, { params }: RouteMeta) {
     }
   );
 }
+
+export async function POST(request: NextRequest, { params }: RouteMeta) {
+  return asOrgAuthorized(
+    {
+      orgId: params.orgId,
+      request: request,
+      roles: ['admin'],
+    },
+    async ({ apiClient, orgId }) => {
+      await mongoose.connect(process.env.MONGODB_URL || '');
+
+      const assignment = await CanvassAssignmentModel.findOne({
+        _id: params.canvassAssId,
+      });
+
+      if (!assignment) {
+        return new NextResponse(null, { status: 404 });
+      }
+
+      const payload = await request.json();
+      const sessionData = {
+        areaId: payload.areaId as string,
+        personId: payload.personId as number,
+      };
+
+      const person = await apiClient.get<ZetkinPerson>(
+        `/api/orgs/${orgId}/people/${sessionData.personId}`
+      );
+
+      const area = await AreaModel.findOne({
+        _id: sessionData.areaId,
+      });
+
+      if (area && person) {
+        assignment.sessions.push(sessionData);
+
+        await assignment.save();
+
+        return NextResponse.json({
+          area: {
+            description: area.description,
+            id: area._id.toString(),
+            organization: {
+              id: orgId,
+            },
+            points: area.points,
+            title: area.title,
+          },
+          assignee: person,
+          assignment: {
+            id: assignment._id.toString(),
+            title: assignment.title,
+          },
+        });
+      } else {
+        return new NextResponse(null, { status: 404 });
+      }
+    }
+  );
+}
