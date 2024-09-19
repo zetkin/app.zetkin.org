ac9123b8c7a268c3692b8f5af39a5bdca7b3e139
Richard Olsson <r@richardolsson.se>
2024-09-19 07:10:15 +0200

    Render canvass assignments in activities lists

---
+++ src/features/campaigns/components/ActivityList/items/CanvassAssignmentListItem.tsx
@@ -0,0 +1,35 @@
+import { FC } from 'react';
+import { Map } from '@mui/icons-material';
+
+import ActivityListItem, { STATUS_COLORS } from './ActivityListItem';
+import useCanvassAssignment from 'features/areas/hooks/useCanvassAssignment';
+
+type Props = {
+  caId: string;
+  orgId: number;
+};
+
+const CanvassAssignmentListItem: FC<Props> = ({ caId, orgId }) => {
+  const { data: assignment } = useCanvassAssignment(orgId, caId);
+
+  if (!assignment) {
+    return null;
+  }
+
+  const color = STATUS_COLORS.GRAY;
+
+  return (
+    <ActivityListItem
+      color={color}
+      endNumber={''}
+      href={`/organize/${orgId}/projects/${
+        assignment?.campaign?.id ?? 'standalone'
+      }/callassignments/${caId}`}
+      PrimaryIcon={Map}
+      SecondaryIcon={Map}
+      title={assignment?.title || ''}
+    />
+  );
+};
+
+export default CanvassAssignmentListItem;
