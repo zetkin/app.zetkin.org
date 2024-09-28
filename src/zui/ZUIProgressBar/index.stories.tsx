0f961daf324d15da9c0548836fe098c8e09f9235
Lewis Conroy <lewis.k.conroy@outlook.com>
2024-09-28 14:35:37 +0100

    add stories :p

--- src/zui/ZUIProgressBar/index.stories.tsx
+++ src/zui/ZUIProgressBar/index.stories.tsx
@@ -22,3 +22,31 @@ export const ThreeSegments: Story = {
     size: 'medium',
   },
 };
+
+export const FourSegments: Story = {
+  args: {
+    progress: [33, 33, 10],
+    size: 'medium',
+  },
+};
+
+export const ExtraSmall: Story = {
+  args: {
+    progress: [33, 33, 10],
+    size: 'extraSmall',
+  },
+};
+
+export const Small: Story = {
+  args: {
+    progress: [33, 33, 10],
+    size: 'small',
+  },
+};
+
+export const Large: Story = {
+  args: {
+    progress: [33, 33, 10],
+    size: 'large',
+  },
+};
