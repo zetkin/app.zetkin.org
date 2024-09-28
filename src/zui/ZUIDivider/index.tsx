22c2afc096ed47a75912f3bebb50a2a0d599eaaa
sarlinkle <saralkleppe@gmail.com>
2024-09-28 17:17:33 +0200

    Change from color to border color

--- src/zui/ZUIDivider/index.tsx
+++ src/zui/ZUIDivider/index.tsx
@@ -4,7 +4,7 @@ interface ZUIDividerProps extends DividerProps {}
 
 const ZUIDivider: React.FC<ZUIDividerProps> = ({ sx, ...props }) => {
   const _sx = {
-    color: '#E0E0E0',
+    borderColor: '#E0E0E0',
     ...sx,
   };
   return <Divider sx={_sx} {...props} />;
