06d75a88438dd7510c270625992a756357153583
ziggabyte <ziggi.willpower@gmail.com>
2024-10-03 19:36:55 +0200

    Add disabled property.

--- src/zui/ZUISwitch/index.tsx
+++ src/zui/ZUISwitch/index.tsx
@@ -3,18 +3,21 @@ import { FC } from 'react';
 
 export interface ZUISwitchProps {
   checked: boolean;
+  disabled?: boolean;
   onChange: (checked: boolean) => void;
   size?: 'small' | 'medium';
 }
 
 const ZUISwitch: FC<ZUISwitchProps> = ({
   checked,
+  disabled = false,
   onChange,
   size = 'medium',
 }) => {
   return (
     <Switch
       checked={checked}
+      disabled={disabled}
       inputProps={{ 'aria-label': 'controlled' }}
       onChange={(event, checked) => onChange(checked)}
       size={size}
