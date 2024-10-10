81364284dd0d26ff3ee9317679b61c4817e6bf1b
ziggabyte <ziggi.willpower@gmail.com>
2024-10-10 09:44:01 +0200

    Create "previous"-button.

--- src/features/areas/components/VisitWizard.tsx
+++ src/features/areas/components/VisitWizard.tsx
@@ -1,4 +1,11 @@
-import { Box, Button, ButtonGroup, TextField, Typography } from '@mui/material';
+import {
+  Box,
+  Button,
+  TextField,
+  ToggleButton,
+  ToggleButtonGroup,
+  Typography,
+} from '@mui/material';
 import { FC, useState } from 'react';
 
 import { Visit } from '../types';
@@ -29,6 +36,8 @@ const VisitWizard: FC<VisitWizardProps> = ({
   >(null);
   const [doorWasOpened, setDoorWasOpened] = useState<boolean | null>(null);
 
+  const showPreviousButton = step && step != 1;
+
   return (
     <Box
       alignItems="center"
@@ -37,10 +46,33 @@ const VisitWizard: FC<VisitWizardProps> = ({
       height="100%"
       justifyContent="center"
     >
+      {showPreviousButton && (
+        <Button
+          onClick={() => {
+            if (step == 2) {
+              onStepChange(1);
+              setMissionAccomplished(null);
+            } else if (
+              step == 3 &&
+              (doorWasOpened == true || doorWasOpened == null)
+            ) {
+              onStepChange(2);
+              setNoteToOfficial(null);
+            } else {
+              //User is on step 3 because the door was not opened
+              onStepChange(1);
+              setNoteToOfficial(null);
+            }
+          }}
+        >
+          Previous
+        </Button>
+      )}
       <>
         <Box
           alignItems="center"
           display="flex"
+          flexDirection="column"
           flexGrow={1}
           justifyContent="center"
         >
@@ -59,44 +91,55 @@ const VisitWizard: FC<VisitWizardProps> = ({
           )}
         </Box>
         {step == 1 && (
-          <ButtonGroup fullWidth>
-            <Button
-              onClick={() => {
-                setDoorWasOpened(true);
-                onStepChange(2);
-              }}
-            >
-              YES
-            </Button>
-            <Button
-              onClick={() => {
-                setDoorWasOpened(false);
-                onStepChange(3);
-              }}
-            >
-              NO
-            </Button>
-          </ButtonGroup>
+          <ToggleButtonGroup
+            exclusive
+            fullWidth
+            onChange={(ev, value) => {
+              if (value != null) {
+                setDoorWasOpened(value);
+
+                if (value) {
+                  onStepChange(2);
+                } else {
+                  onStepChange(3);
+                }
+              }
+
+              //If user has come back and clicks the same option again
+              if (typeof doorWasOpened == 'boolean' && value == null) {
+                if (doorWasOpened) {
+                  onStepChange(2);
+                } else {
+                  onStepChange(3);
+                }
+              }
+            }}
+            value={doorWasOpened}
+          >
+            <ToggleButton value={true}>Yes</ToggleButton>
+            <ToggleButton value={false}>No</ToggleButton>
+          </ToggleButtonGroup>
         )}
         {step == 2 && (
-          <ButtonGroup fullWidth>
-            <Button
-              onClick={() => {
-                setMissionAccomplished(true);
+          <ToggleButtonGroup
+            exclusive
+            fullWidth
+            onChange={(ev, value) => {
+              if (value != null) {
+                setMissionAccomplished(value);
                 onStepChange(3);
-              }}
-            >
-              YES
-            </Button>
-            <Button
-              onClick={() => {
-                setMissionAccomplished(false);
+              }
+
+              //If user has come back and clicks the same option again
+              if (typeof missionAccomplished == 'boolean' && value == null) {
                 onStepChange(3);
-              }}
-            >
-              NO
-            </Button>
-          </ButtonGroup>
+              }
+            }}
+            value={missionAccomplished}
+          >
+            <ToggleButton value={true}>Yes</ToggleButton>
+            <ToggleButton value={false}>No</ToggleButton>
+          </ToggleButtonGroup>
         )}
         {step == 3 && (
           <Button
