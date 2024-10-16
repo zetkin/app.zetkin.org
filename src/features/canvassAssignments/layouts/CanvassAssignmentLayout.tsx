b60770d1ea7244d184c6a1a0d23f4371f582782b
Rebe R <36491300+rebecarubio@users.noreply.github.com>
2024-10-16 13:36:07 +0200

    implement areas and canvassers in header

--- src/features/canvassAssignments/layouts/CanvassAssignmentLayout.tsx
+++ src/features/canvassAssignments/layouts/CanvassAssignmentLayout.tsx
@@ -1,10 +1,17 @@
-import { FC, ReactNode } from 'react';
+import { Box } from '@mui/system';
+import { Typography } from '@mui/material';
 import { useRouter } from 'next/router';
+import { FC, ReactNode } from 'react';
+import { Pentagon, People } from '@mui/icons-material';
 
+import getCanvassers from '../utils/getCanvassers';
 import TabbedLayout from 'utils/layout/TabbedLayout';
 import useCanvassAssignment from '../hooks/useCanvassAssignment';
-import ZUIEditTextinPlace from 'zui/ZUIEditTextInPlace';
 import useCanvassAssignmentMutations from '../hooks/useCanvassAssignmentMutations';
+import useCanvassSessions from '../hooks/useCanvassSessions';
+import useCanvassAssignmentStats from '../hooks/useCanvassAssignmentStats';
+import ZUIEditTextinPlace from 'zui/ZUIEditTextInPlace';
+import ZUIFuture from 'zui/ZUIFuture';
 
 type CanvassAssignmentLayoutProps = {
   campId: number;
@@ -26,6 +33,15 @@ const CanvassAssignmentLayout: FC<CanvassAssignmentLayoutProps> = ({
     canvassAssId
   );
 
+  const allSessions = useCanvassSessions(orgId, canvassAssId).data || [];
+  const sessions = allSessions.filter(
+    (session) => session.assignment.id === canvassAssId
+  );
+
+  const stats = useCanvassAssignmentStats(orgId, canvassAssId);
+
+  const canvassers = getCanvassers(sessions);
+
   const isPlanTab = path.endsWith('/plan');
 
   if (!canvassAssignment) {
@@ -37,6 +53,24 @@ const CanvassAssignmentLayout: FC<CanvassAssignmentLayoutProps> = ({
       baseHref={`/organize/${orgId}/projects/${campId}/canvassassignments/${canvassAssId}`}
       defaultTab="/"
       fixedHeight={isPlanTab}
+      subtitle={
+        <Box alignItems="center" display="flex">
+          <ZUIFuture future={stats} ignoreDataWhileLoading skeletonWidth={100}>
+            {(data) => (
+              <Box display="flex" marginX={1}>
+                <Pentagon />
+                <Typography marginLeft={1}>{data.num_areas} Area(s)</Typography>
+              </Box>
+            )}
+          </ZUIFuture>
+          <Box display="flex" marginX={1}>
+            <People />
+            <Typography marginLeft={1}>
+              {canvassers.length} Canvasser(s)
+            </Typography>
+          </Box>
+        </Box>
+      }
       tabs={[
         { href: '/', label: 'Overview' },
         { href: '/plan', label: 'Plan' },
