47cf402b378cd3bae20af2222352fd8adb44a1a0
ziggabyte <ziggi.willpower@gmail.com>
2024-10-24 18:28:28 +0200

    Fix bug that resulted in areas with no places not having stats items.

--- src/app/beta/orgs/[orgId]/canvassassignments/[canvassAssId]/areastats/route.ts
+++ src/app/beta/orgs/[orgId]/canvassassignments/[canvassAssId]/areastats/route.ts
@@ -106,6 +106,11 @@ export async function GET(request: NextRequest, { params }: RouteMeta) {
       const statsByAreaId: Record<string, ZetkinAssignmentAreaStatsItem> = {};
 
       uniqueAreas.forEach((area) => {
+        statsByAreaId[area.id] = {
+          areaId: area.id,
+          num_households: 0,
+          num_visits: 0,
+        };
         allPlaces.forEach((place) => {
           const placeIsInArea = isPointInsidePolygon(
             { lat: place.position.lat, lng: place.position.lng },
@@ -113,14 +118,6 @@ export async function GET(request: NextRequest, { params }: RouteMeta) {
           );
 
           if (placeIsInArea) {
-            if (!statsByAreaId[area.id]) {
-              statsByAreaId[area.id] = {
-                areaId: area.id,
-                num_households: 0,
-                num_visits: 0,
-              };
-            }
-
             statsByAreaId[area.id].num_households += place.households.length;
 
             place.households.forEach((household) => {
