5f1ea53a2540aac309994e79f67ace3ec80a6b6c
ziggabyte <ziggi.willpower@gmail.com>
2024-10-24 20:08:12 +0200

    Hide custom controls and show pre-made settings instead (with button to show custom).

--- src/features/canvassAssignments/components/LayerSettings.tsx
+++ src/features/canvassAssignments/components/LayerSettings.tsx
@@ -1,5 +1,12 @@
-import { Box, FormControl, InputLabel, MenuItem, Select } from '@mui/material';
-import { FC } from 'react';
+import {
+  Box,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  Typography,
+} from '@mui/material';
+import { FC, useState } from 'react';
 
 type LayerSettingsProps = {
   areaStyle: 'assignees' | 'households' | 'progress' | 'hide';
@@ -24,89 +31,125 @@ const LayerSettings: FC<LayerSettingsProps> = ({
   overlayStyle,
   placeStyle,
 }) => {
+  const [showCustomControls, setShowCustomControls] = useState(false);
+
   return (
-    <Box display="flex" flexDirection="column" gap={2} paddingTop={1}>
-      <FormControl variant="outlined">
-        <InputLabel id="place-style-label">Place</InputLabel>
-        <Select
-          label="Place"
-          labelId="place-style-label"
-          onChange={(ev) => {
-            const newValue = ev.target.value;
-            if (
-              newValue == 'dot' ||
-              newValue == 'households' ||
-              newValue == 'progress' ||
-              newValue == 'hide'
-            ) {
-              onPlaceStyleChange(newValue);
-            }
-          }}
-          sx={{ backgroundColor: 'white', width: '10rem' }}
-          value={placeStyle}
-        >
-          <MenuItem value="dot">Dot</MenuItem>
-          <MenuItem value="households">Number of households</MenuItem>
-          <MenuItem value="progress">
-            Progress (visited in this assignment)
-          </MenuItem>
-          <MenuItem value="hide">Hide</MenuItem>
-        </Select>
-      </FormControl>
-      <FormControl variant="outlined">
-        <InputLabel id="area-style-label">Area</InputLabel>
-        <Select
-          label="Area"
-          labelId="area-style-color"
-          onChange={(ev) => {
-            const newValue = ev.target.value;
-            if (
-              newValue == 'households' ||
-              newValue == 'progress' ||
-              newValue == 'hide' ||
-              newValue == 'assignees'
-            ) {
-              onAreaStyleChange(newValue);
-            }
-          }}
-          sx={{ backgroundColor: 'white', width: '10rem' }}
-          value={areaStyle}
-        >
-          <MenuItem value="default">Assignees</MenuItem>
-          <MenuItem value="households">Number of households</MenuItem>
-          <MenuItem value="progress">
-            Progress (visited in this assignment)
-          </MenuItem>
-          <MenuItem value="hide">Hide</MenuItem>
-        </Select>
-      </FormControl>
-      <FormControl variant="outlined">
-        <InputLabel id="overlay-style-label">Overlay</InputLabel>
-        <Select
-          label="Overlay"
-          labelId="overlay-style-label"
-          onChange={(ev) => {
-            const newValue = ev.target.value;
-            if (
-              newValue == 'assignees' ||
-              newValue == 'households' ||
-              newValue == 'progress' ||
-              newValue == 'hide'
-            ) {
-              onOverlayStyleChange(newValue);
-            }
-          }}
-          sx={{ backgroundColor: 'white', width: '10rem' }}
-          value={overlayStyle}
-        >
-          <MenuItem value="assignees">Assignees</MenuItem>
-          <MenuItem value="households">Number of households</MenuItem>
-          <MenuItem value="progress">
-            Progress (visited in this assignment)
-          </MenuItem>
-          <MenuItem value="hide">Hide</MenuItem>
-        </Select>
-      </FormControl>
+    <Box alignItems="flex-start" display="flex" flexDirection="column" gap={1}>
+      Pick a style for your map layers
+      <Box
+        border={1}
+        onClick={() => {
+          onOverlayStyleChange('households');
+          onAreaStyleChange('households');
+          onPlaceStyleChange('households');
+        }}
+        padding={1}
+        sx={{ cursor: 'pointer' }}
+      >
+        <Typography>Number of households</Typography>
+      </Box>
+      <Box
+        border={1}
+        onClick={() => {
+          onOverlayStyleChange('assignees');
+          onAreaStyleChange('progress');
+          onPlaceStyleChange('progress');
+        }}
+        padding={1}
+        sx={{ cursor: 'pointer' }}
+      >
+        <Typography>Canvasser progress</Typography>
+      </Box>
+      <Box
+        border={1}
+        onClick={() => setShowCustomControls(true)}
+        padding={1}
+        sx={{ cursor: 'pointer' }}
+      >
+        <Typography>Custom</Typography>
+      </Box>
+      {showCustomControls && (
+        <Box display="flex" flexDirection="column" gap={2} paddingTop={1}>
+          <FormControl variant="outlined">
+            <InputLabel id="place-style-label">Place</InputLabel>
+            <Select
+              label="Place"
+              labelId="place-style-label"
+              onChange={(ev) => {
+                const newValue = ev.target.value;
+                if (
+                  newValue == 'dot' ||
+                  newValue == 'households' ||
+                  newValue == 'progress' ||
+                  newValue == 'hide'
+                ) {
+                  onPlaceStyleChange(newValue);
+                }
+              }}
+              value={placeStyle}
+            >
+              <MenuItem value="dot">Dot</MenuItem>
+              <MenuItem value="households">Number of households</MenuItem>
+              <MenuItem value="progress">
+                Progress (visited in this assignment)
+              </MenuItem>
+              <MenuItem value="hide">Hide</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl variant="outlined">
+            <InputLabel id="area-style-label">Area</InputLabel>
+            <Select
+              label="Area"
+              labelId="area-style-color"
+              onChange={(ev) => {
+                const newValue = ev.target.value;
+                if (
+                  newValue == 'households' ||
+                  newValue == 'progress' ||
+                  newValue == 'hide' ||
+                  newValue == 'assignees'
+                ) {
+                  onAreaStyleChange(newValue);
+                }
+              }}
+              value={areaStyle}
+            >
+              <MenuItem value="default">Assignees</MenuItem>
+              <MenuItem value="households">Number of households</MenuItem>
+              <MenuItem value="progress">
+                Progress (visited in this assignment)
+              </MenuItem>
+              <MenuItem value="hide">Hide</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl variant="outlined">
+            <InputLabel id="overlay-style-label">Overlay</InputLabel>
+            <Select
+              label="Overlay"
+              labelId="overlay-style-label"
+              onChange={(ev) => {
+                const newValue = ev.target.value;
+                if (
+                  newValue == 'assignees' ||
+                  newValue == 'households' ||
+                  newValue == 'progress' ||
+                  newValue == 'hide'
+                ) {
+                  onOverlayStyleChange(newValue);
+                }
+              }}
+              value={overlayStyle}
+            >
+              <MenuItem value="assignees">Assignees</MenuItem>
+              <MenuItem value="households">Number of households</MenuItem>
+              <MenuItem value="progress">
+                Progress (visited in this assignment)
+              </MenuItem>
+              <MenuItem value="hide">Hide</MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
+      )}
     </Box>
   );
 };
