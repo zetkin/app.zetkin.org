a46e3af29a141bc713e4a205be0464f06ad58799
ziggabyte <ziggi.willpower@gmail.com>
2024-11-08 10:17:23 +0100

    Let user set icon colors, default to grey.

--- src/zui/components/ZUIIcon/index.tsx
+++ src/zui/components/ZUIIcon/index.tsx
@@ -1,10 +1,20 @@
 import { FC } from 'react';
 import { OverridableComponent } from '@mui/material/OverridableComponent';
-import { SvgIconTypeMap } from '@mui/material';
+import { SvgIconTypeMap, useTheme } from '@mui/material';
 
 import { ZUISize } from '../types';
 
+type IconColor =
+  | 'secondary'
+  | 'primary'
+  | 'data'
+  | 'success'
+  | 'info'
+  | 'warning'
+  | 'danger';
+
 type ZUIIconProps = {
+  color?: IconColor;
   icon: OverridableComponent<SvgIconTypeMap<Record<string, unknown>, 'svg'>>;
   size?: ZUISize;
 };
@@ -20,13 +30,27 @@ const getFontSize = (size: ZUISize) => {
   }
 };
 
-const ZUIIcon: FC<ZUIIconProps> = ({ icon: Icon, size = 'medium' }) => {
+const ZUIIcon: FC<ZUIIconProps> = ({
+  color = 'secondary',
+  icon: Icon,
+  size = 'medium',
+}) => {
+  const theme = useTheme();
+  const iconColors: Record<IconColor, string> = {
+    danger: theme.palette.error.main,
+    data: theme.palette.data.main,
+    info: theme.palette.info.main,
+    primary: theme.palette.text.primary,
+    secondary: theme.palette.text.secondary,
+    success: theme.palette.success.main,
+    warning: theme.palette.warning.main,
+  };
   return (
     <Icon
-      sx={(theme) => ({
-        color: theme.palette.grey[400],
+      sx={{
+        color: iconColors[color],
         fontSize: getFontSize(size),
-      })}
+      }}
     />
   );
 };
