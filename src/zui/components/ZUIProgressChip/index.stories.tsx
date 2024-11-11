5d98bf8fe94bd4481b3021df96f429cd84262262
ziggabyte <ziggi.willpower@gmail.com>
2024-11-11 09:39:34 +0100

    Create ZUIProgressChip component + storybook story

---
+++ src/zui/components/ZUIProgressChip/index.stories.tsx
@@ -0,0 +1,28 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import ZUIProgressChip from './index';
+
+const meta: Meta<typeof ZUIProgressChip> = {
+  component: ZUIProgressChip,
+};
+export default meta;
+
+type Story = StoryObj<typeof ZUIProgressChip>;
+
+export const Two: Story = {
+  args: {
+    values: [243, 2934],
+  },
+};
+
+export const Three: Story = {
+  args: {
+    values: [243, 2934, 3948],
+  },
+};
+
+export const Four: Story = {
+  args: {
+    values: [243, 2934, 34, 1349],
+  },
+};
---
+++ src/zui/components/ZUIProgressChip/index.tsx
@@ -0,0 +1,53 @@
+import { Box, Typography, useTheme } from '@mui/material';
+import { FC } from 'react';
+
+type ZUIProgressChipProps = {
+  /**
+   * Values to be displayed in each of the sections of the chip.
+   * An array of 2, 3 or 4 numbers.
+   */
+  values:
+    | [number, number]
+    | [number, number, number]
+    | [number, number, number, number];
+};
+
+const ZUIProgressChip: FC<ZUIProgressChipProps> = ({ values }) => {
+  const theme = useTheme();
+
+  const colors = [theme.palette.data.main, theme.palette.data.final];
+
+  //Add middle colors to the colors array
+  if (values.length == 3) {
+    colors.splice(1, 0, theme.palette.data.mid2);
+  } else if (values.length == 4) {
+    colors.splice(1, 0, theme.palette.data.mid3, theme.palette.data.mid1);
+  }
+
+  return (
+    <Box display="inline-flex" sx={{ borderRadius: '2em', overflow: 'hidden' }}>
+      {values.map((value, index) => (
+        <Box
+          key={`${value}-${index}`}
+          bgcolor={colors[index]}
+          color={
+            index == values.length - 1
+              ? theme.palette.common.black
+              : theme.palette.common.white
+          }
+          sx={{
+            borderLeft:
+              index > 0 ? `0.063rem solid ${theme.palette.common.white}` : '',
+            paddingLeft: index == 0 ? '0.625rem' : '0.375rem',
+            paddingRight: index == values.length - 1 ? '0.625rem' : '0.375rem',
+            paddingY: '0.438rem',
+          }}
+        >
+          <Typography variant="labelSmMedium">{value}</Typography>
+        </Box>
+      ))}
+    </Box>
+  );
+};
+
+export default ZUIProgressChip;
