8897dc715fc01d4fe2808278a7e88143e1a4b129
Richard Olsson <r@richardolsson.se>
2024-11-13 18:30:36 +0100

    Remove unnecessary dependency for useCallback()

--- src/features/canvassAssignments/components/PlaceDialog/index.tsx
+++ src/features/canvassAssignments/components/PlaceDialog/index.tsx
@@ -51,7 +51,7 @@ const PlaceDialog: FC<PlaceDialogProps> = ({
 
   const back = useCallback(() => {
     history.back();
-  }, [setDialogStep]);
+  }, []);
 
   useEffect(() => {
     function handlePopState(event: PopStateEvent) {
