2f4be17f1c9afd2a03877aa13c04ff4cdeb360c8
Herover <hi+git@leonora.app>
2024-11-24 17:30:03 +0100

    Add dimension on preview

--- src/features/files/components/FileLibraryDialog/FilePreview.tsx
+++ src/features/files/components/FileLibraryDialog/FilePreview.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import Image from 'next/image';
 import { Box, Button, Typography } from '@mui/material';
 
@@ -14,20 +14,31 @@ type Props = {
 };
 
 const FilePreview: FC<Props> = ({ file, onBack, onSelect }) => {
+  const [dimensions, setDimensions] = useState({ height: 0, width: 0 });
+
   return (
     <Box display="flex" flexDirection="column" height="100%">
       <Box height="100%" my={2} overflow="auto">
         <TransparentGridBackground
           interactive={false}
           sx={{
-            height: 'calc(100% - 2em)',
+            height: 'calc(100% - 4em)',
             position: 'relative',
           }}
         >
           <Image
             alt={file.original_name}
             height="800"
+            onLoad={(e) => {
+              if (e.target instanceof HTMLImageElement) {
+                setDimensions({
+                  height: e.target.naturalHeight,
+                  width: e.target.naturalWidth,
+                });
+              }
+            }}
             src={file.url}
+            unoptimized
             style={{
               height: '100%',
               objectFit: 'contain',
@@ -38,6 +49,9 @@ const FilePreview: FC<Props> = ({ file, onBack, onSelect }) => {
         </TransparentGridBackground>
         <Typography color="secondary" mt={1} textAlign="center" variant="body2">
           {file.original_name}
+        </Typography>
+        <Typography color="secondary" mt={1} textAlign="center" variant="body2">
+          {dimensions.width} &times; {dimensions.height} pixels
         </Typography>
       </Box>
       <Box display="flex" gap={1} justifyContent="flex-end">
