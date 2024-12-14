9158ac215dd198ef61e9408a98c8ff20b9302957
Richard Olsson <r@richardolsson.se>
2024-12-14 05:34:37 +0100

    Filter out old events and assignments

--- src/features/events/hooks/useMyEvents.ts
+++ src/features/events/hooks/useMyEvents.ts
@@ -7,9 +7,15 @@ export default function useMyEvents() {
   const apiClient = useApiClient();
   const list = useAppSelector((state) => state.events.userEventList);
 
+  const now = new Date();
+  const today = now.toISOString().slice(0, 10);
+
   return useRemoteList(list, {
     actionOnLoad: () => userEventsLoad(),
     actionOnSuccess: (data) => userEventsLoaded(data),
-    loader: () => apiClient.get<ZetkinEvent[]>(`/api/users/me/actions`),
+    loader: () =>
+      apiClient.get<ZetkinEvent[]>(
+        `/api/users/me/actions?filter=end_time>=${today}`
+      ),
   });
 }
