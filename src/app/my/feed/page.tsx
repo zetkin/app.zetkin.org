5346ed57566c046a0e926a1f94115c54714c3257
Richard Olsson <r@richardolsson.se>
2024-12-15 15:50:07 +0100

    Add auth check on feed page

--- src/app/my/feed/page.tsx
+++ src/app/my/feed/page.tsx
@@ -1,5 +1,8 @@
+import redirectIfLoginNeeded from 'core/utils/redirectIfLoginNeeded';
 import AllEventsPage from 'features/home/pages/AllEventsPage';
 
 export default async function Page() {
+  await redirectIfLoginNeeded();
+
   return <AllEventsPage />;
 }
