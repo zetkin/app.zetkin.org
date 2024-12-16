zetkin/app.zetkin.org

commit 6174cd863b95e6953553095fb0de449bb9448f9d

Place footer always at bottom of page, and add empty states for both pages.

src/features/home/layouts/HomeLayout.tsx
@@ -29,6 +29,9 @@ const HomeLayout: FC<Props> = ({ children }) => {
 
   return (
     <Box
+      display="flex"
+      flexDirection="column"
+      height="100dvh"
       sx={{
         marginX: 'auto',
         maxWidth: 640,
@@ -61,7 +64,7 @@ const HomeLayout: FC<Props> = ({ children }) => {
           />
         </Tabs>
       </Box>
-      {children}
+      <Box flexGrow={1}>{children}</Box>
       <Box
         alignItems="center"
         component="footer"

src/features/home/pages/AllEventsPage.tsx
@@ -14,7 +14,7 @@ const AllEventsPage: FC = () => {
           alignItems="center"
           display="flex"
           flexDirection="column"
-          height="90dvh"
+          height="100%"
           justifyContent="center"
         >
           <ZUILogoLoadingIndicator />

src/features/home/pages/HomePage.tsx
@@ -14,7 +14,7 @@ const HomePage: FC = () => {
           alignItems="center"
           display="flex"
           flexDirection="column"
-          height="90dvh"
+          height="100%"
           justifyContent="center"
         >
           <ZUILogoLoadingIndicator />

src/features/home/components/AllEventsList.tsx
@@ -11,7 +11,7 @@ import {
   Switch,
   Typography,
 } from '@mui/material';
-import { CalendarMonthOutlined, Clear } from '@mui/icons-material';
+import { CalendarMonthOutlined, Clear, Search } from '@mui/icons-material';
 import {
   DateRange,
   DateRangeCalendar,
@@ -178,8 +178,19 @@ const AllEventsList: FC = () => {
         </FilterButton>
       </Box>
       {filteredEvents.length == 0 && (
-        <Box display="flex" justifyContent="center" padding={2}>
-          <Typography>No events</Typography>
+        <Box
+          alignItems="center"
+          display="flex"
+          flexDirection="column"
+          gap={1}
+          justifyContent="center"
+          marginTop={3}
+          padding={2}
+        >
+          <Typography color="secondary">
+            <Msg id={messageIds.feed.emptyListMessage} />
+          </Typography>
+          <Search color="secondary" fontSize="large" />
         </Box>
       )}
       {dates.map((date) => (

src/features/home/components/MyActivitiesList.tsx
@@ -1,9 +1,10 @@
 'use client';
 
 import { FC, useState } from 'react';
-import { Box, Button, Fade } from '@mui/material';
+import { Box, Button, Fade, Typography } from '@mui/material';
 import {
   GroupWorkOutlined,
+  Hotel,
   MapsHomeWork,
   PhoneInTalk,
 } from '@mui/icons-material';
@@ -57,6 +58,22 @@ const MyActivitiesList: FC = () => {
               </FilterButton>
             );
           })}
+        </Box>
+      )}
+      {filteredActivities.length == 0 && (
+        <Box
+          alignItems="center"
+          display="flex"
+          flexDirection="column"
+          gap={1}
+          justifyContent="center"
+          marginTop={3}
+          padding={2}
+        >
+          <Typography color="secondary">
+            <Msg id={messageIds.activityList.emptyListMessage} />
+          </Typography>
+          <Hotel color="secondary" fontSize="large" />
         </Box>
       )}
       {filteredActivities.map((activity) => {

src/features/home/l10n/messageIds.ts
@@ -8,6 +8,7 @@ export default makeMessages('feat.home', {
       signUp: m('Sign up'),
       undoSignup: m('Undo signup'),
     },
+    emptyListMessage: m('You are not signed up for any acitvities'),
     filters: {
       call: m('Call'),
       canvass: m('Canvass'),
@@ -22,6 +23,7 @@ export default makeMessages('feat.home', {
     noLocation: m('No physical location'),
   },
   feed: {
+    emptyListMessage: m('Could not find any events'),
     filters: {
       organizations: m<{ numOrgs: number }>(
         '{numOrgs, plural,=0 {Organizations} =1 {1 organization} other {# organizations}}'

commit aa4f1cd504309dfe007fd2cbbbb0a523f014e39a

Fix sticky tabs that broke after 6174cd863b95e6953553095fb0de449bb9448f9d

src/features/home/layouts/HomeLayout.tsx
@@ -30,9 +30,6 @@ const HomeLayout: FC<Props> = ({ children, title }) => {
 
   return (
     <Box
-      display="flex"
-      flexDirection="column"
-      height="100dvh"
       sx={{
         marginX: 'auto',
         maxWidth: 640,
@@ -73,7 +70,7 @@ const HomeLayout: FC<Props> = ({ children, title }) => {
           />
         </Tabs>
       </Box>
-      <Box flexGrow={1}>{children}</Box>
+      <Box minHeight="90dvh">{children}</Box>
       <Box
         alignItems="center"
         component="footer"

src/features/home/pages/AllEventsPage.tsx
@@ -14,7 +14,7 @@ const AllEventsPage: FC = () => {
           alignItems="center"
           display="flex"
           flexDirection="column"
-          height="100%"
+          height="90dvh"
           justifyContent="center"
         >
           <ZUILogoLoadingIndicator />

src/features/home/pages/HomePage.tsx
@@ -14,7 +14,7 @@ const HomePage: FC = () => {
           alignItems="center"
           display="flex"
           flexDirection="column"
-          height="100%"
+          height="90dvh"
           justifyContent="center"
         >
           <ZUILogoLoadingIndicator />