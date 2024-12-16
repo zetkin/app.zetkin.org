c37b9505714924a15f6c5a5ab861f2b2a9cc5197
Richard Olsson <r@richardolsson.se>
2024-12-16 16:17:50 +0100

    Add title to home pages

--- src/app/my/layout.tsx
+++ src/app/my/layout.tsx
@@ -1,7 +1,21 @@
 import { FC, ReactNode } from 'react';
+import { Metadata } from 'next';
+import { headers } from 'next/headers';
 
 import HomeLayout from 'features/home/layouts/HomeLayout';
 import HomeThemeProvider from 'features/home/components/HomeThemeProvider';
+import { getBrowserLanguage } from 'utils/locale';
+import getServerMessages from 'core/i18n/server';
+import messageIds from 'features/home/l10n/messageIds';
+
+export async function generateMetadata(): Promise<Metadata> {
+  const lang = getBrowserLanguage(headers().get('accept-language') || '');
+  const messages = await getServerMessages(lang, messageIds);
+
+  return {
+    title: process.env.HOME_TITLE || messages.title(),
+  };
+}
 
 type Props = {
   children: ReactNode;
