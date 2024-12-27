2b0cd2e2f6e03c0ca1e8772f18c7c451343b5098
Heli Hård Källoff <vierge.hard@gmail.com>
2024-12-28 00:57:09 +0100

    change warning message

--- src/features/emails/l10n/messageIds.ts
+++ src/features/emails/l10n/messageIds.ts
@@ -63,7 +63,7 @@ export default makeMessages('feat.emails', {
           noButtonText: m('Click to change this text!'),
         },
         settings: {
-          defaultButtonTextWarning: m('This button still has the default text'),
+          buttonTextWarning: m('Make sure to add your text to the button'),
           invalidUrl: m('This is not a valid link'),
           testLink: m('Click to test link'),
           urlLabel: m('Link url'),
