54e678c278217ab93a479a028d9b34733aa280ec
ziggabyte <ziggi.willpower@gmail.com>
2025-01-09 16:40:30 +0100

    WIP commit for richard, working on inline link tool

---
+++ src/zui/ZUIEditor/extensions/LinkExtension.tsx
@@ -0,0 +1,124 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+
+import { TagParseRule } from '@remirror/pm/model';
+import {
+  ApplySchemaAttributes,
+  extension,
+  legacyCommand as command,
+  MarkExtension,
+  MarkExtensionSpec,
+  MarkSpecOverride,
+  toggleMark,
+  PrimitiveSelection,
+  CommandFunction,
+  getTextSelection,
+  FromToProps,
+  updateMark,
+  ProsemirrorAttributes,
+} from 'remirror';
+
+export interface LinkOptions {
+  href: string;
+}
+
+export type LinkAttributes = ProsemirrorAttributes<{
+  /**
+   * The link which is a required property for the link mark.
+   */
+  href: string;
+}>;
+
+@extension<LinkOptions>({ defaultOptions: { href: '' } })
+class LinkExtension extends MarkExtension<LinkOptions> {
+  createMarkSpec(
+    extra: ApplySchemaAttributes,
+    override: MarkSpecOverride
+  ): MarkExtensionSpec {
+    return {
+      ...override,
+      attrs: extra.defaults(),
+      parseDOM: [
+        {
+          getAttrs: extra.parse,
+          tag: 'a',
+          //Sätt href-propertyn till värdet href från options
+        } as TagParseRule,
+        ...(override.parseDOM ?? []),
+      ],
+      toDOM: (node) => {
+        return [
+          'a',
+          {
+            ...extra.dom(node),
+            href: node.attrs.href,
+            style: 'text-decoration: underline;',
+          },
+          0,
+        ];
+      },
+    };
+  }
+
+  get name() {
+    return 'zlink' as const;
+  }
+
+  /* eslint-disable @typescript-eslint/ban-ts-comment */
+  //@ts-ignore
+  @command()
+  removeLink(selection?: PrimitiveSelection): CommandFunction {
+    return ({ tr, dispatch }) => {
+      const { from, to } = getTextSelection(selection ?? tr.selection, tr.doc);
+
+      if (!tr.doc.rangeHasMark(from, to, this.type)) {
+        return false;
+      }
+
+      dispatch?.(tr.removeMark(from, to, this.type));
+
+      return true;
+    };
+  }
+
+  /* eslint-disable @typescript-eslint/ban-ts-comment */
+  //@ts-ignore
+  @command()
+  setLink(selection?: PrimitiveSelection): CommandFunction {
+    return ({ tr, dispatch }) => {
+      const { from, to } = getTextSelection(selection ?? tr.selection, tr.doc);
+      dispatch?.(tr.addMark(from, to, this.type.create()));
+
+      return true;
+    };
+  }
+
+  /* eslint-disable @typescript-eslint/ban-ts-comment */
+  //@ts-ignore
+  @command()
+  toggleLink(selection?: PrimitiveSelection): CommandFunction {
+    return toggleMark({ selection, type: this.type });
+  }
+
+  /* eslint-disable @typescript-eslint/ban-ts-comment */
+  //@ts-ignore
+  @command()
+  updateLink(attrs: LinkAttributes, range?: FromToProps): CommandFunction {
+    return (props) => {
+      const { tr } = props;
+
+      tr.setMeta(this.name, { attrs, command: 'updateLink', range });
+
+      return updateMark({ attrs, range, type: this.type })(props);
+    };
+  }
+}
+
+declare global {
+  namespace Remirror {
+    interface AllExtensions {
+      zlink: LinkExtension;
+    }
+  }
+}
+
+export default LinkExtension;
--- src/zui/ZUIEditor/index.tsx
+++ src/zui/ZUIEditor/index.tsx
@@ -3,14 +3,18 @@ import {
   OnChangeJSON,
   Remirror,
   useActive,
+  useAttrs,
   useCommands,
   useRemirror,
 } from '@remirror/react';
 import { FC, useState } from 'react';
 import { BoldExtension } from 'remirror/extensions';
 import { RemirrorContentType } from 'remirror';
-import { Button } from '@mui/material';
+import { Box, Button, IconButton, TextField } from '@mui/material';
+import { LinkOff, LinkOutlined } from '@mui/icons-material';
+import { FloatingToolbar } from '@remirror/react-ui';
 
+import LinkExtension from './extensions/LinkExtension';
 import ButtonExtension from './extensions/ButtonExtension';
 
 const Menu = () => {
@@ -45,17 +49,85 @@ const Menu = () => {
   );
 };
 
+const InlineToolbar = () => {
+  const active = useActive();
+  const { toggleLink, updateLink } = useCommands();
+  const [linkToolsOpen, setLinkToolsOpen] = useState(false);
+
+  const url = (useAttrs().zlink()?.href as string) ?? '';
+  const [href, setHref] = useState(url);
+
+  //definiera en funktion som uppdaterar attributet href på noden
+
+  if (active.zbutton()) {
+    return null;
+  }
+
+  const selectionHasLink = active.zlink();
+
+  return (
+    <FloatingToolbar
+      placement="top"
+      positioner="selection"
+      style={{ zIndex: 10000 }}
+    >
+      <Box border={1}>
+        {linkToolsOpen && (
+          <Box display="flex" flexDirection="column" padding={1}>
+            <TextField
+              onChange={(ev) => {
+                setHref(ev.target.value);
+                updateLink({ href });
+              }}
+              size="small"
+              value={href}
+            />
+            <Button
+              onClick={() => {
+                toggleLink();
+                //kör funktionen som lägger till href-attributet på länken
+                setLinkToolsOpen(false);
+              }}
+              variant="outlined"
+            >
+              Add the link
+            </Button>
+          </Box>
+        )}
+        {!linkToolsOpen && (
+          <IconButton
+            onClick={() => {
+              if (selectionHasLink) {
+                toggleLink(); //removes the link
+              } else {
+                setLinkToolsOpen(true);
+              }
+            }}
+          >
+            {selectionHasLink ? <LinkOff /> : <LinkOutlined />}
+          </IconButton>
+        )}
+      </Box>
+    </FloatingToolbar>
+  );
+};
+
 const ZUIEditor: FC = () => {
   const [content, setContent] = useState<RemirrorContentType>();
   const { manager, state } = useRemirror({
     content,
-    extensions: () => [new BoldExtension({}), new ButtonExtension()],
+    extensions: () => [
+      new BoldExtension({}),
+      new ButtonExtension(),
+      new LinkExtension(),
+    ],
     selection: 'start',
   });
 
   return (
     <div style={{ minHeight: '200px' }}>
       <Remirror initialContent={state} manager={manager}>
+        <InlineToolbar />
         <EditorComponent />
         <Menu />
         <OnChangeJSON
