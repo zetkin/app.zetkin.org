2561cbda1129050177790aff0607d25377d55375
ziggabyte <ziggi.willpower@gmail.com>
2025-01-22 21:37:59 +0100

    Move block one step up.

---
+++ src/zui/ZUIEditor/EditorOverlays/BlockToolbar/buttons/MoveUpButton.tsx
@@ -0,0 +1,40 @@
+import { ArrowUpward } from '@mui/icons-material';
+import { IconButton } from '@mui/material';
+import { useCommands, useEditorState } from '@remirror/react';
+import { FC } from 'react';
+
+const MoveUpButton: FC = () => {
+  const state = useEditorState();
+  const { delete: deleteRange, moveBlockUp } = useCommands();
+
+  const pos = state.selection.$head.pos;
+  const resolved = state.doc.resolve(pos);
+  const node = resolved.node(1);
+
+  const posBefore = resolved.before(1);
+  const posAfter = resolved.after(1);
+  const resolvedBefore = state.doc.resolve(posBefore);
+
+  const nodeBefore = resolvedBefore.nodeBefore;
+
+  return (
+    <IconButton
+      disabled={!nodeBefore}
+      onClick={() => {
+        if (nodeBefore) {
+          const indexOfCurrentBlock = resolvedBefore.index();
+          const blockBeforePos = resolvedBefore.posAtIndex(
+            indexOfCurrentBlock - 1
+          );
+
+          deleteRange({ from: posBefore, to: posAfter });
+          moveBlockUp(node, blockBeforePos);
+        }
+      }}
+    >
+      <ArrowUpward />
+    </IconButton>
+  );
+};
+
+export default MoveUpButton;
