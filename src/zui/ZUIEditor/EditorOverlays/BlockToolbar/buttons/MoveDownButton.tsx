87b9b78613e0ee78ae1e12bae9dc5c37112fa814
ziggabyte <ziggi.willpower@gmail.com>
2025-01-24 15:36:50 +0100

    Create command for moving block down.

--- src/zui/ZUIEditor/EditorOverlays/BlockToolbar/buttons/MoveDownButton.tsx
+++ src/zui/ZUIEditor/EditorOverlays/BlockToolbar/buttons/MoveDownButton.tsx
@@ -1,41 +1,18 @@
 import { ArrowDownward } from '@mui/icons-material';
 import { IconButton } from '@mui/material';
-import { useCommands, useEditorState } from '@remirror/react';
+import { useCommands } from '@remirror/react';
 import { FC } from 'react';
 
 const MoveDownButton: FC = () => {
-  const state = useEditorState();
-  const { delete: deleteRange, moveBlock } = useCommands();
-
-  const pos = state.selection.$head.pos;
-  const resolved = state.doc.resolve(pos);
-  const node = resolved.node(1);
-
-  const posBefore = resolved.before(1);
-  const posAfter = resolved.after(1);
-  const resolvedBefore = state.doc.resolve(posBefore);
-  const resolvedAfter = state.doc.resolve(posAfter);
-
-  const nodeAfter = resolvedAfter.nodeAfter;
-
-  const indexOfCurrentBlock = resolvedBefore.index();
-  const indexOfBlockAfter = indexOfCurrentBlock + 1;
-
-  const numberOfBlocks = state.doc.children.length;
-  const nextBlockIsLast = indexOfBlockAfter == numberOfBlocks - 1;
-  const nextBlockIsEmptyLastBlock =
-    nextBlockIsLast && nodeAfter?.content.size == 0;
+  const { focus, moveBlockDown } = useCommands();
 
   return (
     <IconButton
-      disabled={!nodeAfter || nextBlockIsEmptyLastBlock}
+      disabled={!moveBlockDown.enabled()}
       onClick={() => {
-        if (nodeAfter && !nextBlockIsEmptyLastBlock) {
-          const blockAfterPos = resolvedBefore.posAtIndex(
-            indexOfCurrentBlock + 2
-          );
-          moveBlock(node, blockAfterPos);
-          deleteRange({ from: posBefore, to: posAfter });
+        if (moveBlockDown.enabled()) {
+          moveBlockDown();
+          focus();
         }
       }}
     >
