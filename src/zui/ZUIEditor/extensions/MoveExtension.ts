ebe6d6e84fcdc0a9a864a883c02c73f6c89ebda4
ziggabyte <ziggi.willpower@gmail.com>
2025-01-24 15:36:50 +0100

    Create command for moving block down.

--- src/zui/ZUIEditor/extensions/MoveExtension.ts
+++ src/zui/ZUIEditor/extensions/MoveExtension.ts
@@ -1,9 +1,9 @@
+import { TextSelection } from '@remirror/pm/state';
 import {
   CommandFunction,
   extension,
   legacyCommand as command,
   PlainExtension,
-  ProsemirrorNode,
 } from 'remirror';
 
 @extension({ customHandlerKeys: [], defaultOptions: {}, staticKeys: [] })
@@ -11,11 +11,35 @@ class MoveExtension extends PlainExtension {
   /* eslint-disable @typescript-eslint/ban-ts-comment */
   //@ts-ignore
   @command()
-  moveBlock(node: ProsemirrorNode, pos: number): CommandFunction {
+  moveBlockDown(): CommandFunction {
     return (props) => {
-      const { dispatch, tr } = props;
+      const { dispatch, state, tr } = props;
 
-      dispatch?.(tr.insert(pos, node));
+      const pos = state.selection.$head.pos;
+      const resolved = tr.doc.resolve(pos);
+      const node = resolved.node(1);
+
+      if (node == tr.doc.lastChild) {
+        return false;
+      }
+
+      const posBeforeSelected = resolved.before(1);
+      const posAfterSelected = resolved.after(1);
+
+      const resolvedNextNode = tr.doc.resolve(posAfterSelected + 1);
+      const posAfterNextNode = resolvedNextNode.after(1);
+
+      tr.insert(posAfterNextNode, node);
+      tr.delete(posBeforeSelected, posAfterSelected);
+
+      const sizeOfNode = tr.doc.resolve(resolved.start()).node().nodeSize;
+      const textSelection = TextSelection.create(
+        tr.doc,
+        posBeforeSelected + sizeOfNode + resolved.parentOffset
+      );
+      tr.setSelection(textSelection);
+
+      dispatch?.(tr);
 
       return true;
     };
