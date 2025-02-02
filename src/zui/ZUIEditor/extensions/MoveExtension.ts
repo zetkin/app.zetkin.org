e7472aa26128b40b7fd91a773d5e952273416173
ziggabyte <ziggi.willpower@gmail.com>
2025-02-02 18:55:58 +0100

    Select and move image blocks.

--- src/zui/ZUIEditor/extensions/MoveExtension.ts
+++ src/zui/ZUIEditor/extensions/MoveExtension.ts
@@ -4,6 +4,7 @@ import {
   extension,
   legacyCommand as command,
   PlainExtension,
+  isNodeSelection,
 } from 'remirror';
 
 @extension({ customHandlerKeys: [], defaultOptions: {}, staticKeys: [] })
@@ -15,33 +16,65 @@ class MoveExtension extends PlainExtension {
     return (props) => {
       const { dispatch, state, tr } = props;
 
-      const pos = state.selection.$head.pos;
-      const resolved = tr.doc.resolve(pos);
-      const node = resolved.node(1);
+      const selection = state.selection;
 
-      if (node == tr.doc.lastChild) {
-        return false;
+      if (isNodeSelection(selection)) {
+        const node = selection.node;
+
+        if (node == tr.doc.lastChild) {
+          return false;
+        }
+
+        const posBeforeSelected = selection.$anchor.before(1);
+        const posAfterSelected = selection.$head.after(1);
+
+        const resolvedNextNode = tr.doc.resolve(posAfterSelected + 1);
+        const posAfterNextNode = resolvedNextNode.after(1);
+
+        tr.insert(posAfterNextNode, node);
+        tr.delete(posBeforeSelected, posAfterSelected);
+
+        dispatch?.(tr);
+
+        return true;
+      } else {
+        const pos = selection.$head.pos;
+        const resolved = tr.doc.resolve(pos);
+        const node = resolved.node(1);
+
+        if (node == tr.doc.lastChild) {
+          return false;
+        }
+
+        const posBeforeSelected = resolved.before(1);
+        const posAfterSelected = resolved.after(1);
+
+        const resolvedNextNode = tr.doc.resolve(posAfterSelected + 1);
+        const posAfterNextNode = resolvedNextNode.after(1);
+
+        tr.insert(posAfterNextNode, node);
+        tr.delete(posBeforeSelected, posAfterSelected);
+
+        const nextNode = resolvedNextNode.node();
+
+        let nextNodeSize = 0;
+        if (nextNode?.type.name == 'doc') {
+          nextNodeSize = 2;
+        } else {
+          nextNodeSize = nextNode?.nodeSize;
+        }
+
+        const textSelection = TextSelection.create(
+          tr.doc,
+          resolved.before(1) + nextNodeSize + resolved.parentOffset
+        );
+        tr.setSelection(textSelection);
+        tr.scrollIntoView();
+
+        dispatch?.(tr);
+
+        return true;
       }
-
-      const posBeforeSelected = resolved.before(1);
-      const posAfterSelected = resolved.after(1);
-
-      const resolvedNextNode = tr.doc.resolve(posAfterSelected + 1);
-      const posAfterNextNode = resolvedNextNode.after(1);
-
-      tr.insert(posAfterNextNode, node);
-      tr.delete(posBeforeSelected, posAfterSelected);
-
-      const sizeOfNode = tr.doc.resolve(resolved.start()).node().nodeSize;
-      const textSelection = TextSelection.create(
-        tr.doc,
-        posBeforeSelected + sizeOfNode + resolved.parentOffset
-      );
-      tr.setSelection(textSelection);
-
-      dispatch?.(tr);
-
-      return true;
     };
   }
 
@@ -52,32 +85,55 @@ class MoveExtension extends PlainExtension {
     return (props) => {
       const { dispatch, state, tr } = props;
 
-      const pos = state.selection.$head.pos;
-      const resolved = tr.doc.resolve(pos);
-      const node = resolved.node(1);
+      const selection = state.selection;
+      if (isNodeSelection(selection)) {
+        const node = selection.node;
 
-      if (node == tr.doc.firstChild) {
-        return false;
+        if (node == tr.doc.firstChild) {
+          return false;
+        }
+
+        const posBeforeSelected = selection.$anchor.before(1);
+        const posAfterSelected = selection.$head.after(1);
+
+        const resolvedEndOfPreviousNode = tr.doc.resolve(posBeforeSelected - 1);
+        const posBeforeNodeBefore = resolvedEndOfPreviousNode.before(1);
+
+        tr.delete(posBeforeSelected, posAfterSelected);
+        tr.insert(posBeforeNodeBefore, node);
+
+        dispatch?.(tr);
+
+        return true;
+      } else {
+        const pos = selection.$head.pos;
+        const resolved = tr.doc.resolve(pos);
+        const node = resolved.node(1);
+
+        if (node == tr.doc.firstChild) {
+          return false;
+        }
+
+        const posBeforeSelected = resolved.before(1);
+        const posAfterSelected = resolved.after(1);
+
+        const resolvedEndOfPreviousNode = tr.doc.resolve(posBeforeSelected - 1);
+        const posBeforeNodeBefore = resolvedEndOfPreviousNode.before(1);
+
+        tr.delete(posBeforeSelected, posAfterSelected);
+        tr.insert(posBeforeNodeBefore, node);
+
+        const textSelection = TextSelection.create(
+          tr.doc,
+          posBeforeNodeBefore + resolved.parentOffset
+        );
+        tr.setSelection(textSelection);
+
+        dispatch?.(tr);
+        tr.scrollIntoView();
+
+        return true;
       }
-
-      const posBeforeSelected = resolved.before(1);
-      const posAfterSelected = resolved.after(1);
-
-      const resolvedEndOfPreviousNode = tr.doc.resolve(posBeforeSelected - 1);
-      const posBeforeNodeBefore = resolvedEndOfPreviousNode.before(1);
-
-      tr.delete(posBeforeSelected, posAfterSelected);
-      tr.insert(posBeforeNodeBefore, node);
-
-      const textSelection = TextSelection.create(
-        tr.doc,
-        posBeforeNodeBefore + resolved.parentOffset
-      );
-      tr.setSelection(textSelection);
-
-      dispatch?.(tr);
-
-      return true;
     };
   }
 
