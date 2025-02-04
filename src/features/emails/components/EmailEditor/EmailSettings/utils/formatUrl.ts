5405ff4cc46ff09fed03bdb856bf061ace0feea5
Niklas P <contact@niklasplessing.net>
2025-02-05 00:12:05 +0100

    allow mailto links in formatUrl

--- src/features/emails/components/EmailEditor/EmailSettings/utils/formatUrl.ts
+++ src/features/emails/components/EmailEditor/EmailSettings/utils/formatUrl.ts
@@ -1,6 +1,10 @@
 import isURL from 'validator/lib/isURL';
 
 export default function formatUrl(url: string): string | null {
+  if (url.startsWith('mailto:')) {
+    return url;
+  }
+
   if (isURL(url, { require_protocol: true })) {
     return url;
   }
