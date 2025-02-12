af8f2269224113ff6beb547dc0ad22349dca5e50
ziggabyte <ziggi.willpower@gmail.com>
2025-02-12 15:22:13 +0100

    Move the new editor into the Compose page.

---
+++ src/pages/organize/[orgId]/projects/[campId]/emails/[emailId]/compose.tsx
@@ -0,0 +1,84 @@
+import Head from 'next/head';
+import { GetServerSideProps } from 'next';
+
+import EmailLayout from 'features/emails/layout/EmailLayout';
+import { PageWithLayout } from 'utils/types';
+import { scaffold } from 'utils/next';
+import useDebounce from 'utils/hooks/useDebounce';
+import useEmail from 'features/emails/hooks/useEmail';
+import useServerSide from 'core/useServerSide';
+import { ZetkinEmailPostBody } from 'utils/types/zetkin';
+import EmailEditor from 'features/emails/components/EmailEditor';
+
+export const getServerSideProps: GetServerSideProps = scaffold(
+  async (ctx) => {
+    const { orgId, campId, emailId } = ctx.params!;
+
+    return {
+      props: {
+        campId,
+        emailId,
+        orgId,
+      },
+    };
+  },
+  {
+    authLevelRequired: 2,
+    localeScope: ['layout.organize.email', 'pages.organizeEmail'],
+  }
+);
+
+type Props = {
+  campId: string;
+  emailId: string;
+  orgId: string;
+};
+
+const EmailPage: PageWithLayout<Props> = ({ emailId, orgId }) => {
+  const { data: email, updateEmail } = useEmail(
+    parseInt(orgId),
+    parseInt(emailId)
+  );
+  const onServer = useServerSide();
+
+  const debouncedUpdateEmail = useDebounce(
+    async (email: Partial<ZetkinEmailPostBody>) => {
+      updateEmail({
+        ...email,
+        locked: undefined,
+      });
+    },
+    400
+  );
+
+  if (onServer) {
+    return null;
+  }
+
+  if (!email) {
+    return null;
+  }
+
+  const now = new Date();
+  const readOnly =
+    !!email.published || (!!email.published && new Date(email.published) > now);
+
+  return (
+    <>
+      <Head>
+        <title>{email.title}</title>
+      </Head>
+      <EmailEditor
+        email={email}
+        onSave={(email) => debouncedUpdateEmail(email)}
+        readOnly={readOnly}
+      />
+    </>
+  );
+};
+
+EmailPage.getLayout = function getLayout(page) {
+  return <EmailLayout fixedHeight>{page}</EmailLayout>;
+};
+
+export default EmailPage;
