137ff198e341e081961f307333061ca11be8973e
Richard Olsson <r@richardolsson.se>
2025-02-26 10:25:41 +0100

    Create crude layout for caller interface

---
+++ src/features/call/l10n/messageIds.ts
@@ -0,0 +1,8 @@
+import { m, makeMessages } from 'core/i18n';
+
+export default makeMessages('feat.call', {
+  nav: {
+    backToHome: m('Back to home'),
+    startCalling: m('Start calling'),
+  },
+});
