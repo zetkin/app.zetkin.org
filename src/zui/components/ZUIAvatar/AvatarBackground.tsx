08a12f1c24405c0848581ff9fe5aabfe0b23aae4
ziggabyte <ziggi.willpower@gmail.com>
2025-03-21 10:29:34 +0100

    Undo change of svg size.

--- src/zui/components/ZUIAvatar/AvatarBackground.tsx
+++ src/zui/components/ZUIAvatar/AvatarBackground.tsx
@@ -41,9 +41,9 @@ const AvatarBackground: FC<AvatarBackgroundProps> = ({
 
   return (
     <svg
-      height={size + 1}
+      height={size}
       version="1.1"
-      width={size + 1}
+      width={size}
       xmlns="http://www.w3.org/2000/svg"
     >
       <defs>
