41aa9ec423e47080566b2b8521da718fe801c74a
Richard Olsson <r@richardolsson.se>
2025-03-26 15:05:27 +0100

    Use recursive actions API to get events with single request

--- src/features/organizations/rpc/getUpcomingOrgEvents.ts
+++ src/features/organizations/rpc/getUpcomingOrgEvents.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 import { makeRPCDef } from 'core/rpc/types';
-import { ZetkinCampaign, ZetkinEvent } from 'utils/types/zetkin';
+import { ZetkinEvent } from 'utils/types/zetkin';
 import IApiClient from 'core/api/client/IApiClient';
 import getEventState from '../../events/utils/getEventState';
 import { EventState } from '../../events/hooks/useEventState';
@@ -21,19 +21,12 @@ export default makeRPCDef<Params, Result>(getUpcomingOrgEventsDef.name);
 
 async function handle(params: Params, apiClient: IApiClient): Promise<Result> {
   const { orgId } = params;
-  const campaigns = await apiClient.get<ZetkinCampaign[]>(
-    `/api/orgs/${orgId}/campaigns?recursive`
-  );
 
   const now = new Date().toISOString();
 
-  const events: ZetkinEvent[] = [];
-  for (const campaign of campaigns) {
-    const eventsOfOrg = await apiClient.get<ZetkinEvent[]>(
-      `/api/orgs/${campaign.organization.id}/campaigns/${campaign.id}/actions?filter=start_time%3E=${now}`
-    );
-    events.push(...eventsOfOrg);
-  }
+  const events = await apiClient.get<ZetkinEvent[]>(
+    `/api/orgs/${orgId}/actions?recursive&filter=start_time%3E=${now}`
+  );
 
   return events.filter((event) => {
     const state = getEventState(event);
