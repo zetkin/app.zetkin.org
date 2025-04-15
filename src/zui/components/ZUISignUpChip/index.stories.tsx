03ab86f6b65f344b600946a30ebe3d12e53627a3
ziggabyte <ziggi.willpower@gmail.com>
2025-04-15 13:26:54 +0200

    Create ZUISignUpChip component + storybook story.

---
+++ src/zui/components/ZUISignUpChip/index.stories.tsx
@@ -0,0 +1,23 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import ZUISignUpChip from './index';
+
+const meta: Meta<typeof ZUISignUpChip> = {
+  component: ZUISignUpChip,
+  title: 'Components/ZUISignUpChip',
+};
+export default meta;
+
+type Story = StoryObj<typeof ZUISignUpChip>;
+
+export const Needed: Story = {
+  args: {
+    status: 'needed',
+  },
+};
+
+export const SignedUp: Story = {
+  args: {
+    status: 'signedUp',
+  },
+};
---
+++ src/zui/components/ZUISignUpChip/index.tsx
@@ -0,0 +1,39 @@
+import { Box } from '@mui/material';
+import { FC } from 'react';
+
+import ZUIText from '../ZUIText';
+import { Msg } from 'core/i18n';
+import messageIds from 'zui/l10n/messageIds';
+
+type ZUISignUpChipProps = {
+  status: 'needed' | 'signedUp';
+};
+
+const ZUISignUpChip: FC<ZUISignUpChipProps> = ({ status }) => {
+  return (
+    <Box
+      sx={(theme) => ({
+        alignItems: 'center',
+        bgcolor:
+          status == 'needed'
+            ? theme.palette.swatches.blue[100]
+            : theme.palette.swatches.green[100],
+        borderRadius: 4,
+        color:
+          status == 'needed'
+            ? theme.palette.swatches.blue[900]
+            : theme.palette.swatches.green[900],
+        display: 'inline-flex',
+        pointerEvents: 'none',
+        px: 1,
+        py: 0.3,
+      })}
+    >
+      <ZUIText color="inherit" variant="bodySmRegular">
+        <Msg id={messageIds.signUpChip[status]} />
+      </ZUIText>
+    </Box>
+  );
+};
+
+export default ZUISignUpChip;
