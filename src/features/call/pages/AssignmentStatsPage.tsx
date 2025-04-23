75a33d0ce0b9f34742d6359a0717610cfbf7c057
Rebe R <36491300+rebecarubio@users.noreply.github.com>
2025-04-23 10:21:38 +0200

    remove Typography, implement design for smaller screens

--- src/features/call/pages/AssignmentStatsPage.tsx
+++ src/features/call/pages/AssignmentStatsPage.tsx
@@ -1,9 +1,8 @@
 'use client';
 
-import { Box, Typography } from '@mui/material';
+import { Box, useMediaQuery, useTheme } from '@mui/material';
 import { FC } from 'react';
 
-import newTheme from 'zui/theme';
 import { ZetkinCallAssignment, ZetkinEvent } from 'utils/types/zetkin';
 import ZUISection from 'zui/components/ZUISection';
 import ZUIText from 'zui/components/ZUIText';
@@ -25,30 +24,81 @@ const AssignmentStatsPage: FC<Props> = ({ assignment }) => {
     assignment.id
   );
 
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
   return (
     <Box m={2}>
       <ZUISection
         renderContent={() => (
-          <Box display="flex" justifyContent="space-between" mt={2}>
-            <Box>
-              <Typography sx={{ color: newTheme.palette.data.main }}>
-                {stats.num_calls_reached}
-              </Typography>
-              <ZUIText variant="headingSm">successful calls </ZUIText>
-            </Box>
-            <Box>
-              <Typography sx={{ color: newTheme.palette.data.main }}>
-                {stats.num_calls_made}
-              </Typography>
-              <ZUIText variant="headingSm"> calls made</ZUIText>
-            </Box>
-            <Box>
-              <Typography sx={{ color: newTheme.palette.data.main }}>
-                {stats.num_target_matches}
-              </Typography>
-              <ZUIText variant="headingSm">people in target group</ZUIText>
-            </Box>
-          </Box>
+          <>
+            {isMobile && (
+              <Box display="flex" gap={10} justifyContent="flex-start" mt={2}>
+                <Box>
+                  <Box display="flex">
+                    <Box
+                      mr={1}
+                      sx={(theme) => ({ color: theme.palette.data.main })}
+                    >
+                      <ZUIText color="inherit">
+                        {stats.num_calls_reached}
+                      </ZUIText>
+                    </Box>
+                    <ZUIText color="secondary">
+                      / {stats.num_calls_made}
+                    </ZUIText>
+                  </Box>
+                  <ZUIText variant="headingSm">successful</ZUIText>
+                </Box>
+                <Box>
+                  <Box
+                    mr={1}
+                    sx={(theme) => ({ color: theme.palette.data.main })}
+                  >
+                    <ZUIText color="inherit">
+                      {stats.num_target_matches}
+                    </ZUIText>
+                  </Box>
+                  <ZUIText variant="headingSm">targets</ZUIText>
+                </Box>
+              </Box>
+            )}
+            {!isMobile && (
+              <Box display="flex" justifyContent="space-between" mt={2}>
+                <Box>
+                  <Box
+                    mr={1}
+                    sx={(theme) => ({ color: theme.palette.data.main })}
+                  >
+                    <ZUIText color="inherit">{stats.num_calls_reached}</ZUIText>
+                  </Box>
+                  <ZUIText variant="headingSm">successful calls </ZUIText>
+                </Box>
+                <Box>
+                  <Box
+                    mr={1}
+                    sx={(theme) => ({ color: theme.palette.data.main })}
+                  >
+                    <ZUIText color="inherit">{stats.num_calls_made}</ZUIText>
+                  </Box>
+                  <Box sx={{ color: theme.palette.data.main }}>
+                    <ZUIText variant="headingSm"> calls made</ZUIText>
+                  </Box>
+                </Box>
+                <Box>
+                  <Box
+                    mr={1}
+                    sx={(theme) => ({ color: theme.palette.data.main })}
+                  >
+                    <ZUIText color="inherit">
+                      {stats.num_target_matches}
+                    </ZUIText>
+                  </Box>
+                  <ZUIText variant="headingSm">people in target group</ZUIText>
+                </Box>
+              </Box>
+            )}
+          </>
         )}
         title={'Assignment'}
       />
@@ -56,7 +106,9 @@ const AssignmentStatsPage: FC<Props> = ({ assignment }) => {
         <Box mt={2}>
           <ZUISection
             renderContent={() => (
-              <ZUIMarkdown markdown={assignment.instructions} />
+              <ZUIText>
+                <ZUIMarkdown markdown={assignment.instructions} />
+              </ZUIText>
             )}
             title={'Instructions'}
           />
