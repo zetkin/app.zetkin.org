6ae24e1505d57224ff87b0f73a0df970850eca7a
Rebe R <36491300+rebecarubio@users.noreply.github.com>
2025-05-23 15:15:07 +0200

    add PreviousCallsSection and CallSwitchDrawer components, add export to constants in PreviousCallsInfo

---
+++ src/features/call/components/CallSwitchDrawer.tsx
@@ -0,0 +1,63 @@
+import React, { FC } from 'react';
+import { Box } from '@mui/material';
+import { Search } from '@mui/icons-material';
+
+import ZUIAlert from 'zui/components/ZUIAlert';
+import ZUITextField from 'zui/components/ZUITextField';
+import { ZetkinCallAssignment } from 'utils/types/zetkin';
+import PreviousCallsSection from './PreviousCallsSection';
+import ZUIModal from 'zui/components/ZUIModal';
+
+type CallSwitchDrawerProps = {
+  assignment: ZetkinCallAssignment;
+  onClose: () => void;
+  open: boolean;
+};
+
+const CallSwitchDrawer: FC<CallSwitchDrawerProps> = ({
+  assignment,
+  onClose,
+  open,
+}) => {
+  return (
+    <ZUIModal
+      open={open}
+      primaryButton={{
+        label: 'Close',
+        onClick: () => {
+          onClose();
+        },
+      }}
+      title="Your calls"
+    >
+      <Box
+        display="flex"
+        flexDirection="column"
+        sx={{
+          overflowX: 'hidden',
+          width: '100%',
+        }}
+      >
+        <Box my={2}>
+          <ZUIAlert
+            button={{
+              label: 'Show unfinished calls',
+              onClick: () => {},
+            }}
+            description="Placeholder for future description"
+            severity={'info'}
+            title={'Title'}
+          />
+        </Box>
+        <ZUITextField fullWidth label="Type to find" startIcon={Search} />
+        <PreviousCallsSection
+          assingmentId={assignment.id}
+          onClose={onClose}
+          orgId={assignment.organization.id}
+        />
+      </Box>
+    </ZUIModal>
+  );
+};
+
+export default CallSwitchDrawer;
---
+++ src/features/call/components/PreviousCallsSection.tsx
@@ -0,0 +1,99 @@
+import { Box } from '@mui/material';
+
+import useCallMutations from '../hooks/useCallMutations';
+import useOutgoingCalls from '../hooks/useOutgoingCalls';
+import ZUIPersonAvatar from 'zui/components/ZUIPersonAvatar';
+import ZUIText from 'zui/components/ZUIText';
+import ZUIButton from 'zui/components/ZUIButton';
+import { labels, colors } from './PreviousCallsInfo';
+import ZUIDateTime from 'zui/ZUIDateTime';
+import ZUIDivider from 'zui/components/ZUIDivider';
+
+type PreviousCallsSectionProps = {
+  assingmentId: number;
+  onClose: () => void;
+  orgId: number;
+};
+
+const PreviousCallsSection: React.FC<PreviousCallsSectionProps> = ({
+  assingmentId,
+  onClose,
+  orgId,
+}) => {
+  assingmentId;
+  const { switchCurrentCall } = useCallMutations(orgId);
+
+  const outgoingCalls = useOutgoingCalls();
+  const previousCallsList = outgoingCalls.filter((call) => call.state !== 0);
+
+  return (
+    <Box>
+      {previousCallsList.map((call) => (
+        <Box key={call.id} mt={1}>
+          <Box
+            alignItems="center"
+            display="flex"
+            justifyContent="space-between"
+          >
+            <Box alignItems="center" display="flex" gap={1}>
+              <ZUIPersonAvatar
+                firstName={call.target.first_name}
+                id={call.target.id}
+                lastName={call.target.last_name}
+                size="medium"
+              />
+
+              <ZUIText noWrap variant="bodyMdSemiBold">
+                {call.target.first_name + ' ' + call.target.last_name}
+              </ZUIText>
+            </Box>
+
+            <ZUIButton
+              label="Log another call"
+              onClick={() => {
+                switchCurrentCall(call);
+                onClose();
+              }}
+              variant="secondary"
+            />
+          </Box>
+          <Box
+            alignItems="center"
+            display="flex"
+            gap={1}
+            justifyContent="space-between"
+            ml="2.5rem"
+            my={0.5}
+          >
+            <ZUIText variant="bodyMdRegular">{call.target.phone}</ZUIText>
+            <Box
+              alignItems="center"
+              display="flex"
+              gap={1}
+              sx={(theme) => {
+                const color = colors[call.state];
+                return {
+                  color:
+                    color === 'warning'
+                      ? theme.palette.warning.dark
+                      : theme.palette[color].main,
+                  minWidth: 0,
+                };
+              }}
+            >
+              <ZUIText color="inherit" noWrap>
+                {labels[call.state]}
+              </ZUIText>
+              <ZUIText color="secondary" noWrap>
+                <ZUIDateTime datetime={call.update_time} />
+              </ZUIText>
+            </Box>
+          </Box>
+          <ZUIDivider />
+        </Box>
+      ))}
+    </Box>
+  );
+};
+
+export default PreviousCallsSection;
