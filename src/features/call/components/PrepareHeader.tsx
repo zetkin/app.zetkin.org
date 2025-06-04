21bb91b395a9e7fec80728ac2efcd6da4c052a44
Rebe R <36491300+rebecarubio@users.noreply.github.com>
2025-06-04 14:10:34 +0200

    refactor PrepareHeader to follow design

--- src/features/call/components/PrepareHeader.tsx
+++ src/features/call/components/PrepareHeader.tsx
@@ -13,6 +13,7 @@ import ZUIDivider from 'zui/components/ZUIDivider';
 import CallSwitchModal from './CallSwitchModal';
 import ZUIBadge from 'zui/components/ZUIBadge';
 import useOutgoingCalls from '../hooks/useOutgoingCalls';
+import useIsMobile from 'utils/hooks/useIsMobile';
 
 type PrepareHeaderProps = {
   assignment: ZetkinCallAssignment;
@@ -32,6 +33,7 @@ const PrepareHeader: FC<PrepareHeaderProps> = ({
   const [showModal, setShowModal] = useState(false);
   const outgoingCalls = useOutgoingCalls();
   const unfinishedCallList = outgoingCalls.filter((call) => call.state === 0);
+  const isMobile = useIsMobile();
 
   if (!call) {
     return null;
@@ -39,67 +41,144 @@ const PrepareHeader: FC<PrepareHeaderProps> = ({
 
   return (
     <>
-      <Box sx={(theme) => ({ backgroundColor: theme.palette.common.white })}>
+      <Box
+        sx={(theme) => ({
+          backgroundColor: theme.palette.common.white,
+        })}
+      >
         <Box p={2}>
-          <Box alignItems="center" display="flex" mb={0.5} minWidth={0}>
-            <ZUIButton
-              label={assignment.title}
-              noWrap
-              onClick={() => {
-                deleteCall(call.id);
-                onBack();
-              }}
-              startIcon={ArrowBackIos}
-            />
-          </Box>
-
-          <Box
-            alignItems="center"
-            display="flex"
-            justifyContent="space-between"
-          >
-            <Box
-              alignItems="center"
-              display="flex"
-              gap={2}
-              mr={1}
-              sx={{ minWidth: 0 }}
-            >
-              <Box
-                sx={{ alignSelf: 'baseline', display: 'flex', flexShrink: 0 }}
-              >
-                <ZUIPersonAvatar
-                  firstName={call.target.first_name}
-                  id={call.target.id}
-                  lastName={call.target.last_name}
-                />
-              </Box>
-
-              <Box minWidth={0}>
-                <ZUIText noWrap variant="headingLg">
-                  {call.target.first_name} {call.target.last_name}
-                </ZUIText>
-                <ZUIText>{call.target.phone}</ZUIText>
-              </Box>
-
-              <ZUIBadge color="warning" number={unfinishedCallList.length}>
+          {isMobile && (
+            <>
+              <Box alignItems="center" display="flex" mb={0.5} minWidth={0}>
                 <ZUIButton
-                  label={'Switch'}
-                  onClick={() => setShowModal(true)}
-                  variant="secondary"
+                  label={assignment.title}
+                  noWrap
+                  onClick={() => {
+                    deleteCall(call.id);
+                    onBack();
+                  }}
+                  startIcon={ArrowBackIos}
                 />
-              </ZUIBadge>
-            </Box>
-            <Box alignItems="center" display="flex" gap={1}>
-              <SkipCallDialog
-                assignment={assignment}
-                callId={call.id}
-                targetName={`${call.target.first_name} ${call.target.last_name}`}
-              />
+              </Box>
+              <Box
+                alignItems="flex-start"
+                display="flex"
+                justifyContent="space-between"
+              >
+                <Box display="flex" flexDirection="column" minWidth={0}>
+                  <ZUIText noWrap variant="headingLg">
+                    {call.target.phone}
+                  </ZUIText>
+                  <Box
+                    alignItems="center"
+                    display="flex"
+                    gap={0.5}
+                    minWidth={0}
+                  >
+                    <ZUIPersonAvatar
+                      firstName={call.target.first_name}
+                      id={call.target.id}
+                      lastName={call.target.last_name}
+                      size="small"
+                    />
+                    <ZUIText noWrap variant="bodyMdRegular">
+                      {call.target.first_name} {call.target.last_name}
+                    </ZUIText>
+                  </Box>
+                </Box>
 
-              <ZUIButton label="Call" onClick={onStartCall} variant="primary" />
-            </Box>
-          </Box>
+                <Box alignItems="center" display="flex">
+                  <ZUIBadge color="warning" number={unfinishedCallList.length}>
+                    <ZUIButton
+                      label="Switch"
+                      onClick={() => setShowModal(true)}
+                      variant="secondary"
+                    />
+                  </ZUIBadge>
+                </Box>
+              </Box>
+              <Box display="flex" gap={2} justifyContent="flex-end">
+                <SkipCallDialog
+                  assignment={assignment}
+                  callId={call.id}
+                  targetName={`${call.target.first_name} ${call.target.last_name}`}
+                />
+                <ZUIButton
+                  label="Call"
+                  onClick={onStartCall}
+                  variant="primary"
+                />
+              </Box>
+            </>
+          )}
+          {!isMobile && (
+            <>
+              <Box mb={1} minWidth={0}>
+                <ZUIButton
+                  label={assignment.title}
+                  noWrap
+                  onClick={() => {
+                    deleteCall(call.id);
+                    onBack();
+                  }}
+                  startIcon={ArrowBackIos}
+                />
+              </Box>
+              <Box
+                alignItems="flex-start"
+                display="flex"
+                justifyContent="space-between"
+              >
+                <Box display="flex">
+                  <Box display="flex" flexDirection="column" minWidth={0}>
+                    <ZUIText variant="headingLg">{call.target.phone}</ZUIText>
+                    <Box
+                      alignItems="center"
+                      display="flex"
+                      gap={1}
+                      minWidth={0}
+                    >
+                      <ZUIPersonAvatar
+                        firstName={call.target.first_name}
+                        id={call.target.id}
+                        lastName={call.target.last_name}
+                        size="small"
+                      />
+                      <ZUIText noWrap variant="bodyMdRegular">
+                        {call.target.first_name} {call.target.last_name}
+                      </ZUIText>
+                    </Box>
+                  </Box>
+
+                  <Box alignItems="center" display="flex" ml={2}>
+                    <ZUIBadge
+                      color="warning"
+                      number={unfinishedCallList.length}
+                    >
+                      <ZUIButton
+                        label="Switch"
+                        onClick={() => setShowModal(true)}
+                        variant="secondary"
+                      />
+                    </ZUIBadge>
+                  </Box>
+                </Box>
+
+                <Box alignItems="center" display="flex" gap={1}>
+                  <SkipCallDialog
+                    assignment={assignment}
+                    callId={call.id}
+                    targetName={`${call.target.first_name} ${call.target.last_name}`}
+                  />
+                  <ZUIButton
+                    label="Call"
+                    onClick={onStartCall}
+                    variant="primary"
+                  />
+                </Box>
+              </Box>
+            </>
+          )}
         </Box>
       </Box>
       <CallSwitchModal
