56f2b38baf9f249ad5c6c7dafd0667ee7bedcc8e
owenboy9 <owenkraus9@gmail.com>
2025-06-06 17:26:32 +0200

    add property to allow target _blank on a tags

--- src/zui/ZUIMarkdown.tsx
+++ src/zui/ZUIMarkdown.tsx
@@ -6,10 +6,33 @@ import ZUICleanHtml from './ZUICleanHtml';
 interface ZUIMarkdownProps {
   BoxProps?: BoxProps;
   markdown: string;
+  forceTargetBlank?: boolean;
 }
 
-const ZUIMarkdown: React.FC<ZUIMarkdownProps> = ({ BoxProps, markdown }) => {
+const ZUIMarkdown: React.FC<ZUIMarkdownProps> = ({
+  BoxProps,
+  markdown,
+  forceTargetBlank,
+}) => {
   const dirtyHtml = marked(markdown, { breaks: true });
+  if (forceTargetBlank) {
+    const parser = new DOMParser();
+    const parsedHtml = parser.parseFromString(dirtyHtml, 'text/html');
+    const aNodes = parsedHtml.querySelectorAll('a');
+    aNodes.forEach((a) => {
+      a.setAttribute('target', '_blank');
+      a.setAttribute('rel', 'noopener noreferrer');
+    });
+    const serializer = new XMLSerializer();
+    const dirtyNewTabHtml = serializer.serializeToString(parsedHtml);
+    return (
+      <ZUICleanHtml
+        allowTargetBlank={true}
+        BoxProps={BoxProps}
+        dirtyHtml={dirtyNewTabHtml}
+      />
+    );
+  }
   return <ZUICleanHtml BoxProps={BoxProps} dirtyHtml={dirtyHtml} />;
 };
 
