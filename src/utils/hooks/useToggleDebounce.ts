ed11394461d320df640ef40c7efc808efced489c
Karl Kristiansen <karlkristians@gmail.com>
2025-06-07 18:36:43 +0200

    MVP: Delay the opening, to give the closing card time to close + evaluate if the user hovers for more than 250ms, before triggering the openPopper event

--- src/utils/hooks/useToggleDebounce.ts
+++ src/utils/hooks/useToggleDebounce.ts
@@ -5,15 +5,32 @@ import { MouseEvent, useRef } from 'react';
 export default function useToggleDebounce(
   open: (ev: MouseEvent<HTMLElement>) => void,
   close: (ev: MouseEvent<HTMLElement>) => void,
-  delay = 250
+  delay = 250,
+  minHoverDuration = 250,
 ) {
+  const targetRef = useRef<HTMLElement | null>(null);
   const timeoutId = useRef<number | undefined>();
+  let hoverStartTime = useRef<number | null>(null);
+
   function openDebounced(ev: MouseEvent<HTMLElement>) {
     clearTimeout(timeoutId.current);
-    open(ev);
+
+    // We capture the currentTarget, otherwise React won't accept the delayed event
+    targetRef.current = ev.currentTarget; 
+
+    // Capture start-point of hovering to evaluate if the user has hovered > minHoverDuration
+    hoverStartTime = Date.now();
+
+    window.setTimeout(() => {
+      if (hoverStartTime && Date.now() - hoverStartTime > minHoverDuration) {
+        hoverStartTime = null;
+        open({ ...ev, currentTarget: targetRef.current } as MouseEvent<HTMLElement>);
+      } 
+    }, minHoverDuration);
   }
 
   function closeDebounced(ev: MouseEvent<HTMLElement>) {
+    hoverStartTime = null;
     timeoutId.current = window.setTimeout(() => {
       close(ev);
     }, delay);
