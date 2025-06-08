60763ccfe100745b84f7eae80ff23a824119be85
Karl Kristiansen <karlkristians@gmail.com>
2025-06-08 07:54:18 +0200

    Better comments and prettier code blocks

--- src/utils/hooks/useToggleDebounce.ts
+++ src/utils/hooks/useToggleDebounce.ts
@@ -2,11 +2,12 @@ import { MouseEvent, useRef } from 'react';
 
 // Close will be called after a delay, which is cancelled if open is called
 // Useful to toggle poppers without closing the instant the cursor is outside the triggering element
+// Open will wait for a 'minHoverDuration' before interpretting it as intentional
 export default function useToggleDebounce(
   open: (ev: MouseEvent<HTMLElement>) => void,
   close: (ev: MouseEvent<HTMLElement>) => void,
   delay = 250,
-  minHoverDuration = 250,
+  minHoverDuration = 250
 ) {
   const targetRef = useRef<HTMLElement | null>(null);
   const timeoutId = useRef<number | undefined>();
@@ -16,7 +17,7 @@ export default function useToggleDebounce(
     clearTimeout(timeoutId.current);
 
     // We capture the currentTarget, otherwise React won't accept the delayed event
-    targetRef.current = ev.currentTarget; 
+    targetRef.current = ev.currentTarget;
 
     // Capture start-point of hovering to evaluate if the user has hovered > minHoverDuration
     hoverStartTime = Date.now();
@@ -24,8 +25,11 @@ export default function useToggleDebounce(
     window.setTimeout(() => {
       if (hoverStartTime && Date.now() - hoverStartTime > minHoverDuration) {
         hoverStartTime = null;
-        open({ ...ev, currentTarget: targetRef.current } as MouseEvent<HTMLElement>);
-      } 
+        open({
+          ...ev,
+          currentTarget: targetRef.current,
+        } as MouseEvent<HTMLElement>);
+      }
     }, minHoverDuration);
   }
 
