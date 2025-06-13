2056a48b91a448cdc2f144c4550796acd0fa42eb
Rebe R <36491300+rebecarubio@users.noreply.github.com>
2025-06-13 14:22:15 +0200

    add custom chip to show that survey is in progress

--- src/features/call/components/SurveyCard.tsx
+++ src/features/call/components/SurveyCard.tsx
@@ -5,6 +5,10 @@ import MyActivityListItem from 'features/home/components/MyActivityListItem';
 import ZUIButton from 'zui/components/ZUIButton';
 import { ZetkinSurveyExtended } from 'utils/types/zetkin';
 import SurveyModal from './SurveyModal';
+import { Box } from '@mui/material';
+import ZUILabel from 'zui/components/ZUILabel';
+import ZUIText from 'zui/components/ZUIText';
+import { useAppSelector } from 'core/hooks';
 
 type SurveyCardProps = {
   survey: ZetkinSurveyExtended;
@@ -13,16 +17,47 @@ type SurveyCardProps = {
 
 const SurveyCard: FC<SurveyCardProps> = ({ survey, targetId }) => {
   const [modalOpen, setModalOpen] = useState(false);
+
+  const surveyList = useAppSelector((state) => state.call.filledSurveys || []);
+
+  const isSurveyInProgress = surveyList.some(
+    (item) => item.surveyId === survey.id && item.targetId === targetId
+  );
+
   return (
     <>
       <MyActivityListItem
         actions={[
-          <ZUIButton
-            key={survey.id}
-            label={'Fill out'}
-            onClick={() => setModalOpen(true)}
-            variant="primary"
-          />,
+          <>
+            <ZUIButton
+              key={survey.id}
+              label={'Fill out'}
+              onClick={() => setModalOpen(true)}
+              variant="primary"
+            />
+            {isSurveyInProgress && (
+              //TODO: Create ZUIComponent
+              <Box
+                sx={(theme) => ({
+                  alignItems: 'center',
+                  bgcolor: theme.palette.swatches.blue[100],
+
+                  borderRadius: 4,
+                  color: theme.palette.swatches.blue[900],
+                  display: 'inline-flex',
+                  pointerEvents: 'none',
+                  px: 1,
+                  py: 0.3,
+                })}
+              >
+                <ZUILabel color="inherit">
+                  <ZUIText color="inherit" variant="bodySmRegular">
+                    {'Survey in progress'}
+                  </ZUIText>
+                </ZUILabel>
+              </Box>
+            )}
+          </>,
         ]}
         iconTitle={Assignment}
         info={[
