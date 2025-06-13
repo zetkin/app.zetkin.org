3e7dc6c2d913490751e45e5eb038061217514fd0
ziggabyte <ziggi.willpower@gmail.com>
2025-06-13 16:31:59 +0200

    Correct type in household scale question report response.

--- src/features/canvass/components/LocationDialog/pages/HouseholdVisitPage.tsx
+++ src/features/canvass/components/LocationDialog/pages/HouseholdVisitPage.tsx
@@ -91,11 +91,11 @@ const HouseholdVisitPage: FC<HouseholdVisitPageProps> = ({
                   },
                 ]
               : [
-                  { label: 1, value: '1' },
-                  { label: 2, value: '2' },
-                  { label: 3, value: '3' },
-                  { label: 4, value: '4' },
-                  { label: 5, value: '5' },
+                  { label: 1, value: 1 },
+                  { label: 2, value: 2 },
+                  { label: 3, value: 3 },
+                  { label: 4, value: 4 },
+                  { label: 5, value: 5 },
                 ];
 
           const stepIsCurrent = index == step;
