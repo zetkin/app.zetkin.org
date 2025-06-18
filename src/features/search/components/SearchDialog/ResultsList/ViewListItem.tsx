dd5c63c898b4752f52b336862fb4938c609c22d0
Richard Olsson <r@richardolsson.se>
2025-06-18 21:43:22 +0200

    Use new URL in links to lists from search dialog

--- src/features/search/components/SearchDialog/ResultsList/ViewListItem.tsx
+++ src/features/search/components/SearchDialog/ResultsList/ViewListItem.tsx
@@ -27,7 +27,7 @@ const ViewListItem: React.FunctionComponent<{ view: ZetkinView }> = ({
   elements.push(messages.results.view());
 
   return (
-    <Link href={`/organize/${orgId}/people/views/${view.id}`}>
+    <Link href={`/organize/${orgId}/people/lists/${view.id}`}>
       <ListItem data-testid="SearchDialog-resultsListItem">
         <ListItemButton>
           <ListItemAvatar>
