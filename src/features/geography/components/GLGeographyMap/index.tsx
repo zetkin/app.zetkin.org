45577a22a7c9ef396ab5546e5eed405b8ac112b9
Richard Olsson <r@richardolsson.se>
2025-06-19 19:46:32 +0200

    Create barebones GL geography map which only renders areas

---
+++ src/features/geography/components/GLGeographyMap/index.tsx
@@ -0,0 +1,102 @@
+import { Box } from '@mui/material';
+import Map, { Layer, Source } from '@vis.gl/react-maplibre';
+import { FC, useMemo, useState } from 'react';
+import { LngLatBounds, Map as MapType } from 'maplibre-gl';
+
+import { Zetkin2Area } from 'features/areas/types';
+import AreaFilterProvider from 'features/areas/components/AreaFilters/AreaFilterContext';
+import ZUIMapControls from 'zui/ZUIMapControls';
+import { useEnv } from 'core/hooks';
+import oldTheme from 'theme';
+
+type Props = {
+  areas: Zetkin2Area[];
+};
+
+const GLGeographyMap: FC<Props> = ({ areas }) => {
+  const env = useEnv();
+  const [map, setMap] = useState<MapType | null>(null);
+
+  const areasGeoJson: GeoJSON.GeoJSON = useMemo(() => {
+    return {
+      features: areas.map((area) => ({
+        geometry: area.boundary,
+        properties: {},
+        type: 'Feature',
+      })),
+      type: 'FeatureCollection',
+    };
+  }, [areas]);
+
+  return (
+    <AreaFilterProvider>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          height: '100%',
+          width: '100%',
+        }}
+      >
+        <ZUIMapControls
+          onFitBounds={() => {
+            if (map) {
+              const firstPolygon = areas[0]?.boundary.coordinates[0];
+              if (firstPolygon.length) {
+                const totalBounds = new LngLatBounds(
+                  firstPolygon[0],
+                  firstPolygon[0]
+                );
+
+                // Extend with all areas
+                areas.forEach((area) => {
+                  area.boundary.coordinates[0]?.forEach((lngLat) =>
+                    totalBounds.extend(lngLat)
+                  );
+                });
+
+                if (totalBounds) {
+                  map.fitBounds(totalBounds, { animate: true, duration: 800 });
+                }
+              }
+            }
+          }}
+          onGeolocate={(lngLat) => {
+            map?.panTo(lngLat, { animate: true, duration: 800 });
+          }}
+          onZoomIn={() => map?.zoomIn()}
+          onZoomOut={() => map?.zoomOut()}
+        />
+        <Map
+          ref={(map) => setMap(map?.getMap() ?? null)}
+          mapStyle={env.vars.MAPLIBRE_STYLE}
+          onClick={(ev) => {
+            ev.target.panTo(ev.lngLat, { animate: true });
+          }}
+          style={{ height: '100%', width: '100%' }}
+        >
+          <Source data={areasGeoJson} id="areas" type="geojson">
+            <Layer
+              id="areaLine"
+              paint={{
+                'line-color': oldTheme.palette.primary.main,
+                'line-width': 2,
+              }}
+              type="line"
+            />
+            <Layer
+              id="areaFill"
+              paint={{
+                'fill-color': oldTheme.palette.primary.main,
+                'fill-opacity': 0.4,
+              }}
+              type="fill"
+            />
+          </Source>
+        </Map>
+      </Box>
+    </AreaFilterProvider>
+  );
+};
+
+export default GLGeographyMap;
