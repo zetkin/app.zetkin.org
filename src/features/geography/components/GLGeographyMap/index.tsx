6f4f9b3da7f584861ff50e67bdebe00b301ea8a8
Richard Olsson <r@richardolsson.se>
2025-06-20 09:45:41 +0200

    Implementing area polygon editing

--- src/features/geography/components/GLGeographyMap/index.tsx
+++ src/features/geography/components/GLGeographyMap/index.tsx
@@ -1,4 +1,5 @@
 import area from '@turf/area';
+import distance from '@turf/distance';
 import { Box } from '@mui/material';
 import Map, { Layer, Source } from '@vis.gl/react-maplibre';
 import { FC, useEffect, useMemo, useState } from 'react';
@@ -6,15 +7,18 @@ import {
   LngLatBounds,
   LngLatLike,
   MapLayerMouseEvent,
+  MapMouseEvent,
   Map as MapType,
 } from 'maplibre-gl';
 
-import { Zetkin2Area } from 'features/areas/types';
+import { Zetkin2Area, Zetkin2AreaLine } from 'features/areas/types';
 import AreaFilterProvider from 'features/areas/components/AreaFilters/AreaFilterContext';
 import ZUIMapControls from 'zui/ZUIMapControls';
 import { useEnv } from 'core/hooks';
 import oldTheme from 'theme';
 import AreaOverlay from 'features/areas/components/AreaOverlay';
+import lngLat from 'features/geography/utils/lngLat';
+import oldAreaFormat from 'features/areas/utils/oldAreaFormat';
 
 type Props = {
   areas: Zetkin2Area[];
@@ -23,8 +27,13 @@ type Props = {
 const GLGeographyMap: FC<Props> = ({ areas }) => {
   const env = useEnv();
   const [map, setMap] = useState<MapType | null>(null);
+  const [editingArea, setEditingArea] = useState<Zetkin2Area | null>(null);
+  const [draggingPoints, setDraggingPoints] = useState<Zetkin2AreaLine | null>(
+    null
+  );
   const [selectedId, setSelectedId] = useState(0);
 
+  const editing = !!editingArea;
   const selectedArea = areas.find((area) => area.id == selectedId) || null;
 
   const bounds: [LngLatLike, LngLatLike] = useMemo(() => {
@@ -59,21 +68,29 @@ const GLGeographyMap: FC<Props> = ({ areas }) => {
         })),
       type: 'FeatureCollection',
     };
-  }, [areas]);
+  }, [areas, selectedArea]);
 
   const selectedAreaGeoJson: GeoJSON.GeoJSON | null = useMemo(() => {
-    return selectedArea
-      ? {
-          geometry: selectedArea.boundary,
-          properties: { id: selectedArea.id },
-          type: 'Feature',
-        }
-      : null;
-  }, [selectedArea]);
+    if (selectedArea && draggingPoints) {
+      return {
+        geometry: { coordinates: [draggingPoints], type: 'Polygon' },
+        properties: { id: selectedArea.id },
+        type: 'Feature',
+      };
+    } else if (selectedArea) {
+      return {
+        geometry: editing ? editingArea.boundary : selectedArea.boundary,
+        properties: { id: selectedArea.id },
+        type: 'Feature',
+      };
+    } else {
+      return null;
+    }
+  }, [selectedArea, draggingPoints, editingArea]);
 
   useEffect(() => {
     if (map) {
-      const handle = (ev: MapLayerMouseEvent) => {
+      const handleClick = (ev: MapLayerMouseEvent) => {
         if (ev.features) {
           const sortedFeatures =
             ev.features.length > 1
@@ -88,13 +105,82 @@ const GLGeographyMap: FC<Props> = ({ areas }) => {
         }
       };
 
-      map.on('click', 'areas', handle);
+      let draggedPointIndex: number | null = null;
+      let updatedPoints: Zetkin2AreaLine | null = null;
+
+      const handleDragStart = (ev: MapLayerMouseEvent) => {
+        const line = editingArea?.boundary.coordinates[0];
+        if (line) {
+          const lineWithoutLast = line.slice(0, -1);
+          const sortedCoords = lineWithoutLast
+            .concat()
+            .sort(
+              (c0, c1) =>
+                distance(c0, ev.lngLat.toArray()) -
+                distance(c1, ev.lngLat.toArray())
+            );
+          const nearestCoord = sortedCoords[0];
+          draggedPointIndex = lineWithoutLast.indexOf(nearestCoord);
+
+          setDraggingPoints(line);
+          map.on('mousemove', handleDragMove);
+          map.on('mouseup', handleDragEnd);
+          ev.preventDefault();
+        } else {
+          draggedPointIndex = null;
+        }
+      };
+
+      const handleDragMove = (ev: MapMouseEvent) => {
+        setDraggingPoints((current) => {
+          if (current) {
+            updatedPoints = current;
+            const isDraggingFirst = draggedPointIndex == 0;
+            return current.map((originalLngLat, index) => {
+              const isDraggedPoint = index == draggedPointIndex;
+              const isLastPoint = index == current.length - 1;
+              const shouldReplace =
+                isDraggedPoint || (isDraggingFirst && isLastPoint);
+
+              return shouldReplace
+                ? lngLat(ev.lngLat.toArray())
+                : originalLngLat;
+            });
+          } else {
+            return null;
+          }
+        });
+      };
+
+      const handleDragEnd = () => {
+        map.off('mousemove', handleDragMove);
+        map.off('mouseup', handleDragEnd);
+        draggedPointIndex = null;
+
+        if (editingArea && updatedPoints) {
+          setDraggingPoints(null);
+          setEditingArea({
+            ...editingArea,
+            boundary: {
+              ...editingArea?.boundary,
+              coordinates: [updatedPoints],
+            },
+          });
+        }
+      };
+
+      setDraggingPoints(null);
+      map.on('click', 'areas', handleClick);
+      map.on('mousedown', 'editPoints', handleDragStart);
 
       return () => {
-        map.off('click', 'areas', handle);
+        map.off('click', 'areas', handleClick);
+        map.off('mousedown', 'editPoints', handleDragStart);
+        map.off('mousemove', handleDragMove);
+        map.off('mouseup', handleDragEnd);
       };
     }
-  }, [map]);
+  }, [map, selectedArea, editingArea]);
 
   return (
     <AreaFilterProvider>
@@ -137,18 +223,14 @@ const GLGeographyMap: FC<Props> = ({ areas }) => {
         />
         {selectedArea && (
           <AreaOverlay
-            area={{
-              // TODO: Switch to Zetkin2Area
-              description: selectedArea.description,
-              id: selectedArea.id,
-              organization_id: selectedArea.organization_id,
-              points: selectedArea.boundary.coordinates[0],
-              tags: [],
-              title: selectedArea.title,
-            }}
-            editing={false}
-            onBeginEdit={() => {}}
-            onCancelEdit={() => {}}
+            area={
+              editingArea
+                ? oldAreaFormat(editingArea)
+                : oldAreaFormat(selectedArea)
+            }
+            editing={editing}
+            onBeginEdit={() => setEditingArea(selectedArea)}
+            onCancelEdit={() => setEditingArea(null)}
             onClose={() => setSelectedId(0)}
           />
         )}
@@ -197,6 +279,16 @@ const GLGeographyMap: FC<Props> = ({ areas }) => {
                 }}
                 type="fill"
               />
+              {!!editing && (
+                <Layer
+                  id="editPoints"
+                  paint={{
+                    'circle-color': oldTheme.palette.primary.main,
+                    'circle-radius': 8,
+                  }}
+                  type="circle"
+                />
+              )}
             </Source>
           )}
         </Map>
