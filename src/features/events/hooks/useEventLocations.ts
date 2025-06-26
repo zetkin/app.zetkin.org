4fea978e0c18360f2f22a83a53f0796d678b2f51
Richard Olsson <r@richardolsson.se>
2025-06-26 18:37:40 +0200

    Filter out v2 locations (from area assignments) temporarily

--- src/features/events/hooks/useEventLocations.ts
+++ src/features/events/hooks/useEventLocations.ts
@@ -1,11 +1,12 @@
 import { loadListIfNecessary } from 'core/caching/cacheUtils';
-import { ZetkinLocation } from 'utils/types/zetkin';
+import { ZetkinLocation as ZetkinEventLocation } from 'utils/types/zetkin';
 import { locationsLoad, locationsLoaded } from '../store';
 import { useApiClient, useAppDispatch, useAppSelector } from 'core/hooks';
+import { ZetkinLocation } from 'features/areaAssignments/types';
 
 export default function useEventLocations(
   orgId: number
-): ZetkinLocation[] | null {
+): ZetkinEventLocation[] | null {
   const apiClient = useApiClient();
   const locationsList = useAppSelector((state) => state.events.locationList);
   const dispatch = useAppDispatch();
@@ -13,9 +14,32 @@ export default function useEventLocations(
   const locations = loadListIfNecessary(locationsList, dispatch, {
     actionOnLoad: () => locationsLoad(),
     actionOnSuccess: (data) => locationsLoaded(data),
-    loader: () =>
-      apiClient.get<ZetkinLocation[]>(`/api/orgs/${orgId}/locations`),
+    loader: async () => {
+      const locations: ZetkinEventLocation[] = await apiClient
+        .get<ZetkinLocation[]>(`/api2/orgs/${orgId}/locations`)
+        .then((locations) =>
+          locations
+            .filter(excludeLocationsCreatedWhileCanvassing)
+            .map((location) => ({
+              id: location.id,
+              info_text: location.description,
+              lat: location.latitude,
+              lng: location.longitude,
+              title: location.title,
+            }))
+        );
+
+      return locations;
+    },
   });
 
   return locations.data;
+}
+
+function excludeLocationsCreatedWhileCanvassing(
+  location: ZetkinLocation
+): boolean {
+  const hasUserInfo = location.created_by_user_id;
+  const createdWithApiV1 = !hasUserInfo;
+  return createdWithApiV1;
 }
