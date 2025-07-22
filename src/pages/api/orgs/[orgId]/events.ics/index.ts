33e5162233ec753fb9dff28eb4c29c91917b52e9
Herover <hi+git@leonora.app>
2025-07-22 21:55:35 +0200

    More fields, output formatting

--- src/pages/api/orgs/[orgId]/events.ics/index.ts
+++ src/pages/api/orgs/[orgId]/events.ics/index.ts
@@ -4,7 +4,9 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import BackendApiClient from 'core/api/client/BackendApiClient';
 import { ZetkinEvent } from 'utils/types/zetkin';
 
-const timeStamp = (t: Dayjs) => t.format('YYYYMMDDTHHmmss');
+const timeStamp = (t: Dayjs): string => t.format('YYYYMMDDTHHmmss');
+const formatString = (str: string): string =>
+  str.match(/.{1,75}/g)?.join('\n\t') ?? '';
 
 export default async function handle(
   req: NextApiRequest,
@@ -15,37 +17,49 @@ export default async function handle(
   const apiClient = new BackendApiClient(req.headers);
 
   const startTime = dayjs()
-    .subtract(1, 'month')
+    .subtract(2, 'month')
     .toDate()
     .toISOString()
     .slice(0, 10);
 
   const events = await apiClient.get<ZetkinEvent[]>(
-    `/api/orgs/${orgId}/actions?filter=start_time>${startTime}`
+    `/api/orgs/${orgId}/actions?frecursive&ilter=start_time>=${startTime}`
   );
 
-  const vEvents = events
+  const vEvents: string[] = [];
+  events
     .filter((event) => !event.cancelled && event.published)
-    .map((event) => {
-      let str = `BEGIN:VEVENT
-UID:${event.id}@zetkin.org
-ORGANIZER;CN=${event.organization.title.replace(
-        '\n',
-        ' '
-      )}:MAILTO:noreply@zetkin.org
-DTSTAMP:${timeStamp(dayjs(event.published))}
-DTSTART:${timeStamp(dayjs(event.start_time))}
-DTEND:${timeStamp(dayjs(event.end_time))}
-SUMMARY:${event.title?.replace('\n', ' ') ?? ''}
-`;
+    .forEach((event) => {
+      vEvents.push(`BEGIN:VEVENT`);
+      vEvents.push(`UID:${event.id}@zetkin.org`);
+      vEvents.push(
+        `ORGANIZER;CN=${event.organization.title.replace(
+          '\n',
+          ' '
+        )}:MAILTO:noreply@zetkin.org`
+      );
+      vEvents.push(`DTSTAMP:${timeStamp(dayjs(event.published))}`);
+      vEvents.push(`DTSTART:${timeStamp(dayjs(event.start_time))}`);
+      vEvents.push(`DTEND:${timeStamp(dayjs(event.end_time))}`);
+      vEvents.push(`SUMMARY:${event.title?.replace('\n', ' ') ?? ''}`);
+      vEvents.push(
+        `URL:${process.env.ZETKIN_APP_HOST}/o/${orgId}/events/${event.id}`
+      );
       if (event.location) {
-        str += `GEO:${event.location.lat};${event.location.lng}\n`;
-        str += `LOCATION:${event.location.title}\n`;
+        vEvents.push(`GEO:${event.location.lat};${event.location.lng}`);
+        vEvents.push(`LOCATION:${event.location.title}`);
       }
-      str += 'END:VEVENT';
 
-      return str;
+      if (event.cover_file?.url) {
+        vEvents.push(
+          `IMAGE;VALUE=URI;DISPLAY=BADGE;FMTTYPE=${event.cover_file.mime_type}:${event.cover_file.url}`
+        );
+      }
+
+      vEvents.push('END:VEVENT');
     });
+
+  const eventsStr = vEvents.map((s) => formatString(s)).join('\n');
 
   res
     .setHeader('Content-Type', 'text/calendar')
@@ -56,6 +70,6 @@ SUMMARY:${event.title?.replace('\n', ' ') ?? ''}
     .status(200).send(`BEGIN:VCALENDAR
 VERSION:2.0
 PRODID:-//Zetkin Foundation//Zetkin App//EN
-${vEvents.join('\n')}
+${eventsStr}
 END:VCALENDAR`);
 }
