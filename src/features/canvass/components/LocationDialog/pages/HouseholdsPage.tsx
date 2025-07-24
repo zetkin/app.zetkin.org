6d882e6f449e4d5a4e9bb9a868013ab5453c8db7
Rebe R <36491300+rebecarubio@users.noreply.github.com>
2025-07-24 15:48:12 +0200

    change || for ?? to avoid level 0 to be treated as null

--- src/features/canvass/components/LocationDialog/pages/HouseholdsPage.tsx
+++ src/features/canvass/components/LocationDialog/pages/HouseholdsPage.tsx
@@ -84,7 +84,7 @@ const HouseholdsPage: FC<Props> = ({
         <List sx={{ overflowY: 'visible' }}>
           {sortedHouseholds.map((household, index) => {
             const prevFloor = sortedHouseholds[index - 1]?.level ?? null;
-            const curFloor = household.level || null;
+            const curFloor = household.level ?? null;
             const firstOnFloor = index == 0 || curFloor != prevFloor;
 
             const mostRecentVisit = lastVisitByHouseholdId[household.id];
