ef6670fd97eeef07cc08019c4f7810d984f1e8bd
ziggabyte <ziggi.willpower@gmail.com>
2025-08-25 11:51:20 +0200

    Use correct colors and add some spacing between them.

--- src/features/canvass/components/HouseholdColorPicker.tsx
+++ src/features/canvass/components/HouseholdColorPicker.tsx
@@ -1,6 +1,14 @@
-import { Close } from '@mui/icons-material';
-import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
 import { FC } from 'react';
+import { Close } from '@mui/icons-material';
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  Typography,
+} from '@mui/material';
+
+import { getContrastColor } from 'utils/colorUtils';
 
 type Props = {
   color: string | null;
@@ -8,16 +16,43 @@ type Props = {
 };
 
 const HouseholdColorPicker: FC<Props> = ({ color, onChange }) => {
+  //TODO: use from theme object, when canvass is moved to ZUI
   const colors = [
-    '#f1a8a8',
-    '#dc2626',
-    '#0284c7',
-    '#93e9eb',
-    '#77e25b',
-    '#ea580c',
+    '#F1A8A8',
+    '#F7BC9E',
+    '#FDDF91',
+    '#DFEF95',
+    '#BBF1AD',
+    '#99E9CC',
+    '#93E9EB',
+    '#B3DAEE',
+    '#CAC7F7',
+    '#E5C0F5',
+    '#F1A9C9',
+    '#DC2626',
+    '#EA580C',
+    '#FBBF24',
+    '#C0DE2B',
+    '#77E25B',
+    '#34D399',
+    '#28D4D7',
+    '#0284C7',
+    '#4F46E5',
+    '#AA2DDF',
+    '#DB2777',
+    '#841717',
+    '#8C3507',
+    '#977316',
+    '#73851A',
+    '#478837',
+    '#1F7F5C',
+    '#187F81',
+    '#014F77',
+    '#2F2A89',
+    '#661B86',
     '#831747',
-    '#73851a',
-    '#cac7f7',
+    '#6D6D6D',
+    '#000000',
   ];
 
   return (
@@ -29,18 +64,27 @@ const HouseholdColorPicker: FC<Props> = ({ color, onChange }) => {
           slotProps: {
             paper: {
               sx: {
-                '& ul': { display: 'flex', flexWrap: 'wrap', paddingY: 0 },
+                '& ul': {
+                  display: 'flex',
+                  flexWrap: 'wrap',
+                  gap: 0.5,
+                  padding: 1,
+                },
               },
             },
           },
           sx: { zIndex: 600000000 },
         }}
         onChange={(ev) => onChange(ev.target.value)}
-        renderValue={() => color}
+        renderValue={() => (
+          <Typography color={color ? getContrastColor(color) : 'primary'}>
+            {color}
+          </Typography>
+        )}
         sx={{ backgroundColor: color }}
         value={color}
       >
-        <MenuItem key="clear" sx={{ flex: 1 }} value="clear">
+        <MenuItem key="clear" sx={{ flex: 1, minWidth: 48 }} value="clear">
           <Close />
         </MenuItem>
         {colors.map((color) => (
@@ -50,7 +94,7 @@ const HouseholdColorPicker: FC<Props> = ({ color, onChange }) => {
               '&:hover': { backgroundColor: color },
               backgroundColor: color,
               flex: 1,
-              minWidth: 55,
+              minWidth: 48,
             }}
             value={color}
           />
