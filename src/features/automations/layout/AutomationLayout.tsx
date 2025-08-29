576c5d9f676c416a6bb32fc24cfdaacac04dea01
Richard Olsson <r@richardolsson.se>
2025-08-29 06:45:22 +0200

    Add more information to job subtitle

--- src/features/automations/layout/AutomationLayout.tsx
+++ src/features/automations/layout/AutomationLayout.tsx
@@ -1,4 +1,5 @@
 import { FC, ReactNode } from 'react';
+import { Box, Divider, Typography } from '@mui/material';
 
 import SimpleLayout from 'utils/layout/SimpleLayout';
 import { useNumericRouteParams } from 'core/hooks';
@@ -7,6 +8,10 @@ import AutomationStatusChip from '../components/AutomationStatusChip';
 import useAutomationMutations from '../hooks/useAutomationMutations';
 import ZUIEditTextinPlace from 'zui/ZUIEditTextInPlace';
 import SchedulingButton from '../components/SchedulingButton';
+import AutomationInterval from '../components/AutomationInterval';
+import { Msg } from 'core/i18n';
+import messageIds from '../l10n/messageIds';
+import ZUIRelativeTime from 'zui/ZUIRelativeTime';
 
 type Props = {
   children: ReactNode;
@@ -20,7 +25,39 @@ const AutomationLayout: FC<Props> = ({ children }) => {
   return (
     <SimpleLayout
       actionButtons={<SchedulingButton automation={automation} />}
-      subtitle={<AutomationStatusChip automation={automation} />}
+      subtitle={
+        <Box
+          sx={{
+            alignItems: 'center',
+            display: 'flex',
+            gap: 2,
+          }}
+        >
+          <AutomationStatusChip automation={automation} />
+          {automation.active && (
+            <>
+              <Divider orientation="vertical" sx={{ height: '1rem' }} />
+              <Typography variant="body1">
+                <AutomationInterval seconds={automation.interval} />
+              </Typography>
+            </>
+          )}
+          <Divider orientation="vertical" sx={{ height: '1rem' }} />
+          <Typography color="secondary" variant="body1">
+            {!automation.last_run && (
+              <Msg id={messageIds.labels.lastRun.never} />
+            )}
+            {automation.last_run && (
+              <Msg
+                id={messageIds.labels.lastRun.relative}
+                values={{
+                  relative: <ZUIRelativeTime datetime={automation.last_run} />,
+                }}
+              />
+            )}
+          </Typography>
+        </Box>
+      }
       title={
         <ZUIEditTextinPlace
           onChange={(title) => updateAutomation({ title })}
