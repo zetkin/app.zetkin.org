e52498f9d241b4d85de7b6135c0953d3753fab28
Johan Rende <johan@rende.se>
2025-09-13 14:16:04 +0200

    Add button to copy ICS urls for project/org pages

--- src/features/campaigns/layout/PublicProjectLayout.tsx
+++ src/features/campaigns/layout/PublicProjectLayout.tsx
@@ -1,8 +1,9 @@
 'use client';
 
 import { Box } from '@mui/material';
-import { FC, ReactNode } from 'react';
+import { FC, ReactNode, useContext } from 'react';
 import NextLink from 'next/link';
+import { CalendarMonth } from '@mui/icons-material';
 
 import { ZetkinCampaign } from 'utils/types/zetkin';
 import ZUIText from 'zui/components/ZUIText';
@@ -12,6 +13,10 @@ import ActivistPortalHeader from 'features/organizations/components/ActivistPort
 import EventMapLayout from 'features/organizations/layouts/EventMapLayout';
 import { useAppDispatch, useAppSelector } from 'core/hooks';
 import { filtersUpdated } from '../store';
+import { Msg, useMessages } from 'core/i18n';
+import messageIds from '../l10n/messageIds';
+import ZUIEllipsisMenu from 'zui/ZUIEllipsisMenu';
+import ZUISnackbarContext from 'zui/ZUISnackbarContext';
 
 type Props = {
   campaign: ZetkinCampaign;
@@ -20,6 +25,8 @@ type Props = {
 
 const PublicProjectLayout: FC<Props> = ({ children, campaign }) => {
   const dispatch = useAppDispatch();
+  const messages = useMessages(messageIds);
+  const { showSnackbar } = useContext(ZUISnackbarContext);
 
   const { allEvents, filteredEvents } = useFilteredCampaignEvents(
     campaign.organization.id,
@@ -37,12 +44,32 @@ const PublicProjectLayout: FC<Props> = ({ children, campaign }) => {
       })
     );
   };
+  function copyUrlToClipboard() {
+    const url = `${location.protocol}//${location.host}/o/${campaign.organization.id}/projects/${campaign.id}/events.ics`;
+    navigator.clipboard.writeText(url);
+    showSnackbar(
+      'success',
+      <Msg id={messageIds.publicProjectPage.calendarLinkCopied} />
+    );
+  }
 
   return (
     <EventMapLayout
       events={filteredEvents}
       header={
         <ActivistPortalHeader
+          button={
+            <ZUIEllipsisMenu
+              items={[
+                {
+                  id: 'copy-ics-url',
+                  label: messages.publicProjectPage.copyIcsUrl(),
+                  onSelect: () => copyUrlToClipboard(),
+                  startIcon: <CalendarMonth />,
+                },
+              ]}
+            />
+          }
           subtitle={campaign.info_text}
           title={campaign.title}
           topLeftComponent={
