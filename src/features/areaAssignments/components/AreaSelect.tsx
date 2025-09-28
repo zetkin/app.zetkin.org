246dd094e3c5f3f56667865e9163e765a22a6798
Herover <hi+git@leonora.app>
2025-09-28 15:17:25 +0200

    Fix visited households counter

--- src/features/areaAssignments/components/AreaSelect.tsx
+++ src/features/areaAssignments/components/AreaSelect.tsx
@@ -269,7 +269,7 @@ const AreaSelect: FC<Props> = ({
                 sx={(theme) => ({ color: theme.palette.primary.main })}
                 variant="h5"
               >
-                {selectedAreaStats?.num_visited_households || 0}
+                {locationsInSelectedArea.reduce((v, e) => v += e?.num_households_visited || 0, 0) || 0}
               </Typography>
               <Typography color="secondary" ml={0.5} variant="h5">
                 / {numberOfHouseholdsInSelectedArea}
