9f6136ec1b414a148b82bfda68ab85a7eafae96f
ziggabyte <ziggi.willpower@gmail.com>
2025-10-08 11:01:22 +0200

    Messages for error.

--- src/features/call/l10n/messageIds.ts
+++ src/features/call/l10n/messageIds.ts
@@ -1,46 +1,11 @@
 import { m, makeMessages } from 'core/i18n';
 
 export default makeMessages('feat.call', {
-  activeEvents: {
-    alreadyBooked: m<{ name: string }>('{name} is already booked'),
-    noBookings: m<{ name: string }>('{name} has no bookings'),
-    signUp: m('Sign Up'),
-    undoSignUp: m('Undo Sign Up'),
-  },
-  instructions: {
-    title: m('Instructions'),
-  },
-  nav: {
-    backToHome: m('Back to home'),
-    startCalling: m('Start calling'),
-  },
-  prepare: {
-    activeEvents: m('Active events'),
-    edit: m('Edit this information?'),
-    editDescription: m(
-      'If something in this tab needs changing, write a message to the organizer in the report after finishing the call.'
+  error: {
+    description: m(
+      'Try refreshing the page. If error persists - try logging out and then in again. If error still persists, contact support.'
     ),
-    noActiveEvents: m('No active events.'),
-    noPreviousCalls: m('Never called'),
-    noPreviousEvents: m<{ name: string }>(
-      '{name} never participated in any events.'
-    ),
-    noSurveys: m('No surveys'),
-    noTags: m('No tags'),
-    previousCalls: m('There are previous calls.'),
-    previousCallsOfTarget: m<{ name: string }>('Previous calls to {name}.'),
-    previousEvents: m('Previous events'),
-    previousEventsOfTarget: m<{
-      eventTitle: string;
-      name: string;
-      numEvents: number;
-    }>(
-      '{name} participated in {numEvents} events, the most recent being {eventTitle} .'
-    ),
-    summary: m('Summary'),
-    surveys: m('Surveys'),
-    tags: m('Tags'),
-    title: m('Personal info'),
+    title: m('An unexpected error occured.'),
   },
   report: {
     steps: {
@@ -224,10 +189,5 @@ export default makeMessages('feat.call', {
     summary: {
       editButtonLabel: m('Edit'),
     },
-  },
-  stats: {
-    callsMade: m('calls made'),
-    callsReached: m('successful calls'),
-    targetMatches: m('people in target group'),
   },
 });
