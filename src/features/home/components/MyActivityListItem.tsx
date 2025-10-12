06ffd533225da51e62f942f2fd94e5525454b674
VectorHector <erik.doering@t-online.de>
2025-10-12 17:54:58 +0200

    add event descriptions

--- src/features/home/components/MyActivityListItem.tsx
+++ src/features/home/components/MyActivityListItem.tsx
@@ -1,12 +1,16 @@
-import { SvgIconTypeMap } from '@mui/material';
+import { Box, SvgIconTypeMap } from '@mui/material';
 import { OverridableComponent } from '@mui/material/OverridableComponent';
 import { FC } from 'react';
 
 import ZUIItemCard from 'zui/components/ZUIItemCard';
 import ZUIIconLabel from 'zui/components/ZUIIconLabel';
+import ZUIModal from '../../../zui/components/ZUIModal';
+import ZUIText from '../../../zui/components/ZUIText';
+import ZUIButton from '../../../zui/components/ZUIButton';
 
 type Props = {
   actions?: JSX.Element[];
+  description?: string;
   href?: string;
   iconTitle?: OverridableComponent<SvgIconTypeMap<unknown, 'svg'>>;
   image?: string;
@@ -23,20 +27,43 @@ const MyActivityListItem: FC<Props> = ({
   iconTitle,
   image,
   info,
+  description,
   title,
 }) => {
   return (
     <ZUIItemCard
       actions={actions}
-      content={info.map((item, index) => (
-        <ZUIIconLabel
-          key={index}
-          color="secondary"
-          icon={item.Icon}
-          label={item.labels}
-          size="small"
-        />
-      ))}
+      content={[
+        ...info.map((item, index) => (
+          <ZUIIconLabel
+            key={index}
+            color="secondary"
+            icon={item.Icon}
+            label={item.labels}
+            size="small"
+          />
+        )),
+        ...(description
+          ? [
+              <Box>
+                <ZUIText
+                  key={'description'}
+                  color="primary"
+                  sx={{
+                    WebkitBoxOrient: 'vertical',
+                    WebkitLineClamp: 3,
+                    display: '-webkit-box',
+                    overflow: 'hidden',
+                  }}
+                  variant="bodySmRegular"
+                >
+                  {description}
+                </ZUIText>
+                <ZUIButton label={'read more'} href={href} />
+              </Box>,
+            ]
+          : []),
+      ]}
       href={href}
       {...(iconTitle ? { icon: iconTitle } : {})}
       src={image}
