cd22d65fba0db5ec9dcf67058dbce64656f0f88c
Rebe R <36491300+rebecarubio@users.noreply.github.com>
2025-10-13 15:41:19 +0200

    activate user in verify/code page

--- src/app/verify/[token]/page.tsx
+++ src/app/verify/[token]/page.tsx
@@ -1,9 +1,28 @@
 import { redirect } from 'next/navigation';
+import { headers } from 'next/headers';
 
 import redirectIfLoginNeeded from 'core/utils/redirectIfLoginNeeded';
+import BackendApiClient from 'core/api/client/BackendApiClient';
 
-export default async function Page() {
-  await redirectIfLoginNeeded();
+interface PageProps {
+  params: {
+    code: string;
+  };
+}
+export default async function Page({ params }: PageProps) {
+  const { code } = params;
+  const headersList = headers();
+  const headersEntries = headersList.entries();
+  const headersObject = Object.fromEntries(headersEntries);
+  const apiClient = new BackendApiClient(headersObject);
 
-  redirect('/my');
+  try {
+    await apiClient.put(`/api/users/me/verification_codes`, { code });
+
+    await redirectIfLoginNeeded();
+    redirect('/my');
+  } catch (err) {
+    const path = headersList.get('x-requested-path');
+    redirect(`/login?redirect=${path}`);
+  }
 }
