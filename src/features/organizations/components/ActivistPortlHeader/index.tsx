8fdb12fd210c1b8cf0db18f647baeb2709772960
VectorHector <erik.doering@t-online.de>
2025-10-14 10:24:41 +0200

    make activist header menu go left

--- src/features/organizations/components/ActivistPortlHeader/index.tsx
+++ src/features/organizations/components/ActivistPortlHeader/index.tsx
@@ -142,6 +142,7 @@ const ActivistPortalHeader: FC<Props> = ({
               </Button>
               <ZUIMenu
                 anchorEl={logoutMenuAnchorEl}
+                invertHorizontalAnchor={true}
                 menuItems={menuItems}
                 onClose={() => setLogoutMenuAnchorEl(null)}
               />
