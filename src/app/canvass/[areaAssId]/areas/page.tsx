91eb288a46adb36a074d99630f99e46f02c3514d
VectorHector <erik.doering@t-online.de>
2025-10-18 00:50:44 +0200

    remove area selection page, add redirect

--- src/app/canvass/[areaAssId]/areas/page.tsx
+++ src/app/canvass/[areaAssId]/areas/page.tsx
@@ -1,9 +1,4 @@
-import { headers } from 'next/headers';
 import { redirect } from 'next/navigation';
-
-import BackendApiClient from 'core/api/client/BackendApiClient';
-import { ZetkinOrganization } from 'utils/types/zetkin';
-import CanvassSelectAreaPage from 'features/canvass/components/CanvassSelectAreaPage';
 
 interface PageProps {
   params: {
@@ -13,16 +8,5 @@ interface PageProps {
 
 export default async function Page({ params }: PageProps) {
   const { areaAssId } = params;
-  const headersList = headers();
-  const headersEntries = headersList.entries();
-  const headersObject = Object.fromEntries(headersEntries);
-  const apiClient = new BackendApiClient(headersObject);
-
-  try {
-    await apiClient.get<ZetkinOrganization>(`/api/users/me`);
-
-    return <CanvassSelectAreaPage areaAssId={areaAssId} />;
-  } catch (err) {
-    return redirect(`/login?redirect=/canvass/${areaAssId}`);
-  }
+  return redirect(`/canvass/${areaAssId}`);
 }
