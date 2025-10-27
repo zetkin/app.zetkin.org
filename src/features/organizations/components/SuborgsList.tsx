d5d290ec28ca5960e13c33191ad3c61f2adf76f2
ziggabyte <ziggi.willpower@gmail.com>
2025-10-27 14:09:02 +0100

    Move list into separate component and add box for single organisation.

---
+++ src/features/organizations/components/SuborgsList.tsx
@@ -0,0 +1,142 @@
+import { FC } from 'react';
+import { Alert, Avatar, Box, Paper, Stack, Typography } from '@mui/material';
+import {
+  AssignmentOutlined,
+  EmailOutlined,
+  EventOutlined,
+  PhoneOutlined,
+} from '@mui/icons-material';
+
+import { SuborgError, SuborgResult } from '../types';
+import useOrganization from '../hooks/useOrganization';
+import useSuborgsWithStats from '../hooks/useSuborgsWithStats';
+import ZUIEmptyState from 'zui/ZUIEmptyState';
+
+export const isError = (result: SuborgResult): result is SuborgError => {
+  return 'error' in result;
+};
+
+const SuborgsList: FC<{ orgId: number }> = ({ orgId }) => {
+  const organization = useOrganization(orgId).data;
+  const suborgsWithStats = useSuborgsWithStats(orgId);
+
+  if (suborgsWithStats.length == 0) {
+    return (
+      <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+        <ZUIEmptyState
+          message={
+            organization ? `${organization.title} has no suborganizations` : ''
+          }
+        />
+      </Box>
+    );
+  }
+
+  return (
+    <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
+      <Typography>{`Suborganizations of ${organization?.title}`}</Typography>
+      <Stack spacing={2}>
+        {suborgsWithStats.map((orgWithStats) => {
+          if (isError(orgWithStats)) {
+            if (orgWithStats.id == 'loadingError') {
+              return <Alert severity="error">{orgWithStats.message}</Alert>;
+            }
+
+            return (
+              <Paper key={orgWithStats.id}>
+                <Box
+                  sx={{
+                    alignContent: 'center',
+                    gap: 1,
+                    padding: 2,
+                  }}
+                >
+                  <Alert severity="error">{orgWithStats.message}</Alert>
+                </Box>
+              </Paper>
+            );
+          }
+          return (
+            <Paper key={orgWithStats.id}>
+              <Box
+                sx={{
+                  alignContent: 'center',
+                  gap: 1,
+                  padding: 2,
+                }}
+              >
+                <Box
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                  }}
+                >
+                  <Box sx={{ alignItems: 'center', display: 'flex', gap: 1 }}>
+                    <Avatar alt="icon" src={`/api/orgs/${orgId}/avatar`} />
+                    <Typography variant="h5">{orgWithStats.title}</Typography>
+                  </Box>
+                  <Box sx={{ alignItems: 'center', display: 'flex', gap: 1 }}>
+                    <Box sx={{ alignItems: 'center', display: 'flex', gap: 1 }}>
+                      <Typography>{orgWithStats.stats.numPeople}</Typography>
+                      <Typography color="secondary">people</Typography>
+                    </Box>
+                    <Box sx={{ alignItems: 'center', display: 'flex', gap: 1 }}>
+                      <Typography>{orgWithStats.stats.numLists}</Typography>
+                      <Typography color="secondary">lists</Typography>
+                    </Box>
+                    <Box sx={{ alignItems: 'center', display: 'flex', gap: 1 }}>
+                      <Typography>{orgWithStats.stats.numProjects}</Typography>
+                      <Typography color="secondary">projects</Typography>
+                    </Box>
+                  </Box>
+                </Box>
+                <Typography color="secondary">
+                  Activity in the past 30 days:
+                </Typography>
+                <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                  <Box sx={{ alignItems: 'center', display: 'flex', gap: 1 }}>
+                    <PhoneOutlined color="secondary" />
+                    <Box sx={{ alignItems: 'center', display: 'flex', gap: 1 }}>
+                      <Typography>{orgWithStats.stats.numCalls}</Typography>
+                      <Typography color="secondary">calls</Typography>
+                    </Box>
+                  </Box>
+                  <Box sx={{ alignItems: 'center', display: 'flex', gap: 1 }}>
+                    <AssignmentOutlined color="secondary" />
+                    <Box sx={{ alignItems: 'center', display: 'flex', gap: 1 }}>
+                      <Typography>
+                        {orgWithStats.stats.numSubmissions}
+                      </Typography>
+                      <Typography color="secondary">submissions</Typography>
+                    </Box>
+                  </Box>
+                  <Box sx={{ alignItems: 'center', display: 'flex', gap: 1 }}>
+                    <EventOutlined color="secondary" />
+                    <Box sx={{ alignItems: 'center', display: 'flex', gap: 1 }}>
+                      <Typography>
+                        {orgWithStats.stats.numEventParticipants}
+                      </Typography>
+                      <Typography color="secondary">
+                        participants in{' '}
+                      </Typography>
+                      <Typography>
+                        {orgWithStats.stats.numEventsWithParticipants}
+                      </Typography>
+                      <Typography color="secondary">events</Typography>
+                    </Box>
+                  </Box>
+                  <Box sx={{ alignItems: 'center', display: 'flex', gap: 1 }}>
+                    <EmailOutlined color="secondary" />
+                    <Typography color="secondary">{`${orgWithStats.stats.numEmailsSent} sent`}</Typography>
+                  </Box>
+                </Box>
+              </Box>
+            </Paper>
+          );
+        })}
+      </Stack>
+    </Box>
+  );
+};
+
+export default SuborgsList;
--- src/pages/organize/[orgId]/suborgOverview/index.tsx
+++ src/pages/organize/[orgId]/suborgOverview/index.tsx
@@ -1,32 +1,16 @@
 import Head from 'next/head';
 import { FC, Suspense } from 'react';
-import {
-  Alert,
-  Avatar,
-  Box,
-  CircularProgress,
-  Paper,
-  Stack,
-  Typography,
-} from '@mui/material';
+import { Box, CircularProgress, Paper, Typography } from '@mui/material';
 import { GetServerSideProps } from 'next';
-import {
-  AssignmentOutlined,
-  EmailOutlined,
-  EventOutlined,
-  PhoneOutlined,
-} from '@mui/icons-material';
 
 import useServerSide from 'core/useServerSide';
 import { PageWithLayout } from 'utils/types';
 import { scaffold } from 'utils/next';
-import useSuborgsWithStats from 'features/organizations/hooks/useSuborgsWithStats';
 import SuborgOverviewLayout from 'features/organizations/layouts/SuborgOverviewLayout';
 import { Msg } from 'core/i18n';
 import messageIds from 'features/organizations/l10n/messageIds';
-import ZUIEmptyState from 'zui/ZUIEmptyState';
+import SuborgsList from 'features/organizations/components/SuborgsList';
 import useOrganization from 'features/organizations/hooks/useOrganization';
-import { SuborgError, SuborgResult } from 'features/organizations/types';
 
 export const getServerSideProps: GetServerSideProps = scaffold(
   async (ctx) => {
@@ -42,123 +26,19 @@ export const getServerSideProps: GetServerSideProps = scaffold(
   }
 );
 
-export const isError = (result: SuborgResult): result is SuborgError => {
-  return 'error' in result;
-};
-
-const SuborgsList: FC<{ orgId: number }> = ({ orgId }) => {
+const SuborgCard: FC<{ orgId: number }> = ({ orgId }) => {
   const organization = useOrganization(orgId).data;
-  const suborgsWithStats = useSuborgsWithStats(orgId);
-
-  if (suborgsWithStats.length == 0) {
-    return (
-      <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-        <ZUIEmptyState
-          message={
-            organization ? `${organization.title} has no suborganizations` : ''
-          }
-        />
-      </Box>
-    );
+  if (!organization) {
+    return null;
   }
 
   return (
-    <Stack spacing={2}>
-      {suborgsWithStats.map((orgWithStats) => {
-        if (isError(orgWithStats)) {
-          if (orgWithStats.id == 'loadingError') {
-            return <Alert severity="error">{orgWithStats.message}</Alert>;
-          }
-
-          return (
-            <Paper key={orgWithStats.id}>
-              <Box
-                sx={{
-                  alignContent: 'center',
-                  gap: 1,
-                  padding: 2,
-                }}
-              >
-                <Alert severity="error">{orgWithStats.message}</Alert>
-              </Box>
-            </Paper>
-          );
-        }
-        return (
-          <Paper key={orgWithStats.id}>
-            <Box
-              sx={{
-                alignContent: 'center',
-                gap: 1,
-                padding: 2,
-              }}
-            >
-              <Box
-                sx={{
-                  display: 'flex',
-                  justifyContent: 'space-between',
-                }}
-              >
-                <Box sx={{ alignItems: 'center', display: 'flex', gap: 1 }}>
-                  <Avatar alt="icon" src={`/api/orgs/${orgId}/avatar`} />
-                  <Typography variant="h5">{orgWithStats.title}</Typography>
-                </Box>
-                <Box sx={{ alignItems: 'center', display: 'flex', gap: 1 }}>
-                  <Box sx={{ alignItems: 'center', display: 'flex', gap: 1 }}>
-                    <Typography>{orgWithStats.stats.numPeople}</Typography>
-                    <Typography color="secondary">people</Typography>
-                  </Box>
-                  <Box sx={{ alignItems: 'center', display: 'flex', gap: 1 }}>
-                    <Typography>{orgWithStats.stats.numLists}</Typography>
-                    <Typography color="secondary">lists</Typography>
-                  </Box>
-                  <Box sx={{ alignItems: 'center', display: 'flex', gap: 1 }}>
-                    <Typography>{orgWithStats.stats.numProjects}</Typography>
-                    <Typography color="secondary">projects</Typography>
-                  </Box>
-                </Box>
-              </Box>
-              <Typography color="secondary">
-                Activity in the past 30 days:
-              </Typography>
-              <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-                <Box sx={{ alignItems: 'center', display: 'flex', gap: 1 }}>
-                  <PhoneOutlined color="secondary" />
-                  <Box sx={{ alignItems: 'center', display: 'flex', gap: 1 }}>
-                    <Typography>{orgWithStats.stats.numCalls}</Typography>
-                    <Typography color="secondary">calls</Typography>
-                  </Box>
-                </Box>
-                <Box sx={{ alignItems: 'center', display: 'flex', gap: 1 }}>
-                  <AssignmentOutlined color="secondary" />
-                  <Box sx={{ alignItems: 'center', display: 'flex', gap: 1 }}>
-                    <Typography>{orgWithStats.stats.numSubmissions}</Typography>
-                    <Typography color="secondary">submissions</Typography>
-                  </Box>
-                </Box>
-                <Box sx={{ alignItems: 'center', display: 'flex', gap: 1 }}>
-                  <EventOutlined color="secondary" />
-                  <Box sx={{ alignItems: 'center', display: 'flex', gap: 1 }}>
-                    <Typography>
-                      {orgWithStats.stats.numEventParticipants}
-                    </Typography>
-                    <Typography color="secondary">participants in </Typography>
-                    <Typography>
-                      {orgWithStats.stats.numEventsWithParticipants}
-                    </Typography>
-                    <Typography color="secondary">events</Typography>
-                  </Box>
-                </Box>
-                <Box sx={{ alignItems: 'center', display: 'flex', gap: 1 }}>
-                  <EmailOutlined color="secondary" />
-                  <Typography color="secondary">{`${orgWithStats.stats.numEmailsSent} sent`}</Typography>
-                </Box>
-              </Box>
-            </Box>
-          </Paper>
-        );
-      })}
-    </Stack>
+    <Paper>
+      <Box sx={{ padding: 2 }}>
+        <Typography variant="h5">{organization.title}</Typography>
+        <Typography>{organization.email}</Typography>
+      </Box>
+    </Paper>
   );
 };
 
@@ -181,20 +61,40 @@ const SuborgsPage: PageWithLayout<Props> = ({ orgId }) => {
           <Msg id={messageIds.overview.suborgs.title} />
         </title>
       </Head>
-      <Suspense
-        fallback={
-          <Box
-            sx={{
-              display: 'flex',
-              justifyContent: 'center',
-            }}
+      <Box sx={{ display: 'flex', gap: 2 }}>
+        <Box sx={{ flex: 1 }}>
+          <Suspense
+            fallback={
+              <Box
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                }}
+              >
+                <CircularProgress />
+              </Box>
+            }
           >
-            <CircularProgress />
-          </Box>
-        }
-      >
-        <SuborgsList orgId={parsedOrgId} />
-      </Suspense>
+            <SuborgCard orgId={parsedOrgId} />
+          </Suspense>
+        </Box>
+        <Box sx={{ flex: 1 }}>
+          <Suspense
+            fallback={
+              <Box
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                }}
+              >
+                <CircularProgress />
+              </Box>
+            }
+          >
+            <SuborgsList orgId={parsedOrgId} />
+          </Suspense>
+        </Box>
+      </Box>
     </>
   );
 };
