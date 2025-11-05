8335807ff4484f43fe8a9e1eec81abb2c5aca554
Vector-Hector <30576258+Vector-Hector@users.noreply.github.com>
2025-11-05 18:45:05 +0100

    fix linter issue

--- src/utils/testing/mocks/mockState.ts
+++ src/utils/testing/mocks/mockState.ts
@@ -143,8 +143,8 @@ export default function mockState(overrides?: RootState) {
       statsByFilterSpec: {},
     },
     surveys: {
-      extendedSurveysBySurveyId: {},
       elementsBySurveyId: {},
+      extendedSurveysBySurveyId: {},
       responseStatsBySurveyId: {},
       statsBySurveyId: {},
       submissionList: remoteList(),
