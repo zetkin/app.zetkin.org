bcf5982c2e9f6e9a1fbebb827a3a37f6499abd9a
Vector-Hector <30576258+Vector-Hector@users.noreply.github.com>
2025-11-06 01:51:12 +0100

    improve previewable input focus

--- src/zui/ZUIPreviewableInput/index.tsx
+++ src/zui/ZUIPreviewableInput/index.tsx
@@ -38,7 +38,11 @@ function ZUIPreviewableInput<
         {renderInput({
           ref: (elem) => {
             if (focusingRef.current) {
-              elem?.focus();
+              if (elem) {
+                elem.focus();
+                elem.setSelectionRange(elem.value.length, elem.value.length);
+                elem.scrollTop = elem.scrollHeight;
+              }
               focusingRef.current = false;
             }
           },
