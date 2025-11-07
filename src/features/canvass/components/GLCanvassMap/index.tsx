26702956d8a8ccd54fc9dc52efc437aac376e6bb
Laura Blum <business@laurablum.com>
2025-11-07 11:31:51 +0100

    add relocation toggle

--- src/features/canvass/components/GLCanvassMap/index.tsx
+++ src/features/canvass/components/GLCanvassMap/index.tsx
@@ -56,6 +56,7 @@ const GLCanvassMap: FC<Props> = ({ assignment, selectedArea }) => {
     null
   );
   const [userLocation, setUserLocation] = useState<PointData | null>(null);
+  const [shouldLocate, setShouldLocate] = useState<boolean>(false);
 
   const areasGeoJson: GeoJSON.GeoJSON = useMemo(() => {
     const earthCover = [
@@ -214,6 +215,42 @@ const GLCanvassMap: FC<Props> = ({ assignment, selectedArea }) => {
     }
   };
 
+  const locateUser = () => {
+    if (!shouldLocate) {
+      setUserLocation(null);
+      return;
+    }
+
+    setTimeout(() => {
+      navigator.geolocation.getCurrentPosition(
+        (pos) => {
+          const lat = pos.coords.latitude as Latitude;
+          const lng = pos.coords.longitude as Longitude;
+          setUserLocation([lng, lat]);
+
+          locateUser();
+        },
+        null,
+        { enableHighAccuracy: true, timeout: 5000 }
+      );
+    }, RELOCATION_TIMEOUT);
+  };
+
+  const onGeoLocate = (lngLat: PointData) => {
+    const nextShouldLocate = !shouldLocate;
+    setShouldLocate(nextShouldLocate);
+
+    if (nextShouldLocate) {
+      setUserLocation(lngLat);
+      map?.panTo(lngLat, {
+        animate: true,
+        duration: 800,
+      });
+    } else {
+      setUserLocation(null);
+    }
+  };
+
   const updateSelection = useCallback(() => {
     let nearestLocation: number | null = null;
     let nearestDistance = Infinity;
@@ -260,6 +297,10 @@ const GLCanvassMap: FC<Props> = ({ assignment, selectedArea }) => {
   }, [map, selectedLocationId, locations]);
 
   useEffect(() => {
+    locateUser();
+  }, [shouldLocate]);
+
+  useEffect(() => {
     if (created) {
       updateSelection();
     }
@@ -267,23 +308,6 @@ const GLCanvassMap: FC<Props> = ({ assignment, selectedArea }) => {
 
   if (!locations.data) {
     return null;
-  }
-
-  function startLocating(lngLat: PointData) {
-    setUserLocation(lngLat);
-
-    setTimeout(() => {
-      navigator.geolocation.getCurrentPosition(
-        (pos) => {
-          const lat = pos.coords.latitude as Latitude;
-          const lng = pos.coords.longitude as Longitude;
-
-          startLocating([lng, lat]);
-        },
-        null,
-        { enableHighAccuracy: true, timeout: 5000 }
-      );
-    }, RELOCATION_TIMEOUT);
   }
 
   return (
@@ -299,13 +323,7 @@ const GLCanvassMap: FC<Props> = ({ assignment, selectedArea }) => {
               });
             }
           }}
-          onGeolocate={(lngLat: PointData) => {
-            startLocating(lngLat);
-            map?.panTo(lngLat, {
-              animate: true,
-              duration: 800,
-            });
-          }}
+          onGeolocate={onGeoLocate}
           onZoomIn={() => map?.zoomIn()}
           onZoomOut={() => map?.zoomOut()}
         />
