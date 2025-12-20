774af236fb8ce181d888331fdf1fb54b6b311162
Johan Rende <johan@rende.se>
2025-12-20 11:04:15 +0100

    Use slotProps for StyledNumberInput

--- src/features/smartSearch/components/filters/Journey/index.tsx
+++ src/features/smartSearch/components/filters/Journey/index.tsx
@@ -186,10 +186,6 @@ const Journey: FC<JourneyProps> = ({
                 {conditionSelect}
                 {selected === JOURNEY_CONDITION_OP.SOME && (
                   <StyledNumberInput
-                    inputProps={{
-                      max: filter.config.tags!.ids.length,
-                      min: '1',
-                    }}
                     onChange={(e) =>
                       setConfig({
                         ...filter.config,
@@ -200,6 +196,12 @@ const Journey: FC<JourneyProps> = ({
                         },
                       })
                     }
+                    slotProps={{
+                      htmlInput: {
+                        max: filter.config.tags!.ids.length,
+                        min: '1',
+                      },
+                    }}
                     sx={{ ml: '0.5rem' }}
                     value={filter.config.tags?.min_matching}
                   />
