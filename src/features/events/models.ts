988b9415525b4913142ca9688a585eef4e32062b
Felix Buchholz <felix@felixbuchholz.com>
2026-01-08 10:08:41 +0100

    public signup: add mongo db model

---
+++ src/features/events/models.ts
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+export type EventSignupModelType = {
+  created: Date | string;
+  email?: string;
+  eventId: number;
+  first_name: string;
+  gdpr_consent: boolean;
+  id: string;
+  last_name: string;
+  orgId: number;
+  person_id?: number;
+  phone?: string;
+};
+
+const eventSignupSchema = new mongoose.Schema<EventSignupModelType>({
+  created: { default: Date.now, type: Date },
+  email: String,
+  eventId: { required: true, type: Number },
+  first_name: { required: true, type: String },
+  gdpr_consent: { required: true, type: Boolean },
+  last_name: { required: true, type: String },
+  orgId: { required: true, type: Number },
+  person_id: { type: Number },
+  phone: String,
+});
+
+export const EventSignupModel: mongoose.Model<EventSignupModelType> =
+  mongoose.models.EventSignup ||
+  mongoose.model<EventSignupModelType>('EventSignup', eventSignupSchema);
