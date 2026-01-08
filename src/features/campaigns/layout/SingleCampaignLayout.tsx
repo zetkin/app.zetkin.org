669a979541439dcec1ac6cf993054c50bc4c4ac7
ziggabyte <ziggi.willpower@gmail.com>
2026-01-08 13:58:35 +0100

    Add project status chip to single project header.

--- src/features/campaigns/layout/SingleCampaignLayout.tsx
+++ src/features/campaigns/layout/SingleCampaignLayout.tsx
@@ -1,5 +1,6 @@
 import { FormattedDate } from 'react-intl';
 import { FunctionComponent } from 'react';
+import { Box } from '@mui/material';
 
 import CampaignActionButtons from 'features/campaigns/components/CampaignActionButtons';
 import EditableCampaignTitle from '../components/EditableCampaignTitle';
@@ -10,6 +11,32 @@ import useCampaign from '../hooks/useCampaign';
 import useCampaignEvents from '../hooks/useCampaignEvents';
 import { useNumericRouteParams } from 'core/hooks';
 import { Msg, useMessages } from 'core/i18n';
+import { ZetkinCampaign } from 'utils/types/zetkin';
+import oldTheme from 'theme';
+
+enum CampaignStatus {
+  ARCHIVED = 'archived',
+  DRAFT = 'draft',
+  PRIVATE = 'private',
+  PUBLIC = 'public',
+}
+
+const getCampaignStatus = (campaign: ZetkinCampaign): CampaignStatus => {
+  if (campaign.archived) {
+    return CampaignStatus.ARCHIVED;
+  }
+
+  if (!campaign.published) {
+    return CampaignStatus.DRAFT;
+  }
+
+  if (campaign.visibility == 'public') {
+    return CampaignStatus.PUBLIC;
+  }
+
+  //Campaign visibility must be "private"
+  return CampaignStatus.PRIVATE;
+};
 
 interface SingleCampaignLayoutProps {
   children: React.ReactNode;
@@ -27,9 +54,18 @@ const SingleCampaignLayout: FunctionComponent<SingleCampaignLayoutProps> = ({
 
   const campaign = campaignFuture.data;
 
+  const colors: Record<CampaignStatus, string> = {
+    archived: oldTheme.palette.grey[500],
+    draft: oldTheme.palette.grey[500],
+    private: oldTheme.palette.statusColors.blue,
+    public: oldTheme.palette.success.main,
+  };
+
   if (!campaign) {
     return null;
   }
+
+  const status = getCampaignStatus(campaign);
 
   return (
     <TabbedLayout
@@ -38,24 +74,42 @@ const SingleCampaignLayout: FunctionComponent<SingleCampaignLayoutProps> = ({
       defaultTab="/"
       fixedHeight={fixedHeight}
       subtitle={
-        firstEvent && lastEvent ? (
-          <>
-            <FormattedDate
-              day="2-digit"
-              month="long"
-              value={removeOffset(firstEvent.start_time)}
-            />
-            {` - `}
-            <FormattedDate
-              day="2-digit"
-              month="long"
-              value={removeOffset(lastEvent.end_time)}
-              year="numeric"
-            />
-          </>
-        ) : (
-          <Msg id={messageIds.indefinite} />
-        )
+        <Box sx={{ alignItems: 'center', display: 'flex', gap: 1 }}>
+          <Box
+            sx={{
+              alignItems: 'center',
+              backgroundColor: colors[status],
+              borderRadius: '2em',
+              color: 'white',
+              display: 'inline-flex',
+              fontSize: 14,
+              fontWeight: 'bold',
+              padding: '0.5em 0.7em',
+            }}
+          >
+            <Msg id={messageIds.singleProject.status[status]} />
+          </Box>
+          <Box>
+            {firstEvent && lastEvent ? (
+              <>
+                <FormattedDate
+                  day="2-digit"
+                  month="long"
+                  value={removeOffset(firstEvent.start_time)}
+                />
+                {` - `}
+                <FormattedDate
+                  day="2-digit"
+                  month="long"
+                  value={removeOffset(lastEvent.end_time)}
+                  year="numeric"
+                />
+              </>
+            ) : (
+              <Msg id={messageIds.indefinite} />
+            )}
+          </Box>
+        </Box>
       }
       tabs={[
         { href: `/`, label: messages.layout.overview() },
