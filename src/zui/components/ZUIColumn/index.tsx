820e419e9ce87c4df216fec67f84cb87b77b28eb
Victor Baquero Wihlborg <v@vbw.se>
2026-01-26 13:49:07 +0100

    Make children required

--- src/zui/components/ZUIColumn/index.tsx
+++ src/zui/components/ZUIColumn/index.tsx
@@ -6,7 +6,7 @@ import theme from 'zui/theme';
 type columnSizes = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 
 type Props = {
-  children?: React.ReactNode;
+  children: React.ReactNode;
   size?: columnSizes;
   sx?: object;
 };
