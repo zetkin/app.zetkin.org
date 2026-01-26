58fb212585ab80f47620dba0206824c63cf4afd3
Victor Baquero Wihlborg <v@vbw.se>
2026-01-26 14:11:16 +0100

    Move and document prop ZUI style

--- src/zui/components/ZUIColumn/index.tsx
+++ src/zui/components/ZUIColumn/index.tsx
@@ -3,11 +3,19 @@ import { FC } from 'react';
 
 import theme from 'zui/theme';
 
-type columnSizes = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
-
 type Props = {
   children: React.ReactNode;
-  size?: columnSizes;
+  /**
+   * Column width based on a 12-column grid. Total width of all columns in one row should not exceed 12.
+   * If size is null or not provided, the column will take up remaining space.
+   * Columns will wexpand to full width on small screens.
+   *
+   * Example usages:
+   * <ZUIColumn size={6}>...</ZUIColumn> - takes up half the width
+   * <ZUIColumn size={4}>...</ZUIColumn> - takes up one third of the width
+   * <ZUIColumn>...</ZUIColumn> - takes up remaining space
+   */
+  size?: null | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
   sx?: object;
 };
 
