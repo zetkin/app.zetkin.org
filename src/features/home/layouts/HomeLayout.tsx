b9c415cc8015181f4735dd73463fcf9f459d919e
Richard Olsson <r@richardolsson.se>
2026-01-30 06:34:54 +0100

    Refactor hooks for memberships to consolidate state and separate use cases

--- src/features/home/layouts/HomeLayout.tsx
+++ src/features/home/layouts/HomeLayout.tsx
@@ -10,10 +10,10 @@ import ZUIPublicFooter from 'zui/components/ZUIPublicFooter';
 import ZUIAlert from 'zui/components/ZUIAlert';
 import useUser from 'core/hooks/useUser';
 import useLocalStorage from 'zui/hooks/useLocalStorage';
-import useMemberships from 'features/organizations/hooks/useMemberships';
 import messageIds from '../l10n/messageIds';
 import ActivistPortalHeader from 'features/organizations/components/ActivistPortalHeader';
 import ZUIText from 'zui/components/ZUIText';
+import useUserMemberships from '../hooks/useUserMemberships';
 
 type Props = {
   children: ReactNode;
@@ -43,7 +43,7 @@ const NewLandingPageAlert: FC<{ userId: number }> = ({ userId }) => {
 const HomeLayout: FC<Props> = ({ children, title }) => {
   const messages = useMessages(messageIds);
   const user = useUser();
-  const memberships = useMemberships().data || [];
+  const memberships = useUserMemberships();
   const isOfficial = memberships.find((membership) => membership.role != null);
 
   const path = usePathname();
