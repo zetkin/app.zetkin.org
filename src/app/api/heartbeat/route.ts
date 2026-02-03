de79d74a94c43fdc4275d5837652f485ff35ac9b
ziggabyte <ziggi.willpower@gmail.com>
2026-02-03 15:38:21 +0100

    Properly pass cookie from server to client.

--- src/app/api/heartbeat/route.ts
+++ src/app/api/heartbeat/route.ts
@@ -1,14 +1,38 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { IncomingHttpHeaders } from 'http';
 
-import BackendApiClient from 'core/api/client/BackendApiClient';
+import { ApiClientError } from 'core/api/errors';
 
 export async function GET(request: NextRequest) {
-  const headers: IncomingHttpHeaders = {};
-  request.headers.forEach((value, key) => (headers[key] = value));
-  const apiClient = new BackendApiClient(headers);
+  const requestHeaders: IncomingHttpHeaders = {};
+  request.headers.forEach((value, key) => (requestHeaders[key] = value));
 
-  await apiClient.get('/api/session');
+  const protocol = process.env.ZETKIN_APP_PROTOCOL || 'http';
+  const host =
+    requestHeaders.host || process.env.ZETKIN_APP_HOST || 'localhost:3000';
 
-  return NextResponse.json({ data: null }, { status: 200 });
+  const serverResponse = await fetch(`${protocol}://${host}/api/session`, {
+    headers: {
+      cookie: requestHeaders.cookie || '',
+    },
+    method: 'GET',
+  });
+
+  if (!serverResponse.ok) {
+    throw await ApiClientError.fromResponse(serverResponse);
+  }
+
+  const responseHeaders = new Headers();
+  responseHeaders.set(
+    'Set-Cookie',
+    serverResponse.headers.get('Set-Cookie') || ''
+  );
+
+  return NextResponse.json(
+    { data: null },
+    {
+      headers: responseHeaders,
+      status: 200,
+    }
+  );
 }
