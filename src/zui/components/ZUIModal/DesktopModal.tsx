45a7d016c1aa792b93757abbafcb8a4fc5e5d0ff
Johan Rende <johan@rende.se>
2026-03-01 16:51:00 +0100

    Remove fade effect from backdrop

--- src/zui/components/ZUIModal/DesktopModal.tsx
+++ src/zui/components/ZUIModal/DesktopModal.tsx
@@ -5,6 +5,7 @@ import { ZUISize } from '../types';
 import ModalBackground from './ModalBackground';
 import { ZUIModalProps } from '.';
 import ModalContent from './ModalContent';
+import { toHash } from 'utils/randomUtils';
 
 const widths: Record<ZUISize | 'auto' | 'full', string> = {
   auto: 'auto',
@@ -22,55 +23,56 @@ const DesktopModal: FC<ZUIModalProps> = ({
   secondaryButton,
   size = 'auto',
   title,
-}) => (
-  <Modal
-    disableRestoreFocus
-    onClose={onClose}
-    open={open}
-    slots={{
-      backdrop: () => (
+}) => {
+  return (
+    <Modal
+      disableRestoreFocus
+      hideBackdrop={true}
+      onClose={onClose}
+      open={open}
+    >
+      <>
         <Fade in={open} timeout={300}>
-          <Box
-            onClick={onClose}
-            sx={{
-              backgroundColor: 'rgba(255,255,255,0.5)',
-              height: '100%',
-              width: '100%',
-            }}
+          <Paper
+            sx={(theme) => ({
+              border: `0.063rem solid ${theme.palette.dividers.main}`,
+              boxShadow: theme.elevation.bottom.big.medium,
+              height: size == 'full' ? 'calc(100dvh - 3.75rem)' : 'auto',
+              left: '50%',
+              maxWidth: 'calc(100dvw - 2.5rem)',
+              minWidth: '25rem',
+              position: 'absolute',
+              top: '50%',
+              transform: 'translate(-50%, -50%)',
+              width: widths[size],
+              zIndex: 10,
+            })}
           >
-            <ModalBackground height="100%" width="100%" />
-          </Box>
+            <ModalContent
+              isMobile={false}
+              onClose={onClose}
+              primaryButton={primaryButton}
+              secondaryButton={secondaryButton}
+              title={title}
+            >
+              {children}
+            </ModalContent>
+          </Paper>
         </Fade>
-      ),
-    }}
-  >
-    <Fade in={open} timeout={300}>
-      <Paper
-        sx={(theme) => ({
-          border: `0.063rem solid ${theme.palette.dividers.main}`,
-          boxShadow: theme.elevation.bottom.big.medium,
-          height: size == 'full' ? 'calc(100dvh - 3.75rem)' : 'auto',
-          left: '50%',
-          maxWidth: 'calc(100dvw - 2.5rem)',
-          minWidth: '25rem',
-          position: 'absolute',
-          top: '50%',
-          transform: 'translate(-50%, -50%)',
-          width: widths[size],
-        })}
-      >
-        <ModalContent
-          isMobile={false}
-          onClose={onClose}
-          primaryButton={primaryButton}
-          secondaryButton={secondaryButton}
-          title={title}
+        <Box
+          onClick={onClose}
+          sx={{
+            backgroundColor: 'rgba(255,255,255,0.5)',
+            height: '100%',
+            width: '100%',
+            zIndex: 0,
+          }}
         >
-          {children}
-        </ModalContent>
-      </Paper>
-    </Fade>
-  </Modal>
-);
+          <ModalBackground height="100%" seed={toHash(title)} width="100%" />
+        </Box>
+      </>
+    </Modal>
+  );
+};
 
 export default DesktopModal;
