322d88a41c2ff9c2ec26da616e1f0c8c8b95ba73
Johan Rende <johan@rende.se>
2026-03-01 17:05:32 +0100

    Fix linter issue

--- src/zui/ZUIModalBackground/index.tsx
+++ src/zui/ZUIModalBackground/index.tsx
@@ -1,6 +1,7 @@
 import { Box } from '@mui/material';
 import { range } from 'lodash';
 import { FC, useEffect, useState } from 'react';
+
 import { getRandom } from 'utils/randomUtils';
 
 type Props = {
