fac4336f4a21a41859b558eeafab3b355798830c
Alexander Girke <alexgirke@posteo.de>
2026-03-16 13:51:39 +0100

    Introduce useIndexedDB hook

--- src/utils/testing/setup.ts
+++ src/utils/testing/setup.ts
@@ -1,5 +1,10 @@
 import { jest } from '@jest/globals';
 
+if (!global.structuredClone) {
+  global.structuredClone = <T>(value: T): T =>
+    JSON.parse(JSON.stringify(value));
+}
+
 jest.mock('next/font/google', () => ({
   Figtree: () => ({
     style: {
