52bfe14400542864586941581eeac142f2707fc4
Vector-Hector <30576258+Vector-Hector@users.noreply.github.com>
2026-03-17 12:34:37 +0100

    more stable move extension and selection persistence

--- src/zui/ZUIEditor/EditorOverlays/BlockToolbar/buttons/MoveDownButton.tsx
+++ src/zui/ZUIEditor/EditorOverlays/BlockToolbar/buttons/MoveDownButton.tsx
@@ -1,16 +1,10 @@
 import { KeyboardArrowDown } from '@mui/icons-material';
 import { IconButton } from '@mui/material';
-import { useCommands, useRemirrorContext } from '@remirror/react';
+import { useCommands } from '@remirror/react';
 import { FC } from 'react';
-
-import {
-  recordSelectionState,
-  recoverSelection,
-} from 'zui/ZUIEditor/utils/recoverSelection';
 
 const MoveDownButton: FC = () => {
   const { moveBlockDown } = useCommands();
-  const { view } = useRemirrorContext();
 
   return (
     <IconButton
@@ -20,9 +14,7 @@ const MoveDownButton: FC = () => {
           return;
         }
 
-        const selState = recordSelectionState(view);
         moveBlockDown();
-        recoverSelection(selState);
       }}
       size="small"
     >
--- src/zui/ZUIEditor/EditorOverlays/BlockToolbar/buttons/MoveUpButton.tsx
+++ src/zui/ZUIEditor/EditorOverlays/BlockToolbar/buttons/MoveUpButton.tsx
@@ -1,17 +1,10 @@
 import { KeyboardArrowUp } from '@mui/icons-material';
 import { IconButton } from '@mui/material';
-import { useCommands, useRemirrorContext } from '@remirror/react';
-import { NodeSelection } from 'prosemirror-state';
+import { useCommands } from '@remirror/react';
 import { FC } from 'react';
-
-import {
-  recordSelectionState,
-  recoverSelection,
-} from 'zui/ZUIEditor/utils/recoverSelection';
 
 const MoveUpButton: FC = () => {
   const { moveBlockUp } = useCommands();
-  const { view } = useRemirrorContext();
 
   return (
     <IconButton
@@ -21,9 +14,7 @@ const MoveUpButton: FC = () => {
           return;
         }
 
-        const selState = recordSelectionState(view);
         moveBlockUp();
-        recoverSelection(selState);
       }}
       size="small"
     >
