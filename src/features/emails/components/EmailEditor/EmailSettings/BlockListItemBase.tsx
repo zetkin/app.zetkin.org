a469a98f65966ceccfa7ff12a398d5e2c6ddc702
Vector-Hector <30576258+Vector-Hector@users.noreply.github.com>
2026-03-17 13:24:01 +0100

    add drag and drop functionality to email overview

--- src/features/emails/components/EmailEditor/EmailSettings/BlockListItemBase.tsx
+++ src/features/emails/components/EmailEditor/EmailSettings/BlockListItemBase.tsx
@@ -1,44 +1,88 @@
 import { Box, SvgIconTypeMap, Typography, useTheme } from '@mui/material';
 import { ErrorOutlineOutlined } from '@mui/icons-material';
-import { FC, MouseEventHandler } from 'react';
+import { FC, MouseEventHandler, RefObject, useEffect, useState } from 'react';
 import { OverridableComponent } from '@mui/material/OverridableComponent';
 
 interface BlockListItemBaseProps {
+  dropRef?: RefObject<HTMLDivElement | null>;
   hasErrors: boolean;
   icon: OverridableComponent<SvgIconTypeMap<Record<string, unknown>, 'svg'>>;
-  selected: boolean;
+  isDragging?: boolean;
+  isOver?: boolean;
   onSelect?: MouseEventHandler<HTMLDivElement>;
+  selected: boolean;
   title: string;
 }
 
 const BlockListItemBase: FC<BlockListItemBaseProps> = ({
+  dropRef,
   hasErrors,
   icon: Icon,
-  selected,
+  isDragging,
+  isOver,
   onSelect,
+  selected,
   title,
 }) => {
   const theme = useTheme();
+
+  const [debouncedIsOver, setDebouncedIsOver] = useState(false);
+
+  useEffect(() => {
+    if (typeof isOver === 'undefined') {
+      return;
+    }
+
+    const timeout = setTimeout(() => {
+      setDebouncedIsOver(isOver);
+    }, 50);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [isOver]);
+
   return (
     <Box
-      bgcolor={selected ? theme.palette.grey[300] : ''}
+      ref={dropRef}
+      bgcolor={
+        selected
+          ? theme.palette.grey[300]
+          : isDragging
+            ? theme.palette.grey[400]
+            : ''
+      }
       display="flex"
       justifyContent="space-between"
       onClick={onSelect}
       padding={2}
-      sx={
-        onSelect && {
+      sx={{
+        ...(onSelect && {
           ':hover': {
             bgcolor: selected ? '' : theme.palette.grey[200],
           },
-          cursor: 'pointer',
+          cursor: 'grab',
           userSelect: 'none',
-        }
-      }
+        }),
+        position: 'relative',
+      }}
       width="100%"
     >
+      {debouncedIsOver && (
+        <Box
+          sx={{
+            borderTop: `2px solid ${theme.palette.primary.main}`,
+            left: 0,
+            position: 'absolute',
+            right: 0,
+            top: -1,
+          }}
+        />
+      )}
       <Box display="flex" gap={2} width="80%">
-        <Icon color="secondary" />
+        <Box sx={{ cursor: 'grab' }}>
+          <Icon color="secondary" />
+        </Box>
         <Box
           sx={{
             cursor: onSelect ? 'pointer' : 'default',
