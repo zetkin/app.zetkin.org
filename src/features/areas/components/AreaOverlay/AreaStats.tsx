79236b68dea38cdf3e1fd046f8c54a8351718037
ziggabyte <ziggi.willpower@gmail.com>
2026-03-23 13:10:36 +0100

    Localise and style the stats about area assignments.

--- src/features/areas/components/AreaOverlay/AreaStats.tsx
+++ src/features/areas/components/AreaOverlay/AreaStats.tsx
@@ -1,8 +1,10 @@
 import { FC } from 'react';
 import { Box } from '@mui/system';
-import { Typography } from '@mui/material';
+import { Divider, Typography } from '@mui/material';
 
 import useAreaStats from 'features/areas/hooks/useAreaStats';
+import { Msg } from 'core/i18n';
+import messageIds from 'features/areas/l10n/messageIds';
 
 type Props = {
   areaId: number;
@@ -17,23 +19,83 @@ const AreaStats: FC<Props> = ({ areaId }) => {
   }
 
   return (
-    <Box padding={2}>
-      <Typography variant="h6">Area Stats (ID: {stats.area_id})</Typography>
-      <Typography>Households: {stats.num_households}</Typography>
-      <Typography>Locations: {stats.num_locations}</Typography>
-      <Typography>Sucessfull visits: {stats.num_successfull_visits}</Typography>
-      <Typography>
-        {Math.round(
-          (stats.num_households_visited /
-            stats.num_households_successfully_visited) *
-            100
+    <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+      <Box
+        sx={{ display: 'flex', flexDirection: 'column', gap: 1, paddingTop: 2 }}
+      >
+        <Typography variant="h6">
+          <Msg id={messageIds.areas.areaDetails.title} />
+        </Typography>
+        <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Box sx={{ width: '50%' }}>
+            <Typography color="primary" variant="h5">
+              {stats.num_locations}
+            </Typography>
+            <Typography>
+              <Msg id={messageIds.areas.areaDetails.locations} />
+            </Typography>
+          </Box>
+          <Box sx={{ width: '50%' }}>
+            <Typography color="primary" variant="h5">
+              {stats.num_households}
+            </Typography>
+            <Typography>
+              <Msg id={messageIds.areas.areaDetails.households} />
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
+      <Divider flexItem />
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 1,
+        }}
+      >
+        <Typography variant="h6">
+          <Msg id={messageIds.areas.assignmentStats.title} />
+        </Typography>
+        <Box>
+          <Typography color="primary" variant="h5">
+            {stats.num_successfull_visits}
+          </Typography>
+          <Typography>
+            <Msg id={messageIds.areas.assignmentStats.successfulVisits} />
+          </Typography>
+        </Box>
+        <Box>
+          <Typography color="primary" variant="h5">
+            {Math.round(
+              (stats.num_locations_visited / stats.num_locations) * 100
+            ) / 100}
+            %
+          </Typography>
+          <Typography>
+            <Msg id={messageIds.areas.assignmentStats.percentVisited} />
+          </Typography>
+        </Box>
+        {stats.num_households_visited > 0 && (
+          <Box>
+            <Typography color="primary" variant="h5">
+              {Math.round(
+                (stats.num_households_visited /
+                  stats.num_households_successfully_visited) *
+                  100
+              ) / 100}
+              %
+            </Typography>
+            <Typography>
+              <Msg
+                id={
+                  messageIds.areas.assignmentStats
+                    .percentSuccessfulHouseholdVisits
+                }
+              />
+            </Typography>
+          </Box>
         )}
-        % of households succesfully visited
-      </Typography>
-      <Typography>
-        {Math.round((stats.num_locations_visited / stats.num_locations) * 100)}%
-        of locations visited
-      </Typography>
+      </Box>
     </Box>
   );
 };
