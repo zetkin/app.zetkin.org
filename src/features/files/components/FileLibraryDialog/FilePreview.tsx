13cf928c196d519c9a3bd4f6a906997c87d1767a
Alexander Girke <alexgirke@posteo.de>
2026-03-28 16:03:08 +0100

    Preview different aspect ratios of the event image

--- src/features/files/components/FileLibraryDialog/FilePreview.tsx
+++ src/features/files/components/FileLibraryDialog/FilePreview.tsx
@@ -1,6 +1,6 @@
 import { FC, useState } from 'react';
 import Image from 'next/image';
-import { Box, Button, Typography } from '@mui/material';
+import { Box, Button, Grid, Typography } from '@mui/material';
 
 import messageIds from 'features/files/l10n/messageIds';
 import { Msg } from 'core/i18n';
@@ -57,6 +57,53 @@ const FilePreview: FC<Props> = ({ file, onBack, onSelect }) => {
           />
         </Typography>
       </Box>
+      <Grid container>
+        <Grid mx={2} size={{ lg: 1, md: 2, xs: 12 }}>
+          <Typography color="secondary" mt={1} textAlign="left" variant="h5">
+            Phone
+          </Typography>
+          <Image
+            alt={`${file.original_name}-phone`}
+            height="100"
+            src={file.url}
+            style={{
+              objectFit: 'cover',
+            }}
+            unoptimized
+            width="100"
+          />
+        </Grid>
+        <Grid mx={2} size={{ lg: 4, md: 4, xs: 12 }}>
+          <Typography color="secondary" mt={1} textAlign="left" variant="h5">
+            Tablet
+          </Typography>
+          <Image
+            alt={`${file.original_name}-phone`}
+            height="150"
+            src={file.url}
+            style={{
+              objectFit: 'cover',
+            }}
+            unoptimized
+            width="350"
+          />
+        </Grid>
+        <Grid mx={2} size={{ lg: 5, md: 8, xs: 12 }}>
+          <Typography color="secondary" mt={1} textAlign="left" variant="h5">
+            Desktop
+          </Typography>
+          <Image
+            alt={`${file.original_name}-phone`}
+            height="100"
+            src={file.url}
+            style={{
+              objectFit: 'cover',
+            }}
+            unoptimized
+            width="550"
+          />
+        </Grid>
+      </Grid>
       <Box display="flex" gap={1} justifyContent="flex-end">
         <Button onClick={() => onBack()} variant="outlined">
           <Msg id={messageIds.libraryDialog.preview.backButton} />
