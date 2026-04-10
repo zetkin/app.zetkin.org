4e5c0cb3a3df82e05836a2675bebee8771974635
ziggabyte <ziggi.willpower@gmail.com>
2026-04-10 14:42:36 +0200

    Show tooltip arrow.

--- src/features/surveys/components/ResponseStatsCards.tsx
+++ src/features/surveys/components/ResponseStatsCards.tsx
@@ -864,6 +864,7 @@ const TextResponseCard = ({
                     }}
                   >
                     <Tooltip
+                      arrow
                       title={
                         hasCopiedResponse
                           ? messages.insights.textFields.copyResponse.wasCopied()
