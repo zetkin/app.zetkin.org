4930d03ae3f8bd06849cc19b8c03b80d3ba8fff9
Theo Zanchi <theo.zanchi@deutschebahn.com>
2026-04-13 00:44:10 +0200

    feat: add defaults for theme elements

--- src/features/emails/hooks/useCreateEmailTheme.ts
+++ src/features/emails/hooks/useCreateEmailTheme.ts
@@ -8,6 +8,57 @@ interface UseCreateEmailThemeReturn {
   createEmailTheme: () => Promise<EmailTheme>;
 }
 
+const defaultBlockAttributes = {
+  button: {},
+  image: {},
+};
+
+const defaultFrameMjml = {
+  tagName: 'mj-wrapper',
+  children: [
+    {
+      tagName: 'mj-section',
+      children: [],
+      attributes: {},
+    },
+    {
+      tagName: 'mj-section',
+      children: [
+        {
+          tagName: 'mj-column',
+          children: [
+            {
+              tagName: 'placeholder',
+              attributes: {},
+            },
+          ],
+          attributes: {},
+        },
+      ],
+      attributes: {},
+    },
+    {
+      tagName: 'mj-section',
+      children: [
+        {
+          content:
+            'To unsubscribe from emails, click <a href="{{unsub_org_url}}">here</a>.',
+          tagName: 'mj-text',
+          attributes: {
+            align: 'center',
+            color: '#a1a1a1',
+            'font-size': '12px',
+          },
+        },
+      ],
+      attributes: {},
+    },
+  ],
+  attributes: {},
+};
+
+const defaultCss = "h1 {}\nh2 {}\nh3 {}\na:link {}\na:visited {}\na:hover {}\np {}\np b {}";
+
 export default function useCreateEmailTheme(
   orgId: number
 ): UseCreateEmailThemeReturn {
@@ -18,7 +69,11 @@ export default function useCreateEmailTheme(
     dispatch(themeCreate);
     const theme = await apiClient.post<EmailTheme>(
       `/api/orgs/${orgId}/email_themes`,
-      { frame_mjml: null }
+      {
+        block_attributes: defaultBlockAttributes,
+        css: defaultCss,
+        frame_mjml: defaultFrameMjml
+      }
     );
     dispatch(themeCreated([theme, orgId]));
     return theme;
