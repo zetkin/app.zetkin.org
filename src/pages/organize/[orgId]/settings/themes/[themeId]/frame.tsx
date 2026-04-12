d951032ca3f439fa108f8292a32c34d22f5dea62
Theo Zanchi <theo.zanchi@deutschebahn.com>
2026-04-12 22:17:04 +0200

    feat: quick-and-dirty email renderer in the frontend-end

--- src/pages/organize/[orgId]/settings/themes/[themeId]/frame.tsx
+++ src/pages/organize/[orgId]/settings/themes/[themeId]/frame.tsx
@@ -1,6 +1,6 @@
 import { GetServerSideProps } from 'next';
 import { Box, CircularProgress, Stack } from '@mui/material';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { scaffold } from 'utils/next';
 import { PageWithLayout } from 'utils/types';
@@ -9,6 +9,7 @@ import { useNumericRouteParams } from 'core/hooks';
 import EmailThemeLayout from 'features/emails/layout/EmailThemeLayout';
 import useEmailTheme from 'features/emails/hooks/useEmailTheme';
 import ThemeEditor from 'features/emails/components/ThemeEditor/ThemeEditor';
+import previewEmailThemeHtml from 'features/emails/utils/rendering/previewEmailThemeHtml';
 
 const scaffoldOptions = {
   authLevelRequired: 2,
@@ -27,7 +28,17 @@ interface ThemePageProps {
 const ThemePage: PageWithLayout<ThemePageProps> = () => {
   const onServer = useServerSide();
   const { orgId, themeId } = useNumericRouteParams();
-  const { isLoading } = useEmailTheme(orgId, themeId);
+  const { isLoading, data } = useEmailTheme(orgId, themeId);
+  const [previewHtml, setPreviewHtml] = useState('');
+
+  useEffect(() => {
+    if (!data) {
+      return;
+    }
+    previewEmailThemeHtml(data).then((html) => {
+      setPreviewHtml(html);
+    });
+  }, [data]);
 
   if (onServer) {
     return null;
@@ -45,7 +56,22 @@ const ThemePage: PageWithLayout<ThemePageProps> = () => {
           themeId={themeId}
         />
       </Box>
-      <Box sx={{ flex: 1, minWidth: '0' }} />
+      <Box sx={{ flex: 1, minWidth: '0' }}>
+        {previewHtml ? (
+          <iframe
+            srcDoc={previewHtml}
+            style={{
+              background: 'white',
+              border: 'none',
+              height: 'calc(100% - 32px)',
+              width: '100%',
+            }}
+            title="Email Theme Preview"
+          />
+        ) : (
+          <CircularProgress size={24} />
+        )}
+      </Box>
     </Stack>
   );
 };
