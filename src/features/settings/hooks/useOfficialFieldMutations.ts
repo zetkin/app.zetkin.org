056204a1d06d7e460fa98d7d3d3a258d288b2bb4
FarahAlh <farah.alhaddad@hotmail.com>
2026-04-16 10:39:53 +0200

    feat(fields): add ability to create custom fields
    - add createField API call
    - add button to trigger field creation

--- src/features/settings/hooks/useOfficialFieldMutations.ts
+++ src/features/settings/hooks/useOfficialFieldMutations.ts
@@ -2,20 +2,29 @@ import { useAppDispatch } from 'core/hooks';
 import useApiClient from 'core/hooks/useApiClient';
 import { fieldRemoved } from 'features/profile/store';
 
-type useFieldsMutationReturn = {
+type UseFieldsMutationReturn = {
+  createField: () => void;
   removeField: (fieldId: number) => void;
 };
 
 export default function useOfficialFieldMutations(
   orgId: number
-): useFieldsMutationReturn {
+): UseFieldsMutationReturn {
   const apiClient = useApiClient();
   const dispatch = useAppDispatch();
+
+  const createField = async () => {
+    await apiClient.post(`/api/orgs/${orgId}/people/fields/`, {
+      slug: 'farahs_test_10',
+      title: 'Farahs Field',
+      type: 'text',
+    });
+  };
 
   const removeField = async (fieldId: number) => {
     await apiClient.delete(`/api/orgs/${orgId}/people/fields/${fieldId}`);
     dispatch(fieldRemoved(fieldId));
   };
 
-  return { removeField };
+  return { createField, removeField };
 }
