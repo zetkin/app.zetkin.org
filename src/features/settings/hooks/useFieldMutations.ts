da730c24242580a10d986a1f9a87c8342e53e4ae
FarahAlh <farah.alhaddad@hotmail.com>
2026-04-16 11:31:05 +0200

    refactor(fields): - move createField functionality into own hook, useCreateField.ts.
    - rename useOfficialFieldMutations.ts to useFieldMutations.ts.

--- src/features/settings/hooks/useFieldMutations.ts
+++ src/features/settings/hooks/useFieldMutations.ts
@@ -3,28 +3,19 @@ import useApiClient from 'core/hooks/useApiClient';
 import { fieldRemoved } from 'features/profile/store';
 
 type UseFieldsMutationReturn = {
-  createField: () => void;
   removeField: (fieldId: number) => void;
 };
 
-export default function useOfficialFieldMutations(
+export default function useFieldMutations(
   orgId: number
 ): UseFieldsMutationReturn {
   const apiClient = useApiClient();
   const dispatch = useAppDispatch();
-
-  const createField = async () => {
-    await apiClient.post(`/api/orgs/${orgId}/people/fields/`, {
-      slug: 'farahs_test_10',
-      title: 'Farahs Field',
-      type: 'text',
-    });
-  };
 
   const removeField = async (fieldId: number) => {
     await apiClient.delete(`/api/orgs/${orgId}/people/fields/${fieldId}`);
     dispatch(fieldRemoved(fieldId));
   };
 
-  return { createField, removeField };
+  return { removeField };
 }
