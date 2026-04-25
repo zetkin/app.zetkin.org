f07b86f007d38382a3c5b531ca22eb44b8277109
Theo Zanchi <theo.zanchi@deutschebahn.com>
2026-04-25 10:21:15 +0200

    refactor: import emailThemePreview content through a hook

--- src/features/emails/components/ThemeEditor/ThemePreview.tsx
+++ src/features/emails/components/ThemeEditor/ThemePreview.tsx
@@ -5,6 +5,7 @@ import { EmailTheme } from 'features/emails/types';
 import previewEmailThemeHtml from 'features/emails/utils/rendering/previewEmailThemeHtml';
 import messageIds from 'features/emails/l10n/messageIds';
 import { useMessages } from 'core/i18n';
+import usePreviewEmailTheme from 'features/emails/hooks/usePreviewEmailTheme';
 
 interface ThemePreviewProps {
   theme: EmailTheme | null;
@@ -12,107 +13,7 @@ interface ThemePreviewProps {
 
 const ThemePreview: React.FC<ThemePreviewProps> = ({ theme }) => {
   const messages = useMessages(messageIds);
-  const content =
-    `
-{
-  "blocks": [
-    {
-      "kind": "header",
-      "data": {
-        "content": [
-          { "kind": "string", "value": "` +
-    messages.themes.themePreview.heading.paragraphPart1() +
-    `" }
-        ],
-        "level": 1
-      }
-    },
-    {
-      "kind": "header",
-      "data": {
-        "content": [
-          { "kind": "string", "value": "` +
-    messages.themes.themePreview.heading.paragraphPart2() +
-    `" }
-        ],
-        "level": 2
-      }
-    },
-    {
-      "kind": "header",
-      "data": {
-        "content": [
-          { "kind": "string", "value": "` +
-    messages.themes.themePreview.heading.paragraphPart1() +
-    `" }
-        ],
-        "level": 3
-      }
-    },
-    {
-      "kind": "paragraph",
-      "data": {
-        "content": [
-          { "kind": "string", "value": "` +
-    messages.themes.themePreview.paragraph.paragraphPart1() +
-    `" },
-          {
-            "kind": "bold",
-            "content": [
-              { "kind": "string", "value": "` +
-    messages.themes.themePreview.paragraph.bold() +
-    `" }
-            ]
-          },
-          { "kind": "string", "value": "` +
-    messages.themes.themePreview.paragraph.paragraphPart2() +
-    `" },
-          {
-            "kind": "italic",
-            "content": [
-              { "kind": "string", "value": "` +
-    messages.themes.themePreview.paragraph.italic() +
-    `" }
-            ]
-          },
-          { "kind": "string", "value": "` +
-    messages.themes.themePreview.paragraph.paragraphPart3() +
-    `" },
-          {
-            "kind": "link",
-            "content": [
-              { "kind": "string", "value": "` +
-    messages.themes.themePreview.paragraph.link() +
-    `" }
-            ],
-            "href": "https://zetkin.org/",
-            "tag": "sjf8dtwj"
-          },
-          { "kind": "string", "value": "." }
-        ]
-      }
-    },
-    {
-      "kind": "image",
-      "data": {
-        "alt": "logo-zetkin.png",
-        "fileId": 2,
-        "src": "http://files.dev.zetkin.org/1/61a0fe34-247c-4c8c-9514-6bf7f9e3d578.png"
-      }
-    },
-    {
-      "kind": "button",
-      "data": {
-        "href": "https://zetkin.org",
-        "tag": "tja96d3p",
-        "text": "` +
-    messages.themes.themePreview.button() +
-    `"
-      }
-    }
-  ]
-}
-`;
+  const content = usePreviewEmailTheme();
   const [previewHtml, setPreviewHtml] = useState('');
 
   useEffect(() => {
