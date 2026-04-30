3d5409fbea17ead5621bbcb7748293cd77476584
Vector-Hector <30576258+Vector-Hector@users.noreply.github.com>
2026-04-30 23:22:08 +0200

    fix mapping hook remote item handling

--- src/utils/hooks/useRemoteListMapping.ts
+++ src/utils/hooks/useRemoteListMapping.ts
@@ -1,10 +1,10 @@
 import { useMemo } from 'react';
 
-import { RemoteList } from 'utils/storeUtils';
+import { RemoteItem, RemoteList } from 'utils/storeUtils';
 
 export default function useRemoteListMapping<DataTypeFrom, DataTypeTo>(
   list: RemoteList<DataTypeFrom> | undefined,
-  mapper: (items: DataTypeFrom[]) => DataTypeTo[]
+  mapper: (item: DataTypeFrom) => DataTypeTo | null
 ): RemoteList<DataTypeTo> | undefined {
   return useMemo(() => {
     if (typeof list === 'undefined') {
@@ -14,12 +14,14 @@ export default function useRemoteListMapping<DataTypeFrom, DataTypeTo>(
     if (list.items) {
       return <RemoteList<DataTypeTo>>{
         ...list,
-        items: mapper(
-          list.items
-            .filter((item) => item.deleted)
-            .map((item) => item.data)
-            .filter((data): data is DataTypeFrom => !!data)
-        ),
+        items: list.items
+          .filter((item) => !item.deleted)
+          .map((item) => {
+            return <RemoteItem<DataTypeTo>>{
+              ...item,
+              data: item.data && mapper(item.data),
+            };
+          }),
       };
     }
   }, [list, mapper]);
