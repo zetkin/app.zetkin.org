f9f355e463699115bc823a6949b825f5cfac4d83
Theo Zanchi <theo.zanchi@deutschebahn.com>
2026-05-04 20:17:14 +0200

    fix: align ThemeEditor and ThemePreview boxes

--- src/features/emails/components/ThemeEditor/ThemePreview.tsx
+++ src/features/emails/components/ThemeEditor/ThemePreview.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { CircularProgress, Stack, Typography } from '@mui/material';
+import { Box } from '@mui/system';
 
 import { EmailTheme } from 'features/emails/types';
 import previewEmailThemeHtml from 'features/emails/utils/rendering/previewEmailThemeHtml';
@@ -26,24 +27,45 @@ const ThemePreview: React.FC<ThemePreviewProps> = ({ theme }) => {
   });
 
   return (
-    <Stack gap={2} sx={{ flex: 1, minWidth: '0' }}>
-      <Typography variant="h5">
-        {messages.themes.themeEditor.previewTitle()}
-      </Typography>
-      {previewHtml ? (
-        <iframe
-          srcDoc={previewHtml}
-          style={{
-            background: 'white',
-            border: 'none',
-            height: 'calc(100% - 32px)',
-            width: '100%',
-          }}
-          title="Email Theme Preview"
-        />
-      ) : (
-        <CircularProgress size={24} />
-      )}
+    <Stack sx={{ flex: 1, height: '100%', minWidth: '0' }}>
+      <Box
+        sx={{
+          alignItems: 'center',
+          borderBottom: 1,
+          borderColor: 'divider',
+          display: 'flex',
+          height: '49px',
+          px: 2,
+        }}
+      >
+        <Typography sx={{ fontWeight: 500 }} variant="subtitle1">
+          {messages.themes.themeEditor.previewTitle()}
+        </Typography>
+      </Box>
+
+      <Box sx={{ flexGrow: 1, p: 2, position: 'relative' }}>
+        {previewHtml ? (
+          <iframe
+            srcDoc={previewHtml}
+            style={{
+              background: 'white',
+              border: '1px solid #e0e0e0',
+              borderRadius: '4px',
+              height: '100%',
+              width: '100%',
+            }}
+            title="Email Theme Preview"
+          />
+        ) : (
+          <Stack
+            alignItems="center"
+            justifyContent="center"
+            sx={{ height: '100%' }}
+          >
+            <CircularProgress size={32} />
+          </Stack>
+        )}
+      </Box>
     </Stack>
   );
 };
