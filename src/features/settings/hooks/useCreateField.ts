5c5e8f0d24691a1e68ed03aed573a683a427bdb6
FarahAlh <farah.alhaddad@hotmail.com>
2026-05-07 13:20:53 +0200

    feat(fields settings): add enum field support with choices input

--- src/features/settings/hooks/useCreateField.ts
+++ src/features/settings/hooks/useCreateField.ts
@@ -1,25 +1,38 @@
 import { useAppDispatch } from 'core/hooks';
 import useApiClient from 'core/hooks/useApiClient';
 import { fieldCreate, fieldCreated } from 'features/profile/store';
-import { CUSTOM_FIELD_TYPE, ZetkinCustomField } from 'utils/types/zetkin';
+import {
+  CUSTOM_FIELD_TYPE,
+  EnumChoice,
+  ZetkinCustomField,
+} from 'utils/types/zetkin';
 
 type UseCreateFieldsReturn = {
-  createField: (title: string) => void;
+  createField: (
+    title: string,
+    type: CUSTOM_FIELD_TYPE,
+    enumChoices?: EnumChoice[]
+  ) => void;
 };
 
 export default function useCreateField(orgId: number): UseCreateFieldsReturn {
   const apiClient = useApiClient();
   const dispatch = useAppDispatch();
 
-  const createField = async (title: string) => {
+  const createField = async (
+    title: string,
+    type: CUSTOM_FIELD_TYPE,
+    enumChoices?: EnumChoice[]
+  ) => {
     dispatch(fieldCreate());
 
     const field = await apiClient.post<ZetkinCustomField>(
       `/api/orgs/${orgId}/people/fields/`,
       {
+        enum_choices: enumChoices,
         slug: title.toLowerCase().replace(/\s+/g, '_'),
         title,
-        type: 'text' as CUSTOM_FIELD_TYPE,
+        type: type,
       }
     );
     dispatch(fieldCreated(field));
