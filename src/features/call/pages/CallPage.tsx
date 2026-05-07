19faef46b84a5d3b89208f2c7c644f32142b11a9
ziggabyte <ziggi.willpower@gmail.com>
2026-05-07 14:40:55 +0200

    Move router.push to parent component.

--- src/features/call/pages/CallPage.tsx
+++ src/features/call/pages/CallPage.tsx
@@ -4,6 +4,7 @@ import { Box } from '@mui/material';
 import { FC, useEffect } from 'react';
 import { redirect } from 'next/navigation';
 import { ErrorBoundary } from 'react-error-boundary';
+import { useRouter } from 'next/navigation';
 
 import useServerSide from 'core/useServerSide';
 import ZUILogoLoadingIndicator from 'zui/ZUILogoLoadingIndicator';
@@ -15,6 +16,7 @@ import { Msg } from 'core/i18n';
 import messageIds from '../l10n/messageIds';
 
 const CallPage: FC = () => {
+  const router = useRouter();
   const { clearCallLanes, clearStaleCallLanes, initialize, canInitialize } =
     useCallInitialization();
 
@@ -69,7 +71,14 @@ const CallPage: FC = () => {
           </Box>
         }
       >
-        {canInitialize && <Call onResetAfterError={() => clearCallLanes()} />}
+        {canInitialize && (
+          <Call
+            onResetAfterError={(urlToNavigateTo: string) => {
+              clearCallLanes();
+              router.push(urlToNavigateTo);
+            }}
+          />
+        )}
       </ErrorBoundary>
     </main>
   );
