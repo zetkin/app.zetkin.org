e92c85ca033b863f63255d220f56016b560b219c
sara.heine <sara.heine@rise-world.com>
2026-05-10 16:17:48 +0200

    Fix removing date value bug

--- src/features/profile/components/EditPersonDialog/EditPersonFields.tsx
+++ src/features/profile/components/EditPersonDialog/EditPersonFields.tsx
@@ -203,6 +203,8 @@ const EditPersonFields: FC<EditPersonFieldsProps> = ({
                   if (date) {
                     const dateStr = makeNaiveDateString(date.utc().toDate());
                     onChange(field.slug, dateStr);
+                  } else {
+                    onChange(field.slug, null);
                   }
                 }}
                 slotProps={{
