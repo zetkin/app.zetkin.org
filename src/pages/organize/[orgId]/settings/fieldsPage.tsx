1a0bd577da8f27517429bb7c0ced7d90a38567ae
FarahAlh <farah.alhaddad@hotmail.com>
2026-05-20 23:59:07 +0200

    refactor(fields): clean up FieldsPage and move titles into components

--- src/pages/organize/[orgId]/settings/fieldsPage.tsx
+++ src/pages/organize/[orgId]/settings/fieldsPage.tsx
@@ -1,5 +1,5 @@
+import { Box } from '@mui/system';
 import { GetServerSideProps } from 'next';
-import { Box, Grid, Typography } from '@mui/material';
 
 import { PageWithLayout } from 'utils/types';
 import { scaffold } from 'utils/next';
@@ -7,8 +7,7 @@ import SettingsLayout from 'features/settings/layout/SettingsLayout';
 import useServerSide from 'core/useServerSide';
 import useNumericRouteParams from 'core/hooks/useNumericRouteParams';
 import FieldsList from 'features/settings/components/FieldsList';
-import Msg from 'core/i18n/Msg';
-import messageIds from 'features/settings/l10n/messageIds';
+import NewFieldForm from 'features/settings/components/NewFieldForm';
 
 export const getServerSideProps: GetServerSideProps = scaffold(
   async () => {
@@ -30,25 +29,15 @@ const FieldsPage: PageWithLayout = () => {
   }
 
   return (
-    <Box>
-      <Grid container spacing={2}>
-        <Grid size={{ md: 8 }}>
-          <Box
-            alignItems="center"
-            display="flex"
-            justifyContent="space-between"
-            sx={{
-              marginBottom: '15px',
-              marginTop: '15px',
-            }}
-          >
-            <Typography variant="h4">
-              <Msg id={messageIds.fields.title} />
-            </Typography>
-          </Box>
-        </Grid>
-      </Grid>
+    <Box
+      sx={{
+        backgroundColor: '#f7f7f7',
+        display: 'flex',
+        gap: 10,
+      }}
+    >
       <FieldsList orgId={orgId} />
+      <NewFieldForm orgId={orgId} />
     </Box>
   );
 };
