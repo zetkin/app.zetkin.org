d6eebe3a591f1b68f3a05bc91e2e3e46de98f3bb
Laura Blum <business@laurablum.com>
2026-05-26 17:14:08 +0200

    revert unrelated tests

--- integrationTesting/tests/organize/views/list/delete.spec.ts
+++ integrationTesting/tests/organize/views/list/delete.spec.ts
@@ -5,10 +5,7 @@ import AllMembers from '../../../../mockData/orgs/KPD/people/views/AllMembers';
 import KPD from '../../../../mockData/orgs/KPD';
 
 const deleteView = async (page: Page) => {
-  await page
-    .locator('main [data-testid=ZUIEllipsisMenu-menuActivator]')
-    .first()
-    .click();
+  await page.click('main [data-testid=ZUIEllipsisMenu-menuActivator]');
   await page.click(`data-testid=ZUIEllipsisMenu-item-delete-item`);
   await page.click('data-testid=SubmitCancelButtons-submitButton');
 };
