70d9e9a8196317ab457f443994fc5f6623653215
Johannes Filter <hi@jfilter.de>
2026-06-07 15:32:26 +0200

    Convert /my pages to non-suspending data loading

--- src/features/canvass/hooks/useMyAreaAssignments.ts
+++ src/features/canvass/hooks/useMyAreaAssignments.ts
@@ -1,19 +1,29 @@
 import { useApiClient, useAppSelector } from 'core/hooks';
 import { myAssignmentsLoad, myAssignmentsLoaded } from '../store';
 import useRemoteList from 'core/hooks/useRemoteList';
+import useRemoteListFuture from 'core/hooks/useRemoteListFuture';
+import { IFuture, ResolvedFuture } from 'core/caching/futures';
 import { ZetkinAreaAssignment } from 'features/areaAssignments/types';
 
-export default function useMyAreaAssignments() {
+function useListWithHooks() {
   const apiClient = useApiClient();
   const list = useAppSelector((state) => state.canvass.myAssignmentsList);
 
-  const assignments = useRemoteList(list, {
-    actionOnLoad: () => myAssignmentsLoad(),
-    actionOnSuccess: (data) => myAssignmentsLoaded(data),
-    loader: () =>
-      apiClient.get<ZetkinAreaAssignment[]>('/api2/users/me/area_assignments'),
-  });
+  return {
+    hooks: {
+      actionOnLoad: () => myAssignmentsLoad(),
+      actionOnSuccess: (data: ZetkinAreaAssignment[]) =>
+        myAssignmentsLoaded(data),
+      loader: () =>
+        apiClient.get<ZetkinAreaAssignment[]>(
+          '/api2/users/me/area_assignments'
+        ),
+    },
+    list,
+  };
+}
 
+function filterActive(assignments: ZetkinAreaAssignment[]) {
   const now = new Date();
   const today = now.toISOString().slice(0, 10);
 
@@ -23,4 +33,20 @@ export default function useMyAreaAssignments() {
       start_date.slice(0, 10) <= today &&
       (end_date == null || end_date.slice(0, 10) >= today)
   );
+}
+
+export default function useMyAreaAssignments() {
+  const { hooks, list } = useListWithHooks();
+  return filterActive(useRemoteList(list, hooks));
+}
+
+export function useMyAreaAssignmentsFuture(): IFuture<ZetkinAreaAssignment[]> {
+  const { hooks, list } = useListWithHooks();
+  const future = useRemoteListFuture(list, hooks);
+
+  if (!future.data) {
+    return future;
+  }
+
+  return new ResolvedFuture(filterActive(future.data));
 }
