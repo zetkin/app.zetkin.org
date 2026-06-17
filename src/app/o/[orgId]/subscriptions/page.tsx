6c4e96a2d69f3024cf7d49144e4cc11de178d00d
djbusstop <eribloodlust@grrlz.net>
2026-06-17 10:14:43 +0200

    improve error handling for page

--- src/app/o/[orgId]/subscriptions/page.tsx
+++ src/app/o/[orgId]/subscriptions/page.tsx
@@ -25,40 +25,38 @@ export default async function Page({ params, searchParams }: PageProps) {
   const headersEntries = headersList.entries();
   const headersObject = Object.fromEntries(headersEntries);
   const apiClient = new BackendApiClient(headersObject);
-  const org = await apiClient.get<ZetkinOrganization>(`/api/orgs/${orgId}`);
-
-  if (!token) {
-    return <SubscriptionsTokenRequestPage org={org} />;
-  }
 
   try {
+    const org = await apiClient.get<ZetkinOrganization>(`/api/orgs/${orgId}`);
+
+    if (!token) {
+      return <SubscriptionsTokenRequestPage org={org} />;
+    }
+
     const tokenExpiry = jwtDecode(token)?.exp;
-    const isExpired = !tokenExpiry || tokenExpiry < new Date().getTime(); // Should we use temporal
+    const isExpired = !tokenExpiry || tokenExpiry < new Date().getTime();
 
     if (isExpired) {
       return <SubscriptionsTokenRequestPage org={org} />;
     }
 
-    try {
-      // const channelsReq = await fetch(`/api2/orgs/${orgId}/channels`, {
-      //   // What exactly should url be.
-      //   headers: new Headers({ Authorization: `Bearer ${token}` }),
-      // });
-      // if (!channelsReq.ok) {
-      //   throw new Error('Channels fetch failed');
-      // }
-      // const initialChannels: EmailChannel[] = await channelsReq.json();
-      return (
-        <SubscriptionsManagementPage
-          initialChannels={initialData}
-          org={org}
-          token={token}
-        />
-      );
-    } catch (err) {
-      return notFound();
-    }
+    // Fetch channels and render subscription management page
+    // const channelsReq = await fetch(`/api2/orgs/${orgId}/channels`, {
+    //   // What exactly should url be.
+    //   headers: new Headers({ Authorization: `Bearer ${token}` }),
+    // });
+    // if (!channelsReq.ok) {
+    //   throw new Error('Channels fetch failed');
+    // }
+    // const initialChannels: EmailChannel[] = await channelsReq.json();
+    return (
+      <SubscriptionsManagementPage
+        initialChannels={initialData}
+        org={org}
+        token={token}
+      />
+    );
   } catch {
-    //TODO: redirect to page to input email for new jwt
+    return notFound();
   }
 }
