4b3f45c755ee0870a81d4a463034e324f31964d9
Vector-Hector <30576258+Vector-Hector@users.noreply.github.com>
2026-08-18 14:51:00 +0200

    make metric sorting a hook + use it

--- src/features/canvass/components/LocationDialog/index.tsx
+++ src/features/canvass/components/LocationDialog/index.tsx
@@ -30,7 +30,7 @@ import useVisitReporting from 'features/canvass/hooks/useVisitReporting';
 import { ZUIConfirmDialogContext } from 'zui/ZUIConfirmDialogProvider';
 import messageIds from 'features/canvass/l10n/messageIds';
 import { useMessages } from 'core/i18n';
-import sortMetrics from 'features/canvass/utils/sortMetrics';
+import useSortedMetrics from 'features/canvass/hooks/useSortedMetrics';
 import BulkHouseholdVisitsPage from './pages/BulkHouseholdVisitsPage';
 import BulkEditHouseholdsPage from './pages/BulkEditHouseholdsPage';
 import useEditHouseholds from 'features/canvass/hooks/useEditHouseholds';
@@ -68,7 +68,7 @@ const LocationDialog: FC<LocationDialogProps> = ({
   );
   const { deleteHousehold, updateHousehold, updateLocation } =
     useLocationMutations(orgId, location.id);
-  const metrics = sortMetrics(metricsList);
+  const metrics = useSortedMetrics(metricsList);
 
   const {
     lastVisitByHouseholdId,
