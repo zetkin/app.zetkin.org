cac7b4b9990a590eb94489692976c91bb01c5b2f
ziggabyte <ziggi.willpower@gmail.com>
2026-09-03 14:25:51 +0200

    Limit how many tags are visible based on how much space is available.

--- src/features/call/components/AboutSection.tsx
+++ src/features/call/components/AboutSection.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import { Box } from '@mui/system';
+import { Stack } from '@mui/material';
 import { HomeOutlined, MailOutline, Phone } from '@mui/icons-material';
 
 import ZUISection from 'zui/components/ZUISection';
@@ -12,6 +13,8 @@ import ZUIIcon from 'zui/components/ZUIIcon';
 import useIsMobile from 'utils/hooks/useIsMobile';
 import { Msg, useMessages } from 'core/i18n';
 import messageIds from '../l10n/messageIds';
+import ZUIResponsiveContainer from 'zui/ZUIResponsiveContainer';
+import ZUITooltip from 'zui/components/ZUITooltip';
 
 type AboutSectionProps = {
   call: UnfinishedCall | null;
@@ -21,7 +24,7 @@ export const AboutContent = ({ call }: { call: UnfinishedCall }) => {
   const isMobile = useIsMobile();
 
   return (
-    <>
+    <Stack gap={1}>
       <Box>
         <Box display="flex" flex={1} flexDirection="column" gap={1}>
           {call.target.phone && (
@@ -85,73 +88,115 @@ export const AboutContent = ({ call }: { call: UnfinishedCall }) => {
         </Box>
       </Box>
       {call.target.tags.length > 0 && (
-        <>
-          <ZUIText variant="headingMd">
-            <Msg id={messageIds.about.tagsHeader} />
-          </ZUIText>
-          <Box
-            sx={{
-              alignItems: 'center',
-              display: 'flex',
-              flexWrap: 'wrap',
-              gap: 1,
-            }}
-          >
-            {call.target.tags.map((tag) => {
-              return <ZUITagChip key={tag.id} tag={tag} />;
-            })}
-          </Box>
-        </>
+        <ZUIResponsiveContainer ssrWidth={300}>
+          {(width) => {
+            const tags = call.target.tags;
+            const maxTags = Math.floor(width / 100);
+            const displayedTags = tags.slice(0, maxTags);
+            const hiddenTags = tags.slice(maxTags);
+
+            const tooltipTitle = hiddenTags.map((tag) => tag.title).join(', ');
+
+            return (
+              <Box sx={{ paddingBottom: 0.5 }}>
+                <ZUIText variant="headingMd">
+                  <Msg id={messageIds.about.tagsHeader} />
+                </ZUIText>
+                <Box
+                  sx={{
+                    alignItems: 'center',
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    gap: 1,
+                    paddingTop: 1,
+                  }}
+                >
+                  {displayedTags.map((tag) => (
+                    <ZUITagChip key={tag.id} tag={tag} />
+                  ))}
+                  {hiddenTags.length > 0 && (
+                    <ZUITooltip label={tooltipTitle}>
+                      <Box
+                        border={2}
+                        sx={(theme) => ({
+                          alignItems: 'center',
+                          borderColor: theme.palette.grey[500],
+                          borderRadius: '1em',
+                          borderWidth: '1px',
+                          color: theme.palette.text.secondary,
+                          cursor: 'default',
+                          display: 'flex',
+                          height: '30.4px',
+                          lineHeight: 'normal',
+                          marginRight: '0.1em',
+                          overflow: 'hidden',
+                          padding: '0.2em 0.7em',
+                          textOverflow: 'ellipsis',
+                        })}
+                      >
+                        <ZUIText>
+                          {`${displayedTags.length > 0 ? '+' : ''}${hiddenTags.length}`}
+                        </ZUIText>
+                      </Box>
+                    </ZUITooltip>
+                  )}
+                </Box>
+              </Box>
+            );
+          }}
+        </ZUIResponsiveContainer>
       )}
-      <ZUIText variant="headingMd">
-        <Msg id={messageIds.about.previousActivityHeader} />
-      </ZUIText>
-      {call.target.past_actions.num_actions > 0 && (
-        <Box sx={{ minWidth: 0, overflowWrap: 'anywhere' }}>
-          <ZUIText component="span" display="inline">
-            <Msg
-              id={messageIds.about.participation}
-              values={{
-                events: (
-                  <ZUIText display="inline" variant="bodyMdSemiBold">
-                    <Msg
-                      id={messageIds.about.events}
-                      values={{
-                        numEvents: call.target.past_actions.num_actions,
-                      }}
+      <Box>
+        <ZUIText variant="headingMd">
+          <Msg id={messageIds.about.previousActivityHeader} />
+        </ZUIText>
+        {call.target.past_actions.num_actions > 0 && (
+          <Box sx={{ minWidth: 0, overflowWrap: 'anywhere' }}>
+            <ZUIText component="span" display="inline">
+              <Msg
+                id={messageIds.about.participation}
+                values={{
+                  events: (
+                    <ZUIText display="inline" variant="bodyMdSemiBold">
+                      <Msg
+                        id={messageIds.about.events}
+                        values={{
+                          numEvents: call.target.past_actions.num_actions,
+                        }}
+                      />
+                    </ZUIText>
+                  ),
+                  name: call.target.first_name,
+                  time: (
+                    <ZUIRelativeTime
+                      datetime={
+                        call.target.past_actions.last_action?.end_time || ''
+                      }
                     />
-                  </ZUIText>
-                ),
-                name: call.target.first_name,
-                time: (
-                  <ZUIRelativeTime
-                    datetime={
-                      call.target.past_actions.last_action?.end_time || ''
-                    }
-                  />
-                ),
-                title: (
-                  <ZUIText display="inline" variant="bodyMdSemiBold">
-                    {call.target.past_actions.last_action?.title}
-                  </ZUIText>
-                ),
-              }}
-            />
-          </ZUIText>
-        </Box>
-      )}
-      {call.target.past_actions.num_actions == 0 && (
-        <Box sx={{ minWidth: 0, overflowWrap: 'anywhere' }}>
-          <ZUIText color="secondary">
-            <Msg
-              id={messageIds.about.noParticipation}
-              values={{ name: call.target.first_name }}
-            />
-          </ZUIText>
-        </Box>
-      )}
+                  ),
+                  title: (
+                    <ZUIText display="inline" variant="bodyMdSemiBold">
+                      {call.target.past_actions.last_action?.title}
+                    </ZUIText>
+                  ),
+                }}
+              />
+            </ZUIText>
+          </Box>
+        )}
+        {call.target.past_actions.num_actions == 0 && (
+          <Box sx={{ minWidth: 0, overflowWrap: 'anywhere' }}>
+            <ZUIText color="secondary">
+              <Msg
+                id={messageIds.about.noParticipation}
+                values={{ name: call.target.first_name }}
+              />
+            </ZUIText>
+          </Box>
+        )}
+      </Box>
       <PreviousCallsInfo call={call} />
-    </>
+    </Stack>
   );
 };
 
