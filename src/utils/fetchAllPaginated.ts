1cb100717df89acfef0764072a0c89dfaf35b2da
djbusstop <eribloodlust@grrlz.net>
2026-09-04 09:41:23 +0200

    Add try catch to stop recursion in case of error

--- src/utils/fetchAllPaginated.ts
+++ src/utils/fetchAllPaginated.ts
@@ -5,11 +5,16 @@ export async function fetchAllPaginated<T>(
   const result: T[] = [];
 
   async function loadNextBatch(page: number = 1) {
-    const batch = await fetchPage(page);
-    result.push(...batch);
+    try {
+      const batch = await fetchPage(page);
+      result.push(...batch);
 
-    if (batch.length >= batchSize) {
-      await loadNextBatch(page + 1);
+      if (batch.length >= batchSize) {
+        await loadNextBatch(page + 1);
+      }
+    } catch (e) {
+      // Stop recursively trying to fetch pages in case of error
+      return;
     }
   }
 
