089c0f4bab2f3695c42f369967f7481b88587ea4
djbusstop <eribloodlust@grrlz.net>
2026-09-21 10:22:49 +0200

    move to own file

--- src/pages/organize/[orgId]/people/[personId]/events/index.tsx
+++ src/pages/organize/[orgId]/people/[personId]/events/index.tsx
@@ -1,8 +1,6 @@
 import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 import { Box } from '@mui/system';
-import { ScheduleOutlined } from '@mui/icons-material';
-import Link from 'next/link';
 import { Card, Divider, List, Skeleton, Typography } from '@mui/material';
 
 import { PageWithLayout } from 'utils/types';
@@ -11,51 +9,10 @@ import SinglePersonLayout from 'features/profile/layout/SinglePersonLayout';
 import usePerson from 'features/profile/hooks/usePerson';
 import usePersonEvents from 'features/profile/hooks/usePersonEvents';
 import ZUIFuture from 'zui/ZUIFuture';
-import ZUITimeSpan from 'zui/ZUITimeSpan';
-import { removeOffset } from 'utils/dateUtils';
-import ZUIIconLabelRow from 'zui/ZUIIconLabelRow';
-import { ZetkinEvent } from 'utils/types/zetkin';
-import { Msg, useMessages } from 'core/i18n';
-import eventMessageIds from 'features/events/l10n/messageIds';
+import { Msg } from 'core/i18n';
 import profileMessageIds from 'features/profile/l10n/messageIds';
-import getEventUrl from 'features/events/utils/getEventUrl';
+import PersonEventListItem from 'features/profile/components/PersonEventListItem';
 import { getPersonScaffoldProps, scaffoldOptions } from '../index';
-
-const EventListItem = ({ event }: { event: ZetkinEvent }) => {
-  const eventMessages = useMessages(eventMessageIds);
-  return (
-    <Link href={getEventUrl(event)} passHref style={{ textDecoration: 'none' }}>
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          padding: '1.0em',
-        }}
-      >
-        <Typography>
-          {event.title ||
-            event.activity?.title ||
-            eventMessages.common.noTitle()}
-        </Typography>
-        <ZUIIconLabelRow
-          color="secondary"
-          iconLabels={[
-            {
-              icon: <ScheduleOutlined color="secondary" fontSize="inherit" />,
-              label: (
-                <ZUITimeSpan
-                  end={new Date(removeOffset(event.start_time))}
-                  start={new Date(removeOffset(event.end_time))}
-                />
-              ),
-            },
-          ]}
-          size="sm"
-        />
-      </Box>
-    </Link>
-  );
-};
 
 export const getServerSideProps: GetServerSideProps = scaffold(
   getPersonScaffoldProps,
@@ -133,7 +90,7 @@ const PersonEventsPage: PageWithLayout<PersonEventsPageProps> = ({
                       return (
                         <>
                           {index > 0 && <Divider variant="fullWidth" />}
-                          <EventListItem event={event.data.action} />
+                          <PersonEventListItem event={event.data.action} />
                         </>
                       );
                     })}
@@ -171,7 +128,7 @@ const PersonEventsPage: PageWithLayout<PersonEventsPageProps> = ({
                       return (
                         <>
                           {index > 0 && <Divider variant="fullWidth" />}
-                          <EventListItem event={event.data.action} />
+                          <PersonEventListItem event={event.data.action} />
                         </>
                       );
                     })}
